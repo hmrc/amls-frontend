4e45ee9b32ae68f5093ff798a389c0bd0143d2ce
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2015-10-21 14:30:21 +0100

    AMLS-102 added tests for text validation

--- test/utils/FormValidatorTest.scala
+++ test/utils/FormValidatorTest.scala
@@ -153,4 +153,19 @@ class FormValidatorTest extends UnitSpec with MockitoSugar with amls.FakeAmlsApp
       optionalCurrency("error.currency").bind(Map("" -> "")) shouldBe Right(None)
     }
   }
+
+  "mandatoryText" should {
+    "return valid string if correct" in {
+      mandatoryText("blank message", "invalid length", "validationMaxLengthFirstName").bind(Map("" -> "barry")) shouldBe Right("barry")
+      mandatoryText("blank message", "invalid length", "validationMaxLengthFirstName").bind(Map("" -> "1234myname1234")) shouldBe Right("1234myname1234")
+    }
+
+    "report an invalid money error" in {
+      mandatoryText("blank message", "invalid length", "validationMaxLengthFirstName").bind(Map("" -> "")) shouldBe
+        Left(List(FormError("", "blank message")))
+      mandatoryText("blank message", "invalid length", "validationMaxLengthFirstName").bind(Map("" -> "a" * 250)) shouldBe
+        Left(List(FormError("", "invalid length")))
+    }
+  }
+
 }
