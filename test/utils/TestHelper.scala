1cec517aa7124773ff907b22f063eee1319ad78a
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2015-10-21 14:19:25 +0100

    AMLS-102 Removed test helper

--- test/utils/TestHelper.scala
+++ test/utils/TestHelper.scala
@@ -1,20 +1,5 @@
 package utils
-import play.api.data.FormError
 
-/**
- * Created by grant on 19/10/15.
- */
 object TestHelper {
-  /**
-   * Check that Either Left result has specified key value.
-   * @param result
-   * @param messageKey
-   * @return
-   */
-  def isErrorMessageKeyEqual(result: Either[Seq[FormError], String], messageKey:String) = {
-    result match {
-      case Left(List(FormError(a, b, c))) if b.contains(messageKey) => true
-      case _ => false
-    }
-  }
+
 }
