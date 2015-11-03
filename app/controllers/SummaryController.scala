444935b3528b79b34e28ddd2c010fd36fdf323b0
Peter Leaman <peter.leaman@digital.hmrc.gov.uk>
2015-11-03 15:38:28 +0000

    AMLS-181 deleted uneeded code

--- app/controllers/SummaryController.scala
+++ app/controllers/SummaryController.scala
@@ -9,24 +9,7 @@ trait SummaryController extends FrontendController {
       Ok(views.html.summaryPage())
   }
 
-//  def onSubmit = AuthorisedFor(AmlsRegime).async {
-//    implicit user =>
-//      implicit request =>
-//        loginDetailsForm.bindFromRequest.fold(
-//          errors => Future.successful(BadRequest(views.html.AmlsLogin(errors))),
-//          details => {
-//            dataCacheConnector.saveDataShortLivedCache[LoginDetails](user.user.oid,"Data",details)
-//            amlsService.submitLoginDetails(details).map { response =>
-//              Ok(response.json)
-//            } recover {
-//              case e:Throwable => throw e
-//            }
-//          }
-//        )
-//  }
-
 }
 
 object SummaryController extends SummaryController {
-
 }
