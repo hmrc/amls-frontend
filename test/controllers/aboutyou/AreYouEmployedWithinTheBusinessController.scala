c51118c219aef5321a4e454d521d87f9d5a9a1fc
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2015-11-09 11:14:26 +0000

    AMLS 59: Unit Test: WIP

---
+++ test/controllers/aboutyou/AreYouEmployedWithinTheBusinessController.scala
@@ -0,0 +1,16 @@
+package controllers.aboutyou
+
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+
+class AreYouEmployedWithinTheBusinessController extends PlaySpec with OneServerPerSuite {
+
+  "on load of page " must {
+    "Authorised users" must {
+      "load the Are You Employed Within the Business page" in {
+
+
+      }
+    }
+  }
+
+}
