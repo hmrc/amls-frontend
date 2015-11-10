5d7d13c4344647406d2214d16550c7c25886622b
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2015-11-10 08:55:15 +0000

    AMLS-167 Changed getSeqFromMessagesKey function to be tail-recursive, to get rid of scalastyle warnings

--- app/utils/CommonHelper.scala
+++ app/utils/CommonHelper.scala
@@ -1,28 +1,20 @@
 package utils
 
-import play.api.i18n.Messages
-
-import scala.collection.mutable.ListBuffer
+import scala.annotation.tailrec
 
 object CommonHelper {
-  /**
-   * Looks in the messages file for all keys starting with the specified key
-   * followed by a dot and an integer. Constructs a Seq of message values in pairs
-   * from the keys it finds, starting from integer 1.
-   */
-  def getSeqFromMessagesKey(key:String, f:String => String): Seq[(String,String)] = {
-    var items = new ListBuffer[ (String, String) ]()
-
-    var a = 1
-    var continue = true
-    while (continue) {
-      val currentKey = key + "." + a.toString
-      val currentValue = f(currentKey)
-      continue = currentValue != currentKey
-      if (continue)
-        items += ( currentValue -> currentValue )
-      a = a + 1
+  @tailrec
+  def getSeqFromMessagesKey(key:String,
+                            messageLookupFunction:String => String,
+                            startValue:Int = 1,
+                            items:Seq[ (String, String) ]  = Nil ): Seq[ (String, String) ]  = {
+    val currentKey = key + "." + startValue.toString
+    val currentValue = messageLookupFunction(currentKey)
+    if (currentValue != currentKey) {
+      getSeqFromMessagesKey(key, messageLookupFunction, startValue + 1, items :+ ( currentValue -> currentValue ))
+    } else {
+      items
     }
-    items.toSeq
   }
 }
+
