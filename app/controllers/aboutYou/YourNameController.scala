8f3aae021c60b69b346e6d611ca6cc2b690c72cb
ashwini.rudrappa <ashwini.rudrappa@digita.hmrc.gov.uk>
2015-11-12 17:23:05 +0000

    AMLS-15:incorporate self review comments

--- app/controllers/aboutYou/YourNameController.scala
+++ app/controllers/aboutYou/YourNameController.scala
@@ -15,9 +15,10 @@ import scala.concurrent.Future
 trait YourNameController extends AMLSGenericController {
 
   val dataCacheConnector: DataCacheConnector
+  val CACHE_KEY_YOURNAME:String  = "yourName"
 
   override def get(implicit user: AuthContext, request: Request[AnyContent]) =
-    dataCacheConnector.fetchDataShortLivedCache[YourName](Messages("save4later.your_name")) map {
+    dataCacheConnector.fetchDataShortLivedCache[YourName](CACHE_KEY_YOURNAME) map {
       case Some(data) => Ok(views.html.yourName(yourNameForm.fill(data)))
       case _ => Ok(views.html.yourName(yourNameForm))
     }
@@ -26,7 +27,7 @@ trait YourNameController extends AMLSGenericController {
     yourNameForm.bindFromRequest().fold(
       errors => Future.successful(BadRequest(views.html.yourName(errors))),
       details => {
-        dataCacheConnector.saveDataShortLivedCache[YourName](Messages("save4later.your_name"), details) map { _=>
+        dataCacheConnector.saveDataShortLivedCache[YourName](CACHE_KEY_YOURNAME, details) map { _=>
           Redirect(controllers.routes.AmlsController.onPageLoad()) // TODO replace with actual next page
         }
       })
