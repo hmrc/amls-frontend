7ab42fbb545934395dce5a81da0ebf56e93bd853
ashwini.rudrappa <ashwini.rudrappa@digita.hmrc.gov.uk>
2015-11-13 14:31:18 +0000

    AMLS-176:Refactore unit tests to incorporate new auth seggregation changes

--- app/controllers/aboutYou/RoleForBusinessController.scala
+++ app/controllers/aboutYou/RoleForBusinessController.scala
@@ -4,13 +4,11 @@ import config.AMLSAuthConnector
 import config.AmlsPropertiesReader._
 import connectors.DataCacheConnector
 import controllers.AMLSGenericController
-import controllers.auth.AmlsRegime
 import forms.AboutYouForms._
 import models.RoleForBusiness
 import play.api.i18n.Messages
 import play.api.mvc.{Result, AnyContent, Request}
-import uk.gov.hmrc.play.frontend.auth.{AuthContext, Actions}
-import uk.gov.hmrc.play.frontend.controller.FrontendController
+import uk.gov.hmrc.play.frontend.auth.AuthContext
 import utils.CommonHelper
 
 import scala.concurrent.Future
@@ -35,31 +33,6 @@ trait RoleForBusinessController extends AMLSGenericController {
         NotImplemented("Not implemented: summary page")
       }
     })
-
-  /*def onPageLoad = AuthorisedFor(AmlsRegime).async {
-    implicit user =>
-      implicit request =>
-        dataCacheConnector.fetchDataShortLivedCache[RoleForBusiness](user.user.oid,"roleForBusiness") map {
-          case Some(data) => {
-            Ok(views.html.roleforbusiness(roleForBusinessForm.fill(data), roles ))
-          }
-          case _ => Ok(views.html.roleforbusiness(roleForBusinessForm, roles))
-        } recover {
-          case e:Throwable => throw e
-        }
-  }
-
-  def onSubmit = AuthorisedFor(AmlsRegime).async {
-    implicit user =>
-      implicit request =>
-        roleForBusinessForm.bindFromRequest().fold(
-          errors => Future.successful(BadRequest(views.html.roleforbusiness(errors, roles))),
-          details => {
-            dataCacheConnector.saveDataShortLivedCache[RoleForBusiness](user.user.oid,"roleForBusiness", details) map { _ =>
-              NotImplemented("Not implemented: summary page")
-            }
-          })
-  }*/
 }
 
 object RoleForBusinessController extends RoleForBusinessController {
