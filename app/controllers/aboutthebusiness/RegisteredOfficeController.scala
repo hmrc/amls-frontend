a35a0312d1dd89b0a4469288bf947364afc88061
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2015-11-25 17:13:39 +0000

    AMLS-200 Put in check that customer frontend business details comes back to amls ok

--- app/controllers/aboutthebusiness/RegisteredOfficeController.scala
+++ app/controllers/aboutthebusiness/RegisteredOfficeController.scala
@@ -4,8 +4,9 @@ import config.AMLSAuthConnector
 import connectors.{AmlsDataCacheConnector, DataCacheConnector}
 import controllers.AMLSGenericController
 import forms.AboutTheBusinessForms._
-import models.RegisteredOffice
+import models.{BusinessCustomerDetails, RegisteredOffice}
 import play.api.mvc.{AnyContent, Request}
+import services.BusinessCustomerService
 import uk.gov.hmrc.play.frontend.auth.AuthContext
 
 import scala.concurrent.Future
@@ -16,11 +17,18 @@ trait RegisteredOfficeController extends AMLSGenericController {
 
   private val CACHE_KEY = "registeredOffice"
 
-  override def get(implicit user: AuthContext, request: Request[AnyContent]) =
+  override def get(implicit user: AuthContext, request: Request[AnyContent]) = {
+//    BusinessCustomerService.getReviewBusinessDetails[BusinessCustomerDetails] map {
+//      case bcs => println("++++++++++++++++++++" + bcs)
+//        ""
+//      case _ => ""
+//    }
+
     dataCacheConnector.fetchDataShortLivedCache[RegisteredOffice](CACHE_KEY) map {
       case Some(cachedData) => Ok(views.html.registeredOffice(registeredOfficeForm.fill(cachedData)))
       case _ => Ok(views.html.registeredOffice(registeredOfficeForm))
     }
+  }
 
   override def post(implicit user: AuthContext, request: Request[AnyContent]) =
     registeredOfficeForm.bindFromRequest().fold(
