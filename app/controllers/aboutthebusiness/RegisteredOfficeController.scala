0dea03200f76ffda850fd2b487bc7a868cafa828
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2015-11-26 08:17:49 +0000

    AMLS-200 Implemented the get method on controller and displayed the address on page

--- app/controllers/aboutthebusiness/RegisteredOfficeController.scala
+++ app/controllers/aboutthebusiness/RegisteredOfficeController.scala
@@ -5,6 +5,7 @@ import connectors.{AmlsDataCacheConnector, DataCacheConnector}
 import controllers.AMLSGenericController
 import forms.AboutTheBusinessForms._
 import models.{BusinessCustomerDetails, RegisteredOffice}
+import play.api.data.Form
 import play.api.mvc.{AnyContent, Request}
 import services.BusinessCustomerService
 import uk.gov.hmrc.play.frontend.auth.AuthContext
@@ -20,26 +21,26 @@ trait RegisteredOfficeController extends AMLSGenericController {
   private val CACHE_KEY = "registeredOffice"
 
   override def get(implicit user: AuthContext, request: Request[AnyContent]) = {
-    /*
-        BusinessCustomerService.getReviewBusinessDetails[BusinessCustomerDetails] map {
-          case bcs => println("++++++++++++++++++++" + bcs)
-          case _ => ""
-        }
-    */
-    dataCacheConnector.fetchDataShortLivedCache[RegisteredOffice](CACHE_KEY) map {
-      case Some(cachedData) => Ok(views.html.registeredOffice(registeredOfficeForm.fill(cachedData)))
-      case _ => Ok(views.html.registeredOffice(registeredOfficeForm))
+    val reviewBusinessDetailsFuture = BusinessCustomerService.getReviewBusinessDetails[BusinessCustomerDetails]
+    val cachedDataFuture = dataCacheConnector.fetchDataShortLivedCache[RegisteredOffice](CACHE_KEY)
+    for {
+      reviewBusinessDetails: BusinessCustomerDetails <- reviewBusinessDetailsFuture
+      cachedData: Option[RegisteredOffice] <- cachedDataFuture
+    } yield {
+      val fm = cachedData.fold(registeredOfficeForm){ x=>registeredOfficeForm.fill(x) }
+      Ok(views.html.registeredOffice(fm, reviewBusinessDetails))
     }
   }
 
   override def post(implicit user: AuthContext, request: Request[AnyContent]) =
-    registeredOfficeForm.bindFromRequest().fold(
-      errors => Future.successful(BadRequest(views.html.registeredOffice(errors))),
-      registeredOffice => {
-        dataCacheConnector.saveDataShortLivedCache[RegisteredOffice](CACHE_KEY, registeredOffice) map { _ =>
-          NotImplemented
-        }
-      })
+//    registeredOfficeForm.bindFromRequest().fold(
+//      errors => Future.successful(BadRequest(views.html.registeredOffice(errors))),
+//      registeredOffice => {
+//        dataCacheConnector.saveDataShortLivedCache[RegisteredOffice](CACHE_KEY, registeredOffice) map { _ =>
+//          NotImplemented
+//        }
+//      })
+  Future.successful(NotImplemented)
 }
 
 object RegisteredOfficeController extends RegisteredOfficeController {
