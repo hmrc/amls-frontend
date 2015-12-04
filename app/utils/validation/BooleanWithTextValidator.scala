8848fc7dc6a85f2f3866f92a8026b5b297ab215d
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2015-12-04 12:36:37 +0000

    AMLS-212 Started refactoring to reduce cyclomatic complexity

--- app/utils/validation/BooleanWithTextValidator.scala
+++ app/utils/validation/BooleanWithTextValidator.scala
@@ -1,45 +1,56 @@
 package utils.validation
 
-import play.api.data.Forms
 import play.api.data.format.Formatter
-import play.api.data.FormError
+import play.api.data.{FormError, Forms}
 
 object BooleanWithTextValidator extends FormValidator {
 
-  private def mandatoryBooleanWithTextFormatter(textFieldKey:String, booleanValueThatMandatesTextValue:String,
-                                           noRadioButtonSelectedMessageKey: String,
-                                           blankValueMessageKey: String,
-                                           notBlankValueMessageKey: String) = new Formatter[Boolean] {
-    override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], Boolean] = {
-      data.get(key) match {
-        case Some(n) =>
-          n.trim match {
-            case p if p.length==0 => Left(Seq(FormError(key, noRadioButtonSelectedMessageKey)))
-            case p if p == booleanValueThatMandatesTextValue =>
-              data.getOrElse(textFieldKey, "") match {
-                case q if q.length == 0 => Left(Seq(FormError(textFieldKey, blankValueMessageKey)))
-                case _ => Right(n.toBoolean)
-              }
-            case p if p != booleanValueThatMandatesTextValue  =>
-              data.getOrElse(textFieldKey, "") match {
-                case q if q.length > 0 => Left(Seq(FormError(textFieldKey, notBlankValueMessageKey)))
-                case _ => Right(n.toBoolean)
-              }
-            //case _ => Right(n.toBoolean)
-            }
+  def mandatoryBooleanWithText(fieldKey: String, booleanValueThatMandatesTextValue: String,
+                               noRadioButtonSelectedMessageKey: String,
+                               blankValueMessageKey: String, notBlankValueMessageKey: String) =
+    Forms.of[Boolean](mandatoryBooleanWithTextFormatter(fieldKey, booleanValueThatMandatesTextValue,
+      noRadioButtonSelectedMessageKey,
+      blankValueMessageKey, notBlankValueMessageKey: String))
+
+  private def mandatoryBooleanWithTextFormatter(fieldKey: String, booleanValueThatMandatesTextValue: String,
+                                                noRadioButtonSelectedMessageKey: String,
+                                                blankValueMessageKey: String,
+                                                notBlankValueMessageKey: String) = new Formatter[Boolean] {
+    override def bind(key: String, formData: Map[String, String]): Either[Seq[FormError], Boolean] = {
+      formData.get(key) match {
+        case Some(fieldValue) => validateFieldValue(fieldValue)
         case _ => {
           Left(Seq(FormError(key, noRadioButtonSelectedMessageKey)))
         }
+      }
+
+
+      def validateFieldValue(fieldValue: String) = {
+        fieldValue.trim match {
+          case p if p.length == 0 => Left(Seq(FormError(key, noRadioButtonSelectedMessageKey)))
+          case _ => {
+            validationHelper({ (a: String, b: String) => a == b }, { (x: Int) => x == 0 }, fieldKey)
+            validationHelper({ (a: String, b: String) => a != b }, { (x: Int) => x > 0 }, fieldKey)
+          }
+        }
+
+      def validationHelper(func1: (String, String) => Boolean, func2: (Int => Boolean), textFieldKey: String) = {
+        if (func1(fieldValue.trim, booleanValueThatMandatesTextValue))
+          checkTextForTheRadioSelection(func2, fieldValue, fieldKey)
+        else
+          Left(Seq(FormError(key, noRadioButtonSelectedMessageKey))) //TODO
+      }
+
+      }
+      def checkTextForTheRadioSelection(func: (Int => Boolean), fieldValue: String, textFieldKey: String): Either[Seq[FormError], Boolean] with Product with Serializable = {
+        formData.getOrElse(textFieldKey, "") match {
+          case q if func(q.length) => Left(Seq(FormError(textFieldKey, blankValueMessageKey)))
+          case _ => Right(fieldValue.toBoolean)
+        }
+
       }
     }
 
     override def unbind(key: String, value: Boolean): Map[String, String] = Map(key -> value.toString)
   }
-
-  def mandatoryBooleanWithText( textFieldKey:String, booleanValueThatMandatesTextValue: String,
-                                noRadioButtonSelectedMessageKey: String,
-                                blankValueMessageKey: String, notBlankValueMessageKey: String) =
-    Forms.of[Boolean](mandatoryBooleanWithTextFormatter(textFieldKey, booleanValueThatMandatesTextValue,
-      noRadioButtonSelectedMessageKey,
-      blankValueMessageKey, notBlankValueMessageKey: String))
 }
