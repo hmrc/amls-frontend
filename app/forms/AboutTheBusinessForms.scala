860f3b0c3f6f657b90555e04232b353130309c69
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2015-12-09 11:28:07 +0000

    AMLS-212 Reinstated text on registered office page which had vanished probably in merge, and tidied up the mappings

--- app/forms/AboutTheBusinessForms.scala
+++ app/forms/AboutTheBusinessForms.scala
@@ -10,22 +10,12 @@ import utils.validation.TextValidator._
 import utils.validation.{EmailValidator, NumberValidator, RadioGroupPrefRegForMLRWithTextValidator, WebAddressValidator}
 
 object AboutTheBusinessForms {
-  def applyRegisteredForMLRFormMapping = (hasMlr:(Boolean, Boolean), mlrNumber: Option[String],
-                                          prevMlrNumber: Option[String]) =>
-    RegisteredForMLR(hasMlr._1, hasMlr._2, mlrNumber, prevMlrNumber)
-
-  def unapplyRegisteredForMLRFormMapping = (registeredForMLR: RegisteredForMLR) =>
-    Some( ( (registeredForMLR.hasDigitalMLR, registeredForMLR.hasNonDigitalMLR), registeredForMLR.mlrNumber,
-      registeredForMLR.prevMlrNumber ) )
-
-  val businessHasWebsiteFormMapping = mapping(
+  val businessHasWebsiteForm = Form(mapping(
     "hasWebsite" -> mandatoryBooleanWithText("website", "true",
       "error.required", "error.required", "error.notrequired"),
     "website" -> optional(WebAddressValidator.webAddress("err.invalidLength", "error.invalid",
       getIntFromProperty("validationMaxLengthWebAddress")))
-  )(BusinessHasWebsite.apply)(BusinessHasWebsite.unapply)
-
-  val businessHasWebsiteForm = Form(businessHasWebsiteFormMapping)
+  )(BusinessHasWebsite.apply)(BusinessHasWebsite.unapply))
 
   val telephoningBusinessForm = Form(mapping(
     "businessPhoneNumber" -> mandatoryPhoneNumber("error.required", "err.invalidLength",
@@ -38,31 +28,31 @@ object AboutTheBusinessForms {
     "isRegisteredOffice" -> mandatoryText("generic.please_specify")
   )(RegisteredOffice.applyString)(RegisteredOffice.unapplyString))
 
-
-  val businessRegForVATFormMapping = mapping(
+  val businessRegForVATForm = Form(mapping(
     "hasVAT" -> mandatoryBooleanWithText("VATNum", "true", "error.required", "error.required", "error.notrequired"),
     "VATNum" -> optional(NumberValidator.validateNumber("err.invalidLength", "error.invalid",
       getIntFromProperty("validationMaxLengthVAT"), getIntFromProperty("validationMaxLengthVAT")))
-  )(BusinessWithVAT.apply)(BusinessWithVAT.unapply)
+  )(BusinessWithVAT.apply)(BusinessWithVAT.unapply))
 
-  val businessRegForVATForm = Form(businessRegForVATFormMapping)
-
-  val BusinessHasEmailFormMapping = mapping(
+  val businessHasEmailForm = Form(mapping(
     "email" -> EmailValidator.mandatoryEmail("error.required", "err.invalidLength", "error.invalid",
       getIntFromProperty("validationMaxLengthEmail"))
-  )(BusinessHasEmail.apply)(BusinessHasEmail.unapply)
+  )(BusinessHasEmail.apply)(BusinessHasEmail.unapply))
 
-  val businessHasEmailForm = Form(BusinessHasEmailFormMapping)
-
-  val RegisteredForMLRFormMapping = mapping(
-    "hasMLR" ->  RadioGroupPrefRegForMLRWithTextValidator.mandatoryBooleanWithText("mlrNumber",
+  val RegisteredForMLRForm = Form(mapping(
+    "hasMLR" -> RadioGroupPrefRegForMLRWithTextValidator.mandatoryBooleanWithText("mlrNumber",
       "prevMlrNumber", "error.required", "error.required", "error.notrequired"),
     "mlrNumber" -> optional(NumberValidator.validateNumber("err.invalidLength", "error.invalid",
       getIntFromProperty("validationMaxLengthMLR"), getIntFromProperty("validationMaxLengthMLR"))),
-
     "prevMlrNumber" -> optional(NumberValidator.validateNumber("err.invalidLength", "error.invalid",
       getIntFromProperty("validationMaxLengthMLR"), getIntFromProperty("validationMaxLengthPrevMLR")))
-  )(applyRegisteredForMLRFormMapping )(unapplyRegisteredForMLRFormMapping)
+  )(applyRegisteredForMLRFormMapping)(unapplyRegisteredForMLRFormMapping))
 
-  val RegisteredForMLRForm = Form(RegisteredForMLRFormMapping)
+  def applyRegisteredForMLRFormMapping = (hasMlr: (Boolean, Boolean), mlrNumber: Option[String],
+                                          prevMlrNumber: Option[String]) =>
+    RegisteredForMLR(hasMlr._1, hasMlr._2, mlrNumber, prevMlrNumber)
+
+  def unapplyRegisteredForMLRFormMapping = (registeredForMLR: RegisteredForMLR) =>
+    Some(((registeredForMLR.hasDigitalMLR, registeredForMLR.hasNonDigitalMLR), registeredForMLR.mlrNumber,
+      registeredForMLR.prevMlrNumber))
 }
--- app/forms/AboutYouForms.scala
+++ app/forms/AboutYouForms.scala
@@ -10,37 +10,30 @@ import utils.validation.TextValidator
 
 object AboutYouForms {
 
-  val yourNameFormMapping = mapping(
+  val yourNameForm = Form(mapping(
     "firstname" -> TextValidator.mandatoryText("err.titleNotEntered.first_name",
       "err.invalidLength", getIntFromProperty("validationMaxLengthFirstName")),
     "middlename" -> optional(TextValidator.mandatoryText("", "err.invalidLength",
       getIntFromProperty("validationMaxLengthFirstName"))),
     "lastname" -> TextValidator.mandatoryText("err.titleNotEntered.last_name", "err.invalidLength",
       getIntFromProperty("validationMaxLengthFirstName"))
-  ) (YourName.apply)(YourName.unapply)
+  )(YourName.apply)(YourName.unapply))
 
-  val yourNameForm = Form(yourNameFormMapping)
-
-  val employedWithTheBusinessFormMapping = mapping(
+  val employedWithinTheBusinessForm = Form(mapping(
     "isEmployed" -> mandatoryBoolean("error.required")
-  )(EmployedWithinTheBusiness.apply)(EmployedWithinTheBusiness.unapply)
+  )(EmployedWithinTheBusiness.apply)(EmployedWithinTheBusiness.unapply))
 
-  val employedWithinTheBusinessForm = Form(employedWithTheBusinessFormMapping)
-
-  val roleWithinBusinessFormMapping = mapping(
+  val roleWithinBusinessForm = Form(mapping(
     "roleWithinBusiness" -> radioGroupWithOther("other", getProperty("roleWithinBusiness").split(",").reverse.head,
       "error.required", "error.required", "error.invalid", getIntFromProperty("validationMaxLengthRoleWithinBusinessOther")),
     "other" -> text
-  )(RoleWithinBusiness.apply)(RoleWithinBusiness.unapply)
+  )(RoleWithinBusiness.apply)(RoleWithinBusiness.unapply))
 
-  val roleWithinBusinessForm = Form(roleWithinBusinessFormMapping)
 
-  val roleForBusinessFormMapping = mapping(
+  val roleForBusinessForm = Form(mapping(
     "roleForBusiness" -> radioGroupWithOther("other", getProperty("roleForBusiness").split(",").reverse.head,
       "error.required", "error.required", "error.invalid", getIntFromProperty("validationMaxLengthRoleForBusinessOther")),
     "other" -> text
-  )(RoleForBusiness.apply)(RoleForBusiness.unapply)
-
-  val roleForBusinessForm = Form(roleForBusinessFormMapping)
+  )(RoleForBusiness.apply)(RoleForBusiness.unapply))
 
 }
