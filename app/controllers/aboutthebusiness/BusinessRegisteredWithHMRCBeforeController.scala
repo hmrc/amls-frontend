8e87a03509c3cf052969344e69c6be4bcfefc935
ashwini.rudrappa <ashwini.rudrappa@digita.hmrc.gov.uk>
2016-01-06 15:48:49 +0000

    AMLS-232:rename html file

--- app/controllers/aboutthebusiness/BusinessRegisteredWithHMRCBeforeController.scala
+++ app/controllers/aboutthebusiness/BusinessRegisteredWithHMRCBeforeController.scala
@@ -18,14 +18,14 @@ trait BusinessRegisteredWithHMRCBeforeController extends AMLSGenericController{
 
   override def get(implicit user: AuthContext, request: Request[AnyContent]): Future[Result] = {
     dataCacheConnector.fetchDataShortLivedCache[RegisteredWithHMRCBefore](CACHE_KEY) map {
-      case Some(data) => Ok(views.html.registered_for_mlr(registeredWithHMRCBeforeForm.fill(data)))
-      case _ => Ok(views.html.registered_for_mlr(registeredWithHMRCBeforeForm))
+      case Some(data) => Ok(views.html.registered_with_HMRC_before(registeredWithHMRCBeforeForm.fill(data)))
+      case _ => Ok(views.html.registered_with_HMRC_before(registeredWithHMRCBeforeForm))
     }
   }
 
   override def post(implicit user: AuthContext, request: Request[AnyContent]): Future[Result] =
     registeredWithHMRCBeforeForm.bindFromRequest().fold(
-      errors => Future.successful(BadRequest(views.html.registered_for_mlr(errors))),
+      errors => Future.successful(BadRequest(views.html.registered_with_HMRC_before(errors))),
       details => {
         dataCacheConnector.saveDataShortLivedCache[RegisteredWithHMRCBefore](CACHE_KEY, details) map { _=>
           Redirect(controllers.aboutthebusiness.routes.BusinessRegForVATController.get())
