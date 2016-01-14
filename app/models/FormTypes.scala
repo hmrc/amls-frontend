5eda8e18ac095909a8ad8562a4743cfa5c983f32
ashwini.rudrappa <ashwini.rudrappa@digita.hmrc.gov.uk>
2016-01-14 22:42:57 +0000

    AMLS-143:create model and add unit test case

--- app/models/FormTypes.scala
+++ app/models/FormTypes.scala
@@ -4,9 +4,25 @@ object FormTypes {
 
   import play.api.data.mapping.forms.Rules._
 
+  val maxLengthName = 35
+  val maxDescriptionType = 255
+  val maxAddressLength = 35
+  val maxPostCodeLength = 10
+  val maxCountryTypeLength = 2
+
   def indivNameType =
-    notEmpty |+| maxLength(35)
+    notEmpty |+| maxLength(maxLengthName)
 
   def descriptionType =
-    notEmpty |+| maxLength(255)
+    notEmpty |+| maxLength(maxDescriptionType)
+
+  def validateAddressType =
+    notEmpty |+| maxLength(maxAddressLength)
+
+  def validPostCode =
+    notEmpty |+| maxLength((maxPostCodeLength))
+
+  def validCountryType =
+    notEmpty |+| maxLength(maxCountryTypeLength)
+
 }
