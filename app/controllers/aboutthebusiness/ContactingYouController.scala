76fbce2e61f33ce5e89fffbc3878b0c0468fd18d
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-01-18 17:27:34 +0000

    AMLS-239: Review comments incorporated

--- app/controllers/aboutthebusiness/ContactingYouController.scala
+++ app/controllers/aboutthebusiness/ContactingYouController.scala
@@ -3,9 +3,8 @@ package controllers.aboutthebusiness
 import config.AMLSAuthConnector
 import connectors.{BusinessCustomerSessionCacheConnector, DataCacheConnector}
 import controllers.BaseController
-import controllers.auth.AmlsRegime
 import forms.{EmptyForm, Form2, InvalidForm, ValidForm}
-import models.aboutthebusiness.{AboutTheBusiness, BusinessCustomerDetails, ContactingYou, ContactingYouDetails}
+import models.aboutthebusiness._
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 
 trait ContactingYouController extends BaseController {
@@ -14,9 +13,9 @@ trait ContactingYouController extends BaseController {
   val businessCustomerSessionCacheConnector: BusinessCustomerSessionCacheConnector
 
 
-  def get(edit: Boolean = false) = AuthorisedFor(AmlsRegime, pageVisibility = GGConfidence).async {
+  def get(edit: Boolean = false) = Authorised.async {
     implicit authContext => implicit request =>
-      val businessCustomerDetailsFuture = businessCustomerSessionCacheConnector.getReviewBusinessDetails[BusinessCustomerDetails]
+      val businessCustomerDetailsFuture = businessCustomerSessionCacheConnector.getReviewBusinessDetails[BusinessCustomerDetails] //TODO Remove after AboutTheBusiness has Address
       val aboutTheBusinessFuture = dataCacheConnector.fetchDataShortLivedCache[AboutTheBusiness](AboutTheBusiness.key)
       for {
         businessCustomerDetails <- businessCustomerDetailsFuture
@@ -28,13 +27,13 @@ trait ContactingYouController extends BaseController {
   }
 
 
-  def post(edit: Boolean = false) = AuthorisedFor(AmlsRegime, pageVisibility = GGConfidence).async {
+  def post(edit: Boolean = false) = Authorised.async {
 
     implicit authContext => implicit request => {
-      Form2[ContactingYouDetails](request.body) match {
+      Form2[ContactingYouForm](request.body) match {
         case f: InvalidForm => {
           for {
-            reviewBusinessDetails <- businessCustomerSessionCacheConnector.getReviewBusinessDetails[BusinessCustomerDetails]
+            reviewBusinessDetails <- businessCustomerSessionCacheConnector.getReviewBusinessDetails[BusinessCustomerDetails] //TODO Get Address from AboutTheBusiness
           } yield {
             reviewBusinessDetails match {
               case Some(data) => BadRequest(views.html.contacting_you(f, Some(data), edit))
@@ -42,12 +41,12 @@ trait ContactingYouController extends BaseController {
             }
           }
         }
-        case ValidForm(_, data: ContactingYouDetails) =>
+        case ValidForm(_, data) =>
           for {
             aboutTheBusiness <- dataCacheConnector.fetchDataShortLivedCache[AboutTheBusiness](AboutTheBusiness.key)
             _ <- dataCacheConnector.saveDataShortLivedCache[AboutTheBusiness](AboutTheBusiness.key, aboutTheBusiness.contactingYou(ContactingYou(data.email, data.phoneNumber, data.website))
             )
-          } yield data.sendLettersToThisAddress match {
+          } yield data.letterToThisAddress match {
             case true => Redirect(routes.ContactingYouController.get(edit)) //TODO Go to the Summary Page as no change in Address
             case false => Redirect(routes.ContactingYouController.get(edit)) //TODO  Go to the Address For Letters
           }
@@ -62,4 +61,3 @@ object ContactingYouController extends ContactingYouController {
   override val dataCacheConnector: DataCacheConnector = DataCacheConnector
   override val businessCustomerSessionCacheConnector = BusinessCustomerSessionCacheConnector
 }
-
