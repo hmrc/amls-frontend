f39a94b7969bc6dc56f308e65eb347761abe41c2
Sajive <sajive.k.sukumara@accenture.com>
2016-01-20 15:51:09 +0000

    AMLS-263 : Change for trading premises - what you need

---
+++ test/controllers/tradingpremises/WhatYouNeedControllerSpec.scala
@@ -0,0 +1,44 @@
+package controllers.tradingpremises
+
+import connectors.DataCacheConnector
+import models.aboutthebusiness._
+import org.jsoup.Jsoup
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.i18n.Messages
+import play.api.test.Helpers._
+import utils.AuthorisedFixture
+
+import scala.concurrent.Future
+
+
+class WhatYouNeedControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar with ScalaFutures {
+  trait Fixture extends AuthorisedFixture {
+    self =>
+
+    val controller = new WhatYouNeedController {
+      override val dataCacheConnector = mock[DataCacheConnector]
+      override val authConnector = self.authConnector
+    }
+  }
+
+  "WhatYouNeedController" must {
+
+    "on get display the what you need page" in new Fixture {
+      val result = controller.get()(request)
+      status(result) must be(OK)
+      contentAsString(result) must include(Messages("tradingpremises.whatyouneed.title"))
+    }
+
+    "on click of continue button, page to be redirectd to " in new Fixture{
+      doc.select("#continue").text shouldBe "change your company car details"
+      doc.select("#continue>a").attr("href") shouldBe "/anti-money-laundering/trading-premises/where_are_trading_premises"
+    }
+  }
+
+}
+
+
