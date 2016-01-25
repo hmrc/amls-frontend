70bff1f925085e37a53501603b4c018114739c5c
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-01-25 15:31:23 +0000

    AMLS-210 Added the Model

---
+++ app/models/estateagentbusiness/PenalisedUnderEstateAgentsAct.scala
@@ -0,0 +1,47 @@
+package models.estateagentbusiness
+
+import play.api.data.mapping.forms.UrlFormEncoded
+import play.api.data.mapping.{From, Rule, Success, Write}
+import play.api.libs.json._
+
+sealed trait PenalisedUnderEstateAgentsAct
+
+case class PenalisedUnderEstateAgentsActYes(value: String) extends PenalisedUnderEstateAgentsAct
+case object PenalisedUnderEstateAgentsActNo extends PenalisedUnderEstateAgentsAct
+
+object PenalisedUnderEstateAgentsAct {
+
+  import models.FormTypes._
+
+  implicit val formRule: Rule[UrlFormEncoded, PenalisedUnderEstateAgentsAct] = From[UrlFormEncoded] { __ =>
+    import play.api.data.mapping.forms.Rules._
+    (__ \ "penalisedunderestateagentsact").read[Boolean] flatMap {
+      case true => (__ \ "penalisedunderestateagentsactdetails").read(penalisedType) fmap (PenalisedUnderEstateAgentsActYes.apply)
+      case false => Rule.fromMapping { _ => Success(PenalisedUnderEstateAgentsActNo) }
+    }
+  }
+
+  implicit val formWrites: Write[PenalisedUnderEstateAgentsAct, UrlFormEncoded] = Write {
+    case PenalisedUnderEstateAgentsActYes(value) =>
+      Map("penalisedunderestateagentsact" -> Seq("true"),
+        "penalisedunderestateagentsactdetails" -> Seq(value)
+      )
+    case PenalisedUnderEstateAgentsActNo => Map("penalised" -> Seq("false"))
+  }
+
+  implicit val jsonReads =
+    (__ \ "penalisedunderestateagentsact").read[Boolean] flatMap[PenalisedUnderEstateAgentsAct] {
+      case true => (__ \ "professionalBody").read[String] map (PenalisedUnderEstateAgentsActYes.apply _)
+      case false => Reads(_ => JsSuccess(PenalisedUnderEstateAgentsActNo))
+    }
+
+  implicit val jsonWrites = Writes[PenalisedUnderEstateAgentsAct] {
+    case PenalisedUnderEstateAgentsActYes(value) => Json.obj(
+      "penalisedunderestateagentsact" -> true,
+      "professionalBody" -> value
+    )
+    case PenalisedUnderEstateAgentsActNo => Json.obj("penalised" -> false)
+  }
+
+}
+
