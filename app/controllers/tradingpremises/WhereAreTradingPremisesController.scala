c497c7c946aa4fc0946e3e5065dd82d7c5c3ab02
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-01-28 13:45:44 +0000

    AMLS-217: Adding changes for Controller and its Test

--- app/controllers/tradingpremises/WhereAreTradingPremisesController.scala
+++ app/controllers/tradingpremises/WhereAreTradingPremisesController.scala
@@ -4,18 +4,24 @@ import config.AMLSAuthConnector
 import connectors.DataCacheConnector
 import controllers.BaseController
 
+import scala.concurrent.Future
+
 trait WhereAreTradingPremisesController extends BaseController {
 
   def dataCacheConnector: DataCacheConnector
 
-  def get(edit : Boolean = false) = Authorised.async {???}
+  def get(edit: Boolean = false) = Authorised.async {
+    implicit authContext => implicit request => Future.successful(Ok(views.html.where_are_trading_premises()))
+  }
 
-  def post(edit : Boolean = false) = Authorised.async {???}
+  def post(edit: Boolean = false) = Authorised.async {
+    ???
+  }
 
 }
 
 object WhereAreTradingPremisesController extends WhereAreTradingPremisesController {
 
-  override val authConnector = AMLSAuthConnector
+  val authConnector = AMLSAuthConnector
   override val dataCacheConnector = DataCacheConnector
 }
--- test/controllers/tradingpremises/WhereAreTradingPremisesControllerSpec.scala
+++ test/controllers/tradingpremises/WhereAreTradingPremisesControllerSpec.scala
@@ -1,9 +1,40 @@
 package controllers.tradingpremises
 
+import connectors.DataCacheConnector
+import models.tradingpremises.YourTradingPremises
+import org.mockito.Matchers._
+import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.i18n.Messages
+import play.api.test.Helpers._
+import utils.AuthorisedFixture
+
+import scala.concurrent.Future
 
 class WhereAreTradingPremisesControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar with ScalaFutures {
+
+  trait Fixture extends AuthorisedFixture {
+    self =>
+    val controller = new WhereAreTradingPremisesController() {
+      override val dataCacheConnector = mock[DataCacheConnector]
+      override val authConnector = self.authConnector
+    }
+  }
+
+  "WhereAreTradingPremisesController" must {
+
+    "load a blank where are trading premises page when the user visits for the first time" in new Fixture {
+      when(controller.dataCacheConnector.fetchDataShortLivedCache[YourTradingPremises](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+
+      val result = controller.get()(request)
+      status(result) must be(OK)
+      contentAsString(result) must include (Messages("tradingpremises.yourtradingpremises.title"))
+
+    }
+
+  }
 
 }
