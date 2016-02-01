fa35cf8a066917859a94cacdc0ca363800c34759
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-02-01 20:00:00 +0000

    AMLS-217: Added the tests for that the data gets stored in save4later and is pulled back and displayed on the view

--- test/controllers/tradingpremises/WhereAreTradingPremisesControllerSpec.scala
+++ test/controllers/tradingpremises/WhereAreTradingPremisesControllerSpec.scala
@@ -2,7 +2,8 @@ package controllers.tradingpremises
 
 import connectors.DataCacheConnector
 import models.aboutthebusiness.AboutTheBusiness
-import models.tradingpremises.{NonUKTradingPremises, TradingPremisesAddress, UKTradingPremises, YourTradingPremises}
+import models.tradingpremises._
+import org.joda.time.LocalDate
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
@@ -27,30 +28,57 @@ class WhereAreTradingPremisesControllerSpec extends PlaySpec with OneServerPerSu
 
   "WhereAreTradingPremisesController" must {
 
+    val ukAddress = UKTradingPremises("ukline_1", "ukline_2", Some("ukline_3"), Some("ukline_4"), Some("NE98 1ZZ"), "UK")
+    val nonUKAddress = UKTradingPremises("nonline _1", "nonline_2", Some("nonline_3"), Some("nonline_4"), Some("226001"), "IN")
+    val yourTradingPremises = YourTradingPremises("Trading Name", ukAddress, PremiseOwnerSelf, new LocalDate("2016-02-01"), ResidentialYes)
+
+
     "load a blank where are trading premises page when the user visits for the first time" in new Fixture {
-      when(controller.dataCacheConnector.fetchDataShortLivedCache[YourTradingPremises](any())
+      when(controller.dataCacheConnector.fetchDataShortLivedCache[TradingPremises](any())
         (any(), any(), any())).thenReturn(Future.successful(None))
 
       val result = controller.get()(request)
       status(result) must be(OK)
-      contentAsString(result) must include (Messages("tradingpremises.yourtradingpremises.title"))
+      contentAsString(result) must include(Messages("tradingpremises.yourtradingpremises.title"))
 
     }
+
+
+    "prepopulate the data when the trading premises page loads" in new Fixture {
+
+      val tradingPremises = TradingPremises(Some(yourTradingPremises), None)
+
+      when(controller.dataCacheConnector.fetchDataShortLivedCache[TradingPremises](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(tradingPremises)))
+
+      val result = controller.get()(request)
+      status(result) must be(OK)
+
+      contentAsString(result) must include(yourTradingPremises.tradingName)
+
+      //Validate that address gets loaded
+      contentAsString(result) must include(Messages("tradingpremises.yourtradingpremises.title"))
+      contentAsString(result) must include(ukAddress.addressLine1)
+      contentAsString(result) must include(ukAddress.addressLine2)
+      contentAsString(result) must include(ukAddress.addressLine3.get)
+      contentAsString(result) must include(ukAddress.addressLine4.get)
+    }
+
 
     "successfully validate a UK Address" in new Fixture {
 
       val validUKAddress = Map(
-      "isUK" -> Seq("true"),
-      "addressLine1" -> Seq("Address Line 1"),
-      "addressLine2" -> Seq("Address Line 2"),
-      "addressLine3" -> Seq("Address Line 3"),
-      "addressLine4" -> Seq("Address Line 4"),
-      "postCode" -> Seq("NE98 1ZZ"),
-      "country" ->  Seq("UK"))
+        "isUK" -> Seq("true"),
+        "addressLine1" -> Seq("Address Line 1"),
+        "addressLine2" -> Seq("Address Line 2"),
+        "addressLine3" -> Seq("Address Line 3"),
+        "addressLine4" -> Seq("Address Line 4"),
+        "postCode" -> Seq("NE98 1ZZ"),
+        "country" -> Seq("UK"))
 
       TradingPremisesAddress.formRule.validate(validUKAddress) must
-      be(Success(UKTradingPremises("Address Line 1", "Address Line 2", Some("Address Line 3"), Some("Address Line 4"), Some("NE98 1ZZ"), "UK")))
-  }
+        be(Success(UKTradingPremises("Address Line 1", "Address Line 2", Some("Address Line 3"), Some("Address Line 4"), Some("NE98 1ZZ"), "UK")))
+    }
 
     "successfully validate a Non UK Address" in new Fixture {
 
@@ -71,7 +99,7 @@ class WhereAreTradingPremisesControllerSpec extends PlaySpec with OneServerPerSu
     "on post of the page with invalid data must reload the page" in new Fixture {
 
       val newRequest = request.withFormUrlEncodedBody()
-      when(controller.dataCacheConnector.fetchDataShortLivedCache[AboutTheBusiness](any())(any(),any(),any())).thenReturn(Future.successful(None))
+      when(controller.dataCacheConnector.fetchDataShortLivedCache[AboutTheBusiness](any())(any(), any(), any())).thenReturn(Future.successful(None))
 
       val result = controller.post()(newRequest)
       status(result) must be(BAD_REQUEST)
