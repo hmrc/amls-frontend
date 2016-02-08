0362f8fb4f888690777cdc3b2ef3c05163d173ef
kapilsanghvi <kapil.sanghvi@accenture.com>
2016-02-08 11:58:21 +0000

    AMLS-251 AMLS-242 : Added tests and basic template for trading premises check your answers

---
+++ test/controllers/tradingpremises/OwnSummaryControllerSpec.scala
@@ -0,0 +1,53 @@
+package controllers.tradingpremises
+
+import config.AMLSAuthConnector
+import connectors.DataCacheConnector
+import models.tradingpremises._
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.test.Helpers._
+import utils.AuthorisedFixture
+
+import scala.concurrent.Future
+
+class OwnSummaryControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar {
+
+  trait Fixture extends AuthorisedFixture {
+    self =>
+
+    val controller = new OwnSummaryController {
+      override val dataCache = mock[DataCacheConnector]
+      override val authConnector = self.authConnector
+    }
+  }
+
+  "Get" must {
+
+    "use correct services" in new Fixture {
+      OwnSummaryController.authConnector must be(AMLSAuthConnector)
+      OwnSummaryController.dataCache must be(DataCacheConnector)
+    }
+
+    "load the your business premises summary page when section data is available" in new Fixture {
+
+      val model = TradingPremises(None, None)
+
+      when(controller.dataCache.fetchDataShortLivedCache[TradingPremises](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(model)))
+
+      val result = controller.get()(request)
+      status(result) must be(OK)
+    }
+
+/*    "redirect to the trading premises summary page when section data is unavailable" in new Fixture {
+
+      when(controller.dataCache.fetchDataShortLivedCache[TradingPremises](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+
+      val result = controller.get()(request)
+      status(result) must be(SEE_OTHER)
+    }*/
+  }
+}
