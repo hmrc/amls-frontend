8f0d946478c3cd0784cf0027bc08c88ceb1647ff
mahipal.padala <mahipal.padala@digital.hmrc.gov.uk>
2016-02-12 15:46:34 +0000

    AMLS-339: Intial commit

---
+++ app/controllers/aboutthebusiness/WhatYouNeedController.scala
@@ -0,0 +1,19 @@
+package controllers.aboutthebusiness
+
+import config.AMLSAuthConnector
+import controllers.BaseController
+
+import scala.concurrent.Future
+
+trait WhatYouNeedController extends BaseController {
+
+  def get = Authorised.async {
+    implicit authContext => implicit request =>
+      Future.successful(Ok(views.html.what_you_need_AboutTheBusiness()))
+  }
+
+}
+
+object WhatYouNeedController extends WhatYouNeedController {
+  override val authConnector = AMLSAuthConnector
+}
