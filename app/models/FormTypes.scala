d5e2b29dd16f6a138afc817decdbdd38ad891e6a
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-02-22 09:53:00 +0000

    AMLS-235:add regex for account number and IBAN

--- app/models/FormTypes.scala
+++ app/models/FormTypes.scala
@@ -88,8 +88,8 @@ object FormTypes {
 
   val ukBankAccountNumberType = notEmpty compose maxLength(maxUKBankAccountNumberLength) compose pattern("^[0-9]{8}$".r)
 
-  val nonUKBankAccountNumberType = notEmpty compose maxLength(maxNonUKBankAccountNumberLength)
+  val nonUKBankAccountNumberType = notEmpty compose maxLength(maxNonUKBankAccountNumberLength) compose pattern("^[0-9a-zA-Z_]{40}$".r)
 
-  val ibanType = notEmpty compose maxLength(maxIBANLength)
+  val ibanType = notEmpty compose maxLength(maxIBANLength) compose pattern("^[0-9a-zA-Z_]{34}$".r)
 
 }
