d1fc566b4214e59dd89d09ab9535b8702ee2ac3a
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-02-24 09:54:56 +0000

    create generic code for util controller

--- app/controllers/bankdetails/BankAccountUtilController.scala
+++ app/controllers/bankdetails/BankAccountUtilController.scala
@@ -4,6 +4,7 @@ import connectors.DataCacheConnector
 import controllers.BaseController
 import models.bankdetails.BankDetails
 import play.api.libs.json
+import typeclasses.MongoKey
 import uk.gov.hmrc.play.frontend.auth.AuthContext
 import uk.gov.hmrc.play.http.HeaderCarrier
 
@@ -13,15 +14,28 @@ trait BankAccountUtilController extends BaseController {
 
   val dataCacheConnector: DataCacheConnector
 
-  def getBankDetails[T](key:String)(implicit user: AuthContext, hc: HeaderCarrier, formats: json.Format[T]): Future[Seq[T]] = {
-    dataCacheConnector.fetchDataShortLivedCache[Seq[T]](key) map {
+  def getBankDetails[T]
+  (implicit
+   user: AuthContext,
+   hc: HeaderCarrier,
+   formats: json.Format[T],
+   key: MongoKey[T]
+  ): Future[Seq[T]] = {
+    dataCacheConnector.fetchDataShortLivedCache[Seq[T]](key()) map {
       _.fold(Seq.empty[T]) {
         identity
       }
     }
   }
 
-  def getBankDetails[T](index: Int)(implicit user: AuthContext, hc: HeaderCarrier, formats: json.Format[T]): Future[Option[T]] = {
+  def getBankDetails[T]
+  (index: Int)
+  (implicit
+   user: AuthContext,
+   hc: HeaderCarrier,
+   formats: json.Format[T],
+   key: MongoKey[T]
+  ): Future[Option[T]] = {
     getBankDetails[T] map {
       case accounts if index > 0 && index <= accounts.length + 1 => accounts lift (index - 1)
       case _ => None
@@ -31,16 +45,25 @@ trait BankAccountUtilController extends BaseController {
   protected def updateBankDetails[T]
   (index: Int)
   (fn: Option[T] => Option[T])
-  (implicit user: AuthContext, hc: HeaderCarrier, formats: json.Format[T]): Future[_] =
+  (implicit
+   user: AuthContext,
+   hc: HeaderCarrier,
+   formats: json.Format[T],
+   key: MongoKey[T]
+  ): Future[_] =
     getBankDetails[T] map {
       accounts => {
-
         putBankDetails(accounts.patch(index - 1, fn(accounts.lift(index - 1)).toSeq, 1))
       }
     }
 
-  protected def putBankDetails[T](accounts: Seq[T], key: String)
-  (implicit user: AuthContext, hc: HeaderCarrier, formats: json.Format[T]): Future[_] =
-    dataCacheConnector.saveDataShortLivedCache[Seq[T]](key, accounts)
+  protected def putBankDetails[T](accounts: Seq[T])
+  (implicit
+   user: AuthContext,
+   hc: HeaderCarrier,
+   formats: json.Format[T],
+   key: MongoKey[T]
+  ): Future[_] =
+    dataCacheConnector.saveDataShortLivedCache[Seq[T]](key(), accounts)
 }
 
