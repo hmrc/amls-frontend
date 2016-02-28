8a0758527c24cb98e85daab3d13f3df1cec32b44
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-02-28 02:35:26 +0000

    AMLS-69:add model and spec

--- app/models/businessactivities/TransactionType.scala
+++ app/models/businessactivities/TransactionType.scala
@@ -1,62 +1,122 @@
 package models.businessactivities
 
 import models.FormTypes._
+import models.businessactivities
 import play.api.data.mapping.forms.UrlFormEncoded
 import play.api.data.mapping._
+import play.api.data.validation.ValidationError
+import play.api.libs.json._
+import play.api.libs.json.Reads.StringReads
 
 sealed trait TransactionRecord
 
 case class TransactionRecordYes(transactionType: Set[TransactionType]) extends TransactionRecord
+
 case object TransactionRecordNo extends TransactionRecord
 
 
 sealed trait TransactionType
 
 case object Paper extends TransactionType
+
 case object DigitalSpreadsheet extends TransactionType
-case class DigitalSoftware(name : String) extends TransactionType
+
+case class DigitalSoftware(name: String) extends TransactionType
 
 object TransactionType {
 
-  implicit val formRule: Rule[UrlFormEncoded, Set[TransactionType]] =
+  import utils.MappingUtils.Implicits._
+
+  implicit val formRule: Rule[UrlFormEncoded, TransactionRecord] =
     From[UrlFormEncoded] { __ =>
       import play.api.data.mapping.forms.Rules._
-
-      (__ \ "transactions").read[Set[String]] flatMap { z =>
-        z.map {
-          case "01" => Rule[UrlFormEncoded, TransactionType](_ => Success(Paper))
-          case "02" => Rule[UrlFormEncoded, TransactionType](_ => Success(DigitalSpreadsheet))
-          case "03" =>
-            (__ \ "name").read(softwareNameType) fmap DigitalSoftware.apply
-        }.foldLeft[Rule[UrlFormEncoded, Set[TransactionType]]](
-          Rule[UrlFormEncoded, Set[TransactionType]](_ => Success(Set.empty))
-        ) {
-          case (m, n) =>
-            n flatMap { x =>
-              m fmap {
-                _ + x
+      (__ \ "isRecorded").read[Boolean].flatMap {
+        case true => {
+          (__ \ "transactions").read[Set[String]] flatMap { z =>
+            if (z.seq.isEmpty) {
+              (Path \ "transactions") -> Seq(ValidationError("error.required"))
+            } else {
+                z.map {
+                  case "01" => Rule[UrlFormEncoded, TransactionType](_ => Success(Paper))
+                  case "02" => Rule[UrlFormEncoded, TransactionType](_ => Success(DigitalSpreadsheet))
+                  case "03" =>
+                    (__ \ "name").read(softwareNameType) fmap DigitalSoftware.apply
+                }.foldLeft[Rule[UrlFormEncoded, Set[TransactionType]]](
+                  Rule[UrlFormEncoded, Set[TransactionType]](_ => Success(Set.empty))
+                ) {
+                  case (m, n) =>
+                    n flatMap { x =>
+                      m fmap {
+                        _ + x
+                      }
+                    }
+                } fmap TransactionRecordYes.apply
               }
             }
         }
+        case false => Rule.fromMapping { _ => Success(TransactionRecordNo) }
       }
-    }
+  }
+
+  def getTupleFromModel(value: Set[TransactionType]) : (Seq[String], String) = {
+      val data = value.toSeq.map {
+          case Paper => ("01", "")
+          case DigitalSpreadsheet => ("02", "")
+          case DigitalSoftware(name) => ("03", name)
+
+      }.foldLeft[(Seq[String], String)](
+          (Nil, "")
+      ) (
+        (result, txValue) =>
+          (result._1 :+ txValue._1, result._2.concat(txValue._2))
+      )
+    data
+  }
+
+  implicit def formWrites = Write[TransactionRecord, UrlFormEncoded] {
+      case TransactionRecordNo => Map("isRecorded" -> "false")
+      case TransactionRecordYes(value) => {
+         val data = getTupleFromModel(value)
+              Map("isRecorded" -> "true",
+                   "transactions" -> data._1,
+                   "name" -> data._2)
+      }
+  }
 }
 
 object TransactionRecord {
 
-  implicit def formReads
-  (
-    implicit p:Path => RuleLike[UrlFormEncoded, Set[TransactionType]]
-  ):Rule[UrlFormEncoded, TransactionRecord] =
-    From[UrlFormEncoded] { __ =>
+  implicit val jsonReads: Reads[TransactionRecord] =
+    (__ \ "isRecorded").read[Boolean] flatMap {
+      case true => (__ \ "transactions").read[Set[String]].flatMap {x =>
+        x.map {
+            case "01" => Reads(_ => JsSuccess(Paper))
+            case "02" => Reads(_ => JsSuccess(DigitalSpreadsheet))
+            case "03" =>
+              (JsPath \ "name").read[String].map (DigitalSoftware.apply  _)
 
-      import play.api.data.mapping.forms.Rules._
-
-      (__ \ "isRecorded").read[Boolean].flatMap {
-        case true => {
-          __.read[Set[TransactionType]] fmap TransactionRecordYes.apply
+          }.foldLeft[Reads[Set[TransactionType]]](
+            Reads[Set[TransactionType]](_ => JsSuccess(Set.empty))
+         ){
+          (result, data) =>
+            data flatMap {u =>
+             result.map {q =>
+               q + u
+             }
+           }
         }
-        case false => Rule.fromMapping { _ => Success(TransactionRecordNo) }
-      }
+      } map TransactionRecordYes.apply
+      case false => Reads(_ => JsSuccess(TransactionRecordNo))
     }
+
+  implicit val jsonWrite = Writes[TransactionRecord] {
+    case TransactionRecordNo => Json.obj("isRecorded" -> true)
+    case TransactionRecordYes(name) => {
+      val data = TransactionType.getTupleFromModel(name)
+            Json.obj("isRecorded" -> true,
+              "transactions" -> data._1,
+              "name" -> data._2)
+            }
+  }
+
 }
--- test/models/businessactivities/TransactionTypeSpec.scala
+++ test/models/businessactivities/TransactionTypeSpec.scala
@@ -1,8 +1,11 @@
 package models.businessactivities
 
+import models.estateagentbusiness.Services
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
-import play.api.data.mapping. Success
+import play.api.data.mapping.{Path, Failure, Success}
+import play.api.data.validation.ValidationError
+import play.api.libs.json.{JsError, JsPath, JsSuccess, Json}
 
 
 class TransactionTypeSpec extends PlaySpec with MockitoSugar {
@@ -19,9 +22,118 @@ class TransactionTypeSpec extends PlaySpec with MockitoSugar {
         "name" -> Seq("test")
       )
 
-      TransactionRecord.formReads.validate(model) must
+      TransactionType.formRule.validate(model) must
         be(Success(TransactionRecordYes(Set(Paper, DigitalSpreadsheet, DigitalSoftware("test")))))
 
+    }
+
+    "fail validation when field is recorded not selected" in {
+
+      val model = Map(
+        "transactions[]" -> Seq("01", "02" ,"03"),
+        "name" -> Seq("test")
+      )
+
+      TransactionType.formRule.validate(model) must
+        be(Failure(List(( Path \ "isRecorded", Seq(ValidationError("error.required"))))))
+
+    }
+
+    "fail validation when none of the check boxes selected" in {
+
+      val model = Map(
+        "isRecorded" -> Seq("true"),
+        "transactions[]" -> Seq(),
+        "name" -> Seq("test")
+      )
+
+      TransactionType.formRule.validate(model) must
+        be(Failure(List(( Path \ "transactions", Seq(ValidationError("error.required"))))))
+
+    }
+
+
+    "fail to validate on empty Map" in {
+
+      TransactionType.formRule.validate(Map.empty) must
+        be(Failure(Seq((Path \ "isRecorded") -> Seq(ValidationError("error.required")))))
+
+    }
+
+    "validate form write for valid transaction record" in {
+
+      val map = Map(
+        "isRecorded" -> Seq("true"),
+        "transactions" -> Seq("03","01"),
+        "name" -> Seq("test")
+      )
+
+      val model = TransactionRecordYes(Set(DigitalSoftware("test"), Paper))
+     TransactionType.formWrites.writes(model) must be (map)
+    }
+
+    "validate form write for option No" in {
+
+      val map = Map(
+        "isRecorded" -> Seq("false")
+      )
+
+      val model = TransactionRecordNo
+      TransactionType.formWrites.writes(model) must be (map)
+    }
+
+    "validate form write for option Yes" in {
+
+      val map = Map(
+        "isRecorded" -> Seq("true"),
+        "transactions" -> Seq("02","01"),
+        "name" -> Seq("")
+      )
+
+      val model = TransactionRecordYes(Set(DigitalSpreadsheet, Paper))
+      TransactionType.formWrites.writes(model) must be (map)
+    }
+
+
+    "form write test" in {
+      val map = Map(
+        "isRecorded" -> Seq("false")
+      )
+
+      val model = TransactionRecordNo
+
+      TransactionType.formWrites.writes(model) must be(map)
+    }
+
+    "JSON validation" must {
+
+      "successfully validate given values" in {
+        val json =  Json.obj("isRecorded" -> true,
+          "transactions" -> Seq("01","02"))
+
+        Json.fromJson[TransactionRecord](json) must
+          be(JsSuccess(TransactionRecordYes(Set(Paper, DigitalSpreadsheet)), JsPath \ "isRecorded" \ "transactions"))
+      }
+
+      "successfully validate given values with option Digital software" in {
+        val json =  Json.obj("isRecorded" -> true,
+          "transactions" -> Seq("03", "02"),
+        "name" -> "test")
+
+        Json.fromJson[TransactionRecord](json) must
+          be(JsSuccess(TransactionRecordYes(Set(DigitalSoftware("test"), DigitalSpreadsheet)), JsPath \ "isRecorded" \ "transactions" \ "name"))
+      }
+
+      "fail when on path is missing" in {
+        Json.fromJson[TransactionRecord](Json.obj("isRecorded" -> true,
+          "transaction" -> Seq("01"))) must
+          be(JsError((JsPath \ "isRecorded" \ "transactions") -> ValidationError("error.path.missing")))
+      }
+
+      "fail when on invalid data" in {
+        Json.fromJson[TransactionRecord](Json.obj("isRecorded" -> true,"transactions" -> Seq("40"))) must
+          be(JsError(((JsPath \ "isRecorded" \ "transactions")(0) \ "transactions") -> ValidationError("error.invalid")))
+      }
     }
   }
 }
