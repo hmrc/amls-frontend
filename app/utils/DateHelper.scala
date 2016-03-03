af52faf7f0b4480c4a6504b0e40a229244b303a0
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-03-03 10:58:43 +0000

    AMLS: Added helper for LocalDate formatting to return the date in d MMMM YYYY format

--- app/utils/DateHelper.scala
+++ app/utils/DateHelper.scala
@@ -1,8 +1,14 @@
 package utils
+
 import org.joda.time.LocalDate
+import org.joda.time.format.DateTimeFormat
 
 object DateHelper {
   def isNotFutureDate = {
     date: LocalDate => !date.isAfter(LocalDate.now())
+  }
+
+  def formatDate(date: LocalDate) = {
+    DateTimeFormat.forPattern("d MMMM yyyy").print(date)
   }
 }
