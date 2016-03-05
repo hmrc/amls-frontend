4bba623d11a747fade8498b7ca9d5ca662663863
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-03-05 11:47:37 +0000

    AMLS-391: Added types for Form validation

--- app/models/FormTypes.scala
+++ app/models/FormTypes.scala
@@ -35,6 +35,8 @@ object FormTypes {
   val maxSortCodeLength = 6
   val maxSoftwareNameLength = 40
   val maxFranchiseName = 140
+  val maxEmployeeLength = 11
+
 
   val notEmptyStrip = Rule.zero[String] fmap { _.trim } compose notEmpty
 
@@ -104,4 +106,6 @@ object FormTypes {
   val franchiseNameType =  notEmptyStrip compose maxLength(maxFranchiseName)
 
   val OtherBusinessActivityType = notEmptyStrip compose maxLength(maxOtherBusinessActivityTypeLength)
+
+  val employeeCountType = notEmptyStrip compose maxLength(maxEmployeeLength) compose pattern("^[0-9]+$".r)
 }
