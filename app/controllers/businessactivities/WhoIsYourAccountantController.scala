584b61deeaf8289b3d12caa062304089490a8a31
Ryan Burke <ryan.burke@digital.hmrc.gov.uk>
2016-03-11 12:57:21 +0000

    AMLS-443: Created who is your accountant view

--- app/controllers/businessactivities/WhoIsYourAccountantController.scala
+++ app/controllers/businessactivities/WhoIsYourAccountantController.scala
@@ -1,20 +1,55 @@
 package controllers.businessactivities
 
 import config.AMLSAuthConnector
+import connectors.DataCacheConnector
 import controllers.BaseController
+import forms.{ValidForm, InvalidForm, EmptyForm, Form2}
+import models.businessactivities.{WhoIsYourAccountant, BusinessActivities}
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 
 import scala.concurrent.Future
 
 trait WhoIsYourAccountantController extends BaseController {
 
-  def get = Authorised.async {
+  val dataCacheConnector: DataCacheConnector
+
+  def get(edit: Boolean = false) = Authorised.async {
     implicit authContext => implicit request =>
-      Future.successful(Ok(views.html.who_is_your_agent()))
+      dataCacheConnector.fetchDataShortLivedCache[BusinessActivities](BusinessActivities.key) map {
+        response =>
+          val form = (for {
+            businessActivities <- response
+            whoIsYourAccountant <- businessActivities.whoIsYourAccountant
+            //TODO: Replace EmptyForm with Form2[WhoIsYourAccountant](whoIsYourAccountant)
+          } yield EmptyForm).getOrElse(EmptyForm)
+          Ok(views.html.who_is_your_accountant(form, edit))
+      }
+  }
+
+  def post(edit : Boolean = false) = Authorised.async {
+    import play.api.data.mapping.forms.Rules._
+    implicit authContext => implicit request =>
+      Form2(request.body) match {
+        case f: InvalidForm =>
+          Future.successful(BadRequest(views.html.who_is_your_accountant(f, edit)))
+        case ValidForm(_, data) => {
+          for {
+            businessActivity <-
+            dataCacheConnector.fetchDataShortLivedCache[BusinessActivities](BusinessActivities.key)
+            _ <- dataCacheConnector.saveDataShortLivedCache[BusinessActivities](BusinessActivities.key,
+              businessActivity//.whoIsYourAccountant(data)
+            )
+          } yield edit match {
+            case true => Redirect(routes.WhatYouNeedController.get())
+            case false => Redirect(routes.AccountantForAMLSRegulationsController.get())
+          }
+        }
+      }
   }
 
 }
 
 object WhoIsYourAccountantController extends WhoIsYourAccountantController {
   override protected def authConnector: AuthConnector = AMLSAuthConnector
+  override val dataCacheConnector: DataCacheConnector = DataCacheConnector
 }
