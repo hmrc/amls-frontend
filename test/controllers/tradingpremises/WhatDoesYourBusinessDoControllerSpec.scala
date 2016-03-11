94e484488b594978eb68452764063415d6f6b18b
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-03-11 14:41:19 +0000

    AMLS-TPTESTS: WIP for Tests

--- test/controllers/tradingpremises/WhatDoesYourBusinessDoControllerSpec.scala
+++ test/controllers/tradingpremises/WhatDoesYourBusinessDoControllerSpec.scala
@@ -3,8 +3,8 @@ package controllers.tradingpremises
 import java.util.UUID
 
 import connectors.DataCacheConnector
-import models.businessactivities.{BusinessActivities, InvolvedInOtherYes}
-import models.businessmatching.{BusinessActivities => BusinessMatchingBusinessActivities, _}
+import models.businessactivities.{BusinessActivities, ExpectedBusinessTurnover, InvolvedInOtherYes}
+import models.businessmatching.{BusinessActivities => BusinessMatchingActivities, _}
 import models.tradingpremises.{TradingPremises, WhatDoesYourBusinessDo}
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -12,7 +12,6 @@ import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
-import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.cache.client.CacheMap
 import uk.gov.hmrc.play.frontend.auth.AuthContext
@@ -23,7 +22,6 @@ import scala.concurrent.Future
 
 class WhatDoesYourBusinessDoControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar {
 
-  implicit val request = FakeRequest
   val userId = s"user-${UUID.randomUUID()}"
   val mockDataCacheConnector = mock[DataCacheConnector]
 
@@ -38,9 +36,9 @@ class WhatDoesYourBusinessDoControllerSpec extends PlaySpec with OneServerPerSui
 
   val fieldElements = Array("report-name", "report-email", "report-action", "report-error")
 
-  "WhatDoesYourBusinessDoController" must {
+  "WhatDoesYourBusinessDoController get" must {
 
-    "when the form is empty then go to what does your business do with empty fields" in new Fixture {
+    "check if the form is empty and then load what does your business do with empty fields" in new Fixture {
 
       val tradingPremises = TradingPremises()
       val mockCacheMap = mock[CacheMap]
@@ -60,27 +58,27 @@ class WhatDoesYourBusinessDoControllerSpec extends PlaySpec with OneServerPerSui
 
       when(mockCacheMap.getEntry[Seq[TradingPremises]](any())(any())).thenReturn(Some(Seq(tradingPremises)))
 
-      val businessMatchingBusinessActivities = BusinessMatchingBusinessActivities(Set(AccountancyServices, BillPaymentServices, EstateAgentBusinessService))
-      when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key)).thenReturn(Some(BusinessMatching(Some(businessMatchingBusinessActivities))))
+      val businessMatchingActivities = BusinessMatchingActivities(Set(AccountancyServices, BillPaymentServices, EstateAgentBusinessService))
+      when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key)).thenReturn(Some(BusinessMatching(Some(businessMatchingActivities))))
 
       val RecordId = 1
       val result = whatDoesYourBusinessDoController.get(RecordId)(request)
 
       status(result) must be(OK)
 
-      /*
-            val document: Document = Jsoup.parse(contentAsString(result))
-            document.select(s"input[id=activities-01]").hasAttr("checked") must be(false)
-      */
+
+      val document: Document = Jsoup.parse(contentAsString(result))
+      document.select(s"input[id=activities-01]").hasAttr("checked") must be(false)
     }
 
-    "when the form is not-empty then go to what does your business do with fields populated" in new Fixture {
+    "check if the the form is not-empty and then load what does your business do with fields populated" in new Fixture {
 
       val wdbd = WhatDoesYourBusinessDo(Set(AccountancyServices, BillPaymentServices))
       val tradingPremises = TradingPremises(None, None, Some(wdbd))
 
       val mockCacheMap = mock[CacheMap]
-      val businessActivities = BusinessActivities(involvedInOther = Some(InvolvedInOtherYes("test")))
+      val businessActivities = BusinessActivities(involvedInOther = Some(InvolvedInOtherYes("test")),
+        expectedBusinessTurnover = Some(ExpectedBusinessTurnover.Fifth))
 
       when(mockDataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
         .thenReturn(Future.successful(Some(mockCacheMap)))
@@ -96,8 +94,8 @@ class WhatDoesYourBusinessDoControllerSpec extends PlaySpec with OneServerPerSui
 
       when(mockCacheMap.getEntry[Seq[TradingPremises]](any())(any())).thenReturn(Some(Seq(tradingPremises)))
 
-      val businessMatchingBusinessActivities = BusinessMatchingBusinessActivities(Set(AccountancyServices, BillPaymentServices, EstateAgentBusinessService))
-      when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key)).thenReturn(Some(BusinessMatching(Some(businessMatchingBusinessActivities))))
+      val businessMatchingActivities = BusinessMatchingActivities(Set(AccountancyServices, BillPaymentServices, EstateAgentBusinessService))
+      when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key)).thenReturn(Some(BusinessMatching(Some(businessMatchingActivities))))
 
       val RecordId = 1
       val result = whatDoesYourBusinessDoController.get(RecordId)(request)
@@ -109,13 +107,13 @@ class WhatDoesYourBusinessDoControllerSpec extends PlaySpec with OneServerPerSui
 
     }
 
-    "when no Business Matching then redirect to the trading premises with recordId " in new Fixture {
+    "must redirect to the trading premises with recordId when no Business Activity" in new Fixture {
 
       val tradingPremises = TradingPremises()
       val mockCacheMap = mock[CacheMap]
       when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key)).thenReturn(None)
 
-      when(whatDoesYourBusinessDoController.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
+      when(mockDataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
         .thenReturn(Future.successful(Some(mockCacheMap)))
 
       when(mockDataCacheConnector.fetchDataShortLivedCache[Seq[TradingPremises]](any())
@@ -130,8 +128,135 @@ class WhatDoesYourBusinessDoControllerSpec extends PlaySpec with OneServerPerSui
 
       status(result) must be(SEE_OTHER)
     }
+  }
+
+  "WhatDoesYourBusinessDoController post" must {
+
+    "for an Invalid Request must give a Bad Request" in new Fixture {
+
+      val tradingPremises = TradingPremises(None, None, None)
+      val mockCacheMap = mock[CacheMap]
+
+      when(mockDataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
+        .thenReturn(Future.successful(Some(mockCacheMap)))
+
+      when(mockDataCacheConnector.fetchDataShortLivedCache[Seq[TradingPremises]](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(tradingPremises))))
+
+      when(mockDataCacheConnector.saveDataShortLivedCache[Seq[TradingPremises]](any(), any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(tradingPremises))))
+
+      when(mockCacheMap.getEntry[Seq[TradingPremises]](any())(any())).thenReturn(Some(Seq(tradingPremises)))
+
+      val businessMatchingActivities = BusinessMatchingActivities(Set(AccountancyServices, BillPaymentServices, EstateAgentBusinessService))
+      when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key)).thenReturn(Some(BusinessMatching(Some(businessMatchingActivities))))
+
+      val invalidRequest = request.withFormUrlEncodedBody(
+        "activities" -> ""
+      )
+
+      val RecordId = 1
+      val result = whatDoesYourBusinessDoController.post(RecordId)(invalidRequest)
+      status(result) must be(BAD_REQUEST)
+      //      headers(result).foreach( x => println(s" Key: ${x._1} Value : ${x._2}"))
+    }
 
 
+    "for a Valid Request with SINGLE Activity must redirect to Summary Controller" in new Fixture {
+
+      val wdbd = WhatDoesYourBusinessDo(Set(AccountancyServices))
+      val tradingPremises = TradingPremises(None, None, Some(wdbd))
+      val mockCacheMap = mock[CacheMap]
+
+      when(mockDataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
+        .thenReturn(Future.successful(Some(mockCacheMap)))
+
+      when(mockDataCacheConnector.fetchDataShortLivedCache[Seq[TradingPremises]](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(tradingPremises))))
+
+      when(mockDataCacheConnector.saveDataShortLivedCache[Seq[TradingPremises]](any(), any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(tradingPremises))))
+
+      when(mockCacheMap.getEntry[Seq[TradingPremises]](any())(any())).thenReturn(Some(Seq(tradingPremises)))
+
+      val businessMatchingActivities = BusinessMatchingActivities(Set(AccountancyServices))
+      when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key)).thenReturn(Some(BusinessMatching(Some(businessMatchingActivities))))
+
+      val newRequest = request.withFormUrlEncodedBody("activities[0]" -> "01")
+
+      val RecordId = 1
+      val result = whatDoesYourBusinessDoController.post(RecordId)(newRequest)
+      status(result) must be(SEE_OTHER)
+    }
+
+
+    "for a Valid Request with multiple ACTIVITIES must redirect to Summary Controller" in new Fixture {
+
+      val wdbd = WhatDoesYourBusinessDo(Set(AccountancyServices, BillPaymentServices))
+      val tradingPremises = TradingPremises(None, None, Some(wdbd))
+      val businessMatchingActivities = BusinessMatchingActivities(Set(AccountancyServices,
+        BillPaymentServices, EstateAgentBusinessService))
+      val mockCacheMap = mock[CacheMap]
+
+      when(mockDataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
+        .thenReturn(Future.successful(Some(mockCacheMap)))
+
+      when(mockDataCacheConnector.fetchDataShortLivedCache[Seq[TradingPremises]](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(tradingPremises))))
+
+      when(mockDataCacheConnector.saveDataShortLivedCache[Seq[TradingPremises]](any(), any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(tradingPremises))))
+
+      when(mockCacheMap.getEntry[Seq[TradingPremises]](any())(any())).thenReturn(Some(Seq(tradingPremises)))
+
+      when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key)).thenReturn(Some(BusinessMatching(Some(businessMatchingActivities))))
+
+      val newRequest = request.withFormUrlEncodedBody(
+        "activities[0]" -> "01",
+        "activities[1]" -> "02",
+        "activities[2]" -> "03"
+      )
+
+      val RecordId = 1
+      val result = whatDoesYourBusinessDoController.post(RecordId)(newRequest)
+      status(result) must be(SEE_OTHER)
+      redirectLocation(result) must be(Some(s"/anti-money-laundering/trading-premises/summary"))
+    }
+
+
+    "for a Valid Request in EDIT Mode must redirect to the trading premises summary with record id" in new Fixture {
+
+      val wdbd = WhatDoesYourBusinessDo(Set(AccountancyServices, BillPaymentServices))
+      val tradingPremises = TradingPremises(None, None, Some(wdbd))
+      val businessMatchingActivities = BusinessMatchingActivities(Set(AccountancyServices,
+        BillPaymentServices, EstateAgentBusinessService))
+      val mockCacheMap = mock[CacheMap]
+
+      when(mockDataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
+        .thenReturn(Future.successful(Some(mockCacheMap)))
+
+      when(mockDataCacheConnector.fetchDataShortLivedCache[Seq[TradingPremises]](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(tradingPremises))))
+
+      when(mockDataCacheConnector.saveDataShortLivedCache[Seq[TradingPremises]](any(), any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(tradingPremises))))
+
+      when(mockCacheMap.getEntry[Seq[TradingPremises]](any())(any())).thenReturn(Some(Seq(tradingPremises)))
+
+      when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key)).thenReturn(Some(BusinessMatching(Some(businessMatchingActivities))))
+
+      val newRequest = request.withFormUrlEncodedBody(
+        "activities[0]" -> "01",
+        "activities[1]" -> "02",
+        "activities[2]" -> "03"
+      )
+
+      val RecordId = 1
+      val result = whatDoesYourBusinessDoController.post(RecordId, true)(newRequest)
+
+      status(result) must be(SEE_OTHER)
+      redirectLocation(result) must be(Some(s"/anti-money-laundering/trading-premises/summary/${RecordId}"))
+    }
   }
 
 }
---
+++ test/controllers/tradingpremises/WhereAreTradingPremisesControllerSpec.scala
@@ -0,0 +1,8 @@
+package controllers.tradingpremises
+
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+
+class WhereAreTradingPremisesControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar{
+
+}
