fc19ce93d7b2e4ae51e18f269223e1713b7ce25c
Joe Simmonds <joe.simmonds@digital.hmrc.gov.uk>
2016-03-11 14:45:56 +0000

    added accountants reference field

--- app/models/businessactivities/WhoIsYourAccountant.scala
+++ app/models/businessactivities/WhoIsYourAccountant.scala
@@ -1,16 +1,40 @@
 package models.businessactivities
 
+import play.api.libs.json._
+
+sealed trait DoesAccountantAlsoDealWithTax
+
+case object AccountantDoesNotAlsoDealWithTax extends DoesAccountantAlsoDealWithTax
+
+case class AccountantDoesAlsoDealWithTax(accountantsRef: String) extends DoesAccountantAlsoDealWithTax
+
+object DoesAccountantAlsoDealWithTax {
+
+  implicit val jsonReads: Reads[DoesAccountantAlsoDealWithTax] =
+    (__ \ "doesAccountantAlsoDealWithTax").read[Boolean] flatMap {
+      case true => (__ \ "accountantsReference").read[String] map AccountantDoesAlsoDealWithTax.apply
+      case false => Reads(_ => JsSuccess(AccountantDoesNotAlsoDealWithTax))
+    }
+
+  implicit val jsonWrites = Writes[DoesAccountantAlsoDealWithTax] {
+    case AccountantDoesAlsoDealWithTax(ref) => Json.obj(
+      "doesAccountantAlsoDealWithTax" -> true,
+      "accountantsReference" -> ref
+    )
+    case AccountantDoesNotAlsoDealWithTax => Json.obj("doesAccountantAlsoDealWithTax" -> false)
+  }
+}
+
 case class WhoIsYourAccountant(name: String,
                                tradingName: Option[String],
                                address: AccountantsAddress,
-                               alsoDealsWithTax: Boolean) {
+                               alsoDealsWithTax: DoesAccountantAlsoDealWithTax) {
 
 
   def name(newName: String) : WhoIsYourAccountant = this.copy(name = newName)
   def tradingName(newTradingName: String) : WhoIsYourAccountant = this.copy(tradingName = Some(newTradingName))
   def address(newAddress: AccountantsAddress) : WhoIsYourAccountant = this.copy(address = newAddress)
-  def alsoDealsWithTax(newAlsoDealsWithTax: Boolean) : WhoIsYourAccountant = this.copy(alsoDealsWithTax = newAlsoDealsWithTax)
-
+  def alsoDealsWithTax(newAlsoDealsWithTax: DoesAccountantAlsoDealWithTax) : WhoIsYourAccountant = this.copy(alsoDealsWithTax = newAlsoDealsWithTax)
 }
 
 object WhoIsYourAccountant {
@@ -21,3 +45,5 @@ object WhoIsYourAccountant {
 
   implicit val formats = Json.format[WhoIsYourAccountant]
 }
+
+
--- test/models/businessactivities/WhoIsYourAccountantSpec.scala
+++ test/models/businessactivities/WhoIsYourAccountantSpec.scala
@@ -1,13 +1,13 @@
 package models.businessactivities
 
-import org.scalatest.{Matchers, WordSpec}
-import play.api.libs.json.JsSuccess
+import org.scalatest.{MustMatchers, Matchers, WordSpec}
+import play.api.libs.json.{JsPath, Json, JsSuccess}
 
 class WhoIsYourAccountantSpec extends WordSpec with Matchers {
 
   val DefaultName = "Default Name"
   val DefaultTradingName = Some("Default Trading Name")
-  val DefaultDealsWithTax = true
+  val DefaultDealsWithTaxYes = AccountantDoesAlsoDealWithTax("Accountant reference")
 
   val DefaultAddressLine1 = "Default Line 1"
   val DefaultAddressLine2 = "Default Line 2"
@@ -25,7 +25,7 @@ class WhoIsYourAccountantSpec extends WordSpec with Matchers {
   val DefaultWhoIsYourAccountant = WhoIsYourAccountant(DefaultName,
     DefaultTradingName,
     DefaultUKAddress,
-    DefaultDealsWithTax)
+    DefaultDealsWithTaxYes)
 
   "WhoIsYourAccountant" must {
 
@@ -34,7 +34,55 @@ class WhoIsYourAccountantSpec extends WordSpec with Matchers {
         WhoIsYourAccountant.formats.writes(DefaultWhoIsYourAccountant)
       ) shouldBe JsSuccess(DefaultWhoIsYourAccountant)
     }
+  }
 
+  "DoesAccountantalsoDealWithTax" must {
+    "Serialise yes to json correctly" in {
+        val expected = Json.obj(
+          "doesAccountantAlsoDealWithTax" -> true,
+          "accountantsReference" -> "Accountant reference"
+        )
+        DoesAccountantAlsoDealWithTax.jsonWrites.writes(DefaultDealsWithTaxYes) should be (expected)
+    }
+
+    "Serialise no to json correctly" in {
+      val expected = Json.obj(
+        "doesAccountantAlsoDealWithTax" -> false
+      )
+      DoesAccountantAlsoDealWithTax.jsonWrites.writes(AccountantDoesNotAlsoDealWithTax) should be (expected)
+    }
+
+    "Deserialise yes from json Correctly" in {
+      val json = Json.obj(
+        "doesAccountantAlsoDealWithTax" -> true,
+        "accountantsReference" -> "Accountant reference"
+      )
+      DoesAccountantAlsoDealWithTax.jsonReads.reads(json) should be
+        (JsSuccess(DefaultDealsWithTaxYes,
+         JsPath \ "doesAccountantAlsoDealWithTax" \ "accountantsReference"))
+    }
+
+    "Deserialise no from json Correctly" in {
+      val json = Json.obj(
+        "doesAccountantAlsoDealWithTax" -> false
+      )
+      DoesAccountantAlsoDealWithTax.jsonReads.reads(json) should be (
+        JsSuccess(AccountantDoesNotAlsoDealWithTax, JsPath \"doesAccountantAlsoDealWithTax"))
+    }
+
+    "Round trip yes" in {
+      DoesAccountantAlsoDealWithTax.jsonReads.reads (
+        DoesAccountantAlsoDealWithTax.jsonWrites.writes(DefaultDealsWithTaxYes)
+      ) should be (
+          JsSuccess(DefaultDealsWithTaxYes,
+            JsPath \ "doesAccountantAlsoDealWithTax" \ "accountantsReference"))
+    }
+
+    "Round trip no" in {
+      DoesAccountantAlsoDealWithTax.jsonReads.reads (
+        DoesAccountantAlsoDealWithTax.jsonWrites.writes(AccountantDoesNotAlsoDealWithTax)
+      ) should be (JsSuccess(AccountantDoesNotAlsoDealWithTax, JsPath \ "doesAccountantAlsoDealWithTax"))
+    }
   }
 
 }
