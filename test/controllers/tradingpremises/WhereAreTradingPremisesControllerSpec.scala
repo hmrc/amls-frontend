32e0389e63e81d1a74955e0bfb8336a06d1db089
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-03-12 10:18:22 +0000

    AMLS-TPTESTS: WIP for Tests

--- test/controllers/tradingpremises/WhereAreTradingPremisesControllerSpec.scala
+++ test/controllers/tradingpremises/WhereAreTradingPremisesControllerSpec.scala
@@ -1,8 +1,44 @@
 package controllers.tradingpremises
 
+import config.AMLSAuthConnector
+import connectors.DataCacheConnector
+import models.tradingpremises.TradingPremises
+import org.mockito.Matchers._
+import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.i18n.Messages
+import play.api.test.Helpers._
+import utils.AuthorisedFixture
 
-class WhereAreTradingPremisesControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar{
+import scala.concurrent.Future
+
+class WhereAreTradingPremisesControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar {
+
+  trait Fixture extends AuthorisedFixture {
+    self =>
+
+    val controller = new WhereAreTradingPremisesController {
+      override val dataCacheConnector = mock[DataCacheConnector]
+      override val authConnector = self.authConnector
+    }
+  }
+
+  "WhereAreTradingPremisesController" must {
+
+    "use correct services" in new Fixture {
+      WhereAreTradingPremisesController.authConnector must be(AMLSAuthConnector)
+      WhereAreTradingPremisesController.dataCacheConnector must be(DataCacheConnector)
+    }
+
+
+    "on get display Where are Trading Premises page" in new Fixture {
+      when(controller.dataCacheConnector.fetchDataShortLivedCache[TradingPremises](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+      val result = controller.get(any())(request)
+      status(result) must be(OK)
+      contentAsString(result) must include(Messages("tradingpremises.yourtradingpremises.title"))
+    }
+  }
 
 }
