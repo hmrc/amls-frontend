5205608763fb32c288472e539a387f70debf1dc7
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-03-16 18:38:07 +0000

    AMLS-432 Added the model, form rules/writes and json reads/writes

---
+++ app/models/declaration/AddPerson.scala
@@ -0,0 +1,69 @@
+package models.declaration
+
+import models.aboutyou.RoleWithinBusiness
+import play.api.data.mapping.forms._
+import play.api.data.mapping.{From, Rule, To, Write}
+
+case class AddPerson(firstName: String,
+                     middleName: Option[String],
+                     lastName: String,
+                     roleWithinTheBusiness: RoleWithinBusiness
+                    )
+
+object AddPerson {
+
+  import play.api.libs.json._
+
+  val key = "add-person"
+
+  implicit val reads: Reads[AddPerson] = {
+    import play.api.libs.functional.syntax._
+    import play.api.libs.json.Reads._
+    import play.api.libs.json._
+    (
+      (__ \ "firstName").read[String] and
+        (__ \ "middleName").read[Option[String]] and
+        (__ \ "lastName").read[String] and
+        (__.read[RoleWithinBusiness])
+      ) (AddPerson.apply _)
+
+  }
+
+
+  implicit val writes: Writes[AddPerson] = {
+    import play.api.libs.functional.syntax._
+    import play.api.libs.json.Writes._
+    import play.api.libs.json._
+    (
+      (__ \ "firstName").write[String] and
+        (__ \ "middleName").write[Option[String]] and
+        (__ \ "lastName").write[String] and
+        __.write[RoleWithinBusiness]
+      ) (unlift(AddPerson.unapply))
+  }
+
+
+  implicit val formRule: Rule[UrlFormEncoded, AddPerson] = From[UrlFormEncoded] { __ =>
+    import models.FormTypes._
+    import play.api.data.mapping.forms.Rules._
+    (
+      (__ \ "firstName").read(indivNameType) and
+        (__ \ "middleName").read(optionR(indivNameType)) and
+        (__ \ "lastName").read(indivNameType) and
+        __.read[RoleWithinBusiness]
+      ) (AddPerson.apply _)
+  }
+
+  implicit val formWrites: Write[AddPerson, UrlFormEncoded] = To[UrlFormEncoded] { __ =>
+    import play.api.data.mapping.forms.Writes._
+    import play.api.libs.functional.syntax.unlift
+    (
+      (__ \ "firstName").write[String] and
+        (__ \ "middleName").write[Option[String]] and
+        (__ \ "lastName").write[String] and
+        __.write[RoleWithinBusiness]
+      ) (unlift(AddPerson.unapply))
+  }
+
+
+}
