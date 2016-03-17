bafad82c82e4d8f6b3bc76e16ee0faaeab9e7848
Ryan Burke <ryan.burke@digital.hmrc.gov.uk>
2016-03-17 09:57:04 +0000

    Added AmlsRegime test

---
+++ test/controllers/auth/AmlsRegimeSpec.scala
@@ -0,0 +1,31 @@
+package controllers.auth
+
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import uk.gov.hmrc.play.frontend.auth.connectors.domain.{OrgAccount, SaAccount, CtAccount, Accounts}
+
+class AmlsRegimeSpec extends PlaySpec with OneServerPerSuite with MockitoSugar {
+
+  "AmlsRegimeSpec" should {
+
+    "Correctly calculate isAuthorised" in {
+
+      val ct = Accounts(ct = Some(mock[CtAccount]))
+      val org = Accounts(org = Some(mock[OrgAccount]))
+      val sa = Accounts(sa = Some(mock[SaAccount]))
+
+      AmlsRegime.isAuthorised(ct) must be (true)
+      AmlsRegime.isAuthorised(org) must be (true)
+      AmlsRegime.isAuthorised(sa) must be (true)
+      AmlsRegime.isAuthorised(Accounts()) must be (false)
+
+    }
+
+    "return the correct route for unauthorised page" in {
+      AmlsRegime.unauthorisedLandingPage must be (Some(controllers.routes.AmlsController.unauthorised().url))
+    }
+
+
+  }
+
+}
