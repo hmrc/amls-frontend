8858871116d5ea333c686822196f7b1955e45fd1
Michael Wolfendale <michael.wolfendale@gmail.com>
2016-03-26 11:42:59 +0000

    AMLS-358 fixes for business matching journey

--- app/services/SubscriptionService.scala
+++ app/services/SubscriptionService.scala
@@ -1,14 +1,17 @@
 package services
 
-import connectors.{DataCacheConnector, DESConnector}
+import connectors.{DESConnector, DataCacheConnector}
 import models.SubscriptionRequest
 import models.aboutthebusiness.AboutTheBusiness
 import models.bankdetails.BankDetails
+import models.businessactivities.BusinessActivities
 import models.businessmatching.BusinessMatching
+import models.declaration.AddPerson
 import models.estateagentbusiness.EstateAgentBusiness
 import models.tradingpremises.TradingPremises
+import play.api.libs.json.Json
 import uk.gov.hmrc.play.frontend.auth.AuthContext
-import uk.gov.hmrc.play.http.{NotFoundException, HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.play.http.{HeaderCarrier, HttpResponse, NotFoundException}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -30,17 +33,23 @@ trait SubscriptionService extends DataCacheService {
         } map {
           reviewDetails =>
             val request = SubscriptionRequest(
-              businessType =
-                for {
-                  businessMatching <- cache.getEntry[BusinessMatching](BusinessMatching.key)
-                  reviewDetails <- businessMatching.reviewDetails
-                  businessType <- reviewDetails.businessType
-                } yield businessType,
+//              businessType =
+//                for {
+//                  businessMatching <- cache.getEntry[BusinessMatching](BusinessMatching.key)
+//                  reviewDetails <- businessMatching.reviewDetails
+//                  businessType <- reviewDetails.businessType
+//                } yield businessType,
+              businessMatchingSection = cache.getEntry[BusinessMatching](BusinessMatching.key),
               eabSection = cache.getEntry[EstateAgentBusiness](EstateAgentBusiness.key),
-              aboutTheBusinessSection = cache.getEntry[AboutTheBusiness](AboutTheBusiness.key),
               tradingPremisesSection = cache.getEntry[Seq[TradingPremises]](TradingPremises.key),
-              bankDetailsSection = cache.getEntry[Seq[BankDetails]](BankDetails.key)
+              aboutTheBusinessSection = cache.getEntry[AboutTheBusiness](AboutTheBusiness.key),
+              bankDetailsSection = cache.getEntry[Seq[BankDetails]](BankDetails.key),
+              aboutYouSection = cache.getEntry[AddPerson](AddPerson.key),
+              businessActivitiesSection = cache.getEntry[BusinessActivities](BusinessActivities.key)
             )
+            println()
+            println(Json.prettyPrint(Json.toJson(request)))
+            println()
             desConnector.subscribe(request, reviewDetails.safeId)
         } getOrElse Future.failed {
           new NotFoundException("No subscription data found for user")
