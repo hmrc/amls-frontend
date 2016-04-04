ddccd597191927e5172dfaa30cf557f96cad2688
Michael Wolfendale <michael.wolfendale@gmail.com>
2016-04-04 13:50:39 +0100

    remove GG call to show successful journey in dev for ST

--- app/services/GovernmentGatewayService.scala
+++ app/services/GovernmentGatewayService.scala
@@ -3,6 +3,7 @@ package services
 import connectors.GovernmentGatewayConnector
 import models.governmentgateway.{EnrolmentRequest, EnrolmentResponse}
 import uk.gov.hmrc.play.http.{HeaderCarrier, HttpResponse}
+import play.api.http.Status.OK
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -16,10 +17,11 @@ trait GovernmentGatewayService {
    hc: HeaderCarrier,
    ec: ExecutionContext
   ): Future[HttpResponse] =
-    ggConnector.enrol(EnrolmentRequest(
-      mlrRefNo = mlrRefNo,
-      safeId = safeId
-    ))
+    Future.successful(HttpResponse(OK))
+//    ggConnector.enrol(EnrolmentRequest(
+//      mlrRefNo = mlrRefNo,
+//      safeId = safeId
+//    ))
 }
 
 object GovernmentGatewayService extends GovernmentGatewayService {
