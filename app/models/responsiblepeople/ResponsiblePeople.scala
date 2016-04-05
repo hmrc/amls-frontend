3a089caf1510f994327967a7314dd8a6d60d29aa
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-04-05 14:35:42 +0100

    AMLS-83: Model changes for repeating section and test

--- app/models/responsiblepeople/ResponsiblePeople.scala
+++ app/models/responsiblepeople/ResponsiblePeople.scala
@@ -15,30 +15,10 @@ object ResponsiblePeople {
   val key = "responsible-people"
 
   implicit val mongoKey = new MongoKey[ResponsiblePeople] {
-    override def apply(): String = "responsible-people"
+    override def apply(): String = key
   }
 
   implicit val format = Json.format[ResponsiblePeople]
-
-  /*  implicit val reads: Reads[ResponsiblePeople] =  {
-  /*
-      import play.api.libs.functional.syntax._
-      import play.api.libs.json.Reads._
-      import play.api.libs.json._
-  */
-      (
-        (__.read[Option[AddPerson]])
-      ) (ResponsiblePeople.apply _)
-    }
-
-    implicit val writes: Writes[ResponsiblePeople] = Writes[ResponsiblePeople] {
-      model =>
-        Seq(
-          Json.toJson(model.addPerson).asOpt[JsObject]
-        ).flatten.fold(Json.obj()) {
-          _ ++ _
-        }
-    } */
 
   implicit def default(responsiblePeople: Option[ResponsiblePeople]): ResponsiblePeople =
     responsiblePeople.getOrElse(ResponsiblePeople())
