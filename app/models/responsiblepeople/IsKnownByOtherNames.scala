7f212ff6cbb8a09053ed41b0249c65879255e1f0
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-04-06 15:56:50 +0100

    Changes to model for formrules/writes and json read/writes to save all the names supplied  and read it back

--- app/models/responsiblepeople/IsKnownByOtherNames.scala
+++ app/models/responsiblepeople/IsKnownByOtherNames.scala
@@ -4,11 +4,14 @@ import play.api.data.mapping._
 import play.api.data.mapping.forms.UrlFormEncoded
 import play.api.libs.json._
 import play.api.data.mapping.forms.Rules._
+import play.api.libs.functional.syntax._
 import utils.MappingUtils.Implicits._
 
 sealed trait IsKnownByOtherNames
 
-case class IsKnownByOtherNamesYes(value: String) extends IsKnownByOtherNames
+case class IsKnownByOtherNamesYes(otherfirstnames: String,
+                                  othermiddlenames: Option[String],
+                                  otherlastnames: String) extends IsKnownByOtherNames
 
 case object IsKnownByOtherNamesNo extends IsKnownByOtherNames
 
@@ -20,30 +23,43 @@ object IsKnownByOtherNames {
 
   implicit val formRule: Rule[UrlFormEncoded, IsKnownByOtherNames] = From[UrlFormEncoded] { __ =>
     import play.api.data.mapping.forms.Rules._
+    import utils.MappingUtils.Implicits._
     (__ \ "isKnownByOtherNames").read[Boolean] flatMap {
-      case true => (__ \ "otherfirstnames").read(firstNameType) fmap (IsKnownByOtherNamesYes.apply)
+      case true => (
+        (__ \ "otherfirstnames").read(firstNameType) and
+        (__ \ "othermiddlenames").read(optionR(firstNameType)) and
+        (__ \ "otherlastnames").read(firstNameType)
+        )(IsKnownByOtherNamesYes.apply _)
       case false => Rule.fromMapping { _ => Success(IsKnownByOtherNamesNo) }
     }
   }
 
   implicit val formWrites: Write[IsKnownByOtherNames, UrlFormEncoded] = Write {
-    case IsKnownByOtherNamesYes(value) =>
-      Map("isKnownByOtherNames" -> Seq("true"),
-        "otherfirstnames" -> Seq(value)
-      )
+    case a: IsKnownByOtherNamesYes => Map(
+      "isKnownByOtherNames" -> Seq("true"),
+      "otherfirstnames" -> Seq(a.otherfirstnames),
+      "othermiddlenames" -> Seq(a.othermiddlenames.getOrElse("")),
+      "otherlastnames" -> Seq(a.otherlastnames)
+    )
     case IsKnownByOtherNamesNo => Map("isKnownByOtherNames" -> Seq("false"))
   }
 
   implicit val jsonReads: Reads[IsKnownByOtherNames] =
     (__ \ "isKnownByOtherNames").read[Boolean] flatMap {
-      case true => (__ \ "otherfirstnames").read[String] map (IsKnownByOtherNamesYes.apply _)
+      case true => (
+        (__ \ "otherfirstnames").read[String] and
+          (__ \ "othermiddlenames").readNullable[String] and
+          (__ \ "otherlastnames").read[String]
+        ) (IsKnownByOtherNamesYes.apply _)
       case false => Reads(_ => JsSuccess(IsKnownByOtherNamesNo))
     }
 
   implicit val jsonWrites = Writes[IsKnownByOtherNames] {
-    case IsKnownByOtherNamesYes(value) => Json.obj(
+    case a : IsKnownByOtherNamesYes => Json.obj(
       "isKnownByOtherNames" -> true,
-      "otherfirstnames" -> value
+      "otherfirstnames" -> a.otherfirstnames,
+      "othermiddlenames" -> a.othermiddlenames,
+      "otherlastnames" -> a.otherlastnames
     )
     case IsKnownByOtherNamesNo => Json.obj("isKnownByOtherNames" -> false)
   }
