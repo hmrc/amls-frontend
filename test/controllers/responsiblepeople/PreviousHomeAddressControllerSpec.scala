36cf0ff0c6d3e9644b4e0d0e9786badd125fb444
Ryan Burke <ryan.burke@digital.hmrc.gov.uk>
2016-04-07 12:15:11 +0100

    AMLS-347: Added PreviousHomeAddressController test

---
+++ test/controllers/responsiblepeople/PreviousHomeAddressControllerSpec.scala
@@ -0,0 +1,218 @@
+package controllers.responsiblepeople
+
+import java.util.UUID
+
+import config.AMLSAuthConnector
+import connectors.DataCacheConnector
+import models.Country
+import models.responsiblepeople._
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.i18n.Messages
+import play.api.test.Helpers._
+import uk.gov.hmrc.http.cache.client.CacheMap
+import utils.AuthorisedFixture
+
+import scala.concurrent.Future
+
+class PreviousHomeAddressControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar {
+
+  val mockDataCacheConnector = mock[DataCacheConnector]
+  val RecordId = 1
+
+  trait Fixture extends AuthorisedFixture {
+    self =>
+
+    val previousHomeAddressController = new PreviousHomeAddressController {
+      override val dataCacheConnector = mockDataCacheConnector
+      override val authConnector = self.authConnector
+    }
+  }
+
+  val emptyCache = CacheMap("", Map.empty)
+
+  "PreviousHomeAddressController" must {
+
+    "use the correct services" in new Fixture {
+      PreviousHomeAddressController.dataCacheConnector must be(DataCacheConnector)
+      PreviousHomeAddressController.authConnector must be(AMLSAuthConnector)
+    }
+
+    "on get() display the persons page when no existing data in keystore" in new Fixture {
+
+      when(previousHomeAddressController.dataCacheConnector.fetch[ResponsiblePeople](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+
+      val result = previousHomeAddressController.get(RecordId)(request)
+      status(result) must be(OK)
+
+      val document = Jsoup.parse(contentAsString(result))
+      document.select("input[name=isUK][value=true]").hasAttr("checked") must be(false)
+      document.select("input[name=isUK][value=false]").hasAttr("checked") must be(false)
+      document.select("input[name=addressLine1]").`val` must be("")
+      document.select("input[name=addressLine2]").`val` must be("")
+      document.select("input[name=addressLine3]").`val` must be("")
+      document.select("input[name=addressLine4]").`val` must be("")
+      document.select("input[name=addressLineNonUK1]").`val` must be("")
+      document.select("input[name=addressLineNonUK2]").`val` must be("")
+      document.select("input[name=addressLineNonUK3]").`val` must be("")
+      document.select("input[name=addressLineNonUK4]").`val` must be("")
+      document.select("input[name=postcode]").`val` must be("")
+      document.select("input[name=country]").`val` must be("")
+      document.select("input[name=timeAtAddress][value=01]").hasAttr("checked") must be(false)
+      document.select("input[name=timeAtAddress][value=02]").hasAttr("checked") must be(false)
+      document.select("input[name=timeAtAddress][value=03]").hasAttr("checked") must be(false)
+      document.select("input[name=timeAtAddress][value=04]").hasAttr("checked") must be(false)
+    }
+
+    "on get() display the previous home address with UK fields populated" in new Fixture {
+
+      val prevAddress = PreviousHomeAddressUK("Line 1", "Line 2", Some("Line 3"), None, "NE17YH", ZeroToFiveMonths)
+      val responsiblePeople = ResponsiblePeople(previousHomeAddress = Some(prevAddress))
+
+      when(previousHomeAddressController.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(responsiblePeople))))
+
+      val result = previousHomeAddressController.get(RecordId)(request)
+      status(result) must be(OK)
+
+      val document = Jsoup.parse(contentAsString(result))
+      document.select("input[name=isUK][value=true]").hasAttr("checked") must be(true)
+      document.select("input[name=addressLine1]").`val` must be("Line 1")
+      document.select("input[name=addressLine2]").`val` must be("Line 2")
+      document.select("input[name=addressLine3]").`val` must be("Line 3")
+      document.select("input[name=addressLine4]").`val` must be("")
+      document.select("input[name=postcode]").`val` must be("NE17YH")
+      document.select("input[name=timeAtAddress][value=01]").hasAttr("checked") must be(true)
+    }
+
+    "on get() display the previous home address with non-UK fields populated" in new Fixture {
+
+      val prevAddress = PreviousHomeAddressNonUK("Line 1", "Line 2", None, None, Country("Spain", "ES"), SixToElevenMonths)
+      val responsiblePeople = ResponsiblePeople(previousHomeAddress = Some(prevAddress))
+
+      when(previousHomeAddressController.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Seq(responsiblePeople))))
+
+      val result = previousHomeAddressController.get(RecordId)(request)
+      status(result) must be(OK)
+
+      val document = Jsoup.parse(contentAsString(result))
+      document.select("input[name=isUK][value=false]").hasAttr("checked") must be(true)
+      document.select("input[name=addressLineNonUK1]").`val` must be("Line 1")
+      document.select("input[name=addressLineNonUK2]").`val` must be("Line 2")
+      document.select("input[name=addressLineNonUK3]").`val` must be("")
+      document.select("input[name=addressLineNonUK4]").`val` must be("")
+      document.select("select[name=country] > option[value=ES]").hasAttr("selected") must be(true)
+      document.select("input[name=timeAtAddress][value=02]").hasAttr("checked") must be(true)
+    }
+
+    "must pass on post with all the mandatory UK parameters supplied" in new Fixture {
+
+      val requestWithParams = request.withFormUrlEncodedBody(
+        "isUK" -> "true",
+        "addressLine1" -> "Line 1",
+        "addressLine2" -> "Line 2",
+        "postCode" -> "NE17YH",
+        "timeAtAddress" -> "01"
+      )
+
+      when(previousHomeAddressController.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+
+      when(previousHomeAddressController.dataCacheConnector.save[AddPerson](any(), any())
+        (any(), any(), any())).thenReturn(Future.successful(emptyCache))
+
+      val result = previousHomeAddressController.post(RecordId)(requestWithParams)
+      status(result) must be(SEE_OTHER)
+    }
+
+    "must pass on post with all the mandatory non-UK parameters supplied" in new Fixture {
+
+      val requestWithParams = request.withFormUrlEncodedBody(
+        "isUK" -> "false",
+        "addressLineNonUK1" -> "Line 1",
+        "addressLineNonUK2" -> "Line 2",
+        "country" -> "ES",
+        "timeAtAddress" -> "02"
+      )
+
+      when(previousHomeAddressController.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+
+      when(previousHomeAddressController.dataCacheConnector.save[AddPerson](any(), any())
+        (any(), any(), any())).thenReturn(Future.successful(emptyCache))
+
+      val result = previousHomeAddressController.post(RecordId)(requestWithParams)
+      status(result) must be(SEE_OTHER)
+    }
+
+    "must fail on post if isUK field not supplied" in new Fixture {
+
+      val line1MissingRequest = request.withFormUrlEncodedBody()
+
+      when(previousHomeAddressController.dataCacheConnector.save[AddPerson](any(), any())
+        (any(), any(), any())).thenReturn(Future.successful(emptyCache))
+
+      val result = previousHomeAddressController.post(RecordId)(line1MissingRequest)
+      status(result) must be(BAD_REQUEST)
+
+      val document: Document = Jsoup.parse(contentAsString(result))
+      document.select("a[href=#isUK]").html() must include(Messages("error.required"))
+    }
+
+    "must fail on post if default fields for UK not supplied" in new Fixture {
+
+      val requestWithMissingParams = request.withFormUrlEncodedBody(
+        "isUK" -> "true",
+        "addressLine1" -> "",
+        "addressLine2" -> "",
+        "postCode" -> "",
+        "timeAtAddress" -> ""
+      )
+
+      when(previousHomeAddressController.dataCacheConnector.save[AddPerson](any(), any())
+        (any(), any(), any())).thenReturn(Future.successful(emptyCache))
+
+      val result = previousHomeAddressController.post(RecordId)(requestWithMissingParams)
+      status(result) must be(BAD_REQUEST)
+
+      val document: Document = Jsoup.parse(contentAsString(result))
+      document.select("a[href=#addressLine1]").html() must include(Messages("error.required.address.line1"))
+      document.select("a[href=#addressLine2]").html() must include(Messages("error.required.address.line2"))
+      document.select("a[href=#postcode]").html() must include(Messages("error.required.postcode"))
+      document.select("a[href=#timeAtAddress]").html() must include(Messages("error.required.timeAtAddress"))
+    }
+
+    "must fail on post if default fields for overseas not supplied" in new Fixture {
+
+      val requestWithMissingParams = request.withFormUrlEncodedBody(
+        "isUK" -> "false",
+        "addressLineNonUK1" -> "",
+        "addressLineNonUK2" -> "",
+        "country" -> "",
+        "timeAtAddress" -> ""
+      )
+
+      when(previousHomeAddressController.dataCacheConnector.save[AddPerson](any(), any())
+        (any(), any(), any())).thenReturn(Future.successful(emptyCache))
+
+      val result = previousHomeAddressController.post(RecordId)(requestWithMissingParams)
+      status(result) must be(BAD_REQUEST)
+
+      val document: Document = Jsoup.parse(contentAsString(result))
+      document.select("a[href=#addressLineNonUK1]").html() must include(Messages("error.required.address.line1"))
+      document.select("a[href=#addressLineNonUK2]").html() must include(Messages("error.required.address.line2"))
+      document.select("a[href=#country]").html() must include(Messages("error.required.country"))
+      document.select("a[href=#timeAtAddress]").html() must include(Messages("error.required.timeAtAddress"))
+    }
+
+
+
+  }
+
+}
