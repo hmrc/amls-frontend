1470b59d4c044655d9165505e626d92f68a0d48b
Vivek Kumar <vivek.kumar@digital.hmrc.gov.uk>
2016-04-14 16:33:13 +0100

    AMLS-353: Added the customised error messages

--- app/models/responsiblepeople/Training.scala
+++ app/models/responsiblepeople/Training.scala
@@ -2,16 +2,14 @@ package models.responsiblepeople
 
 import play.api.data.mapping._
 import play.api.data.mapping.forms.Rules._
-import play.api.data.mapping.forms._
 import play.api.libs.json.{Writes => _}
 import utils.MappingUtils.Implicits._
 import play.api.data.mapping.forms.UrlFormEncoded
-import play.api.libs.json._
 
 
 sealed trait Training
 
-case class HadTrainingYes(datesandduration: String) extends Training
+case class HadTrainingYes(information: String) extends Training
 
 case object HadTrainingNo extends Training
 
@@ -19,17 +17,17 @@ object Training {
 
   import play.api.libs.json._
 
-  val maxNameTypeLength = 35
+  val maxInformationTypeLength = 255
 
-  val otherFirstNameType = notEmpty.withMessage("error.required.datesandduration") compose
-    maxLength(maxNameTypeLength).withMessage("error.invalid.length.firstname")
+  val informationType = notEmpty.withMessage("error.required.rp.training.information") compose
+    maxLength(maxInformationTypeLength).withMessage("error.invalid.length.rp.training.information")
 
 
   implicit val formRule: Rule[UrlFormEncoded, Training] = From[UrlFormEncoded] { __ =>
     import play.api.data.mapping.forms.Rules._
-    (__ \ "hadTraining").read[Boolean].withMessage("error.required.sa.registration") flatMap {
+    (__ \ "hadTraining").read[Boolean].withMessage("error.required.rp.training") flatMap {
       case true =>
-        (__ \ "datesandduration").read(otherFirstNameType) fmap (HadTrainingYes.apply)
+        (__ \ "information").read(informationType) fmap (HadTrainingYes.apply)
       case false => Rule.fromMapping { _ => Success(HadTrainingNo) }
     }
   }
@@ -37,21 +35,21 @@ object Training {
   implicit val formWrites: Write[Training, UrlFormEncoded] = Write {
     case a: HadTrainingYes => Map(
       "hadTraining" -> Seq("true"),
-      "datesandduration" -> Seq(a.datesandduration)
+      "information" -> Seq(a.information)
     )
     case HadTrainingNo => Map("hadTraining" -> Seq("false"))
   }
 
   implicit val jsonReads: Reads[Training] =
     (__ \ "hadTraining").read[Boolean] flatMap {
-      case true => (__ \ "datesandduration").read[String] map (HadTrainingYes.apply _)
+      case true => (__ \ "information").read[String] map (HadTrainingYes.apply _)
       case false => Reads(_ => JsSuccess(HadTrainingNo))
     }
 
   implicit val jsonWrites = Writes[Training] {
-    case HadTrainingYes(datesandduration) => Json.obj(
+    case HadTrainingYes(information) => Json.obj(
       "hadTraining" -> true,
-      "datesandduration" -> datesandduration
+      "information" -> information
     )
     case HadTrainingNo => Json.obj("hadTraining" -> false)
   }
