a64536ce49f1526d0641d9d58706039462500c57
Ryan Burke <ryan.burke@digital.hmrc.gov.uk>
2016-04-26 15:54:49 +0100

    better name code

--- app/models/responsiblepeople/PreviousName.scala
+++ app/models/responsiblepeople/PreviousName.scala
@@ -2,7 +2,6 @@ package models.responsiblepeople
 
 import org.joda.time.LocalDate
 import play.api.data.mapping._
-import play.api.data.mapping.forms.Rules._
 import play.api.data.mapping.forms.UrlFormEncoded
 import play.api.data.validation.ValidationError
 import play.api.libs.json.Json
@@ -17,16 +16,13 @@ case class PreviousName(
 
   val formattedDate = DateHelper.formatDate(date)
 
-  private def nameFormatter(f: String, m: String, l: String) = Seq(f, m, l).mkString(" ")
-
-  def formattedPreviousName(that: PersonName) = this match {
-    case PreviousName(Some(f), None, None, _) => nameFormatter(f, that.middleName.getOrElse(""), that.lastName)
-    case PreviousName(None, Some(m), None, _) => nameFormatter(that.firstName, m, that.lastName)
-    case PreviousName(None, None, Some(l), _) => nameFormatter(that.firstName, that.middleName.getOrElse(""), l)
-    case PreviousName(None, Some(m), Some(l), _) => nameFormatter(that.firstName, m, l)
-    case PreviousName(Some(f), None, Some(l), _) => nameFormatter(f, that.middleName.getOrElse(""), l)
-    case PreviousName(Some(f), Some(m), None, _) => nameFormatter(f, m, that.lastName)
-    case PreviousName(Some(f), Some(m), Some(l), _) => nameFormatter(f, m, l)
+  def formattedPreviousName(that: PersonName): String = that match {
+      case PersonName(first, middle, last, _, _) =>
+        Seq(
+          firstName orElse Some(first),
+          middleName orElse middle,
+          lastName orElse Some(last)
+        ).flatten[String].mkString(" ")
   }
 
 }
