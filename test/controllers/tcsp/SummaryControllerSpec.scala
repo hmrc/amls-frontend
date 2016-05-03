a0ab0360d0663883e57b563db8bb80820d77bd07
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-05-03 16:58:27 +0100

    AMLS-206:add vie , controller and test

---
+++ test/controllers/tcsp/SummaryControllerSpec.scala
@@ -0,0 +1,47 @@
+package controllers.tcsp
+
+import connectors.DataCacheConnector
+import models.tcsp.Tcsp
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
+import play.api.test.Helpers._
+import utils.AuthorisedFixture
+
+import scala.concurrent.Future
+
+class SummaryControllerSpec extends PlaySpec with OneServerPerSuite with MockitoSugar {
+
+  trait Fixture extends AuthorisedFixture {
+    self =>
+
+    val controller = new SummaryController {
+      override val dataCache = mock[DataCacheConnector]
+      override val authConnector = self.authConnector
+    }
+  }
+
+  "Get" must {
+
+    "load the summary page when section data is available" in new Fixture {
+
+      val model = Tcsp(None)
+
+      when(controller.dataCache.fetch[Tcsp](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(model)))
+
+      val result = controller.get()(request)
+      status(result) must be(OK)
+    }
+
+    "redirect to the main summary page when section data is unavailable" in new Fixture {
+
+      when(controller.dataCache.fetch[Tcsp](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+
+      val result = controller.get()(request)
+      status(result) must be(SEE_OTHER)
+    }
+  }
+}
