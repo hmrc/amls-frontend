ee46e4ad2fa04c99bfe419f58b5d5d8ace467447
Joe Simmonds <joe.simmonds@digital.hmrc.gov.uk>
2016-05-20 15:17:11 +0100

    Base models and tests for djson

--- app/models/moneyservicebusiness/MsbServices.scala
+++ app/models/moneyservicebusiness/MsbServices.scala
@@ -1,10 +1,48 @@
 package models.moneyservicebusiness
 
-case class MsbServices(services : Seq[MsbService])
+import play.api.data.validation.ValidationError
+import play.api.libs.json.{Reads, Writes, Json, Format}
 
 sealed trait MsbService
+
 
 case object TransmittingMoney extends MsbService
 case object CurrencyExchange extends MsbService
 case object ChequeCashingNotScrapMetal extends MsbService
 case object ChequeCashingScrapMetal extends MsbService
+
+case class MsbServices(services : Set[MsbService])
+
+object MsbServices {
+  implicit val jsonReads : Reads[MsbServices] = {
+    import play.api.libs.json._
+
+    (__ \ "msbServices").read[Set[String]].flatMap[Set[MsbService]] { strs : Set[String] =>
+      strs.map {
+        case "01" => Reads(_ => JsSuccess(TransmittingMoney)) map identity[MsbService]
+        case "02" => Reads(_ => JsSuccess(CurrencyExchange)) map identity[MsbService]
+        case "03" => Reads(_ => JsSuccess(ChequeCashingNotScrapMetal)) map identity[MsbService]
+        case "04" => Reads(_ => JsSuccess(ChequeCashingScrapMetal)) map identity[MsbService]
+        case _ => Reads(_ => JsError(__ \ "msbServices", ValidationError("error.invalid")))
+      }.foldLeft[Reads[Set[MsbService]]](Reads[Set[MsbService]](_ => JsSuccess(Set.empty[MsbService]))
+      ){
+        (result, next) =>
+          next flatMap { service:MsbService =>
+            result.map { services =>
+              services + service
+            }
+          }
+      }
+    } map MsbServices.apply
+  }
+
+  implicit val jsonWrites : Writes[MsbServices] = Writes { msbServices: MsbServices =>
+    Json.obj("msbServices" -> msbServices.services.map {
+      case TransmittingMoney => "01"
+      case CurrencyExchange => "02"
+      case ChequeCashingNotScrapMetal => "03"
+      case ChequeCashingScrapMetal => "04"
+    })
+  }
+}
+
--- test/models/moneyservicebusiness/MoneyServiceBusinessSpec.scala
+++ test/models/moneyservicebusiness/MoneyServiceBusinessSpec.scala
@@ -1,47 +1,84 @@
 package models.moneyservicebusiness
 
-import org.scalatest.Pending
+import models.registrationprogress.{Completed, NotStarted, Section}
+import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json
 import typeclasses.MongoKey
+import uk.gov.hmrc.http.cache.client.CacheMap
+import org.mockito.Mockito._
+import play.api.mvc.Call
 
-class MoneyServiceBusinessSpec extends PlaySpec {
+class MoneyServiceBusinessSpec extends PlaySpec with MockitoSugar with MoneyServiceBusinessTestData{
   "MoneyServiceBusiness" should {
     "have an implicit conversion from Option which" when {
       "called with None" should {
         "return a default version of MoneyServiceBusiness" in {
           val res:MoneyServiceBusiness = None
-          res must be (MoneyServiceBusiness())
+          res must be (emptyModel)
         }
       }
 
       "called with a concrete value" should {
         "return the value passed in extracted from the option" in {
-          val res:MoneyServiceBusiness = Some(MoneyServiceBusiness(Some(MsbServices(Seq(ChequeCashingScrapMetal, ChequeCashingNotScrapMetal)))))
-          res must be (MoneyServiceBusiness(Some(MsbServices(Seq(ChequeCashingScrapMetal, ChequeCashingNotScrapMetal)))))
+          val res:MoneyServiceBusiness = Some(completeModel)
+          res must be (completeModel)
         }
       }
     }
 
     "Provide an implicit mongo-key" in {
-      def x(implicit mongoKey: MongoKey[MoneyServiceBusiness]) = {
-        mongoKey()
-      }
+      def x(implicit mongoKey: MongoKey[MoneyServiceBusiness]) = mongoKey()
 
-      val res = x
-      res must be("money-service-business")
+      x must be("money-service-business")
     }
 
-    "have a section function that" must {
-      "return a NotStarted Section when model is empty" in Pending
-      "return a Completed Section when model is complete" in Pending
-      "return a Started Section when model is incomplete" in Pending
+    "have a section function that" when {
+      implicit val cacheMap = mock[CacheMap]
+
+      "model is empty" should {
+        "return a NotStarted Section" in {
+          when(cacheMap.getEntry[MoneyServiceBusiness]("money-service-business")) thenReturn None
+          MoneyServiceBusiness.section must be (Section("money_service_business", NotStarted, controllers.msb.routes.WhatYouNeedController.get()))
+        }
+      }
+
+      "model is complete" should {
+        "return a Completed Section" in {
+          when(cacheMap.getEntry[MoneyServiceBusiness]("money-service-business")) thenReturn Some(completeModel)
+          MoneyServiceBusiness.section must be (Section("money_service_business", Completed, controllers.msb.routes.WhatYouNeedController.get()))
+        }
+      }
     }
 
     "have an isComplete function that" must {
 
-      "correctly show if the model is complete" in Pending
+      "correctly show if the model is complete" in  {
+        completeModel.isComplete must be (true)
+      }
 
-      "correctly show if the model is incomplete" in Pending
+      "correctly show if the model is incomplete" in {
+        emptyModel.isComplete must be (false)
+      }
+    }
+
+    "Serialise to expected Json" when {
+      "model is complete" in {Json.toJson(completeModel) must be (completeJson)}
+    }
+
+    "Deserialise from Json as expected" when {
+      "model is complete" in {completeJson.as[MoneyServiceBusiness] must be (completeModel)}
     }
   }
+}
+
+trait MoneyServiceBusinessTestData {
+  val completeModel = MoneyServiceBusiness(Some(MsbServices(Set(ChequeCashingScrapMetal, ChequeCashingNotScrapMetal))))
+  val emptyModel = MoneyServiceBusiness(None)
+
+  val completeJson = Json.obj(
+    "msbServices" -> Json.arr("04", "03")
+  )
+
+  val emptyJson = Json.obj("msbServices" -> Json.arr())
 }
