44db8a97b29cb8382778a5da1f223f618c4f2c02
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-06-23 12:10:48 +0100

    AMLS-1308:add model

--- app/models/hvd/CashPayment.scala
+++ app/models/hvd/CashPayment.scala
@@ -2,7 +2,7 @@ package models.hvd
 
 import models.FormTypes._
 import org.joda.time.LocalDate
-import play.api.data.mapping.{Success, From, Rule}
+import play.api.data.mapping.{Write, Success, From, Rule}
 import play.api.data.mapping.forms._
 
 sealed trait CashPayment
@@ -15,11 +15,18 @@ object CashPayment {
 
   implicit val formRule: Rule[UrlFormEncoded, CashPayment] = From[UrlFormEncoded] { __ =>
     import play.api.data.mapping.forms.Rules._
-    (__ \ "registeredForVAT").read[Boolean].withMessage("error.required.atb.registered.for.vat") flatMap {
+    (__ \ "registeredForVAT").read[Boolean].withMessage("error.required.hvd.accepted.cash.payment") flatMap {
       case true =>
         (__ \ "paymentDate").read(localDateRule) fmap CashPaymentYes.apply
       case false => Rule.fromMapping { _ => Success(CashPaymentNo) }
     }
   }
 
+  implicit val formWrites: Write[CashPayment, UrlFormEncoded] = Write {
+    case CashPaymentYes(value) =>
+      Map("registeredForVAT" -> Seq("true"),
+        "vrnNumber" -> Seq(value)
+      )
+    case CashPaymentNo => Map("registeredForVAT" -> Seq("false"))
+  }
 }
