a891d1a51c6c520f11b97a9558e646305da3fab7
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-07-27 15:11:39 +0100

    AMLS-929:update customeroutside uk model and ui to incorporate the changes

--- test/models/businessactivities/CustomersOutsideUKSpec.scala
+++ test/models/businessactivities/CustomersOutsideUKSpec.scala
@@ -1,149 +1,116 @@
 package models.businessactivities
 
 import models.Country
-import models.FormTypes
 import org.scalatestplus.play.PlaySpec
-import play.api.data.mapping.{Failure, Path, Success}
+import play.api.data.mapping.forms._
+import play.api.data.mapping._
 import play.api.data.validation.ValidationError
-import play.api.libs.json.{JsNull, JsPath, JsSuccess, Json}
+import play.api.libs.json.{JsSuccess, Json}
 
 class CustomersOutsideUKSpec extends PlaySpec {
 
 
   "CustomersOutsideUK" must {
+    val rule = implicitly[Rule[UrlFormEncoded, CustomersOutsideUK]]
+    val write = implicitly[Write[CustomersOutsideUK, UrlFormEncoded]]
 
-    "validate toLines for UK address" in {
-      CustomersOutsideUKYes(Countries(Country("United Kingdom", "GB"))).toLines must be (Seq("United Kingdom"))
+    "round trip through Json correctly" in {
+
+      val model: CustomersOutsideUK = CustomersOutsideUK(Some(Seq(Country("United Kingdom", "GB"))))
+
+      Json.fromJson[CustomersOutsideUK](Json.toJson(model)) mustBe JsSuccess(model)
     }
 
-    "validate toLines for Non UK address" in {
-      CustomersOutsideUKNo.toLines mustBe Seq.empty
+    "round trip through forms correctly" in {
+
+      val model: CustomersOutsideUK = CustomersOutsideUK(Some(Seq(Country("United Kingdom", "GB"))))
+
+      rule.validate(write.writes(model)) mustBe Success(model)
     }
 
-    "successfully validate the form Rule with option No" in {
-      CustomersOutsideUK.formRule.validate(Map("isOutside" -> Seq("false"))) must
-        be(Success(CustomersOutsideUKNo))
+    "successfully validate when isOutside is false" in {
+
+      val form: UrlFormEncoded = Map(
+        "isOutside" -> Seq("false")
+      )
+
+      val model: CustomersOutsideUK = CustomersOutsideUK(None)
+
+      rule.validate(form) mustBe Success(model)
     }
 
-    "successfully validate the form Rule with option Yes" in {
-      CustomersOutsideUK.formRule.validate(
-        Map(
-          "isOutside" -> Seq("true"),
-          "country_1" -> Seq("AL"),
-          "country_2" -> Seq("DZ"),
-          "country_3" -> Seq("AS"),
-          "country_4" -> Seq("AD"),
-          "country_5" -> Seq("AO"),
-          "country_6" -> Seq("AI"),
-          "country_7" -> Seq("AQ"),
-          "country_8" -> Seq("AG"),
-          "country_9" -> Seq("AR"),
-          "country_10" -> Seq("AM")
-        )
-      ) mustBe {
-        Success(CustomersOutsideUKYes(
-          Countries(
-            Country("Albania", "AL"),
-            Some(Country("Algeria", "DZ")),
-            Some(Country("American Samoa", "AS")),
-            Some(Country("Andorra", "AD")),
-            Some(Country("Angola", "AO")),
-            Some(Country("Anguilla", "AI")),
-            Some(Country("Antarctica", "AQ")),
-            Some(Country("Antigua and Barbuda", "AG")),
-            Some(Country("Argentina", "AR")),
-            Some(Country("Armenia", "AM"))
-          )
-        ))
-      }
-    }
+    "successfully validate when isOutside is true and there is at least 1 country selected" in {
 
-    "validate mandatory field when isOutside is not selected" in {
-      CustomersOutsideUK.formRule.validate(Map.empty) must
-        be(Failure(Seq(
-          (Path \ "isOutside") -> Seq(ValidationError("error.required.ba.select.country"))
-        )))
-    }
-
-    "validate mandatory field when isOutside is  selected as Yes and country with invalid data" in {
-      val json = Map("isOutside" -> Seq("true"),
-      "country_1" -> Seq("ABC"))
-
-      CustomersOutsideUK.formRule.validate(json) must
-        be(Failure(Seq(
-          (Path \ "country_1") -> Seq(ValidationError("error.required.country.name"))
-        )))
-    }
-
-    "validate mandatory field for min length when isOutside is  selected as Yes and country with invalid data" in {
-      val json = Map("isOutside" -> Seq("true"),
-        "country_1" -> Seq("A"))
-
-      CustomersOutsideUK.formRule.validate(json) must
-        be(Failure(Seq(
-          (Path \ "country_1") -> Seq(ValidationError("error.required.country.name"))
-        )))
-    }
-
-    "validate mandatory country field" in {
-      CustomersOutsideUK.formRule.validate(Map("isOutside" -> Seq("true"),"country_1" -> Seq(""))) must
-        be(Failure(Seq(
-          (Path \ "country_1") -> Seq(ValidationError("error.required.country.name"))
-        )))
-    }
-
-    "successfully write model with formWrite" in {
-
-      val model = CustomersOutsideUKYes(Countries(Country("United Kingdom", "GB"), Some(Country("United Kingdom", "GB"))))
-      CustomersOutsideUK.formWrites.writes(model) must
-        contain allOf (
+      val form: UrlFormEncoded = Map(
         "isOutside" -> Seq("true"),
-        "country_1" -> Seq("GB"),
-        "country_2" -> Seq("GB")
+        "countries" -> Seq("GB")
+      )
+
+      val model: CustomersOutsideUK =
+        CustomersOutsideUK(
+          Some(Seq(Country("United Kingdom", "GB")))
         )
+
+      rule.validate(form) mustBe Success(model)
     }
 
+    "fail to validate when isOutside is true and there are no countries selected" in {
 
-    "successfully write model with formWrite and option No" in {
+      val form: UrlFormEncoded = Map(
+        "isOutside" -> Seq("true"),
+        "countries" -> Seq.empty
+      )
 
-      val model = CustomersOutsideUKNo
-      CustomersOutsideUK.formWrites.writes(model) mustBe Map("isOutside" -> Seq("false"))
+      rule.validate(form) mustBe Failure(
+        Seq((Path \ "countries") -> Seq(ValidationError("error.invalid.ba.select.country")))
+      )
     }
 
-    "JSON validation" must {
-      "successfully validate givcen values" in {
-        val json =  Json.obj("isOutside" -> true,
-          "country_1" -> "GB")
+    "fail to validate when isOutside is true and there are more than 10 countries" in {
 
-        Json.fromJson[CustomersOutsideUK](json) must
-          be(JsSuccess(CustomersOutsideUKYes(Countries(Country("United Kingdom", "GB"))), JsPath \ "isOutside"))
-      }
+      val form: UrlFormEncoded = Map(
+        "isOutside" -> Seq("true"),
+        "countries[]" -> Seq.fill(11)("GB")
+      )
 
-      "successfully validate given values with option No" in {
-        val json =  Json.obj("isOutside" -> false)
+      rule.validate(form) mustBe Failure(
+        Seq((Path \ "countries") -> Seq(ValidationError("error.maxLength", 10)))
+      )
+    }
 
-        Json.fromJson[CustomersOutsideUK](json) must
-          be(JsSuccess(CustomersOutsideUKNo, JsPath \ "isOutside"))
-      }
+    "fail to validate when isOutside isn't selected" in {
 
-      "write valid data using json write" in {
-        Json.toJson[CustomersOutsideUK](CustomersOutsideUKYes(Countries(Country("United Kingdom", "GB")))) must be (Json.obj("isOutside" -> true,
-          "country_1" -> "GB",
-          "country_2" -> JsNull,
-          "country_3" -> JsNull,
-          "country_4" -> JsNull,
-          "country_5" -> JsNull,
-          "country_6" -> JsNull,
-          "country_7" -> JsNull,
-          "country_8" -> JsNull,
-          "country_9" -> JsNull,
-          "country_10" ->JsNull
-        ))
-      }
+      val form: UrlFormEncoded = Map.empty
 
-      "write valid data using json write with option No" in {
-        Json.toJson[CustomersOutsideUK](CustomersOutsideUKNo) must be (Json.obj("isOutside" -> false))
-      }
+      rule.validate(form) mustBe Failure(
+        Seq((Path \ "isOutside") -> Seq(ValidationError("error.required.ba.select.country")))
+      )
+    }
+
+    "successfully validate when there are empty values in the seq" in {
+
+      val form: UrlFormEncoded = Map(
+        "isOutside" -> Seq("true"),
+        "countries[]" -> Seq("GB", "", "US", "")
+      )
+
+      rule.validate(form) mustBe Success(CustomersOutsideUK(Some(Seq(
+        Country("United Kingdom", "GB"),
+        Country("United States", "US")
+      ))))
+    }
+
+    "test" in {
+
+      val form: UrlFormEncoded = Map(
+        "isOutside" -> Seq("true"),
+        "countries[0]" -> Seq("GB"),
+        "countries[1]" -> Seq("")
+      )
+
+      rule.validate(form) mustBe Success(CustomersOutsideUK(Some(Seq(
+        Country("United Kingdom", "GB")
+      ))))
     }
   }
 }
