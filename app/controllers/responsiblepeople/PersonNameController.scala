44c2b0a4dcb1b3dc4c98fe35e9ae555914849318
Joe Simmonds <joe.simmonds@digital.hmrc.gov.uk>
2016-08-01 12:28:34 +0100

    AMLS-996 tightened up the residencetype controller and added a default not found view to the base controller

--- app/controllers/responsiblepeople/PersonNameController.scala
+++ app/controllers/responsiblepeople/PersonNameController.scala
@@ -5,6 +5,8 @@ import connectors.DataCacheConnector
 import controllers.BaseController
 import forms._
 import models.responsiblepeople.{PersonName, ResponsiblePeople}
+import play.api.i18n.Messages
+import play.api.mvc.Request
 import utils.RepeatingSection
 import views.html.responsiblepeople.person_name
 
@@ -13,6 +15,7 @@ import scala.concurrent.Future
 trait PersonNameController extends RepeatingSection with BaseController {
 
   val dataCacheConnector: DataCacheConnector
+
 
   def get(index: Int, edit: Boolean = false) =
     ResponsiblePeopleToggle {
@@ -24,7 +27,7 @@ trait PersonNameController extends RepeatingSection with BaseController {
             case Some(ResponsiblePeople(_, _, _, _, _, _, _, _, _, _))
                 => Ok(person_name(EmptyForm, edit, index))
             case _
-                => NotFound
+                => NotFound(notFoundView)
           }
       }
     }
@@ -46,7 +49,7 @@ trait PersonNameController extends RepeatingSection with BaseController {
                 case false => Redirect(routes.PersonResidentTypeController.get(index, edit))
               }
             }.recoverWith {
-              case _: IndexOutOfBoundsException => Future.successful(NotFound)
+              case _: IndexOutOfBoundsException => Future.successful(NotFound(notFoundView))
             }
           }
         }
