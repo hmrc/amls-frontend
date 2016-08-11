dda36e89b411c680fbd444af1d490dc31e4f6ebe
lorenzo.mugnai <lorenzo.mugnai@hmrc.gsi.gov.uk>
2016-08-11 16:04:07 +0100

    AMLS-1243

--- test/controllers/StatusControllerSpec.scala
+++ test/controllers/StatusControllerSpec.scala
@@ -17,6 +17,7 @@ import play.api.mvc.Call
 import play.api.test.Helpers._
 import services.{AuthEnrolmentsService, LandingService, ProgressService}
 import uk.gov.hmrc.http.cache.client.CacheMap
+import uk.gov.hmrc.play.frontend.auth.AuthContext
 import utils.AuthorisedFixture
 
 import scala.concurrent.Future
@@ -93,7 +94,7 @@ class StatusControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSuga
 
     }
 
-    "show correct status classes  " when {
+    //"show correct status classes  " when {
 
       "submission incomplete" in new Fixture {
 
@@ -165,7 +166,10 @@ class StatusControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSuga
         when(cacheMap.getEntry[BusinessMatching](Matchers.contains(BusinessMatching.key))(any())).thenReturn(
           Some(BusinessMatching(Some(reviewDtls), None)))
 
-        val readStatusResponse = ReadStatusResponse(LocalDateTime.now(), "Approved", None, None, None, false)
+        when(cacheMap.getEntry[SubscriptionResponse](Matchers.contains(SubscriptionResponse.key))(any())).thenReturn(
+          Some(SubscriptionResponse("","",0,None,0,0,"")))
+
+        val readStatusResponse = ReadStatusResponse(LocalDateTime.now(), "None", None, None, None, false)
 
         when(controller.progressService.sections(any(), any(), any())).thenReturn(Future.successful(Seq(Section("test", Completed, Call("", "")))))
         when(controller.desConnector.status(any())(any(),any(),any(),any())).thenReturn(Future.successful(readStatusResponse))
@@ -175,17 +179,57 @@ class StatusControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSuga
 
         val document = Jsoup.parse(contentAsString(result))
         document.getElementsByClass("status-list").first().child(0).hasClass("status-list--complete") must be(true)
-        document.getElementsByClass("status-list").first().child(1).hasClass("current") must be(true)
+        document.getElementsByClass("status-list").first().child(1).hasClass("status-list--complete") must be(true)
+        document.getElementsByClass("status-list").first().child(2).hasClass("current") must be(true)
 
-        for (index <- 2 to 4) {
+        for (index <- 3 to 4) {
           document.getElementsByClass("status-list").first().child(index).hasClass("status-list--upcoming") must be(true)
         }
 
-        document.getElementsByClass("status-detail").first().child(0).html() must be(Messages("status.submissionready.heading"))
-        document.getElementsByClass("status-detail").first().child(1).html() must be(Messages("status.submissionready.description"))
+        document.getElementsByClass("status-detail").first().child(0).html() must be(Messages("status.submissionfeesdue.heading"))
+        document.getElementsByClass("status-detail").first().child(1).html() must be(Messages("status.submissionfeesdue.description"))
 
       }
-    }
+
+      "under review" in new Fixture {
+
+        val reviewDtls = ReviewDetails("BusinessName", Some(BusinessType.LimitedCompany),
+          Address("line1", "line2", Some("line3"), Some("line4"), Some("NE77 0QQ"), Country("United Kingdom", "GB")), "XE0001234567890")
+
+        val cacheMap = mock[CacheMap]
+        when(controller.landingService.cacheMap(any(), any(), any())) thenReturn Future.successful(Some(cacheMap))
+
+        when(cacheMap.getEntry[BusinessMatching](Matchers.contains(BusinessMatching.key))(any())).thenReturn(
+          Some(BusinessMatching(Some(reviewDtls), None)))
+
+        when(cacheMap.getEntry[SubscriptionResponse](Matchers.contains(SubscriptionResponse.key))(any())).thenReturn(
+          Some(SubscriptionResponse("","",0,None,0,0,"")))
+
+        val readStatusResponse = ReadStatusResponse(LocalDateTime.now(), "Pending", None, None, None, false)
+
+        when(controller.progressService.sections(any(), any(), any())).thenReturn(Future.successful(Seq(Section("test", Completed, Call("", "")))))
+        when(controller.desConnector.status(any())(any(),any(),any(),any())).thenReturn(Future.successful(readStatusResponse))
+
+        val result = controller.get()(request)
+        status(result) must be(OK)
+
+        val document = Jsoup.parse(contentAsString(result))
+
+        for (index <- 0 to 2) {
+          document.getElementsByClass("status-list").first().child(index).hasClass("status-list--complete") must be(true)
+        }
+
+        document.getElementsByClass("status-list").first().child(3).hasClass("current") must be(true)
+
+        document.getElementsByClass("status-list").first().child(4).hasClass("status-list--upcoming") must be(true)
+
+
+        document.getElementsByClass("status-detail").first().child(0).html() must be(Messages("status.submissionreadyforreview.heading"))
+        document.getElementsByClass("status-detail").first().child(1).html() must be(Messages("status.submissionreadyforreview.description"))
+        document.getElementsByClass("status-detail").first().child(2).html() must be(Messages("status.submissionreadyforreview.description2"))
+
+      }
+    //}
   }
 
 
