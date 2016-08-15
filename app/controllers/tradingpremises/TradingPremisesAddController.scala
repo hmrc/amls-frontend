55f752aa0561379052016e6a66608a5972313b43
Mahipal Padala <mahipal.padala@digital.hmrc.gov.uk>
2016-08-15 13:30:24 +0100

    AMLS-838:add controller

--- app/controllers/tradingpremises/TradingPremisesAddController.scala
+++ app/controllers/tradingpremises/TradingPremisesAddController.scala
@@ -8,12 +8,12 @@ import models.tradingpremises.TradingPremises
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import utils.RepeatingSection
 
+import scala.concurrent.Future
 
-
-trait TradingPremisesAddController  extends BaseController with RepeatingSection {
+trait TradingPremisesAddController extends BaseController with RepeatingSection {
   def isMSBSelected(bm: Option[BusinessMatching]): Boolean = {
     bm match {
-      case Some(matching) => matching.activities.foldLeft(false){(x, y) =>
+      case Some(matching) => matching.activities.foldLeft(false) { (x, y) =>
         y.businessActivities.contains(MoneyServiceBusiness)
 
       }
@@ -21,29 +21,26 @@ trait TradingPremisesAddController  extends BaseController with RepeatingSection
     }
   }
 
-  def get(displayGuidance : Boolean = true) = Authorised.async {
+  def get(displayGuidance: Boolean = true) = Authorised.async {
     implicit authContext => implicit request =>
-          addData[TradingPremises](TradingPremises.default(None)).map { idx =>
-            Redirect {
-              displayGuidance match {
-                case true => controllers.tradingpremises.routes.WhatYouNeedController.get(idx)
-                case false => {
-                 // val bm = dataCacheConnector.fetch[BusinessMatching](BusinessMatching.key).value.get.get
-                  //  println("------------------"+ bm)
-
-                    controllers.tradingpremises.routes.RegisteringAgentPremisesController.get(idx)
-                  }
-                //  else
-                   // controllers.tradingpremises.routes.WhereAreTradingPremisesController.get(idx)
-                }
-
+      addData[TradingPremises](TradingPremises.default(None)).map { idx =>
+          displayGuidance match {
+            case true => Redirect(controllers.tradingpremises.routes.WhatYouNeedController.get(idx))
+            case false =>
+            {
+              val data = dataCacheConnector.fetch[BusinessMatching](BusinessMatching.key) map {
+                response =>
+                  isMSBSelected(response)
               }
+              data.flatMap{
+                  case true => Redirect(controllers.tradingpremises.routes.WhereAreTradingPremisesController.get(idx))
+                  case false => Redirect(controllers.tradingpremises.routes.WhereAreTradingPremisesController.get(idx))
+                }
+              //Redirect(controllers.tradingpremises.routes.WhereAreTradingPremisesController.get(idx))
             }
-          }
-
-
-
-
+        }
+      }
+  }
 }
 
 //if (isMSBSelected(Option(BusinessMatching.apply())))
@@ -52,5 +49,6 @@ trait TradingPremisesAddController  extends BaseController with RepeatingSection
 object TradingPremisesAddController extends TradingPremisesAddController {
   // $COVERAGE-OFF$
   override def dataCacheConnector: DataCacheConnector = DataCacheConnector
+
   override protected def authConnector: AuthConnector = AMLSAuthConnector
 }
