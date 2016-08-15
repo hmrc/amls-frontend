d6a5ed4ab20e02fc6104d5595aabac551653f635
Mahipal Padala <mahipal.padala@digital.hmrc.gov.uk>
2016-08-15 14:54:50 +0100

    AMLS-838: Updating the controller

--- app/controllers/tradingpremises/TradingPremisesAddController.scala
+++ app/controllers/tradingpremises/TradingPremisesAddController.scala
@@ -8,8 +8,6 @@ import models.tradingpremises.TradingPremises
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import utils.RepeatingSection
 
-import scala.concurrent.Future
-
 trait TradingPremisesAddController extends BaseController with RepeatingSection {
   def isMSBSelected(bm: Option[BusinessMatching]): Boolean = {
     bm match {
@@ -28,15 +26,15 @@ trait TradingPremisesAddController extends BaseController with RepeatingSection 
             case true => Redirect(controllers.tradingpremises.routes.WhatYouNeedController.get(idx))
             case false =>
             {
-              val data = dataCacheConnector.fetch[BusinessMatching](BusinessMatching.key) map {
+             /* val data = dataCacheConnector.fetch[BusinessMatching](BusinessMatching.key) map {
                 response =>
                   isMSBSelected(response)
               }
               data.flatMap{
                   case true => Redirect(controllers.tradingpremises.routes.WhereAreTradingPremisesController.get(idx))
                   case false => Redirect(controllers.tradingpremises.routes.WhereAreTradingPremisesController.get(idx))
-                }
-              //Redirect(controllers.tradingpremises.routes.WhereAreTradingPremisesController.get(idx))
+                }*/
+              Redirect(controllers.tradingpremises.routes.RegisteringAgentPremisesController.get(idx))
             }
         }
       }
