c887acae547db6a8502e2b76921e7277ba3bfaf5
Joe Simmonds <joe.simmonds@digital.hmrc.gov.uk>
2016-09-07 18:12:17 +0100

    AMLS-1685 tests implemented for Landing controller with amendments on

--- app/controllers/LandingController.scala
+++ app/controllers/LandingController.scala
@@ -3,8 +3,10 @@ package controllers
 import config.{AMLSAuthConnector, ApplicationConfig}
 import models.SubscriptionResponse
 import play.api.Logger
-import play.api.mvc.Call
-import services.LandingService
+import play.api.mvc.{Request, Call}
+import services.{AuthEnrolmentsService, LandingService}
+import uk.gov.hmrc.http.cache.client.CacheMap
+import uk.gov.hmrc.play.frontend.auth.AuthContext
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 
 import scala.concurrent.Future
@@ -12,29 +14,83 @@ import scala.concurrent.Future
 trait LandingController extends BaseController {
 
   private[controllers] def landingService: LandingService
+  private[controllers] def enrolmentsService: AuthEnrolmentsService
 
   // TODO: GG Enrolment routing
   def get() = Authorised.async {
     implicit authContext => implicit request =>
-      landingService.cacheMap flatMap {
-        case Some(cache) =>
-          ApplicationConfig.statusToggle match {
-            case true =>
-              Future.successful(Redirect(controllers.routes.StatusController.get()))
-            case _ =>
-              Future.successful(Redirect(controllers.routes.RegistrationProgressController.get()))
-          }
-        case None =>
-          landingService.reviewDetails flatMap {
-            case Some(reviewDetails) =>
-              landingService.updateReviewDetails(reviewDetails) map {
-                _ =>
-                  Redirect(controllers.businessmatching.routes.BusinessTypeController.get())
-              }
-            case None =>
-              Future.successful(Redirect(Call("GET", ApplicationConfig.businessCustomerUrl)))
-          }
+      if (AmendmentsToggle.feature) {
+        getWithAmendments
+      } else {
+        getWithoutAmendments
       }
+  }
+
+  def getWithoutAmendments(implicit authContext: AuthContext, request: Request[_]) = {
+    testOot("getWithoutAmendments")
+    landingService.cacheMap flatMap {
+      case Some(cache) =>
+        ApplicationConfig.statusToggle match {
+          case true =>
+            Future.successful(Redirect(controllers.routes.StatusController.get()))
+          case _ =>
+            Future.successful(Redirect(controllers.routes.RegistrationProgressController.get()))
+        }
+      case None =>
+        landingService.reviewDetails flatMap {
+          case Some(reviewDetails) =>
+            landingService.updateReviewDetails(reviewDetails) map {
+              _ =>
+                Redirect(controllers.businessmatching.routes.BusinessTypeController.get())
+            }
+          case None =>
+            Future.successful(Redirect(Call("GET", ApplicationConfig.businessCustomerUrl)))
+        }
+    }
+  }
+
+  private def refreshAndRedirect() = {
+    landingService.refreshCache
+    Redirect(controllers.routes.StatusController.get())
+  }
+
+  private def dataHasChanged(cacheMap : CacheMap) = {
+    true
+  }
+
+  private def testOot(message: String)(implicit request: Request[_]) = {
+    val c = request.headers.get("test-context").getOrElse("No Test Context")
+    println(s"[$c] $message")
+
+  }
+
+  def getWithAmendments(implicit authContext: AuthContext, request : Request[_]) = {
+    testOot("getWithAmendments")
+    (authContext.enrolmentsUri match {
+      case Some(uri) => enrolmentsService.amlsRegistrationNumber(uri)
+      case _ => Future.successful(None)
+    }) flatMap  {
+      case Some(_) => landingService.cacheMap.map { cm => //enrolment exists
+        testOot("enrolment Exists")
+        cm match {
+          case Some(cacheMap) => {
+            //there is data in S4l
+            if (dataHasChanged(cacheMap)) {
+              cacheMap.getEntry[SubscriptionResponse](SubscriptionResponse.key) match {
+                case Some(_) => refreshAndRedirect()
+                case _ => Redirect(controllers.routes.StatusController.get())
+              }
+            } else {
+              //DataHasNotChanged
+              refreshAndRedirect()
+            }
+          }
+          case _ => refreshAndRedirect()
+        }
+      }
+
+      case _ => getWithoutAmendments //no enrolment exists
+    }
   }
 }
 
@@ -42,4 +98,5 @@ object LandingController extends LandingController {
   // $COVERAGE-OFF$
   override private[controllers] val landingService = LandingService
   override protected val authConnector = AMLSAuthConnector
+  override private[controllers] val enrolmentsService = AuthEnrolmentsService
 }
--- test/controllers/LandingControllerSpec.scala
+++ test/controllers/LandingControllerSpec.scala
@@ -1,26 +1,37 @@
 package controllers
 
 import config.ApplicationConfig
+import models.asp.Asp
+import models.moneyservicebusiness.MoneyServiceBusiness
 import models.{Country, SubscriptionResponse}
 import models.businesscustomer.{Address, ReviewDetails}
 import org.mockito.Matchers._
 import org.mockito.Mockito._
+import org.mockito.Matchers.{eq => meq}
 import org.scalatest.mock.MockitoSugar
+import org.scalatest.MustMatchers
+import org.scalatest.fixture.WordSpec
 import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
+import play.api.test.FakeApplication
 import play.api.test.Helpers._
-import services.LandingService
+import services.{AuthEnrolmentsService, LandingService}
 import uk.gov.hmrc.http.cache.client.CacheMap
+import uk.gov.hmrc.play.frontend.auth.AuthContext
+import uk.gov.hmrc.play.http.HeaderCarrier
 import utils.AuthorisedFixture
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class LandingControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSugar {
+class LandingControllerWithoutAmendmentsSpec extends PlaySpec with OneAppPerSuite with MockitoSugar {
+
+  implicit override lazy val app = FakeApplication(additionalConfiguration = Map("Test.microservice.services.feature-toggle.amendments" -> false) )
 
   trait Fixture extends AuthorisedFixture {
     self =>
     val controller = new LandingController {
       override val landingService = mock[LandingService]
       override val authConnector = self.authConnector
+      override val enrolmentsService = mock[AuthEnrolmentsService]
     }
   }
 
@@ -39,7 +50,7 @@ class LandingControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSug
         "the form has been submitted" in new Fixture {
           val cacheMap = mock[CacheMap]
           when(cacheMap.getEntry[SubscriptionResponse](SubscriptionResponse.key))
-            .thenReturn(Some(SubscriptionResponse("","",1.00,None,1.00,1.00,"")))
+            .thenReturn(Some(SubscriptionResponse("", "", 1.00, None, 1.00, 1.00, "")))
           when(controller.landingService.cacheMap(any(), any(), any())) thenReturn Future.successful(Some(cacheMap))
           val result = controller.get()(request)
           status(result) must be(SEE_OTHER)
@@ -48,15 +59,14 @@ class LandingControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSug
       }
 
 
-
       "the landing service has no saved form and " when {
 
         "the landing service has valid review details" in new Fixture {
 
           val details = Some(ReviewDetails(businessName = "Test",
-                                           businessType = None,
-                                           businessAddress = Address("Line 1", "Line 2", None, None, None, Country("United Kingdom", "GB")),
-                                           safeId = ""))
+            businessType = None,
+            businessAddress = Address("Line 1", "Line 2", None, None, None, Country("United Kingdom", "GB")),
+            safeId = ""))
 
           when(controller.landingService.cacheMap(any(), any(), any())) thenReturn Future.successful(None)
           when(controller.landingService.reviewDetails(any(), any())).thenReturn(Future.successful(details))
@@ -75,61 +85,205 @@ class LandingControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSug
         }
 
       }
+    }
+  }
+}
 
-      "an enrolment exists and"  when {
-        "there is data in S4L and" when {
-          "data has changed and" when{
-            "there is a subscription response" should{
-              "redirect to status controller" in new Fixture {
-                1 must be(2)
-              }
-            }
-            "there is no subscription response" should{
-              "refresh from API5 and redirect to status controller" in new Fixture{
-                1 must be(2)
-              }
+class LandingControllerWithAmendmentsSpec extends PlaySpec with OneAppPerSuite with MockitoSugar with MustMatchers {
 
-            }
+  val businessCustomerUrl = "TestUrl"
 
-          }
-          "data has not changed" should{
+  implicit override lazy val app = FakeApplication(additionalConfiguration = Map(
+    "Test.microservice.services.feature-toggle.amendments" -> true,
+    "Test.microservice.services.business-customer.url" -> businessCustomerUrl
+  ))
+
+
+  trait Fixture extends AuthorisedFixture {
+    self =>
+    val controller = new LandingController {
+      override val landingService = mock[LandingService]
+      override val authConnector = self.authConnector
+      override val enrolmentsService = mock[AuthEnrolmentsService]
+    }
+  }
+
+  def setUpMocksForNoEnrolment(controller : LandingController) = {
+    when(controller.enrolmentsService.amlsRegistrationNumber(any())(any[HeaderCarrier], any[ExecutionContext]))
+      .thenReturn(Future.successful(None))
+  }
+
+  def setUpMocksForAnEnrolmentExists(controller : LandingController) = {
+    when(controller.enrolmentsService.amlsRegistrationNumber(any())(any[HeaderCarrier], any[ExecutionContext]))
+      .thenReturn(Future.successful(Some("TESTREGNO")))
+  }
+
+  def setUpMocksForNoDataInSaveForLater(controller : LandingController) = {
+    when(controller.landingService.cacheMap(any[HeaderCarrier], any[ExecutionContext], any[AuthContext]))
+      .thenReturn(Future.successful(None))
+  }
+
+  def setUpMocksForDataExistsInKeystore(controller : LandingController) = {
+    val reviewDetails = ReviewDetails(
+      "Business Name",
+      None,
+      Address("Line1", "Line2", None, None, None, Country("United Kingdom", "UK")),
+      "testSafeId")
+
+    when(controller.landingService.reviewDetails(any[HeaderCarrier], any[ExecutionContext]))
+      .thenReturn(Future.successful(Some(reviewDetails)))
+
+    reviewDetails
+  }
+
+  def setUpMocksForNoDataInKeyStore(controller : LandingController) = {
+    when(controller.landingService.reviewDetails(any[HeaderCarrier], any[ExecutionContext]))
+      .thenReturn(Future.successful(None))
+  }
+
+  def setUpMocksForDataExistsInSaveForLater(controller : LandingController, testData : CacheMap = mock[CacheMap]) = {
+    when(controller.landingService.cacheMap(any[HeaderCarrier], any[ExecutionContext], any[AuthContext]))
+      .thenReturn(Future.successful(Some(testData)))
+  }
+
+  def buildTestCacheMap(hasChanged : Boolean, includesResponse : Boolean) : CacheMap = {
+    val result = mock[CacheMap]
+
+    if (hasChanged) {
+      val testASP = Asp(hasChanged = true)
+
+      when(result.getEntry[Asp](Asp.key))
+       .thenReturn(Some(testASP))
+    }
+
+    if (includesResponse) {
+      val testResponse = SubscriptionResponse(
+        "TESTFORMBUNDLENUMBER",
+          "TESTAMLSREFNNO",
+          100.45,
+          None,
+          123.78,
+          17623.76,
+          "TESTPAYMENTREF"
+      )
+
+      when(result.getEntry[SubscriptionResponse](SubscriptionResponse.key))
+        .thenReturn(Some(testResponse))
+
+    }
+
+    result
+  }
+
+  "Landing Controller" when {
+    "an enrolment exists and" when {
+      "there is data in S4L and" when {
+        "data has changed and" when {
+          "there is a subscription response" should {
             "refresh from API5 and redirect to status controller" in new Fixture {
-              1 must be(2)
+              setUpMocksForAnEnrolmentExists(controller)
+              setUpMocksForDataExistsInSaveForLater(controller, buildTestCacheMap(true, false))
+
+              val result = controller.get()(request.withHeaders("test-context" ->"ESCS"))
+
+              verify(controller.landingService, atLeastOnce()).refreshCache
+              status(result) must be (SEE_OTHER)
+              redirectLocation(result) must be (Some(controllers.routes.StatusController.get().url))
+            }
+
+
+
+          }
+
+          "there is no subscription response" should {
+            "redirect to status controller without refreshing API5" in new Fixture {
+              setUpMocksForAnEnrolmentExists(controller)
+              setUpMocksForDataExistsInSaveForLater(controller, buildTestCacheMap(true, true))
+
+              val result = controller.get()(request)
+
+              verify(controller.landingService, never()).refreshCache
+              status(result) must be (SEE_OTHER)
+              redirectLocation(result) must be (Some(controllers.routes.StatusController.get().url))
             }
           }
         }
 
-        "there is no data in S4L" should {
+        "data has not changed" should {
           "refresh from API5 and redirect to status controller" in new Fixture {
-            1 must be(2)
+            setUpMocksForAnEnrolmentExists(controller)
+            setUpMocksForDataExistsInSaveForLater(controller, buildTestCacheMap(false, false))
+
+            val result = controller.get()(request)
+
+            verify(controller.landingService, atLeastOnce()).refreshCache
+            status(result) must be (SEE_OTHER)
+            redirectLocation(result) must be (Some(controllers.routes.StatusController.get().url))
           }
         }
       }
 
-      "an enrolment does not exist" when {
-        "there is data in S4L " should {
-          "redirect to status controller" in new Fixture {
-            1 must be(2)
-          }
+      "there is no data in S4L" should {
+        "refresh from API5 and redirect to status controller" in new Fixture {
+          setUpMocksForAnEnrolmentExists(controller)
+          setUpMocksForNoDataInSaveForLater(controller)
+
+          val result = controller.get()(request)
+
+          verify(controller.landingService, atLeastOnce()).refreshCache
+          status(result) must be (SEE_OTHER)
+          redirectLocation(result) must be (Some(controllers.routes.StatusController.get().url))
         }
+      }
+    }
 
-        "there is no data in S4L" when {
-          "there is data in keystore " should{
-            "copy keystore data to S4L and redirect to business type controler" in new Fixture {
-              1 must be(2)
-            }
-          }
-          "there is no data in keystore" should{
-            "redirect to business customer" in new Fixture {
-              1 must be(2)
-            }
+    "an enrolment does not exist" when {
+      "there is data in S4L " should {
+        "do not refresh API5 and redirect to status controller" in new Fixture{
+          setUpMocksForNoEnrolment(controller)
+          setUpMocksForDataExistsInSaveForLater(controller)
 
-          }
+          val result = controller.get()(request)
 
+          verify(controller.landingService, never()).refreshCache
+          status(result) must be (SEE_OTHER)
+          redirectLocation(result) must be (Some(controllers.routes.StatusController.get().url))
         }
-
       }
 
+      "there is no data in S4L" when {
+        "there is data in keystore " should {
+          "copy keystore data to S4L and redirect to business type controler" in new Fixture {
+            setUpMocksForNoEnrolment(controller)
+            setUpMocksForNoDataInSaveForLater(controller)
+            val reviewDetails = setUpMocksForDataExistsInKeystore(controller)
+
+            when(controller.landingService.updateReviewDetails(any[ReviewDetails])(any[HeaderCarrier], any[ExecutionContext], any[AuthContext]))
+              .thenReturn(Future.successful(mock[CacheMap]))
+
+            val result = controller.get()(request)
+
+            verify(controller.landingService, atLeastOnce())
+              .updateReviewDetails(meq(reviewDetails))(any[HeaderCarrier], any[ExecutionContext], any[AuthContext])
+
+            status(result) must be (SEE_OTHER)
+            redirectLocation(result) must be (Some(controllers.businessmatching.routes.BusinessTypeController.get().url))
+
+          }
+        }
+
+        "there is no data in keystore" should {
+          "redirect to business customer" in new Fixture {
+            setUpMocksForNoEnrolment(controller)
+            setUpMocksForNoDataInSaveForLater(controller)
+            setUpMocksForNoDataInKeyStore(controller)
+
+            val result = controller.get()(request)
+            status(result) must be (SEE_OTHER)
+            redirectLocation(result) must be (Some(businessCustomerUrl))
+          }
+        }
+      }
     }
   }
 }
