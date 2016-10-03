bb2a4802808a92cdeb5bca47d3f199e0d35e8ce2
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2016-10-03 15:19:43 +0100

    updating tests

--- test/controllers/responsiblepeople/SummaryControllerSpec.scala
+++ test/controllers/responsiblepeople/SummaryControllerSpec.scala
@@ -19,7 +19,7 @@ class SummaryControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSug
     self =>
 
     val controller = new CheckYourAnswersController {
-      override val dataCache = mock[DataCacheConnector]
+      override val dataCacheConnector = mock[DataCacheConnector]
       override val authConnector = self.authConnector
     }
   }
@@ -28,14 +28,14 @@ class SummaryControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSug
 
     "use correct services" in new Fixture {
       CheckYourAnswersController.authConnector must be(AMLSAuthConnector)
-      CheckYourAnswersController.dataCache must be(DataCacheConnector)
+      CheckYourAnswersController.dataCacheConnector must be(DataCacheConnector)
     }
 
     "load the summary page when section data is available" in new Fixture {
 
       val model = ResponsiblePeople(None, None)
 
-      when(controller.dataCache.fetch[Seq[ResponsiblePeople]](any())
+      when(controller.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())
         (any(), any(), any())).thenReturn(Future.successful(Some(Seq(model))))
       val result = controller.get()(request)
 
@@ -43,7 +43,7 @@ class SummaryControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSug
     }
 
     "redirect to the main amls summary page when section data is unavailable" in new Fixture {
-      when(controller.dataCache.fetch[Seq[ResponsiblePeople]](any())
+      when(controller.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())
         (any(), any(), any())).thenReturn(Future.successful(None))
       val result = controller.get()(request)
       redirectLocation(result) must be(Some(controllers.routes.RegistrationProgressController.get.url))
