e68af1a2a5bec722812ea8b96f9328dc41cc9a22
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2016-10-03 17:34:22 +0100

    updating routes and tests to use correct parameters

--- app/controllers/responsiblepeople/RemoveResponsiblePersonController.scala
+++ app/controllers/responsiblepeople/RemoveResponsiblePersonController.scala
@@ -4,7 +4,6 @@ import config.AMLSAuthConnector
 import connectors.DataCacheConnector
 import controllers.BaseController
 import forms.EmptyForm
-import models.bankdetails.BankDetails
 import models.responsiblepeople.ResponsiblePeople
 import utils.{StatusConstants, RepeatingSection}
 
@@ -14,31 +13,24 @@ trait RemoveResponsiblePersonController extends RepeatingSection with BaseContro
 
   val dataCacheConnector: DataCacheConnector
 
-//  def get() = Authorised.async {
-//    implicit authContext => implicit request =>
-//      Future.successful(Ok(views.html.responsiblepeople.remove_responsible_person()))
-//  }
-
-
   def get(index: Int, complete: Boolean = false) = Authorised.async {
     implicit authContext => implicit request =>
       getData[ResponsiblePeople](index) map {
         case Some(ResponsiblePeople(Some(personName),_,_,_,_,_,_,_,_,_,_,_,_)) =>
-          Ok(views.html.bankdetails.remove_bank_details(EmptyForm, index, personName.fullName, complete))
+          Ok(views.html.responsiblepeople.remove_responsible_person(EmptyForm, index, personName.fullName, complete))
         case _ => NotFound(notFoundView)
       }
   }
 
-//
-//  def remove(index: Int, complete: Boolean = false) = Authorised.async {
-//    implicit authContext => implicit request => {
-//      for {
-//        rs <- updateDataStrict[BankDetails](index) { ba =>
-//          ba.copy(status = Some(StatusConstants.Deleted), hasChanged = true)
-//        }
-//      } yield Redirect(routes.SummaryController.get(complete))
-//    }
-//  }
+  def remove(index: Int, complete: Boolean = false) = Authorised.async {
+    implicit authContext => implicit request => {
+      for {
+        rs <- updateDataStrict[ResponsiblePeople](index) { rp =>
+          rp.copy(status = Some(StatusConstants.Deleted), hasChanged = true)
+        }
+      } yield Redirect(routes.CheckYourAnswersController.get())
+    }
+  }
 }
 
 object RemoveResponsiblePersonController extends RemoveResponsiblePersonController {
--- test/controllers/responsiblepeople/RemoveResponsiblePersonControllerSpec.scala
+++ test/controllers/responsiblepeople/RemoveResponsiblePersonControllerSpec.scala
@@ -2,13 +2,19 @@ package controllers.responsiblepeople
 
 import connectors.DataCacheConnector
 import controllers.responsiblepeople.RemoveResponsiblePersonController
+import models.responsiblepeople.{PersonName, ResponsiblePeople}
+import org.mockito.Matchers._
+import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{MustMatchers, WordSpecLike}
 import org.scalatestplus.play.OneAppPerSuite
+import uk.gov.hmrc.http.cache.client.CacheMap
 import utils.AuthorisedFixture
 import play.api.test.Helpers._
+
+import scala.concurrent.Future
 
 class RemoveResponsiblePersonControllerSpec extends WordSpecLike
   with MustMatchers with MockitoSugar with ScalaFutures with OneAppPerSuite with PropertyChecks {
@@ -23,24 +29,40 @@ class RemoveResponsiblePersonControllerSpec extends WordSpecLike
   }
 
   "RemoveResponsiblePersonController" when {
-
     "get is called" must {
+      "respond with OK when the index is valid" in new Fixture {
 
-      "respond with OK" in new Fixture {
+        when(controller.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())(any(), any(), any()))
+          .thenReturn(Future.successful(Some(Seq(ResponsiblePeople(Some(PersonName("firstName", None, "lastName", None, None)))))))
 
         val result = controller.get(1, false)(request)
 
         status(result) must be(OK)
 
       }
+      "respond with NOT_FOUND when the index is out of bounds" in new Fixture {
+
+        when(controller.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())(any(), any(), any()))
+          .thenReturn(Future.successful(Some(Seq(ResponsiblePeople()))))
+
+        val result = controller.get(100, false)(request)
+
+        status(result) must be(NOT_FOUND)
+
+      }
     }
 
-//    "remove is called" must {
-//      "respond with ???" in new Fixture {
-//
-//        val result = controller.remove()(request)
-//      }
-//    }
-  }
+    "remove is called" must {
+      "respond with SEE_OTHER when the index is valid" in new Fixture {
 
+        when(controller.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())(any(), any(), any()))
+          .thenReturn(Future.successful(Some(Seq(ResponsiblePeople()))))
+
+        val result = controller.remove(1, false)(request)
+
+        status(result) must be(SEE_OTHER)
+
+      }
+    }
+  }
 }
