d195b0c1977d8aa86b3f9799a34c7c23d29ddba1
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-10-03 11:38:58 +0100

    AMLS-1751:increase coverage to 92.49

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -44,7 +44,7 @@ trait MicroService {
     import scoverage.ScoverageSbtPlugin._
     Seq(
       // Semicolon-separated list of regexs matching classes to exclude
-      ScoverageKeys.coverageExcludedPackages := "<empty>;Reverse.*;.*AuthService.*;models/.data/..*;view.*;forms.*;config.*;.*BuildInfo.*;prod.Routes;app.Routes;testOnlyDoNotUseInAppConf.Routes;controllers.ExampleController;controllers.testonly.TestOnlyController",
+      ScoverageKeys.coverageExcludedPackages := "<empty>;Reverse.*;.*AuthService.*;modgiels/.data/..*;view.*;forms.*;config.*;.*BuildInfo.*;prod.Routes;app.Routes;testOnlyDoNotUseInAppConf.Routes;controllers.ExampleController;controllers.testonly.TestOnlyController",
       ScoverageKeys.coverageMinimum := 92.49,
       ScoverageKeys.coverageFailOnMinimum := false,
       ScoverageKeys.coverageHighlighting := true,
