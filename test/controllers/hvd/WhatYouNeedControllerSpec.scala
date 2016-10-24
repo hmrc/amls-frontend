351fcdd5b5134aa4e35e81aff6c98532ebe1fe17
Phil Sherry <philip.sherry@digital.hmrc.gov.uk>
2016-10-24 12:00:38 +0100

    Fixes tests;

--- test/controllers/hvd/WhatYouNeedControllerSpec.scala
+++ test/controllers/hvd/WhatYouNeedControllerSpec.scala
@@ -21,9 +21,14 @@ class WhatYouNeedControllerSpec extends PlaySpec with OneAppPerSuite with Mockit
     "get" must {
 
       "load the page" in new Fixture {
+
+        val pageTitle = Messages("title.wyn") + " - " +
+          Messages("summary.hvd") + " - " +
+          Messages("title.amls") + " - " + Messages("title.gov")
+
         val result = controller.get(request)
         status(result) must be(OK)
-        contentAsString(result) must include(Messages("hvd.whatyouneed.title"))
+        contentAsString(result) must include(pageTitle)
       }
     }
   }
