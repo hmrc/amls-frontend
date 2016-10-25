1825b75ce58d364cfbc88a6f80f47c52b12baaea
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-10-25 16:48:15 +0100

    AMLS-1847:initial commit

--- app/controllers/businessactivities/SummaryController.scala
+++ app/controllers/businessactivities/SummaryController.scala
@@ -4,18 +4,34 @@ import config.AMLSAuthConnector
 import connectors.DataCacheConnector
 import controllers.BaseController
 import models.businessactivities.BusinessActivities
+import models.status.{NotCompleted, SubmissionReady, SubmissionDecisionApproved, SubmissionStatus}
+import services.StatusService
 import views.html.businessactivities.summary
+
+import scala.concurrent.Future
 
 trait SummaryController extends BaseController {
 
   protected def dataCache: DataCacheConnector
 
+  private[controllers] def statusService: StatusService
+
+  def isLinkEditable(status: Future[SubmissionStatus]): Future[Boolean] = {
+    status.map {
+      case SubmissionReady | NotCompleted => true
+      case _ => false
+    }
+  }
+
   def get = Authorised.async {
     implicit authContext => implicit request =>
-      dataCache.fetch[BusinessActivities](BusinessActivities.key) map {
+      for {
+        ba <- dataCache.fetch[BusinessActivities](BusinessActivities.key)
+        showEdit <- isLinkEditable(statusService.getStatus)
+      } yield ba match {
         case Some(data) => Ok(summary(data))
         case _ => Redirect(controllers.routes.RegistrationProgressController.get())
-      }
+     }
   }
 }
 
@@ -23,4 +39,5 @@ object SummaryController extends SummaryController {
   // $COVERAGE-OFF$
   override val dataCache = DataCacheConnector
   override val authConnector = AMLSAuthConnector
+  override val statusService: StatusService = StatusService
 }
--- test/controllers/businessactivities/SummaryControllerSpec.scala
+++ test/controllers/businessactivities/SummaryControllerSpec.scala
@@ -1,14 +1,16 @@
 package controllers.businessactivities
 
-import config.AMLSAuthConnector
 import connectors.DataCacheConnector
-import models.businessactivities.BusinessActivities
-import models.estateagentbusiness.EstateAgentBusiness
+import models.Country
+import models.businessactivities._
+import models.status.NotCompleted
+import org.jsoup.Jsoup
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
 import play.api.test.Helpers._
+import services.StatusService
 import utils.AuthorisedFixture
 
 import scala.concurrent.Future
@@ -21,15 +23,28 @@ class SummaryControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSug
     val controller = new SummaryController {
       override val dataCache = mock[DataCacheConnector]
       override val authConnector = self.authConnector
+      override val statusService: StatusService = mock[StatusService]
     }
   }
 
   "Get" must {
 
-    "use correct services" in new Fixture {
-      SummaryController.authConnector must be(AMLSAuthConnector)
-      SummaryController.dataCache must be(DataCacheConnector)
-    }
+    val completeModel = BusinessActivities(
+      involvedInOther = Some(BusinessActivitiesValues.DefaultInvolvedInOther),
+      expectedBusinessTurnover = Some(BusinessActivitiesValues.DefaultBusinessTurnover),
+      expectedAMLSTurnover = Some(BusinessActivitiesValues.DefaultAMLSTurnover),
+      businessFranchise = Some(BusinessActivitiesValues.DefaultBusinessFranchise),
+      transactionRecord = Some(BusinessActivitiesValues.DefaultTransactionRecord),
+      customersOutsideUK = Some(BusinessActivitiesValues.DefaultCustomersOutsideUK),
+      ncaRegistered = Some(BusinessActivitiesValues.DefaultNCARegistered),
+      accountantForAMLSRegulations = Some(BusinessActivitiesValues.DefaultAccountantForAMLSRegulations),
+      riskAssessmentPolicy = Some(BusinessActivitiesValues.DefaultRiskAssessments),
+      howManyEmployees = Some(BusinessActivitiesValues.DefaultHowManyEmployees),
+      identifySuspiciousActivity = Some(BusinessActivitiesValues.DefaultIdentifySuspiciousActivity),
+      whoIsYourAccountant = Some(BusinessActivitiesValues.DefaultWhoIsYourAccountant),
+      taxMatters = Some(BusinessActivitiesValues.DefaultTaxMatters),
+      hasChanged = false
+    )
 
     "load the summary page when section data is available" in new Fixture {
 
@@ -44,11 +59,50 @@ class SummaryControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSug
 
     "redirect to the main summary page when section data is unavailable" in new Fixture {
 
-      when(controller.dataCache.fetch[EstateAgentBusiness](any())
+      when(controller.dataCache.fetch[BusinessActivities](any())
         (any(), any(), any())).thenReturn(Future.successful(None))
 
       val result = controller.get()(request)
       status(result) must be(SEE_OTHER)
     }
+
+    "hide edit link in variation mode" in new Fixture {
+      when(controller.dataCache.fetch[BusinessActivities](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(completeModel)))
+
+      when(controller.statusService.getStatus(any(), any(), any()))
+        .thenReturn(Future.successful(NotCompleted))
+
+      val result = controller.get()(request)
+      status(result) must be(OK)
+      val document = Jsoup.parse(contentAsString(result))
+
+      document.getElementsByTag("section").get(0).getElementsByClass("edit").isEmpty must be(true)
+
+
+    }
   }
+}
+
+object BusinessActivitiesValues {
+  val DefaultFranchiseName = "DEFAULT FRANCHISE NAME"
+  val DefaultSoftwareName = "DEFAULT SOFTWARE"
+  val DefaultBusinessTurnover = ExpectedBusinessTurnover.First
+  val DefaultAMLSTurnover = ExpectedAMLSTurnover.First
+  val DefaultInvolvedInOtherDetails = "DEFAULT INVOLVED"
+  val DefaultInvolvedInOther = InvolvedInOtherYes(DefaultInvolvedInOtherDetails)
+  val DefaultBusinessFranchise = BusinessFranchiseYes(DefaultFranchiseName)
+  val DefaultTransactionRecord = TransactionRecordYes(Set(Paper, DigitalSoftware(DefaultSoftwareName)))
+  val DefaultCustomersOutsideUK = CustomersOutsideUK(Some(Seq(Country("United Kingdom", "GB"))))
+  val DefaultNCARegistered = NCARegistered(true)
+  val DefaultAccountantForAMLSRegulations = AccountantForAMLSRegulations(true)
+  val DefaultRiskAssessments = RiskAssessmentPolicyYes(Set(PaperBased))
+  val DefaultHowManyEmployees = HowManyEmployees("5","4")
+  val DefaultWhoIsYourAccountant = WhoIsYourAccountant(
+    "Accountant's name",
+    Some("Accountant's trading name"),
+    UkAccountantsAddress("address1", "address2", Some("address3"), Some("address4"), "POSTCODE")
+  )
+  val DefaultIdentifySuspiciousActivity = IdentifySuspiciousActivity(true)
+  val DefaultTaxMatters = TaxMatters(false)
 }
