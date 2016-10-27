406b54dea196d91913b482bd83c007b286b3510d
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-10-27 13:02:03 +0100

    AMLS-1854:modify code to not show edit link in variation

--- app/controllers/hvd/ReceiveCashPaymentsController.scala
+++ app/controllers/hvd/ReceiveCashPaymentsController.scala
@@ -5,7 +5,9 @@ import connectors.DataCacheConnector
 import controllers.BaseController
 import forms.{EmptyForm, Form2, InvalidForm, ValidForm}
 import models.hvd.{Hvd, ReceiveCashPayments}
+import services.StatusService
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
+import utils.ControllerHelper
 import views.html.hvd.receiving
 
 import scala.concurrent.Future
@@ -14,16 +16,22 @@ trait ReceiveCashPaymentsController extends BaseController {
 
   def cacheConnector: DataCacheConnector
 
+  implicit val statusService: StatusService
+
   def get(edit: Boolean = false) = HvdToggle {
     Authorised.async {
       implicit authContext => implicit request =>
-        cacheConnector.fetch[Hvd](Hvd.key) map {
-          response =>
-            val form: Form2[ReceiveCashPayments] = (for {
-              hvd <- response
-              receivePayments <- hvd.receiveCashPayments
-            } yield Form2[ReceiveCashPayments](receivePayments)).getOrElse(EmptyForm)
-            Ok(receiving(form, edit))
+        ControllerHelper.allowedToEdit flatMap {
+          case true =>
+            cacheConnector.fetch[Hvd](Hvd.key) map {
+              response =>
+                val form: Form2[ReceiveCashPayments] = (for {
+                  hvd <- response
+                  receivePayments <- hvd.receiveCashPayments
+                } yield Form2[ReceiveCashPayments](receivePayments)).getOrElse(EmptyForm)
+                Ok(receiving(form, edit))
+            }
+          case false => Future.successful(NotFound(notFoundView))
         }
     }
   }
@@ -48,10 +56,10 @@ trait ReceiveCashPaymentsController extends BaseController {
       }
     }
   }
-
 }
 
 object ReceiveCashPaymentsController extends ReceiveCashPaymentsController {
   override val cacheConnector: DataCacheConnector = DataCacheConnector
   override protected val authConnector: AuthConnector = AMLSAuthConnector
+  override val statusService: StatusService = StatusService
 }
