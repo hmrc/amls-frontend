c25dc2236b2f06872ff718ddb9f660d1ea9e0c28
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2016-10-28 11:17:31 +0100

    adding tests to check year validation

--- test/models/responsiblepeople/ResponsiblePersonEndDateSpec.scala
+++ test/models/responsiblepeople/ResponsiblePersonEndDateSpec.scala
@@ -10,63 +10,101 @@ import play.api.libs.json.{JsPath, JsSuccess, Json}
 
 class ResponsiblePersonEndDateSpec extends PlaySpec {
 
-  "ResponsiblePersonEndDate" must {
-    "Form" must {
-      "read successfully" in {
-        val model =  Map (
-            "endDate.day" -> Seq("24"),
-            "endDate.month" -> Seq("2"),
-            "endDate.year" -> Seq("1990")
-        )
-        // scalastyle:off
-        ResponsiblePersonEndDate.formRule.validate(model) must be (Success(ResponsiblePersonEndDate(new LocalDate(1990, 2, 24))))
+  val validYear = 1990
+  val validDay = 24
+  val validMonth = 2
 
-      }
+  "ResponsiblePersonEndDate Form" must {
+    "successfully read the model" in {
+      val validModel = Map(
+        "endDate.day" -> Seq("24"),
+        "endDate.month" -> Seq("2"),
+        "endDate.year" -> Seq("1990")
+      )
 
-      "throw error message when data entered is invalid" in {
-        val model =  Map (
+      ResponsiblePersonEndDate.formRule.validate(validModel) must be(
+        Success(ResponsiblePersonEndDate(new LocalDate(validYear, validMonth, validDay))))
+    }
+
+    "successfully write the model" in {
+      ResponsiblePersonEndDate.formWrites.writes(ResponsiblePersonEndDate(new LocalDate(validYear, validMonth, validDay))) must be(
+        Map(
+          "endDate.day" -> Seq("24"),
+          "endDate.month" -> Seq("2"),
+          "endDate.year" -> Seq("1990")
+        ))
+    }
+
+    "throw error message" when {
+      "day entered is invalid" in {
+        val errorDayModel = Map(
           "endDate.day" -> Seq("2466"),
           "endDate.month" -> Seq("2"),
           "endDate.year" -> Seq("1990")
         )
-        ResponsiblePersonEndDate.formRule.validate(model) must be(Failure(Seq(Path \ "endDate" -> Seq(
-          ValidationError("error.expected.jodadate.format", "yyyy-MM-dd")))))
 
+        ResponsiblePersonEndDate.formRule.validate(errorDayModel) must be(
+          Failure(Seq(Path \ "endDate" -> Seq(ValidationError("error.expected.jodadate.format", "yyyy-MM-dd")))))
       }
 
-      "throw error message when data entered is empty" in {
-        val model =  Map (
+      "month entered is invalid" in {
+        val errorDayModel = Map(
+          "endDate.day" -> Seq("24"),
+          "endDate.month" -> Seq("29"),
+          "endDate.year" -> Seq("1990")
+        )
+
+        ResponsiblePersonEndDate.formRule.validate(errorDayModel) must be(
+          Failure(Seq(Path \ "endDate" -> Seq(ValidationError("error.expected.jodadate.format", "yyyy-MM-dd")))))
+      }
+
+      "year entered is too long" in {
+        val errorDayModel = Map(
+          "endDate.day" -> Seq("24"),
+          "endDate.month" -> Seq("29"),
+          "endDate.year" -> Seq("199000")
+        )
+
+        ResponsiblePersonEndDate.formRule.validate(errorDayModel) must be(
+          Failure(Seq(Path \ "endDate" -> Seq(ValidationError("error.expected.jodadate.format", "yyyy-MM-dd")))))
+      }
+
+      "year entered is too short" in {
+        val errorDayModel = Map(
+          "endDate.day" -> Seq("24"),
+          "endDate.month" -> Seq("29"),
+          "endDate.year" -> Seq("16")
+        )
+
+        ResponsiblePersonEndDate.formRule.validate(errorDayModel) must be(
+          Failure(Seq(Path \ "endDate" -> Seq(ValidationError("error.expected.jodadate.format", "yyyy-MM-dd")))))
+      }
+
+      "all fields are empty" in {
+        val noContentModel = Map(
           "endDate.day" -> Seq(""),
           "endDate.month" -> Seq(""),
           "endDate.year" -> Seq("")
         )
-        ResponsiblePersonEndDate.formRule.validate(model) must be(Failure(Seq(Path \ "endDate" -> Seq(
-          ValidationError("error.expected.jodadate.format", "yyyy-MM-dd")))))
 
-      }
-
-      "successfully write the model" in {
-
-        ResponsiblePersonEndDate.formWrites.writes(ResponsiblePersonEndDate(new LocalDate(1990, 2, 24)))  mustBe Map(
-          "endDate.day" -> Seq("24"),
-          "endDate.month" -> Seq("2"),
-          "endDate.year" -> Seq("1990")
-        )
-      }
-    }
-
-    "Json" should {
-
-      "Read and write successfully" in {
-
-        ResponsiblePersonEndDate.format.reads(ResponsiblePersonEndDate.format.writes(ResponsiblePersonEndDate(new LocalDate(1990, 2, 24)))) must be(
-          JsSuccess(ResponsiblePersonEndDate(new LocalDate(1990, 2, 24)), JsPath \ "endDate"))
-
-      }
-
-      "write successfully" in {
-        ResponsiblePersonEndDate.format.writes(ResponsiblePersonEndDate(new LocalDate(1990, 2, 24))) must be(Json.obj("endDate" ->"1990-02-24"))
+        ResponsiblePersonEndDate.formRule.validate(noContentModel) must be(
+          Failure(Seq(Path \ "endDate" -> Seq(ValidationError("error.expected.jodadate.format", "yyyy-MM-dd")))))
       }
     }
   }
- }
+
+  "ResponsiblePersonEndDate Json" must {
+
+    "Read and write successfully" in {
+
+      ResponsiblePersonEndDate.format.reads(ResponsiblePersonEndDate.format.writes(ResponsiblePersonEndDate(new LocalDate(1990, 2, 24)))) must be(
+        JsSuccess(ResponsiblePersonEndDate(new LocalDate(1990, 2, 24)), JsPath \ "endDate"))
+
+    }
+
+    "write successfully" in {
+      ResponsiblePersonEndDate.format.writes(ResponsiblePersonEndDate(new LocalDate(1990, 2, 24))) must be(Json.obj("endDate" -> "1990-02-24"))
+    }
+  }
+
+}
