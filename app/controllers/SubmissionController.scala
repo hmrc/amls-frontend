767bc0fe961390c78f8aeb30781bfe3a7a8f2fc0
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2016-10-28 15:12:21 +0100

    AMLS-1854:revert compilation warning fix from  SubmissionController

--- app/controllers/SubmissionController.scala
+++ app/controllers/SubmissionController.scala
@@ -19,7 +19,7 @@ trait SubmissionController extends BaseController {
 
   def post() = Authorised.async {
     implicit authContext => implicit request => {
-      statusService.getStatus flatMap [SubmissionResponse]{
+      statusService.getStatus flatMap {
         case SubmissionReadyForReview => subscriptionService.update
         case SubmissionDecisionApproved => subscriptionService.variation
         case _ => subscriptionService.subscribe
