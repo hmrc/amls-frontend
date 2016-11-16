75d01b9e55d800152d7896928148dc970bd869b1
darryl-murphy <darryl.murphy@digital.hmrc.gov.uk>
2016-11-16 14:02:22 +0000

    Remove writes from survey model, add link to status page

--- app/models/SatisfactionSurvey.scala
+++ app/models/SatisfactionSurvey.scala
@@ -10,11 +10,11 @@ sealed trait SatisfactionSurvey
 
 object SatisfactionSurvey {
 
-  case class First(details: String) extends SatisfactionSurvey
-  case class Second(details: String) extends SatisfactionSurvey
-  case class Third(details: String) extends SatisfactionSurvey
-  case class Fourth(details: String) extends SatisfactionSurvey
-  case class Fifth(details: String) extends SatisfactionSurvey
+  case class First(details: Option[String]) extends SatisfactionSurvey
+  case class Second(details: Option[String]) extends SatisfactionSurvey
+  case class Third(details: Option[String]) extends SatisfactionSurvey
+  case class Fourth(details: Option[String]) extends SatisfactionSurvey
+  case class Fifth(details: Option[String]) extends SatisfactionSurvey
 
   import utils.MappingUtils.Implicits._
 
@@ -25,73 +25,28 @@ object SatisfactionSurvey {
     import play.api.data.mapping.forms.Rules._
     import models.FormTypes._
     (__ \ "satisfaction").read[String].withMessage("error.invalid") flatMap {
-      case "01" => (__ \ "details").read(detailsRule) fmap First.apply
-      case "02" => (__ \ "details").read(detailsRule) fmap Second.apply
-      case "03" => (__ \ "details").read(detailsRule) fmap Third.apply
-      case "04" => (__ \ "details").read(detailsRule) fmap Fourth.apply
-      case "05" => (__ \ "details").read(detailsRule) fmap Fifth.apply
+      case "01" => (__ \ "details").read(optionR(detailsRule)) fmap First.apply
+      case "02" => (__ \ "details").read(optionR(detailsRule)) fmap Second.apply
+      case "03" => (__ \ "details").read(optionR(detailsRule)) fmap Third.apply
+      case "04" => (__ \ "details").read(optionR(detailsRule)) fmap Fourth.apply
+      case "05" => (__ \ "details").read(optionR(detailsRule)) fmap Fifth.apply
       case _ =>
         (Path \ "satisfaction") -> Seq(ValidationError("error.invalid"))
     }
 
   }
 
-  implicit val formWrites: Write[SatisfactionSurvey, UrlFormEncoded] = Write {
-    case First(details) => Map(
-      "satisfaction" -> Seq("01"),
-      "details" -> Seq(details)
-    )
-    case Second(details) => Map(
-      "satisfaction" -> Seq("02"),
-      "details" -> Seq(details)
-    )
-    case Third(details) => Map(
-      "satisfaction" -> Seq("03"),
-      "details" -> Seq(details)
-    )
-    case Fourth(details) => Map(
-      "satisfaction" -> Seq("04"),
-      "details" -> Seq(details)
-    )
-    case Fifth(details) => Map(
-      "satisfaction" -> Seq("05"),
-      "details" -> Seq(details)
-    )
-  }
-
   implicit val jsonReads = {
     import play.api.libs.json.Reads.StringReads
     (__ \ "satisfaction").read[String].flatMap[SatisfactionSurvey] {
-      case "01" => (__ \ "details").read[String] map First.apply
-      case "02" => (__ \ "details").read[String] map Second.apply
-      case "03" => (__ \ "details").read[String] map Third.apply
-      case "04" => (__ \ "details").read[String] map Fourth.apply
-      case "05" => (__ \ "details").read[String] map Fifth.apply
+      case "01" => (__ \ "details").readNullable[String] map First.apply
+      case "02" => (__ \ "details").readNullable[String] map Second.apply
+      case "03" => (__ \ "details").readNullable[String] map Third.apply
+      case "04" => (__ \ "details").readNullable[String] map Fourth.apply
+      case "05" => (__ \ "details").readNullable[String] map Fifth.apply
       case _ =>
         ValidationError("error.invalid")
     }
   }
 
-  implicit val jsonWrites = Writes[SatisfactionSurvey] {
-    case First(details) => Json.obj(
-      "satisfaction" -> "01",
-      "details" -> details
-    )
-    case Second(details) => Json.obj(
-      "satisfaction" -> "02",
-      "details" -> details
-    )
-    case Third(details) => Json.obj(
-      "satisfaction" -> "03",
-      "details" -> details
-    )
-    case Fourth(details) => Json.obj(
-      "satisfaction" -> "04",
-      "details" -> details
-    )
-    case Fifth(details) => Json.obj(
-      "satisfaction" -> "05",
-      "details" -> details
-    )
-  }
 }
--- test/models/SatisfactionSurveySpec.scala
+++ test/models/SatisfactionSurveySpec.scala
@@ -19,7 +19,7 @@ class SatisfactionSurveySpec extends PlaySpec with MockitoSugar {
       )
 
       SatisfactionSurvey.formRule.validate(data) must
-        be(Success(First("")))
+        be(Success(First(None)))
     }
 
     "successfully validate given feedback with details" in {
@@ -30,7 +30,18 @@ class SatisfactionSurveySpec extends PlaySpec with MockitoSugar {
       )
 
       SatisfactionSurvey.formRule.validate(data) must
-        be(Success(Second("123")))
+        be(Success(Second(Some("123"))))
+    }
+
+    "successfully validate missing mandatory details" in {
+
+      val data = Map(
+        "satisfaction" -> Seq("02")
+      )
+
+      SatisfactionSurvey.formRule.validate(data) must
+        be(Success(Second(None)))
+
     }
 
     "fail to validate missing mandatory satisfaction" in {
@@ -42,18 +53,6 @@ class SatisfactionSurveySpec extends PlaySpec with MockitoSugar {
       SatisfactionSurvey.formRule.validate(data) must
         be(Failure(Seq(
           (Path \ "satisfaction") -> Seq(ValidationError("error.invalid"))
-        )))
-    }
-
-    "fail to validate missing mandatory details" in {
-
-      val data = Map(
-        "satisfaction" -> Seq("02")
-      )
-
-      SatisfactionSurvey.formRule.validate(data) must
-        be(Failure(Seq(
-          (Path \ "details") -> Seq(ValidationError("error.required"))
         )))
     }
 
@@ -78,26 +77,14 @@ class SatisfactionSurveySpec extends PlaySpec with MockitoSugar {
         )))
     }
 
-    "write correct data for feedback with empty details" in {
-
-      SatisfactionSurvey.formWrites.writes(Third("")) must
-        be(Map("satisfaction" -> Seq("03"), "details" -> Seq("")))
-
-    }
-
-    "write correct data for feedback with details" in {
-
-      SatisfactionSurvey.formWrites.writes(Second("123")) must
-        be(Map("satisfaction" -> Seq("02"), "details" -> Seq("123")))
-    }
   }
 
   "JSON validation" must {
 
     "successfully validate given feedback with empty details" in {
 
-      Json.fromJson[SatisfactionSurvey](Json.obj("satisfaction" -> "04", "details" -> "")) must
-        be(JsSuccess(Fourth(""), JsPath \ "satisfaction" \ "details"))
+      Json.fromJson[SatisfactionSurvey](Json.obj("satisfaction" -> "04")) must
+        be(JsSuccess(Fourth(None), JsPath \ "satisfaction"))
     }
 
     "successfully validate given feedback with details" in {
@@ -105,28 +92,17 @@ class SatisfactionSurveySpec extends PlaySpec with MockitoSugar {
       val json = Json.obj("satisfaction" -> "03", "details" ->"123")
 
       Json.fromJson[SatisfactionSurvey](json) must
-        be(JsSuccess(Third("123"), JsPath \ "satisfaction" \ "details"))
+        be(JsSuccess(Third(Some("123")), JsPath \ "satisfaction" \ "details"))
     }
 
-    "fail to validate given no details" in {
+    "fail to validate given no data" in {
 
-      val json = Json.obj("satisfaction" -> "04")
+      val json = Json.obj()
 
       Json.fromJson[SatisfactionSurvey](json) must
-        be(JsError((JsPath \ "satisfaction" \ "details") -> ValidationError("error.path.missing")))
+        be(JsError((JsPath \ "satisfaction") -> ValidationError("error.path.missing")))
     }
 
-    "write the correct value" in {
-
-      Json.toJson(Fifth("")) must
-        be(Json.obj("satisfaction" -> "05", "details" -> ""))
-
-      Json.toJson(Fifth("123")) must
-        be(Json.obj(
-          "satisfaction" -> "05",
-          "details" -> "123"
-        ))
-    }
   }
 
 
