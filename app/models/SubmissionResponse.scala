2d6e50d4f7be916b15b602e8f26bc6f192284f09
adam-coles <adam.coles@digital.hmrc.gov.uk>
2016-11-24 11:53:19 +0000

    fixed incorrect fPFee field name

--- app/models/SubmissionResponse.scala
+++ app/models/SubmissionResponse.scala
@@ -3,7 +3,7 @@ package models
 trait SubmissionResponse {
   val etmpFormBundleNumber: String
   val registrationFee: BigDecimal
-  val fpFee: Option[BigDecimal]
+  val fPFee: Option[BigDecimal]
   val premiseFee: BigDecimal
   val totalFees: BigDecimal
 }
