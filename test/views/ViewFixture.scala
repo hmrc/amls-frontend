f770976efbc590e16759abc4ccf3cb94400b3bdf
adam-coles <adam.coles@digital.hmrc.gov.uk>
2016-12-21 14:00:19 +0000

    added view tests for experience_training

---
+++ test/views/ViewFixture.scala
@@ -0,0 +1,17 @@
+package views
+
+import org.jsoup.Jsoup
+import play.api.mvc.Request
+import play.api.test.FakeRequest
+import play.twirl.api.HtmlFormat
+
+trait ViewFixture {
+  implicit val request : Request[_] = FakeRequest()
+
+  def view: HtmlFormat.Appendable
+  lazy val html = view.body
+  lazy val doc = Jsoup.parse(html)
+  lazy val form = doc.getElementsByTag("form").first()
+  lazy val heading = doc.getElementsByTag("h1").first()
+  lazy val errorSummary = doc.getElementsByClass("amls-error-summary").first()
+}
