9fc83935075e54b9cd97054778388a363285d5e3
Joe Simmonds <joe.simmonds@digital.hmrc.gov.uk>
2016-12-28 16:33:34 +0000

    AMLS-2066 added content tests for asp-servcies of business

---
+++ test/views/asp/services_of_businessSpec.scala
@@ -0,0 +1,62 @@
+package views.asp
+
+import forms.{InvalidForm, ValidForm, Form2}
+import models.asp._
+import org.scalatest.{MustMatchers, WordSpec}
+import org.scalatestplus.play.OneAppPerSuite
+import play.api.data.mapping.Path
+import play.api.data.validation.ValidationError
+import play.api.i18n.Messages
+import views.ViewFixture
+
+
+class services_of_businessSpec extends WordSpec with MustMatchers with OneAppPerSuite {
+
+  "services_of_business view" must {
+    "have correct title" in new ViewFixture {
+
+      val form2: ValidForm[ServicesOfBusiness] = Form2(ServicesOfBusiness(
+                                                    Set(Accountancy,
+                                                      PayrollServices,
+                                                      BookKeeping,
+                                                      Auditing,
+                                                      FinancialOrTaxAdvice)))
+
+      def view = views.html.asp.services_of_business(form2, true)
+
+      doc.title must startWith(Messages("asp.services.title") + " - " + Messages("summary.asp"))
+    }
+
+    "have correct headings" in new ViewFixture {
+
+      val form2: ValidForm[ServicesOfBusiness] = Form2(ServicesOfBusiness(
+                                                    Set(Accountancy,
+                                                      PayrollServices,
+                                                      BookKeeping,
+                                                      Auditing,
+                                                      FinancialOrTaxAdvice)))
+
+      def view = views.html.asp.services_of_business(form2, true)
+
+      heading.html must be(Messages("asp.services.title"))
+      subHeading.html must include(Messages("summary.asp"))
+
+    }
+
+    "show errors in the correct locations" in new ViewFixture {
+
+      val form2: InvalidForm = InvalidForm(Map.empty,
+        Seq(
+          (Path \ "services") -> Seq(ValidationError("not a message Key"))
+        ))
+
+      def view = views.html.asp.services_of_business(form2, true)
+
+      errorSummary.html() must include("not a message Key")
+
+      doc.getElementById("services")
+        .getElementsByClass("error-notification").first().html() must include("not a message Key")
+
+    }
+  }
+}
