6af59b7407911c1a281fb8fb5e6309357a6172ee
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2016-12-29 16:44:52 +0000

    AMLS-2065: tidy validation tests - contacting you

--- test/models/aboutthebusiness/ContactingYouSpec.scala
+++ test/models/aboutthebusiness/ContactingYouSpec.scala
@@ -2,11 +2,25 @@ package models.aboutthebusiness
 
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
-import play.api.data.mapping.{Path, Failure, Success}
+import play.api.data.mapping.Success
 import play.api.libs.json.Json
 
 class ContactingYouSpec extends PlaySpec with MockitoSugar {
   "Contacting You Form Details" must {
+
+    "successfully validate" when {
+      "given a phone number, email and letterToThisAddress" in {
+
+        val data = Map(
+          "phoneNumber" -> Seq("1234567890"),
+          "email" -> Seq("test@test.com"),
+          "letterToThisAddress" -> Seq("true")
+        )
+
+        ContactingYouForm.formRule.validate(data) must
+          be(Success(ContactingYouForm("1234567890", "test@test.com", true)))
+      }
+    }
 
     val completeJson = Json.obj(
       "phoneNumber" -> "1234567890",
@@ -16,36 +30,12 @@ class ContactingYouSpec extends PlaySpec with MockitoSugar {
 
     val completeModel = ContactingYouForm("1234567890", "test@test.com", true)
 
-    "successfully validate given a phone number, email and letterToThisAddress" in {
-
-      val data = Map(
-        "phoneNumber" -> Seq("1234567890"),
-        "email" -> Seq("test@test.com"),
-        "letterToThisAddress" -> Seq("true")
-      )
-
-      ContactingYouForm.formRule.validate(data) must
-        be(Success(ContactingYouForm("1234567890", "test@test.com", true)))
-    }
-
-    "successfully validate given a phone number, email, website address and letterToThisAddress " in {
-
-      val data = Map(
-        "phoneNumber" -> Seq("1234567890"),
-        "email" -> Seq("test@test.com"),
-        "letterToThisAddress" -> Seq("true")
-      )
-
-      ContactingYouForm.formRule.validate(data) must
-        be(Success(ContactingYouForm("1234567890", "test@test.com", true)))
-    }
-
     "serialize as expected" in {
       Json.toJson(completeModel) must be(completeJson)
     }
 
     "deserialize as expected" in {
-     completeJson.as[ContactingYouForm] must be(completeModel)
+      completeJson.as[ContactingYouForm] must be(completeModel)
     }
 
     "write correct data" in {
