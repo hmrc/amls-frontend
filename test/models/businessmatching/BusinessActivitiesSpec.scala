aec4e7122aecaeef0dcfe87cebcd1cf16bbb2b30
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2016-12-30 15:37:41 +0000

    AMLS-2071: tidy validation tests - register services

--- test/models/businessmatching/BusinessActivitiesSpec.scala
+++ test/models/businessmatching/BusinessActivitiesSpec.scala
@@ -2,7 +2,7 @@ package models.businessmatching
 
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
-import play.api.data.mapping.{Path, Failure, Success}
+import play.api.data.mapping.{Failure, Path, Success}
 import play.api.data.validation.ValidationError
 import play.api.libs.json._
 
@@ -10,52 +10,51 @@ import play.api.libs.json._
 class BusinessActivitiesSpec extends PlaySpec with MockitoSugar {
 
   "BusinessActivitiesSpec" must {
-    import play.api.data.mapping.forms.Rules._
+    "successfully validate" when {
+      "a few check boxes are selected" in {
 
-    "validate model with few check box selected" in {
+        val model1 = Map("businessActivities[]" -> Seq("03", "01", "02"))
 
-      val model = Map("businessActivities[]" -> Seq("03", "01", "02"))
-      val ba = BusinessActivities(Set(EstateAgentBusinessService, AccountancyServices, BillPaymentServices))
+        BusinessActivities.formReads.validate(model1) must
+          be(Success(BusinessActivities(Set(EstateAgentBusinessService, AccountancyServices, BillPaymentServices))))
 
-      BusinessActivities.formReads.validate(model) must be(Success(ba))
+        val model2 = Map("businessActivities[]" -> Seq("04", "05", "06"))
 
-      val model1 = Map("businessActivities[]" -> Seq("04", "05", "06"))
-      val ba1 = BusinessActivities(Set(HighValueDealing, MoneyServiceBusiness, TrustAndCompanyServices))
+        BusinessActivities.formReads.validate(model2) must
+          be(Success(BusinessActivities(Set(HighValueDealing, MoneyServiceBusiness, TrustAndCompanyServices))))
 
-      BusinessActivities.formReads.validate(model1) must be(Success(ba1))
+        BusinessActivities.formReads.validate(Map("businessActivities[]" -> Seq("07"))) must
+          be(Success(BusinessActivities(Set(TelephonePaymentService))))
+      }
 
-      BusinessActivities.formReads.validate(Map("businessActivities[]" -> Seq("07"))) must be(Success(BusinessActivities(Set(TelephonePaymentService))))
+      "residential business activity check box is selected" in {
+        val model = Map("businessActivities" -> Seq("07"))
 
+        BusinessActivities.formReads.validate(model) must
+          be(Success(BusinessActivities(Set(TelephonePaymentService))))
+      }
     }
 
-    "validate model with residential business activity check box selected" in {
-      val model = Map(
-        "businessActivities" -> Seq("07")
-      )
+    "fail validation" when {
+      "given missing data represented by an empty Map" in {
 
-      BusinessActivities.formReads.validate(model) must
-      be(Success(BusinessActivities(Set(TelephonePaymentService))))
-    }
+        BusinessActivities.formReads.validate(Map.empty) must
+          be(Failure(Seq((Path \ "businessActivities") -> Seq(ValidationError("error.required.bm.register.service")))))
+      }
 
-    "fail to validate on empty Map" in {
+      "given invalid data" in {
 
-      BusinessActivities.formReads.validate(Map.empty) must
-        be(Failure(Seq((Path \ "businessActivities") -> Seq(ValidationError("error.required.bm.register.service")))))
-    }
+        val model = Map("businessActivities[]" -> Seq("01", "99", "03"))
 
-    "fail to validate when given invalid data" in {
-      val model = Map(
-        "businessActivities[]" -> Seq("01", "99", "03")
-      )
-      val ba = BusinessActivities.formReads.validate(model)
-      ba must
-        be(Failure(Seq((Path \ "businessActivities" \ 1 \ "businessActivities") -> Seq(ValidationError("error.invalid")))))
+        BusinessActivities.formReads.validate(model) must
+          be(Failure(Seq((Path \ "businessActivities" \ 1 \ "businessActivities") -> Seq(ValidationError("error.invalid")))))
+      }
     }
 
     "write correct data for businessActivities value" in {
 
       BusinessActivities.formWrites.writes(BusinessActivities(Set(EstateAgentBusinessService, BillPaymentServices, MoneyServiceBusiness))) must
-        be(Map("businessActivities[]" -> Seq("03","02", "05")))
+        be(Map("businessActivities[]" -> Seq("03", "02", "05")))
     }
 
     "write correct data for businessActivities value on checkbox selected" in {
@@ -73,35 +72,35 @@ class BusinessActivitiesSpec extends PlaySpec with MockitoSugar {
     "get the value for each activity type" in {
       val ba = BusinessActivities(Set(EstateAgentBusinessService, AccountancyServices, HighValueDealing,
         MoneyServiceBusiness, TrustAndCompanyServices, TelephonePaymentService))
-      ba.getValue(EstateAgentBusinessService) must be ("03")
-      ba.getValue(AccountancyServices) must be ("01")
-      ba.getValue(HighValueDealing) must be ("04")
-      ba.getValue(MoneyServiceBusiness) must be ("05")
-      ba.getValue(TrustAndCompanyServices) must be ("06")
-      ba.getValue(TelephonePaymentService) must be ("07")
+      ba.getValue(EstateAgentBusinessService) must be("03")
+      ba.getValue(AccountancyServices) must be("01")
+      ba.getValue(HighValueDealing) must be("04")
+      ba.getValue(MoneyServiceBusiness) must be("05")
+      ba.getValue(TrustAndCompanyServices) must be("06")
+      ba.getValue(TelephonePaymentService) must be("07")
 
     }
 
     "get the message for each activity type" in {
-      AccountancyServices.getMessage must be ("businessmatching.registerservices.servicename.lbl.01")
-      BillPaymentServices.getMessage must be ("businessmatching.registerservices.servicename.lbl.02")
-      EstateAgentBusinessService.getMessage must be ("businessmatching.registerservices.servicename.lbl.03")
-      HighValueDealing.getMessage must be ("businessmatching.registerservices.servicename.lbl.04")
-      MoneyServiceBusiness.getMessage must be ("businessmatching.registerservices.servicename.lbl.05")
-      TrustAndCompanyServices.getMessage must be ("businessmatching.registerservices.servicename.lbl.06")
-      TelephonePaymentService.getMessage must be ("businessmatching.registerservices.servicename.lbl.07")
+      AccountancyServices.getMessage must be("businessmatching.registerservices.servicename.lbl.01")
+      BillPaymentServices.getMessage must be("businessmatching.registerservices.servicename.lbl.02")
+      EstateAgentBusinessService.getMessage must be("businessmatching.registerservices.servicename.lbl.03")
+      HighValueDealing.getMessage must be("businessmatching.registerservices.servicename.lbl.04")
+      MoneyServiceBusiness.getMessage must be("businessmatching.registerservices.servicename.lbl.05")
+      TrustAndCompanyServices.getMessage must be("businessmatching.registerservices.servicename.lbl.06")
+      TelephonePaymentService.getMessage must be("businessmatching.registerservices.servicename.lbl.07")
 
     }
 
     "JSON validation" must {
 
       "successfully validate given an enum value" in {
-        val json =  Json.obj("businessActivities" -> Seq("05","06", "07"))
+        val json = Json.obj("businessActivities" -> Seq("05", "06", "07"))
 
         Json.fromJson[BusinessActivities](json) must
           be(JsSuccess(BusinessActivities(Set(MoneyServiceBusiness, TrustAndCompanyServices, TelephonePaymentService)), JsPath \ "businessActivities"))
 
-        Json.fromJson[BusinessActivities](Json.obj("businessActivities" -> Seq("01","02", "03"))) must
+        Json.fromJson[BusinessActivities](Json.obj("businessActivities" -> Seq("01", "02", "03"))) must
           be(JsSuccess(BusinessActivities(Set(AccountancyServices, BillPaymentServices, EstateAgentBusinessService)), JsPath \ "businessActivities"))
 
         Json.fromJson[BusinessActivities](Json.obj("businessActivities" -> Seq("04"))) must
@@ -121,14 +120,14 @@ class BusinessActivitiesSpec extends PlaySpec with MockitoSugar {
     }
 
     "successfully validate json write" in {
-      val json = Json.obj("businessActivities" -> Seq("02","07", "01"))
+      val json = Json.obj("businessActivities" -> Seq("02", "07", "01"))
       Json.toJson(BusinessActivities(Set(BillPaymentServices, TelephonePaymentService, AccountancyServices))) must be(json)
 
     }
 
     "throw error for invalid data" in {
       Json.fromJson[BusinessActivities](Json.obj("businessActivities" -> Seq(JsString("20")))) must
-        be(JsError((JsPath \ "businessActivities")(0) \"businessActivities" , ValidationError("error.invalid")))
+        be(JsError((JsPath \ "businessActivities") (0) \ "businessActivities", ValidationError("error.invalid")))
     }
   }
 }
