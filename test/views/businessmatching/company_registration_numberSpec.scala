e599006de74671d0242ffe65a167e5cbe16f638e
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2017-01-03 16:38:08 +0000

    AMLS-2071: view spec - company registration number

---
+++ test/views/businessmatching/company_registration_numberSpec.scala
@@ -0,0 +1,53 @@
+package views.businessmatching
+
+import forms.{Form2, InvalidForm, ValidForm}
+import models.businessmatching.CompanyRegistrationNumber
+import org.scalatest.{MustMatchers, WordSpec}
+import org.scalatestplus.play.OneAppPerSuite
+import play.api.data.mapping.Path
+import play.api.data.validation.ValidationError
+import play.api.i18n.Messages
+import views.ViewFixture
+
+
+class company_registration_numberSpec extends WordSpec with MustMatchers with OneAppPerSuite {
+
+  "company_registration_number view" must {
+    "have correct title" in new ViewFixture {
+
+      val form2: ValidForm[CompanyRegistrationNumber] = Form2(CompanyRegistrationNumber("12345678"))
+
+      def view = views.html.businessmatching.company_registration_number(form2, true)
+
+      doc.title must startWith(Messages("businessmatching.registrationnumber.title") + " - " + Messages("summary.businessmatching"))
+    }
+
+    "have correct headings" in new ViewFixture {
+
+      val form2: ValidForm[CompanyRegistrationNumber] = Form2(CompanyRegistrationNumber("12345678"))
+
+      def view = views.html.businessmatching.company_registration_number(form2, true)
+
+      heading.html must be(Messages("businessmatching.registrationnumber.title"))
+      subHeading.html must include(Messages("summary.businessmatching"))
+
+    }
+
+    "show errors in the correct locations" in new ViewFixture {
+
+      val form2: InvalidForm = InvalidForm(Map.empty,
+        Seq(
+          (Path \ "companyRegistrationNumber") -> Seq(ValidationError("not a message Key"))
+        ))
+
+      def view = views.html.businessmatching.company_registration_number(form2, true)
+
+      errorSummary.html() must include("not a message Key")
+
+      doc.getElementById("companyRegistrationNumber")
+        .parent
+        .getElementsByClass("error-notification").first().html() must include("not a message Key")
+
+    }
+  }
+}
