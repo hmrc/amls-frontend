96fc1554930c26c878c8c24f2569896a13a50c3f
lorenzo.mugnai <lorenzo.mugnai@hmrc.gsi.gov.uk>
2017-01-10 11:52:02 +0000

    AMLS-1627 added changeofdate to model

--- app/models/tradingpremises/AgentName.scala
+++ app/models/tradingpremises/AgentName.scala
@@ -1,6 +1,7 @@
 package models.tradingpremises
 
 import models.FormTypes._
+import models.aboutthebusiness.DateOfChange
 import play.api.data.mapping._
 import play.api.data.mapping.forms.Rules._
 import play.api.libs.json._
@@ -10,7 +11,9 @@ import play.api.libs.json._
 import typeclasses.MongoKey
 import utils.{JsonMapping, TraversableValidators}
 
-case class AgentName(agentName: String)
+case class AgentName(agentName: String,
+                     dateOfChange: Option[DateOfChange] = None
+                    )
 
 object AgentName {
 
@@ -28,11 +31,14 @@ object AgentName {
 
   implicit val formReads: Rule[UrlFormEncoded, AgentName] = From[UrlFormEncoded] { __ =>
     import play.api.data.mapping.forms.Rules._
-    (__ \ "agentName").read(agentNameType) fmap AgentName.apply
+    (
+      (__ \ "agentName").read(agentNameType) and
+      (__ ).read(registeredOfficeDateOfChangeRule compose dateOfChangeMapping)
+      )(AgentName.apply _)
   }
 
   implicit val formWrites: Write[AgentName, UrlFormEncoded] = Write {
-    case AgentName(crn) => Map("agentName" -> Seq(crn))
+    case AgentName(crn, _) => Map("agentName" -> Seq(crn))
   }
 
   implicit def convert(data: AgentName): Option[TradingPremises] = {
--- test/models/tradingpremises/AgentNameSpec.scala
+++ test/models/tradingpremises/AgentNameSpec.scala
@@ -1,5 +1,7 @@
 package models.tradingpremises
 
+import models.aboutthebusiness.DateOfChange
+import org.joda.time.LocalDate
 import org.scalatestplus.play.PlaySpec
 import play.api.data.mapping.{Failure, Path, Success}
 import play.api.data.validation.ValidationError
@@ -10,8 +12,15 @@ class AgentNameSpec extends PlaySpec {
   "AgentName" must {
 
     "validate form Read" in {
-      val formInput = Map("agentName" -> Seq("sometext"))
-      AgentName.formReads.validate(formInput) must be(Success(AgentName("sometext")))
+      val formInput = Map(
+        "agentName" -> Seq("sometext"),
+        "dateOfChange.day" -> Seq("12"),
+        "dateOfChange.month" -> Seq("1"),
+        "dateOfChange.year" -> Seq("2016"),
+        "activityStartDate" -> Seq(new LocalDate(2016,1,1).toString("yyyy-MM-dd"))
+      )
+
+      AgentName.formReads.validate(formInput) must be(Success(AgentName("sometext", Some(DateOfChange(new LocalDate(2016, 1, 12))))))
     }
 
     "throw error when required field is missing" in {
@@ -28,13 +37,30 @@ class AgentNameSpec extends PlaySpec {
       AgentName.formWrites.writes(AgentName("sometext")) must be(Map("agentName" -> Seq("sometext")))
     }
 
+    "given a dateOfChange before business activity start date" in {
+
+      val data = Map(
+        "agentName" -> Seq("sometext"),
+        "dateOfChange.day" -> Seq("12"),
+        "dateOfChange.month" -> Seq("01"),
+        "dateOfChange.year" -> Seq("2016"),
+        "activityStartDate" -> Seq("2017-01-01")
+      )
+      AgentName.formReads.validate(data) must
+        be(Failure(Seq(
+          (Path \ "dateOfChange") -> Seq(ValidationError("error.expected.regofficedateofchange.date.after.activitystartdate", "01-01-2017"))
+        )))
+    }
 
   }
 
   "Json Validation" must {
     "Successfully read/write Json data" in {
       AgentName.format.reads(AgentName.format.writes(
-        AgentName("test"))) must be(JsSuccess(AgentName("test"), JsPath \ "agentName"))
+        AgentName("test", Some(DateOfChange(new LocalDate(2017,1,1)))))) must be(
+        JsSuccess(
+          AgentName("test", Some(DateOfChange(new LocalDate(2017,1,1)))),
+          JsPath \ "agentName"))
     }
 
   }
