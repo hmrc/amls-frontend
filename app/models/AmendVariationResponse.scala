ecae431655009dcc29c9e8050c855360761d3797
adam-coles <adam.coles@digital.hmrc.gov.uk>
2017-01-13 17:21:17 +0000

    not working yet

--- app/models/AmendVariationResponse.scala
+++ app/models/AmendVariationResponse.scala
@@ -1,12 +1,13 @@
 package models
 
-import play.api.libs.json.Json
+import models.supervision.{AnotherBody, ProfessionalBody, ProfessionalBodyMember}
+import play.api.libs.json.{Json, Reads}
 
 case class AmendVariationResponse (
                                     processingDate: String,
                                     etmpFormBundleNumber: String,
                                     registrationFee: BigDecimal,
-                                    fPFee: Option[BigDecimal],
+                                    fpFee: Option[BigDecimal],
                                     premiseFee: BigDecimal,
                                     totalFees: BigDecimal,
                                     paymentReference: Option[String],
@@ -22,4 +23,24 @@ object AmendVariationResponse {
 
   val key = "AmendVariationResponse"
   implicit val format = Json.format[AmendVariationResponse]
+
+  implicit val reads: Reads[AmendVariationResponse] = {
+    import play.api.libs.functional.syntax._
+    import play.api.libs.json._
+    (
+      (__ \ "processingDate").read[String] and
+        (__ \ "etmpFormBundleNumber").read[String] and
+        (__ \ "registrationFee").read[BigDecimal] and
+        (__ \ "fpFee").read[BigDecimal].orElse((__ \ "fPFee").read[BigDecimal]).orElse(Reads.pure(None)) and
+        (__ \ "premiseFee").read[BigDecimal] and
+        (__ \ "totalFees").read[BigDecimal] and
+        (__ \ "paymentReference").readNullable[String] and
+        (__ \ "difference").readNullable[BigDecimal] and
+        (__ \ "addedResponsiblePeople").read[Int] and
+        (__ \ "addedResponsiblePeopleFitAndProper").read[Int] and
+        (__ \ "addedFullYearTradingPremises").read[Int] and
+        (__ \ "halfYearlyTradingPremises").read[Int] and
+        (__ \ "zeroRatedTradingPremises").read[Int]
+      ) apply AmendVariationResponse.apply _
+  }
 }
---
+++ test/models/AmendVariationResponseSpec.scala
@@ -0,0 +1,79 @@
+package models
+
+
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.{JsSuccess, Json}
+
+class AmendVariationResponseSpec extends PlaySpec {
+
+  val response = AmendVariationResponse("pdate", "12345", 115.0, Some(125.0), 0, 240.0, Some("ref"), None)
+
+  "AmendVariationResponse" must {
+
+    "Deserialize correctly where Fit and Proper Fee is fPFee" in {
+
+      val json =
+        """{
+  "processingDate" : "pdate",
+  "etmpFormBundleNumber" : "12345",
+  "registrationFee" : 115.0,
+  "fPFee" : 125.0,
+  "premiseFee" : 0,
+  "totalFees" : 240.0,
+  "paymentReference" : "ref",
+  "addedResponsiblePeople" : 0,
+  "addedResponsiblePeopleFitAndProper" : 0,
+  "addedFullYearTradingPremises" : 0,
+  "halfYearlyTradingPremises" : 0,
+  "zeroRatedTradingPremises" : 0
+}"""
+
+      AmendVariationResponse.reads.reads(Json.parse(json)) must be(JsSuccess(response))
+
+    }
+
+    "Deserialize correctly where Fit and Proper Fee is fpFee" in {
+
+      val json =
+        """{
+  "processingDate" : "pdate",
+  "etmpFormBundleNumber" : "12345",
+  "registrationFee" : 115.0,
+  "fpFee" : 125.0,
+  "premiseFee" : 0,
+  "totalFees" : 240.0,
+  "paymentReference" : "ref",
+  "addedResponsiblePeople" : 0,
+  "addedResponsiblePeopleFitAndProper" : 0,
+  "addedFullYearTradingPremises" : 0,
+  "halfYearlyTradingPremises" : 0,
+  "zeroRatedTradingPremises" : 0
+}"""
+
+      AmendVariationResponse.reads.reads(Json.parse(json)) must be(JsSuccess(response))
+
+    }
+
+    "Deserialize correctly where Fit and Proper Fee is not returned" in {
+
+      val json =
+        """{
+  "processingDate" : "pdate",
+  "etmpFormBundleNumber" : "12345",
+  "registrationFee" : 115.0,
+  "premiseFee" : 0,
+  "totalFees" : 240.0,
+  "paymentReference" : "ref",
+  "addedResponsiblePeople" : 0,
+  "addedResponsiblePeopleFitAndProper" : 0,
+  "addedFullYearTradingPremises" : 0,
+  "halfYearlyTradingPremises" : 0,
+  "zeroRatedTradingPremises" : 0
+}"""
+
+      AmendVariationResponse.reads.reads(Json.parse(json)) must be(JsSuccess(response.copy(fpFee = None)))
+
+    }
+  }
+
+}
