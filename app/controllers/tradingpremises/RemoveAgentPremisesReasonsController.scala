742f258ed99adbc8e164bc43d4bf7950686bd8f6
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-02-10 12:07:52 +0000

    Fixed up remove agent controller tests

--- app/controllers/tradingpremises/RemoveAgentPremisesReasonsController.scala
+++ app/controllers/tradingpremises/RemoveAgentPremisesReasonsController.scala
@@ -3,29 +3,22 @@ package controllers.tradingpremises
 import config.AMLSAuthConnector
 import connectors.DataCacheConnector
 import controllers.BaseController
-import controllers.hvd.routes
-import forms.{EmptyForm, Form2, InvalidForm, ValidForm}
-import models.hvd.{Alcohol, Hvd, Products, Tobacco}
-import models.status.SubmissionDecisionApproved
+import forms.EmptyForm
 import models.tradingpremises.TradingPremises
-import services.{AuthEnrolmentsService, StatusService}
+import services.StatusService
 import utils.RepeatingSection
-import views.html.tradingpremises.remove_agent_premises_reasons
-
-import scala.concurrent.Future
-
 
 trait RemoveAgentPremisesReasonsController extends RepeatingSection with BaseController {
 
   val dataCacheConnector: DataCacheConnector
+
   private[controllers] def statusService: StatusService
-  //private[controllers] def authEnrolmentsService: AuthEnrolmentsService
 
   def get(index: Int, complete: Boolean = false) = Authorised.async {
     implicit authContext => implicit request =>
       for {
         tp <- getData[TradingPremises](index)
-      } yield (tp) match {
+      } yield tp match {
         case (Some(tradingPremises)) => {
           Ok(views.html.tradingpremises.remove_agent_premises_reasons(EmptyForm, index, complete,
             tp.yourTradingPremises.fold("")(_.tradingName)))
@@ -34,30 +27,13 @@ trait RemoveAgentPremisesReasonsController extends RepeatingSection with BaseCon
       }
   }
 
-  def post(index: Boolean = false, complete: Boolean = false, tradingName: String) =
+  def post(index: Int, complete: Boolean = false) =
     Authorised.async {
       implicit authContext => implicit request =>
-        Form2[Products](request.body) match {
-          case f: InvalidForm =>
-            ??? //Future.successful(BadRequest(products(f, edit)))
-          case ValidForm(_, data) => {
-            for {
-              hvd <- dataCacheConnector.fetch[Hvd](Hvd.key)
-              status <- statusService.getStatus
-              _ <- dataCacheConnector.save[Hvd](Hvd.key,
-                hvd.products(data)
-              )
-            } yield status match {
-              case SubmissionDecisionApproved =>
-                ???
-              case _ => ???
-              }
-            }
-          }
-        }
+        ???
     }
-}
 
+}
 
 
 object RemoveAgentPremisesReasonsController extends RemoveAgentPremisesReasonsController {
--- test/controllers/tradingpremises/RemoveAgentPremisesReasonsControllerSpec.scala
+++ test/controllers/tradingpremises/RemoveAgentPremisesReasonsControllerSpec.scala
@@ -1,47 +1,62 @@
 package controllers.tradingpremises
 
-import models.hvd.Hvd
-import models.tradingpremises.TradingPremises
+import connectors.DataCacheConnector
+import models.tradingpremises.{TradingPremises, YourTradingPremises}
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
-import play.api.i18n.Messages
 import play.api.test.Helpers._
-import utils.GenericTestHelper
+import services.StatusService
+import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
+import utils.{AuthorisedFixture, GenericTestHelper}
 
 import scala.concurrent.Future
 
-/**
-  * Created by lorenzo on 09/02/17.
-  */
 class RemoveAgentPremisesReasonsControllerSpec extends GenericTestHelper with MockitoSugar{
 
-  import connectors.DataCacheConnector
-  import services.StatusService
-  import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
-  import utils.AuthorisedFixture
-
   trait Fixture extends AuthorisedFixture {
-    self => val request = addToken(authRequest)
+    self =>
+
+    implicit val request = addToken(authRequest)
 
     val controller = new RemoveAgentPremisesReasonsController {
-
       override val dataCacheConnector: DataCacheConnector = mock[DataCacheConnector]
       override protected def authConnector: AuthConnector = self.authConnector
       override val statusService: StatusService = mock[StatusService]
     }
+
+    val tradingPremises = mock[TradingPremises]
+    val yourTradingPremises = mock[YourTradingPremises]
+
+    when(tradingPremises.yourTradingPremises) thenReturn Some(yourTradingPremises)
+    when(yourTradingPremises.tradingName) thenReturn "My Company"
+
+    def mockFetch(model: Option[Seq[TradingPremises]]) =
+      when(controller.dataCacheConnector.fetch[Seq[TradingPremises]](any())
+        (any(), any(), any())).thenReturn(Future.successful(model))
   }
 
-  "Remove agent premises reasons controller" must {
-    "load the 'Why are you removing this trading premises?' page" in new Fixture  {
+  "Remove agent premises reasons controller" when {
 
-      when(controller.dataCacheConnector.fetch[TradingPremises](any())
-        (any(), any(), any())).thenReturn(Future.successful(None))
+    "invoking the GET action" must {
 
-      val result = controller.get()(request)
-      status(result) must be(OK)
-      contentAsString(result) must include(Messages("hvd.products.title"))
+      "load the 'Why are you removing this trading premises?' page" in new Fixture {
 
+        mockFetch(Some(Seq(tradingPremises)))
+
+        val result = controller.get(1)(request)
+        status(result) must be(OK)
+
+      }
+
+      "return NOT FOUND when there is no trading premises found" in new Fixture {
+
+        mockFetch(None)
+
+        val result = controller.get(1)(request)
+        status(result) must be(NOT_FOUND)
+
+      }
     }
   }
 
