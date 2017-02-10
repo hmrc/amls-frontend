71ae1e8152cdc9a3ff8347696eacd0ffc86c7518
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-02-10 15:10:43 +0000

    Added form model + tests

---
+++ app/models/tradingpremises/AgentRemovalReason.scala
@@ -0,0 +1,39 @@
+package models.tradingpremises
+
+import jto.validation.forms.Rules._
+import jto.validation.forms.UrlFormEncoded
+import jto.validation.{From, Rule, Write}
+import models.FormTypes._
+import play.api.libs.json.Json
+
+case class AgentRemovalReason(removalReason: String, removalReasonOther: Option[String] = None)
+
+object AgentRemovalReason {
+
+  import utils.MappingUtils.Implicits._
+
+  implicit val formats = Json.format[AgentRemovalReason]
+
+  val otherDetailsLength = 255
+  val otherDetailsType = notEmptyStrip andThen notEmpty andThen maxLength(otherDetailsLength).
+    withMessage("error.invalid.hvd.business.sell.other.details")
+
+  implicit val formReader: Rule[UrlFormEncoded, AgentRemovalReason] = From[UrlFormEncoded] { __ =>
+    (__ \ "removalReason").read[String] flatMap { reason =>
+      if (reason == "Other") {
+        (__ \ "removalReasonOther").read(otherDetailsType) map { o =>
+          AgentRemovalReason(reason, Some(o))
+        }
+      } else
+        AgentRemovalReason(reason)
+    }
+  }
+
+  implicit val formWriter: Write[AgentRemovalReason, UrlFormEncoded] = Write { a =>
+    Map(
+      "removalReason" -> Seq(a.removalReason),
+      "removalReasonOther" -> Seq(a.removalReasonOther.getOrElse(""))
+    )
+  }
+
+}
---
+++ test/models/tradingpremises/AgentRemovalReasonSpec.scala
@@ -0,0 +1,117 @@
+package models.tradingpremises
+
+import jto.validation.{Invalid, Path, Valid, ValidationError}
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json
+
+
+class AgentRemovalReasonSpec extends PlaySpec {
+
+  "The AgentRemovalReason model" when {
+
+    "given a valid form" must {
+
+      "return the model" in {
+
+        val form = Map(
+          "removalReason" -> Seq("Other"),
+          "removalReasonOther" -> Seq("Some other reason")
+          )
+
+        val result = AgentRemovalReason.formReader.validate(form)
+
+        result must be(Valid(AgentRemovalReason("Other", Some("Some other reason"))))
+
+      }
+
+      "return the model" when {
+        "something other than 'Other' was given" in {
+
+          val form = Map(
+            "removalReason" -> Seq("Serious compliance failures")
+          )
+
+          val result = AgentRemovalReason.formReader.validate(form)
+
+          result must be(Valid(AgentRemovalReason("Serious compliance failures")))
+
+        }
+      }
+
+    }
+
+    "given an invalid form" must {
+
+      "fail validation" when {
+
+        "there is no removal reason specified" in {
+
+          val form = Map.empty[String, Seq[String]]
+
+          val result = AgentRemovalReason.formReader.validate(form)
+
+          result must be(Invalid(Seq((Path \ "removalReason", Seq(ValidationError("error.required"))))))
+
+        }
+
+        "there is no other removal reason when 'Other' is selected" in {
+
+          val form = Map("removalReason" -> Seq("Other"))
+
+          val result = AgentRemovalReason.formReader.validate(form)
+
+          result must be(Invalid(Seq(Path \ "removalReasonOther" -> Seq(ValidationError("error.required")))))
+
+        }
+
+      }
+
+    }
+
+    "given a valid model" must {
+
+      "return the form" in {
+
+        val model = AgentRemovalReason("Other", Some("Some other reason"))
+
+        val result = AgentRemovalReason.formWriter.writes(model)
+
+        result must be(Map(
+          "removalReason" -> Seq("Other"),
+          "removalReasonOther" -> Seq("Some other reason")
+        ))
+
+      }
+
+      "return the correct json" in {
+        val model = AgentRemovalReason("Other", Some("Some other reason"))
+
+        val json = Json.toJson(model) must be(Json.obj(
+          "removalReason" -> "Other",
+          "removalReasonOther" -> "Some other reason"
+        ))
+      }
+
+    }
+
+    "given valid json" must {
+
+      val json =
+        """
+          | {
+          | "removalReason": "Other",
+          | "removalReasonOther": "Some reason"
+          | }
+        """.stripMargin
+
+      "return the model" in {
+
+        Json.parse(json).asOpt[AgentRemovalReason] must be(Some(AgentRemovalReason("Other", Some("Some reason"))))
+
+      }
+
+    }
+
+  }
+
+}
