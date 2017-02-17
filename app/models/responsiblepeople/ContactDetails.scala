aedce925ff3228aeb6a76798bf4de273cf29315e
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-02-17 15:20:15 +0000

    Updated validation message for responsible people contact email address

--- app/models/responsiblepeople/ContactDetails.scala
+++ app/models/responsiblepeople/ContactDetails.scala
@@ -1,34 +1,32 @@
 package models.responsiblepeople
 
-import jto.validation.{To, Write, From, Rule}
 import jto.validation.forms._
+import jto.validation.{From, Rule, To, Write}
 import play.api.libs.json.Json
-import models.FormTypes._
 
 case class ContactDetails(phoneNumber: String, emailAddress: String)
 
 object ContactDetails {
 
-  import utils.MappingUtils.Implicits._
-
   implicit val formats = Json.format[ContactDetails]
 
   implicit val formReads: Rule[UrlFormEncoded, ContactDetails] = From[UrlFormEncoded] { __ =>
-    import models.FormTypes._
     import jto.validation.forms.Rules._
+    import models.FormTypes._
     (
       (__ \ "phoneNumber").read(phoneNumberType) ~
         (__ \ "emailAddress").read(emailType)
-    )(ContactDetails.apply _)
+    )(ContactDetails.apply)
   }
 
   implicit val formWrites: Write[ContactDetails, UrlFormEncoded] = To[UrlFormEncoded] { __ =>
     import jto.validation.forms.Writes._
+
     import scala.Function.unlift
     (
       (__ \ "phoneNumber").write[String] ~
         (__ \ "emailAddress").write[String]
-      ) (unlift(ContactDetails.unapply _))
+      ) (unlift(ContactDetails.unapply))
   }
 
 }
