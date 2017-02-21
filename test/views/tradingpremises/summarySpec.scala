8652d83ce75bf4af57ee5c43f785db8d86eee90f
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-02-21 14:21:19 +0000

    Fixed broken unit tests

--- test/views/tradingpremises/summarySpec.scala
+++ test/views/tradingpremises/summarySpec.scala
@@ -1,6 +1,6 @@
 package views.tradingpremises
 
-import models.status.SubmissionDecisionApproved
+import models.status.{SubmissionDecisionApproved, SubmissionReadyForReview}
 import models.tradingpremises.{Address, RegisteringAgentPremises, TradingPremises, YourTradingPremises}
 import org.joda.time.LocalDate
 import play.api.inject.guice.GuiceApplicationBuilder
@@ -12,6 +12,7 @@ trait ViewTestHelper extends GenericTestHelper {
   val tradingPremises = Seq(TradingPremises(
     registeringAgentPremises = Some(RegisteringAgentPremises(true)),
     status = Some(StatusConstants.Added),
+    lineId = Some(11),
     yourTradingPremises = Some(YourTradingPremises("Test", Address("Line 1", "Line 2", None, None, "TEST", None), true, LocalDate.now, None))
   ))
 
@@ -26,14 +27,14 @@ class summarySpec extends ViewTestHelper {
       .configure(Map("Test.microservice.services.feature-toggle.release7" -> false))
       .build()
 
-  "The summary page (release 6)" must {
+  "The summary page" must {
 
     "redirect to the 'remove trading premises' page when 'delete' is clicked" in new ViewFixture {
 
-      def view = views.html.tradingpremises.summary(tradingPremises, true, SubmissionDecisionApproved)
+      def view = views.html.tradingpremises.summary(tradingPremises, add = true, SubmissionDecisionApproved)
 
       doc.getElementsByClass("check-your-answers__listing").select("a:nth-child(2)").attr("href") must be(
-        controllers.tradingpremises.routes.RemoveTradingPremisesController.get(1, true).url
+        controllers.tradingpremises.routes.RemoveTradingPremisesController.get(1, complete = true).url
       )
     }
   }
@@ -46,14 +47,22 @@ class summarySpecRelease7 extends ViewTestHelper {
     .configure(Map("Test.microservice.services.feature-toggle.release7" -> true))
     .build()
 
-  "The summary page (release 6)" must {
+  "The summary page" must {
 
-    "redirect to the 'remove trading premises' page when 'delete' is clicked" in new ViewFixture {
+    "redirect to the 'remove agent premises reasons' page when 'delete' is clicked" in new ViewFixture {
 
-      def view = views.html.tradingpremises.summary(tradingPremises, true, SubmissionDecisionApproved)
+      def view = views.html.tradingpremises.summary(tradingPremises, add = true, SubmissionDecisionApproved)
 
       doc.getElementsByClass("check-your-answers__listing").select("a:nth-child(2)").attr("href") must be(
-        controllers.tradingpremises.routes.RemoveAgentPremisesReasonsController.get(1, true).url
+        controllers.tradingpremises.routes.RemoveAgentPremisesReasonsController.get(1, complete = true).url
+      )
+    }
+
+    "redirect to the 'remove trading premises' page when 'delete' is clicked and it's an agent premises and it's an amendment" in new ViewFixture {
+      def view = views.html.tradingpremises.summary(tradingPremises, add = true, SubmissionReadyForReview)
+
+      doc.getElementsByClass("check-your-answers__listing").select("a:nth-child(2)").attr("href") must be (
+        controllers.tradingpremises.routes.RemoveTradingPremisesController.get(1, complete = true).url
       )
     }
   }
