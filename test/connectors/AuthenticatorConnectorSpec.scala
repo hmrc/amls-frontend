1aac06a5a8ed2df78d512361c5405faa6fa1ef35
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-02-24 13:36:32 +0000

    Fixed broken tests by reworking how authenticator test injection is done

--- test/connectors/AuthenticatorConnectorSpec.scala
+++ test/connectors/AuthenticatorConnectorSpec.scala
@@ -2,13 +2,11 @@ package connectors
 
 import org.mockito.Matchers.{eq => eqTo, _}
 import org.mockito.Mockito._
-import org.mockito.internal.verification.Times
-import org.scalatest.BeforeAndAfter
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 import play.api.inject.bind
-import play.inject.guice.GuiceInjectorBuilder
+import play.api.inject.guice.GuiceApplicationBuilder
 import uk.gov.hmrc.play.config.ServicesConfig
 import uk.gov.hmrc.play.http.{HeaderCarrier, HttpPost, HttpResponse}
 
@@ -22,17 +20,17 @@ class AuthenticatorConnectorSpec extends PlaySpec with ScalaFutures with Mockito
     val http = mock[HttpPost]
     val serviceConfig = mock[ServicesConfig]
 
-    val defaultBuilder = new GuiceInjectorBuilder()
-      .bindings(bind[HttpPost].to(http))
-      .bindings(bind[ServicesConfig].to(serviceConfig))
-
-    val builder = defaultBuilder
-
-    lazy val injector = builder.build()
-
-    lazy val connector = injector.instanceOf(classOf[AuthenticatorConnector])
-
     implicit val hc = HeaderCarrier()
+
+    val featureToggleSetting: Boolean
+
+    lazy val app = new GuiceApplicationBuilder()
+      .disable[com.kenshoo.play.metrics.PlayModule]
+      .configure("Test.microservice.services.feature-toggle.refresh-profile" -> featureToggleSetting)
+      .overrides(bind[HttpPost].to(http))
+      .build()
+
+    lazy val connector = app.injector.instanceOf(classOf[AuthenticatorConnector])
 
     when(serviceConfig.baseUrl(any())) thenReturn "authenticator.service"
   }
@@ -41,7 +39,7 @@ class AuthenticatorConnectorSpec extends PlaySpec with ScalaFutures with Mockito
 
     "connect to the authenticator service to refresh the auth profile" in new TestFixture {
 
-      when(serviceConfig.getConfBool(any(), any())) thenReturn true
+      val featureToggleSetting = true
 
       when(http.POSTEmpty[HttpResponse](any())(any(), any())) thenReturn Future.successful(HttpResponse(200))
 
@@ -49,13 +47,13 @@ class AuthenticatorConnectorSpec extends PlaySpec with ScalaFutures with Mockito
 
       result.status must be(200)
 
-      verify(http).POSTEmpty(eqTo("authenticator.service/authenticator/refresh-profile"))(any(), any())
+      verify(http).POSTEmpty(eqTo("http://localhost:9905/authenticator/refresh-profile"))(any(), any())
 
     }
 
     "return a default successful result when the feature is toggled off" in new TestFixture {
 
-      when(serviceConfig.getConfBool(any(), any())) thenReturn false
+      val featureToggleSetting = false
 
       val result = Await.result(connector.refreshProfile, 5 seconds)
 
