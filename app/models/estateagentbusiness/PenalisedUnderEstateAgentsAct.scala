a24bcaffef377e8f11d179c7aa60f0663673d87b
lorenzo.mugnai <lorenzo.mugnai@hmrc.gsi.gov.uk>
2017-02-27 09:56:17 +0000

    AMLS-2164 moved validation to own model

--- app/models/estateagentbusiness/PenalisedUnderEstateAgentsAct.scala
+++ app/models/estateagentbusiness/PenalisedUnderEstateAgentsAct.scala
@@ -17,9 +17,13 @@ object PenalisedUnderEstateAgentsAct {
 
   import utils.MappingUtils.Implicits._
 
-
   implicit val formRule: Rule[UrlFormEncoded, PenalisedUnderEstateAgentsAct] = From[UrlFormEncoded] { __ =>
     import jto.validation.forms.Rules._
+
+    val penalisedMaxLength = 255
+    val penalisedLength = maxWithMsg(penalisedMaxLength, "error.invalid.eab.info.about.penalty")
+    val penalisedRequired = required("error.required.eab.info.about.penalty")
+    val penalisedType = penalisedRequired andThen penalisedLength andThen basicPunctuationPattern
 
     (__ \ "penalisedUnderEstateAgentsAct").read[Boolean].withMessage("error.required.eab.penalised.under.act") flatMap {
       case true => (__ \ "penalisedUnderEstateAgentsActDetails").read(penalisedType) map PenalisedUnderEstateAgentsActYes.apply
