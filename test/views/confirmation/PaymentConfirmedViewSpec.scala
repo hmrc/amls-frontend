4feb2c7236b0b277b9cf6425fceb485cd3b44ed9
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-03-09 11:30:32 +0000

    Added test for heading

--- test/views/confirmation/PaymentConfirmedViewSpec.scala
+++ test/views/confirmation/PaymentConfirmedViewSpec.scala
@@ -13,14 +13,21 @@ class PaymentConfirmedViewSpec extends GenericTestHelper with MustMatchers {
     val businessName = "Test Business Ltd"
     val paymentReference = "XMHSG357567686"
 
+    override def view = views.html.confirmation.payment_confirmation(businessName, paymentReference)
+
   }
 
   "The payment confirmation view" must {
 
     "show the correct title" in new ViewFixture {
-      override def view = views.html.confirmation.payment_confirmation(businessName, paymentReference)
 
       doc.title must startWith(Messages("confirmation.payment.title"))
+
+    }
+
+    "show the correct heading" in new ViewFixture {
+
+      heading.text must be(Messages("confirmation.payment.lede"))
 
     }
 
