99a839231733283117ec27e39471d04c4c935ec1
adam-coles <adam.coles@digital.hmrc.gov.uk>
2017-03-14 17:11:05 +0000

    added logging of business type from keystore

--- app/models/businesscustomer/ReviewDetails.scala
+++ app/models/businesscustomer/ReviewDetails.scala
@@ -2,19 +2,20 @@ package models.businesscustomer
 
 import models.businessmatching.BusinessType
 import models.businessmatching.BusinessType.{LPrLLP, LimitedCompany, Partnership, SoleProprietor, UnincorporatedBody}
+import play.api.Logger
 import play.api.libs.json.{Json, Reads, Writes}
 
 case class ReviewDetails(
-                        businessName: String,
-                        businessType: Option[BusinessType],
-                        businessAddress: Address,
-//                        sapNumber: String,
-                        safeId: String
-//                        isAGroup: Boolean,
-//                        directMatch: Boolean,
-//                        agentReferenceNumber: Option[String],
-//                        firstName: Option[String],
-//                        lastName: Option[String]
+                          businessName: String,
+                          businessType: Option[BusinessType],
+                          businessAddress: Address,
+                          //                        sapNumber: String,
+                          safeId: String
+                          //                        isAGroup: Boolean,
+                          //                        directMatch: Boolean,
+                          //                        agentReferenceNumber: Option[String],
+                          //                        firstName: Option[String],
+                          //                        lastName: Option[String]
                         )
 
 object ReviewDetails {
@@ -26,16 +27,20 @@ object ReviewDetails {
     (
       (__ \ "businessName").read[String] and
         (__ \ "businessType").readNullable[String].map[Option[BusinessType]] {
-          case Some("Sole Trader") => Some(SoleProprietor)
-          case Some("Corporate Body") => Some(LimitedCompany)
-          case Some("Partnership") => Some(Partnership)
-          case Some("LLP") => Some(LPrLLP)
-          case Some("Unincorporated Body") => Some(UnincorporatedBody)
-          case _ => None
-      } and
-      (__ \ "businessAddress").read[Address] and
-      (__ \ "safeId").read[String]
-    )(ReviewDetails.apply _)
+          thing =>
+            Logger.debug(s"[ReviewDetails - BusinessType: $thing}")
+            thing match {
+              case Some("Sole Trader") => Some(SoleProprietor)
+              case Some("Corporate Body") => Some(LimitedCompany)
+              case Some("Partnership") => Some(Partnership)
+              case Some("LLP") => Some(LPrLLP)
+              case Some("Unincorporated Body") => Some(UnincorporatedBody)
+              case _ => None
+            }
+        } and
+        (__ \ "businessAddress").read[Address] and
+        (__ \ "safeId").read[String]
+      ) (ReviewDetails.apply _)
   }
 
   implicit val writes = Json.writes[ReviewDetails]
