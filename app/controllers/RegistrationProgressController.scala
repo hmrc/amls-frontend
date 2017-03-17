5606770f8aabed3feca1c4f84b9c9964825263d8
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-03-17 13:49:25 +0000

    Separated templates for progress sections

--- app/controllers/RegistrationProgressController.scala
+++ app/controllers/RegistrationProgressController.scala
@@ -4,25 +4,26 @@ import config.AMLSAuthConnector
 import connectors.DataCacheConnector
 import models.businessmatching.BusinessMatching
 import models.registrationprogress.{Completed, Section}
-import models.responsiblepeople.{NominatedOfficer, ResponsiblePeople}
+import models.responsiblepeople.ResponsiblePeople
 import models.status.{NotCompleted, SubmissionReady, SubmissionReadyForReview, SubmissionStatus}
+import play.api.Logger
 import play.api.mvc.{Action, AnyContent, Request}
 import services.{AuthEnrolmentsService, ProgressService, StatusService}
+import uk.gov.hmrc.http.cache.client.CacheMap
+import uk.gov.hmrc.play.frontend.auth.AuthContext
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import uk.gov.hmrc.play.http.HeaderCarrier
-import uk.gov.hmrc.play.frontend.auth.AuthContext
+import utils.ControllerHelper
 import views.html.registrationamendment.registration_amendment
 import views.html.registrationprogress.registration_progress
-import uk.gov.hmrc.http.cache.client.CacheMap
-import utils.ControllerHelper
 
-import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 
 trait RegistrationProgressController extends BaseController {
 
-  protected[controllers] def service: ProgressService
+  protected[controllers] def progressService: ProgressService
 
   protected[controllers] def dataCache: DataCacheConnector
 
@@ -47,37 +48,22 @@ trait RegistrationProgressController extends BaseController {
   }
 
   def get() = Authorised.async {
-    implicit authContext =>
-      implicit request =>
-        if (AmendmentsToggle.feature) {
-          getWithAmendments
-        } else {
-          getWithoutAmendments
+    implicit authContext => implicit request =>
+        dataCache.fetchAll.flatMap {
+          _.map { cacheMap =>
+            val sections = progressService.sections(cacheMap)
+
+            preApplicationComplete(cacheMap) map {
+              case Some(x) => x match {
+                case true => Ok(registration_amendment(sections, amendmentDeclarationAvailable(sections)))
+                case _ => Ok(registration_progress(sections, declarationAvailable(sections)))
+              }
+              case None => Redirect(controllers.routes.LandingController.get())
+            }
+
+          }.getOrElse(Future.successful(Ok(registration_progress(Seq.empty[Section], false))))
         }
   }
-
-  private def getWithAmendments(implicit hc: HeaderCarrier, ac: AuthContext, r: Request[_]) = {
-    val x = dataCache.fetchAll
-    x.flatMap { cacheMapO =>
-      cacheMapO.map { cacheMap: CacheMap =>
-        val sections = service.sections(cacheMap)
-        preApplicationComplete(cacheMap) map {
-          case Some(x) => x match {
-            case true => Ok(registration_amendment(sections, amendmentDeclarationAvailable(sections)))
-            case false => Ok(registration_progress(sections, declarationAvailable(sections)))
-          }
-          case None => Redirect(controllers.routes.LandingController.get())
-        }
-      }.getOrElse(Future.successful(Ok(registration_progress(Seq.empty[Section], false))))
-    }
-  }
-
-  private def getWithoutAmendments(implicit hc: HeaderCarrier, ac: AuthContext, r: Request[_]) =
-    service.sections map {
-      sections => {
-        Ok(registration_progress(sections, declarationAvailable(sections)))
-      }
-    }
 
   private def preApplicationComplete(cache: CacheMap)(implicit hc: HeaderCarrier, ac: AuthContext): Future[Option[Boolean]] = {
     (for {
@@ -130,7 +116,7 @@ trait RegistrationProgressController extends BaseController {
 object RegistrationProgressController extends RegistrationProgressController {
   // $COVERAGE-OFF$
   override protected[controllers] val authConnector: AuthConnector = AMLSAuthConnector
-  override protected[controllers] val service = ProgressService
+  override protected[controllers] val progressService = ProgressService
   override protected[controllers] val dataCache = DataCacheConnector
   override protected[controllers] val enrolmentsService = AuthEnrolmentsService
   override protected[controllers] val statusService = StatusService
