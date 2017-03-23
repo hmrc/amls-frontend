cc3891dec3f0976332a858a0539c77d4f0814072
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2017-03-23 11:24:20 +0000

    Test on non release 7 redirects on timeAtAddressController

--- test/controllers/responsiblepeople/TimeAtAddressControllerNoRelease7Spec.scala
+++ test/controllers/responsiblepeople/TimeAtAddressControllerNoRelease7Spec.scala
@@ -1,26 +1,21 @@
 package controllers.responsiblepeople
 
-import config.AMLSAuthConnector
 import connectors.DataCacheConnector
-import models.Country
-import models.responsiblepeople.TimeAtAddress.{OneToThreeYears, SixToElevenMonths, ZeroToFiveMonths}
+import models.responsiblepeople.TimeAtAddress.{OneToThreeYears, ZeroToFiveMonths}
 import models.responsiblepeople._
-import models.status.{SubmissionDecisionApproved, SubmissionReadyForReview}
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
+import models.status.SubmissionDecisionApproved
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
-import  utils.GenericTestHelper
-import play.api.i18n.Messages
 import play.api.test.FakeApplication
 import play.api.test.Helpers._
 import services.StatusService
 import uk.gov.hmrc.http.cache.client.CacheMap
-import utils.AuthorisedFixture
+import utils.{AuthorisedFixture, GenericTestHelper}
 
 import scala.concurrent.Future
-class CurrentAddressControllerNoRelease7Spec extends GenericTestHelper with MockitoSugar {
+
+class TimeAtAddressControllerNoRelease7Spec extends GenericTestHelper with MockitoSugar {
 
   val mockDataCacheConnector = mock[DataCacheConnector]
   val recordId = 1
@@ -28,7 +23,7 @@ class CurrentAddressControllerNoRelease7Spec extends GenericTestHelper with Mock
   trait Fixture extends AuthorisedFixture {
     self => val request = addToken(authRequest)
 
-    val currentAddressController = new CurrentAddressController {
+    val timeAtAddressController = new TimeAtAddressController {
       override val dataCacheConnector = mockDataCacheConnector
       override val authConnector = self.authConnector
       override val statusService = mock[StatusService]
@@ -45,39 +40,32 @@ class CurrentAddressControllerNoRelease7Spec extends GenericTestHelper with Mock
         "redirect to the AdditionalAddressController" in new Fixture {
 
           val requestWithParams = request.withFormUrlEncodedBody(
-            "isUK" -> "true",
-            "addressLine1" -> "new Line 1",
-            "addressLine2" -> "new Line 2",
-            "postCode" -> "NE17YH"
+            "timeAtAddress" -> "01"
           )
           val ukAddress = PersonAddressUK("Line 1", "Line 2", Some("Line 3"), None, "NE17YH")
           val additionalAddress = ResponsiblePersonCurrentAddress(ukAddress, Some(ZeroToFiveMonths))
           val history = ResponsiblePersonAddressHistory(currentAddress = Some(additionalAddress))
           val responsiblePeople = ResponsiblePeople(addressHistory = Some(history), lineId = Some(1))
 
-
-          when(currentAddressController.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())(any(), any(), any()))
+          when(timeAtAddressController.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())(any(), any(), any()))
             .thenReturn(Future.successful(Some(Seq(responsiblePeople))))
-          when(currentAddressController.dataCacheConnector.save[PersonName](any(), any())(any(), any(), any()))
+          when(timeAtAddressController.dataCacheConnector.save[PersonName](any(), any())(any(), any(), any()))
             .thenReturn(Future.successful(emptyCache))
 
-          when(currentAddressController.statusService.getStatus(any(), any(), any()))
+          when(timeAtAddressController.statusService.getStatus(any(), any(), any()))
             .thenReturn(Future.successful(SubmissionDecisionApproved))
 
-          val result = currentAddressController.post(recordId, true)(requestWithParams)
+          val result = timeAtAddressController.post(recordId, true)(requestWithParams)
 
           status(result) must be(SEE_OTHER)
-          redirectLocation(result) must be(Some(routes.TimeAtAddressController.get(recordId, true).url))
+          redirectLocation(result) must be(Some(routes.AdditionalAddressController.get(recordId, true).url))
         }
       }
       "time at address is more than 1 year" must {
         "redirect to the correct location" in new Fixture {
 
           val requestWithParams = request.withFormUrlEncodedBody(
-            "isUK" -> "true",
-            "addressLine1" -> "new Line 1",
-            "addressLine2" -> "new Line 2",
-            "postCode" -> "NE17YH"
+            "timeAtAddress" -> "03"
           )
           val ukAddress = PersonAddressUK("Line 1", "Line 2", Some("Line 3"), None, "NE17YH")
           val additionalAddress = ResponsiblePersonCurrentAddress(ukAddress, Some(OneToThreeYears))
@@ -85,15 +73,15 @@ class CurrentAddressControllerNoRelease7Spec extends GenericTestHelper with Mock
           val responsiblePeople = ResponsiblePeople(addressHistory = Some(history), lineId = Some(1))
 
 
-          when(currentAddressController.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())(any(), any(), any()))
+          when(timeAtAddressController.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())(any(), any(), any()))
             .thenReturn(Future.successful(Some(Seq(responsiblePeople))))
-          when(currentAddressController.dataCacheConnector.save[PersonName](any(), any())(any(), any(), any()))
+          when(timeAtAddressController.dataCacheConnector.save[PersonName](any(), any())(any(), any(), any()))
             .thenReturn(Future.successful(emptyCache))
 
-          when(currentAddressController.statusService.getStatus(any(), any(), any()))
+          when(timeAtAddressController.statusService.getStatus(any(), any(), any()))
             .thenReturn(Future.successful(SubmissionDecisionApproved))
 
-          val result = currentAddressController.post(recordId, true)(requestWithParams)
+          val result = timeAtAddressController.post(recordId, true)(requestWithParams)
 
           status(result) must be(SEE_OTHER)
           redirectLocation(result) must be(Some(routes.DetailedAnswersController.get(recordId, true).url))
