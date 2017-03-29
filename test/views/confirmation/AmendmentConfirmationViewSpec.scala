dea9d74e080b53085cd1559bc848860f010cda58
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-03-29 16:21:01 +0100

    Variation confirmation tests + additional for amendments

--- test/views/confirmation/AmendmentConfirmationViewSpec.scala
+++ test/views/confirmation/AmendmentConfirmationViewSpec.scala
@@ -11,7 +11,9 @@ class AmendmentConfirmationViewSpec extends GenericTestHelper with MustMatchers 
   trait ViewFixture extends Fixture {
     implicit val requestWithToken = addToken(request)
 
-    override def view = views.html.confirmation.confirm_amendment("ref number", Currency(100), Seq.empty, None, "http://google.co.uk")
+    val continueHref = "http://google.co.uk"
+
+    override def view = views.html.confirmation.confirm_amendment("ref number", Currency(100), Seq.empty, Some(Currency(150)), continueHref)
   }
 
   "The amendment confirmation view" must {
@@ -25,11 +27,23 @@ class AmendmentConfirmationViewSpec extends GenericTestHelper with MustMatchers 
     }
 
     "show the correct secondary header" in new ViewFixture {
-      doc.select(".heading-secondary").text must include(Messages("confirmation.header.secondary"))
+      doc.select(".heading-secondary").text must include(Messages("confirmation.amendment.header.secondary"))
     }
 
     "show the correct informational paragraph" in new ViewFixture {
       doc.select(".info").text mustBe Messages("confirmation.amendment.info")
+    }
+
+    "show the correct fee" in new ViewFixture {
+      doc.select(".reg-online--pay-fee .heading-large").first.text mustBe "£150.00"
+    }
+
+    "show the correct reference" in new ViewFixture {
+      doc.select(".reg-online--pay-fee .heading-large").get(1).text mustBe "ref number"
+    }
+
+    "continue button has the right text" in new ViewFixture {
+      doc.select(s".button[href=$continueHref]").text mustBe Messages("button.continue")
     }
 
   }
