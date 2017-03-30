f264dee92f1de28c1b6ae2efc70791dfedd2de64
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2017-03-30 10:27:59 +0100

    AMLS-2503: adding blank controller for next page

---
+++ app/controllers/renewal/CashPaymentController.scala
@@ -0,0 +1,21 @@
+package controllers.renewal
+
+import javax.inject.{Inject, Singleton}
+
+import connectors.DataCacheConnector
+import controllers.BaseController
+import services.RenewalService
+import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
+
+@Singleton
+class InvolvedInOtherController @Inject()(
+                                           val dataCacheConnector: DataCacheConnector,
+                                           val authConnector: AuthConnector,
+                                           val renewalService: RenewalService
+                                         ) extends BaseController {
+
+  def get = ???
+
+  def post = ???
+
+}
