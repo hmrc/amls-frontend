9d5a8c77c3beb258df4d8c910be2cc08f7ea2993
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2017-04-05 08:24:09 +0100

    AMLS-2499: implementing get method

--- app/controllers/renewal/SendTheLargestAmountsOfMoneyController.scala
+++ app/controllers/renewal/SendTheLargestAmountsOfMoneyController.scala
@@ -2,28 +2,31 @@ package controllers.renewal
 
 import javax.inject.{Inject, Singleton}
 
-import config.AMLSAuthConnector
 import connectors.DataCacheConnector
 import controllers.BaseController
-import forms.{EmptyForm, Form2, InvalidForm, ValidForm}
-import models.moneyservicebusiness.{SendTheLargestAmountsOfMoney, MoneyServiceBusiness}
-import services.{RenewalService, StatusService}
+import forms.{EmptyForm, Form2}
+import models.renewal.{Renewal, SendTheLargestAmountsOfMoney}
+import services.RenewalService
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
-import utils.ControllerHelper
 import views.html.msb.send_largest_amounts_of_money
-
-import scala.concurrent.Future
 
 @Singleton
 class SendTheLargestAmountsOfMoneyController @Inject()(
-                                            val dataCacheConnector: DataCacheConnector,
-                                            val authConnector: AuthConnector,
-                                            val renewalService: RenewalService
-                                          ) extends BaseController {
+                                                        val dataCacheConnector: DataCacheConnector,
+                                                        val authConnector: AuthConnector,
+                                                        val renewalService: RenewalService
+                                                      ) extends BaseController {
 
   def get(edit: Boolean = false) = Authorised.async {
     implicit authContext => implicit request =>
-      ???
+      dataCacheConnector.fetch[Renewal](Renewal.key) map {
+        response =>
+          val form: Form2[SendTheLargestAmountsOfMoney] = (for {
+            renewal <- response
+            amount <- renewal.sendTheLargestAmountsOfMoney
+          } yield Form2[SendTheLargestAmountsOfMoney](amount)).getOrElse(EmptyForm)
+          Ok(send_largest_amounts_of_money(form, edit))
+      }
   }
 
   def post(edit: Boolean = false) = Authorised.async {
