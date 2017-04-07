7bad9bd1150bf635ab55630d3410c5209ef1c6a6
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2017-04-07 13:06:34 +0100

    Change test to match renewal routing

--- test/controllers/renewal/MsbWhichCurrenciesControllerSpec.scala
+++ test/controllers/renewal/MsbWhichCurrenciesControllerSpec.scala
@@ -31,12 +31,12 @@ class MsbWhichCurrenciesControllerSpec extends GenericTestHelper with MockitoSug
   }
 
   trait FormSubmissionFixture extends Fixture {
-    val validFormRequest = request.withFormUrlEncodedBody (
+    val validFormRequest = request.withFormUrlEncodedBody(
       "currencies[0]" -> "USD",
       "currencies[1]" -> "GBP",
       "currencies[2]" -> "BOB",
-      "bankMoneySource" ->"Yes",
-      "bankNames" ->"Bank names",
+      "bankMoneySource" -> "Yes",
+      "bankNames" -> "Bank names",
       "wholesalerMoneySource" -> "Yes",
       "wholesalerNames" -> "wholesaler names",
       "customerMoneySource" -> "Yes",
@@ -50,79 +50,79 @@ class MsbWhichCurrenciesControllerSpec extends GenericTestHelper with MockitoSug
 
   "Calling the GET action" must {
     "return the correct view" when {
-        "edit is false" in new Fixture {
-            val result = controller.get()(request)
+      "edit is false" in new Fixture {
+        val result = controller.get()(request)
 
-              status(result) mustBe OK
+        status(result) mustBe OK
 
-              val doc = Jsoup.parse(contentAsString(result))
-            doc.select(".heading-xlarge").text mustBe Messages("renewal.msb.whichcurrencies.header")
-          }
-
-          "edit is true" in new Fixture {
-            val result = controller.get(true)(request)
-
-              status(result) mustBe OK
-
-              val doc = Jsoup.parse(contentAsString(result))
-            doc.select("form").first.attr("action") mustBe routes.MsbWhichCurrenciesController.post(true).url
-          }
-
-          "reads the current value from the renewals model" in new Fixture {
-
-              when {
-                renewalService.getRenewal(any(), any(), any())
-              } thenReturn Future.successful(Renewal(msbWhichCurrencies = MsbWhichCurrencies(Seq("EUR"),None,None,None,None).some).some)
-
-              val result = controller.get(true)(request)
-            val doc = Jsoup.parse(contentAsString(result))
-
-              doc.select("select[name=currencies[0]] option[selected]").attr("value") mustBe "EUR"
-
-              verify(renewalService).getRenewal(any(), any(), any())
-          }
+        val doc = Jsoup.parse(contentAsString(result))
+        doc.select(".heading-xlarge").text mustBe Messages("renewal.msb.whichcurrencies.header")
       }
+
+      "edit is true" in new Fixture {
+        val result = controller.get(true)(request)
+
+        status(result) mustBe OK
+
+        val doc = Jsoup.parse(contentAsString(result))
+        doc.select("form").first.attr("action") mustBe routes.MsbWhichCurrenciesController.post(true).url
+      }
+
+      "reads the current value from the renewals model" in new Fixture {
+
+        when {
+          renewalService.getRenewal(any(), any(), any())
+        } thenReturn Future.successful(Renewal(msbWhichCurrencies = MsbWhichCurrencies(Seq("EUR"), None, None, None, None).some).some)
+
+        val result = controller.get(true)(request)
+        val doc = Jsoup.parse(contentAsString(result))
+
+        doc.select("select[name=currencies[0]] option[selected]").attr("value") mustBe "EUR"
+
+        verify(renewalService).getRenewal(any(), any(), any())
+      }
+    }
   }
 
   "Calling the POST action" when {
-        "posting valid data" must {
-            "redirect to the next page in the flow" in new FormSubmissionFixture {
-                val result = controller.post()(validFormRequest)
+    "posting valid data" must {
+      "redirect to the next page in the flow" in new FormSubmissionFixture {
+        val result = controller.post()(validFormRequest)
 
-                  status(result) mustBe SEE_OTHER
-                redirectLocation(result) mustBe controllers.renewal.routes.SummaryController.get().url.some
-              }
-
-              "redirect to the summary page when edit = true" in new FormSubmissionFixture {
-                val result = controller.post(edit = true)(validFormRequest)
-
-                  status(result) mustBe SEE_OTHER
-                redirectLocation(result) mustBe controllers.renewal.routes.SummaryController.get().url.some
-              }
-
-              "return a bad request" when {
-                "the form fails validation" in new FormSubmissionFixture {
-                    val result = controller.post()(request)
-
-                      status(result) mustBe BAD_REQUEST
-                    verify(renewalService, never()).updateRenewal(any())(any(), any(), any())
-                  }
-              }
-
-              "save the model data into the renewal object" in new FormSubmissionFixture {
-                val result = await(controller.post()(validFormRequest))
-                val captor = ArgumentCaptor.forClass(classOf[Renewal])
-
-                  verify(renewalService).updateRenewal(captor.capture())(any(), any(), any())
-
-                  captor.getValue.msbWhichCurrencies mustBe Some(MsbWhichCurrencies(
-                    Seq("USD", "GBP", "BOB"),
-                    Some(true),
-                    Some(BankMoneySource("Bank names")),
-                    Some(WholesalerMoneySource("wholesaler names")),
-                    Some(true)
-                  ))
-              }
-          }
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe controllers.renewal.routes.PercentageOfCashPaymentOver15000Controller.get().url.some
       }
+
+      "redirect to the summary page when edit = true" in new FormSubmissionFixture {
+        val result = controller.post(edit = true)(validFormRequest)
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result) mustBe controllers.renewal.routes.SummaryController.get().url.some
+      }
+
+      "return a bad request" when {
+        "the form fails validation" in new FormSubmissionFixture {
+          val result = controller.post()(request)
+
+          status(result) mustBe BAD_REQUEST
+          verify(renewalService, never()).updateRenewal(any())(any(), any(), any())
+        }
+      }
+
+      "save the model data into the renewal object" in new FormSubmissionFixture {
+        val result = await(controller.post()(validFormRequest))
+        val captor = ArgumentCaptor.forClass(classOf[Renewal])
+
+        verify(renewalService).updateRenewal(captor.capture())(any(), any(), any())
+
+        captor.getValue.msbWhichCurrencies mustBe Some(MsbWhichCurrencies(
+          Seq("USD", "GBP", "BOB"),
+          Some(true),
+          Some(BankMoneySource("Bank names")),
+          Some(WholesalerMoneySource("wholesaler names")),
+          Some(true)
+        ))
+      }
+    }
+  }
 }
