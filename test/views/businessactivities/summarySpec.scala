6d4174638cb221be3aa9d65ee94bf6f8be81e07b
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2017-04-19 15:31:15 +0100

    adding summarySpec

---
+++ test/views/businessactivities/summarySpec.scala
@@ -0,0 +1,96 @@
+package views.businessactivities
+
+import forms.{InvalidForm, ValidForm, Form2}
+import models.Country
+import models.businessactivities._
+import org.joda.time.LocalDate
+import org.jsoup.nodes.Element
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{MustMatchers}
+import  utils.GenericTestHelper
+import play.api.i18n.Messages
+import views.{Fixture, HtmlAssertions}
+
+import scala.collection.JavaConversions._
+
+
+class summarySpec extends GenericTestHelper
+  with MustMatchers
+  with HtmlAssertions
+  with TableDrivenPropertyChecks {
+
+  trait ViewFixture extends Fixture {
+    implicit val requestWithToken = addToken(request)
+  }
+
+  "summary view" must {
+    "have correct title" in new ViewFixture {
+
+      def view = views.html.businessactivities.summary(BusinessActivities(), None, true)
+
+      doc.title must startWith(Messages("title.cya") + " - " + Messages("summary.businessactivities"))
+    }
+
+    "have correct headings" in new ViewFixture {
+
+      def view = views.html.businessactivities.summary(BusinessActivities(), None, true)
+
+      heading.html must be(Messages("title.cya"))
+      subHeading.html must include(Messages("summary.businessactivities"))
+
+    }
+
+    val sectionChecks = Table[String, Element => Boolean](
+      ("title key", "check"),
+      ("businessactivities.confirm-activities.title",checkElementTextIncludes(_, "OtherActivities")),
+      ("businessactivities.business-turnover.title",checkElementTextIncludes(_, "businessactivities.turnover.lbl.01")),
+      ("businessactivities.turnover.title",checkElementTextIncludes(_, "businessactivities.business-turnover.lbl.01")),
+      ("businessactivities.businessfranchise.title",checkElementTextIncludes(_, "FranchiseName")),
+      ("businessactivities.employees.line1.cya",checkElementTextIncludes(_, "123", "456")),
+      ("businessactivities.keep.customer.records.title",
+        checkElementTextIncludes(_, "businessactivities.transactiontype.lbl.01", "businessactivities.transactiontype.lbl.02", "businessactivities.transactiontype.lbl.03", "SoftwareName")),
+      ("businessactivities.identify-suspicious-activity.title",checkElementTextIncludes(_, "lbl.yes")),
+      ("businessactivities.ncaRegistered.title",checkElementTextIncludes(_, "lbl.yes")),
+      ("businessactivities.riskassessment.policy.title",
+        checkElementTextIncludes(_, "businessactivities.RiskAssessmentType.lbl.01", "businessactivities.RiskAssessmentType.lbl.02")),
+      ("businessactivities.accountantForAMLSRegulations.title",checkElementTextIncludes(_, "lbl.yes")),
+      ("businessactivities.whoisyouraccountant.title",
+        checkElementTextIncludes(_, "AccountantName","tradingName","line1","line2","line3","line4","AB12CD")),
+      ("businessactivities.tax.matters.title",checkElementTextIncludes(_, "lbl.yes"))
+    )
+
+    "include the provided data" in new ViewFixture {
+
+      def view = views.html.businessactivities.summary(
+        BusinessActivities(
+          Some(InvolvedInOtherYes("OtherActivities")),
+          Some(ExpectedBusinessTurnover.First),
+          Some(ExpectedAMLSTurnover.First),
+          Some(BusinessFranchiseYes("FranchiseName")),
+          Some(TransactionRecordYes(Set(Paper, DigitalSpreadsheet, DigitalSoftware("SoftwareName")))),
+          None, // This is only present in renewal
+          Some(NCARegistered(true)),
+          Some(AccountantForAMLSRegulations(true)),
+          Some(IdentifySuspiciousActivity(true)),
+          Some(RiskAssessmentPolicyYes(Set(PaperBased, Digital))),
+          Some(HowManyEmployees("123", "456")),
+          Some(WhoIsYourAccountant("AccountantName",Some("tradingName"),UkAccountantsAddress("line1","line2",Some("line3"),Some("line4"),"AB12CD"))),
+          Some(TaxMatters(true))
+        ),
+        None,
+        true
+      )
+
+      forAll(sectionChecks) { (key, check) => {
+        val hTwos = doc.select("section.check-your-answers h2")
+
+        val hTwo = hTwos.toList.find(e => e.text() == Messages(key))
+
+        hTwo must not be (None)
+        val section = hTwo.get.parents().select("section").first()
+        check(section) must be(true)
+      }
+      }
+    }
+  }
+}
