d53f1a8022d97463acb16f131738e517c72a46e9
divya.tadisetti <divya.tadisetti@digital.hmrc.gov.uk>
2017-04-21 13:21:00 +0100

    AMLS-2380:services your business provide - unit tests refactoring

--- test/models/tcsp/ProvidedServicesSpec.scala
+++ test/models/tcsp/ProvidedServicesSpec.scala
@@ -8,66 +8,79 @@ import play.api.libs.json.{JsError, JsPath, JsSuccess, Json}
 
 class ProvidedServicesSpec extends PlaySpec with MockitoSugar {
 
-  "Form Rules and Writes" must {
+  "Provided services" must {
 
-    "successfully validate given one selected values" in {
-      val urlFormEncoded = Map("services[]" -> Seq("01"))
-      val expected = Valid(ProvidedServices(Set(PhonecallHandling)))
-      ProvidedServices.formReads.validate(urlFormEncoded) must be(expected)
+    "pass validation" when {
+
+      "successfully validate given one option selected" in {
+        val urlFormEncoded = Map("services[]" -> Seq("01"))
+        val expected = Valid(ProvidedServices(Set(PhonecallHandling)))
+        ProvidedServices.formReads.validate(urlFormEncoded) must be(expected)
+      }
+
+      "successfully validate given multiple options selected without other option" in {
+        val urlFormEncoded = Map("services[]" -> Seq("01", "02", "03", "04", "05", "06", "07"))
+        val allServices = Set[TcspService](PhonecallHandling, EmailHandling, EmailServer,
+          SelfCollectMailboxes, MailForwarding, Receptionist, ConferenceRooms)
+        val expected = Valid(ProvidedServices(allServices))
+        ProvidedServices.formReads.validate(urlFormEncoded) must be(expected)
+      }
+
+      "successfully validate given other value when other option selected" in {
+        val urlFormEncoded = Map("services[]" -> Seq("08"), "details" -> Seq("other service"))
+        val expected = Valid(ProvidedServices(Set(Other("other service"))))
+        ProvidedServices.formReads.validate(urlFormEncoded) must be(expected)
+      }
     }
 
-    "successfully validate given multiple selected values" in {
-      val urlFormEncoded = Map("services[]" -> Seq("01", "02", "03", "04", "05", "06", "07"))
-      val allServices = Set[TcspService](PhonecallHandling, EmailHandling, EmailServer,
-                                         SelfCollectMailboxes, MailForwarding, Receptionist, ConferenceRooms)
-      val expected = Valid(ProvidedServices(allServices))
-      ProvidedServices.formReads.validate(urlFormEncoded) must be(expected)
+    "form validation" when {
+
+      "successfully write given one values" in {
+        val services = ProvidedServices(Set(PhonecallHandling))
+        val expected = Map("services[]" -> Seq("01"))
+        ProvidedServices.formWrites.writes(services) must be(expected)
+      }
+
+      "successfully write given multiple values" in {
+        val services = ProvidedServices(Set(EmailServer, MailForwarding))
+        val expected = Map("services[]" -> Seq("03", "05"))
+        ProvidedServices.formWrites.writes(services) must be(expected)
+      }
+
+      "successfully write given 'other' value" in {
+        val services = ProvidedServices(Set(Other("other service")))
+        val expected = Map("services[]" -> Seq("08"), "details" -> Seq("other service"))
+        ProvidedServices.formWrites.writes(services) must be(expected)
+      }
     }
 
-    "successfully validate given other value" in {
-      val urlFormEncoded = Map("services[]" -> Seq("08"), "details" -> Seq("other service"))
-      val expected = Valid(ProvidedServices(Set(Other("other service"))))
-      ProvidedServices.formReads.validate(urlFormEncoded) must be(expected)
+    "fail validation" when {
+
+      "show an error with an invalid value" in {
+        val urlFormEncoded = Map("services[]" -> Seq("1738"))
+        val expected = Invalid(Seq((Path \ "services") -> Seq(ValidationError("error.invalid"))))
+        ProvidedServices.formReads.validate(urlFormEncoded) must be(expected)
+      }
+
+      "show an error with empty value" in {
+        val expected = Invalid(Seq((Path \ "services") -> Seq(ValidationError("error.required.tcsp.provided_services.services"))))
+        ProvidedServices.formReads.validate(Map.empty) must be(expected)
+      }
+
+      "show an error with a missing details value" in {
+        val form = Map("services[]" -> Seq("08"), "details" -> Seq(""))
+        val expectedResult = Invalid(Seq(Path \ "details" -> Seq(ValidationError("error.required.tcsp.provided_services.details"))))
+
+        ProvidedServices.formReads.validate(form) must be(expectedResult)
+      }
+
+      "show an error with an invalid details value" in {
+        val form = Map("services[]" -> Seq("08"), "details" -> Seq("$3<>7485/45"))
+        val expectedResult = Invalid(Seq(Path \ "details" -> Seq(ValidationError("err.text.validation"))))
+
+        ProvidedServices.formReads.validate(form) must be(expectedResult)
+      }
     }
-
-    "successfully write given one values" in {
-      val services = ProvidedServices(Set(PhonecallHandling))
-      val expected = Map("services[]" -> Seq("01"))
-      ProvidedServices.formWrites.writes(services) must be(expected)
-    }
-
-    "successfully write given multiple values" in {
-      val services = ProvidedServices(Set(EmailServer, MailForwarding))
-      val expected = Map("services[]" -> Seq("03", "05"))
-      ProvidedServices.formWrites.writes(services) must be(expected)
-    }
-
-    "successfully write given 'other' value" in {
-      val services = ProvidedServices(Set(Other("other service")))
-      val expected = Map("services[]" -> Seq("08"), "details" -> Seq("other service"))
-      ProvidedServices.formWrites.writes(services) must be(expected)
-    }
-
-    "show an error with an invalid value" in {
-      val urlFormEncoded = Map("services[]" -> Seq("1738"))
-      val expected = Invalid(Seq((Path \ "services") -> Seq(ValidationError("error.invalid"))))
-      ProvidedServices.formReads.validate(urlFormEncoded) must be(expected)
-    }
-
-    "show an error with a missing details value" in {
-      val form = Map("services[]" -> Seq("08"), "details" -> Seq(""))
-      val expectedResult = Invalid(Seq(Path \ "details" -> Seq(ValidationError("error.required.tcsp.provided_services.details"))))
-
-      ProvidedServices.formReads.validate(form) must be(expectedResult)
-    }
-
-    "show an error with an invalid details value" in {
-      val form = Map("services[]" -> Seq("08"), "details" -> Seq("$3<>7485/45"))
-      val expectedResult = Invalid(Seq(Path \ "details" -> Seq(ValidationError("err.text.validation"))))
-
-      ProvidedServices.formReads.validate(form) must be(expectedResult)
-    }
-
 
   }
 
@@ -82,7 +95,7 @@ class ProvidedServicesSpec extends PlaySpec with MockitoSugar {
       val allServices = Set[TcspService](PhonecallHandling, EmailHandling, EmailServer,
         SelfCollectMailboxes, MailForwarding, Receptionist, ConferenceRooms)
       val result = Json.toJson(ProvidedServices(allServices)).\("services").as[Seq[String]]
-      result must contain allOf ("01", "02", "03", "04", "05", "06", "07")
+      result must contain allOf("01", "02", "03", "04", "05", "06", "07")
     }
 
     "Serialise 'other' service as expected" in {
@@ -91,22 +104,22 @@ class ProvidedServicesSpec extends PlaySpec with MockitoSugar {
 
     "Deserialise single service as expected" in {
       val json = Json.obj("services" -> Set("01"))
-      val expected = JsSuccess(ProvidedServices(Set(PhonecallHandling)), JsPath )
-      Json.fromJson[ProvidedServices](json) must be (expected)
+      val expected = JsSuccess(ProvidedServices(Set(PhonecallHandling)), JsPath)
+      Json.fromJson[ProvidedServices](json) must be(expected)
     }
 
     "Deserialise multiple service as expected" in {
       val json = Json.obj("services" -> Seq("01", "02", "03", "04", "05", "06", "07"))
       val allServices = Set[TcspService](PhonecallHandling, EmailHandling, EmailServer,
-                                         SelfCollectMailboxes, MailForwarding, Receptionist, ConferenceRooms)
-      val expected = JsSuccess(ProvidedServices(allServices), JsPath )
-      Json.fromJson[ProvidedServices](json) must be (expected)
+        SelfCollectMailboxes, MailForwarding, Receptionist, ConferenceRooms)
+      val expected = JsSuccess(ProvidedServices(allServices), JsPath)
+      Json.fromJson[ProvidedServices](json) must be(expected)
     }
 
     "Deserialise 'other' service as expected" in {
       val json = Json.obj("services" -> Set("08"), "details" -> "other service")
-      val expected = JsSuccess(ProvidedServices(Set(Other("other service"))), JsPath )
-      Json.fromJson[ProvidedServices](json) must be (expected)
+      val expected = JsSuccess(ProvidedServices(Set(Other("other service"))), JsPath)
+      Json.fromJson[ProvidedServices](json) must be(expected)
     }
 
     "fail when on invalid data" in {
---
+++ test/views/tcsp/provided_servicesSpec.scala
@@ -0,0 +1,53 @@
+package views.tcsp
+
+import forms.{EmptyForm, InvalidForm}
+import jto.validation.{Path, ValidationError}
+import org.scalatest.MustMatchers
+import play.api.i18n.Messages
+import utils.GenericTestHelper
+import views.Fixture
+
+
+class provided_servicesSpec extends GenericTestHelper with MustMatchers {
+
+  trait ViewFixture extends Fixture {
+    implicit val requestWithToken = addToken(request)
+  }
+
+  "provided_services view" must {
+    "have correct title, heading amd subheading" in new ViewFixture {
+
+      val form2 = EmptyForm
+
+      def view = views.html.tcsp.provided_services(form2, true)
+
+      val title = Messages("tcsp.provided_services.title") + " - " + Messages("summary.tcsp") + " - " +
+                  Messages("title.amls") + " - " + Messages("title.gov")
+
+      doc.title must be(title)
+      heading.html must be(Messages("tcsp.provided_services.title"))
+      subHeading.html must include(Messages("summary.tcsp"))
+    }
+
+    "show errors in the correct locations" in new ViewFixture {
+
+      val form2: InvalidForm = InvalidForm(Map.empty,
+        Seq(
+          (Path \ "services") -> Seq(ValidationError("not a message Key")),
+          (Path \ "details") -> Seq(ValidationError("second not a message Key"))
+        ))
+
+      def view = views.html.tcsp.provided_services(form2, true)
+
+      errorSummary.html() must include("not a message Key")
+      errorSummary.html() must include("second not a message Key")
+
+      doc.getElementById("services")
+        .getElementsByClass("error-notification").first().html() must include("not a message Key")
+println("!!!!!!!!!!!!!!! "+doc)
+      doc.getElementById("details-fieldset")
+        .getElementsByClass("error-notification").first().html() must include("second not a message Key")
+
+    }
+  }
+}
