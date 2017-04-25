e1da082571787b0a222886c70297c2defb25b7d2
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2017-04-25 10:45:13 +0100

    add new view spec for TP what_does_your_business_do

--- test/views/tradingpremises/what_does_your_business_doSpec.scala
+++ test/views/tradingpremises/what_does_your_business_doSpec.scala
@@ -2,9 +2,11 @@ package views.tradingpremises
 
 import forms.{EmptyForm, InvalidForm}
 import jto.validation.{Path, ValidationError}
+import models.businessmatching.{AccountancyServices, BillPaymentServices, BusinessActivities, EstateAgentBusinessService}
 import org.scalatest.MustMatchers
 import play.api.i18n.Messages
 import utils.GenericTestHelper
+import models.businessmatching.{BusinessActivities => BusinessMatchingActivities, _}
 import views.Fixture
 
 
@@ -14,24 +16,29 @@ class what_does_your_business_doSpec extends GenericTestHelper with MustMatchers
     implicit val requestWithToken = addToken(request)
   }
 
-  "remove_trading_premises view" must {
+  "what_does_your_business_do view" must {
+
+    val businessMatchingActivitiesAll = BusinessMatchingActivities(
+      Set(AccountancyServices, BillPaymentServices, EstateAgentBusinessService))
+
     "have correct title, heading and load UI with empty form" in new ViewFixture {
 
       val form2 = EmptyForm
 
-      val pageTitle = Messages("tradingpremises.remove.trading.premises.title") + " - " +
+      val pageTitle = Messages("tradingpremises.whatdoesyourbusinessdo.title") + " - " +
         Messages("summary.tradingpremises") + " - " +
         Messages("title.amls") + " - " + Messages("title.gov")
 
-      def view = views.html.tradingpremises.remove_trading_premises(form2, 1, false, "trading name", false )
+
+
+      def view = views.html.tradingpremises.what_does_your_business_do(form2, businessMatchingActivitiesAll, false ,1 )
 
       doc.title must be(pageTitle)
-      heading.html must be(Messages("tradingpremises.remove.trading.premises.title"))
+      heading.html must be(Messages("tradingpremises.whatdoesyourbusinessdo.title"))
       subHeading.html must include(Messages("summary.tradingpremises"))
 
-      doc.getElementsMatchingOwnText(Messages("tradingpremises.remove.trading.premises.text", "trading name")).hasText must be(true)
-      doc.getElementsMatchingOwnText(Messages("tradingpremises.remove.trading.premises.btn")).last().html() must be(
-        Messages("tradingpremises.remove.trading.premises.btn"))
+      doc.select("input[type=checkbox]").size must be(3)
+
     }
 
     "show errors in the correct locations" in new ViewFixture {
@@ -41,7 +48,7 @@ class what_does_your_business_doSpec extends GenericTestHelper with MustMatchers
           (Path \ "some path") -> Seq(ValidationError("not a message Key"))
         ))
 
-      def view = views.html.tradingpremises.remove_trading_premises(form2, 1, true,"trading name", true)
+      def view = views.html.tradingpremises.what_does_your_business_do(form2, businessMatchingActivitiesAll, true,1)
 
       errorSummary.html() must include("not a message Key")
     }
