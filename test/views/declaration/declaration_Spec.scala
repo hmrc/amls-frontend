fc2f87e51364d697930ae68e6a371b2ed1fb971b
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-05-02 13:05:22 +0100

    Declaration test for replay of name on the page

--- test/views/declaration/declaration_Spec.scala
+++ test/views/declaration/declaration_Spec.scala
@@ -15,18 +15,25 @@ class declaration_Spec extends GenericTestHelper with MustMatchers  {
   "declaration view" must {
     "have correct title" in new ViewFixture {
 
-      def view = views.html.declaration.declare(("string1","string2"), "Name")
+      def view = views.html.declaration.declare(("string1", "string2"), "Name")
 
       doc.title must startWith("string1")
     }
 
     "have correct headings" in new ViewFixture {
 
-      def view = views.html.declaration.declare(("string1","string2"), "Name")
+      def view = views.html.declaration.declare(("string1", "string2"), "Name")
 
       heading.html must be(Messages("declaration.declaration.title"))
       subHeading.html must include(Messages("string2"))
-
     }
+
+    "replay the person's name in the first line of the declaration text" in new ViewFixture {
+      val name = "Some Person"
+      def view = views.html.declaration.declare(("string1", "string2"), name)
+
+      doc.select(".notice").text() must include(Messages("declaration.declaration.fullname", name))
+    }
+
   }
 }
