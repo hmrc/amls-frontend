9e1916e90e3d52d1f50e48a1d7db01643176e407
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2017-05-10 09:30:36 +0100

    Check for soleProprietor as true in rp detailed answers

--- test/views/responsiblepeople/detailed_answersSpec.scala
+++ test/views/responsiblepeople/detailed_answersSpec.scala
@@ -61,7 +61,7 @@ class detailed_answersSpec extends GenericTestHelper
       (Messages("responsiblepeople.detailed_answers.position"), checkElementTextIncludes(_, "Beneficial owner")),
       (Messages("responsiblepeople.detailed_answers.position"), checkElementTextIncludes(_, "Nominated officer")),
       (Messages("responsiblepeople.detailed_answers.position_start"), checkElementTextIncludes(_, "24 February 1990")),
-      (Messages("responsiblepeople.detailed_answers.soleproprietor_for_other_business"), checkElementTextIncludes(_, "No")),
+      (Messages("responsiblepeople.detailed_answers.soleproprietor_for_other_business"), checkElementTextIncludes(_, "Yes")),
       (Messages("responsiblepeople.detailed_answers.registered_for_vat"), checkElementTextIncludes(_, "No")),
       (Messages("responsiblepeople.detailed_answers.registered_for_sa"), checkElementTextIncludes(_, "Registered for Self Assessment")),
       (Messages("responsiblepeople.detailed_answers.previous_experience"), checkElementTextIncludes(_, "experience")),
@@ -150,7 +150,7 @@ class detailed_answersSpec extends GenericTestHelper
       experienceTraining = Some(ExperienceTrainingYes("experience")),
       training = Some(TrainingYes("training")),
       hasAlreadyPassedFitAndProper = Some(true),
-      soleProprietorOfAnotherBusiness = Some(SoleProprietorOfAnotherBusiness(false))
+      soleProprietorOfAnotherBusiness = Some(SoleProprietorOfAnotherBusiness(true))
     )
 
     "include the provided data" when {
