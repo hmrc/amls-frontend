00548f2e10717304ce12eec26e09792a56617b71
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-05-17 10:44:16 +0100

    Added conversion for PersonAddressUK + tests

---
+++ app/audit/AddressCreatedEvent.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package audit
+
+import uk.gov.hmrc.play.audit.AuditExtensions._
+import models.responsiblepeople.PersonAddressUK
+import uk.gov.hmrc.play.audit.model.DataEvent
+import uk.gov.hmrc.play.config.AppName
+import uk.gov.hmrc.play.http.HeaderCarrier
+import Utils.toMap
+import play.api.libs.json.Json
+
+case class AuditAddress(addressLine1: String, addressLine2: String, addressLine3: Option[String], country: String, postCode: String)
+
+object AuditAddress {
+  implicit val format = Json.format[AuditAddress]
+}
+
+object AddressCreatedEvent {
+
+  def apply(address: AuditAddress)(implicit hc: HeaderCarrier) = DataEvent(
+    auditSource = AppName.appName,
+    auditType = "manualAddressSubmitted",
+    tags = hc.toAuditTags("manualBankAccountSubmitted", "n/a"),
+    detail = hc.toAuditDetails() ++ toMap(address)
+  )
+
+}
+
+object AddressConversions {
+
+  implicit def convert(address: PersonAddressUK): AuditAddress =
+    AuditAddress(address.addressLine1, address.addressLine2, address.addressLine3, "GB", address.postCode)
+
+}
---
+++ test/audit/AddressCreatedEventSpec.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package audit
+
+import models.responsiblepeople.PersonAddressUK
+import org.scalatest.MustMatchers
+import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
+import cats.implicits._
+import uk.gov.hmrc.play.http.HeaderCarrier
+import uk.gov.hmrc.play.audit.AuditExtensions._
+import audit.AddressConversions._
+
+class AddressCreatedEventSpec extends PlaySpec with MustMatchers with OneAppPerSuite {
+
+  implicit val hc = HeaderCarrier()
+
+  "The AddressCreatedAuditEvent" must {
+    "create the proper detail" when {
+      "given an address of a responsible person in the UK" in {
+
+        val address = PersonAddressUK("Line 1", "Line 2", "Line 3".some, None, "postcode")
+        val event = AddressCreatedEvent(address)
+        val expected = hc.toAuditDetails() ++ Map(
+          "addressLine1" -> "Line 1",
+          "addressLine2" -> "Line 2",
+          "addressLine3" -> "Line 3",
+          "country" -> "GB",
+          "postCode" -> "postcode"
+        )
+
+        event.detail mustBe expected
+      }
+    }
+  }
+
+}
