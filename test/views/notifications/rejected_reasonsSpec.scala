a2c6e666aa13484912c4ea60680d9dac479cb6c1
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2017-05-17 14:57:45 +0100

    reasons_rejected view created, tested

---
+++ test/views/notifications/rejected_reasonsSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.notifications
+
+import org.scalatest.MustMatchers
+import play.api.i18n.Messages
+import utils.GenericTestHelper
+import views.Fixture
+
+class rejected_reasonsSpec extends GenericTestHelper with MustMatchers {
+
+  trait ViewFixture extends Fixture {
+
+    implicit val requestWithToken = addToken(request)
+
+    val businessName = "Fake Name Ltd."
+
+  }
+
+  "rejected_reasons view" must {
+
+    "have correct title" in new ViewFixture {
+
+      def view = views.html.notifications.rejected_reasons("msgContent", "amlsRegNo", businessName)
+
+      doc.title must be(Messages("notifications.rejr.title") +
+        " - " + Messages("status.title") +
+        " - " + Messages("title.amls") +
+        " - " + Messages("title.gov"))
+    }
+
+    "have correct headings" in new ViewFixture {
+
+      def view = views.html.notifications.rejected_reasons("msgContent", "amlsRegNo", businessName)
+
+      heading.html must be(Messages("notifications.rejr.title"))
+      subHeading.html must include(Messages("status.title"))
+
+    }
+
+  }
+
+
+}
