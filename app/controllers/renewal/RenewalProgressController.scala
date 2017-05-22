79f65fddca72acdd64b01200da862af7b196c39c
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2017-05-22 12:17:42 +0100

    hack to fix renewal section background changed visibility

--- app/controllers/renewal/RenewalProgressController.scala
+++ app/controllers/renewal/RenewalProgressController.scala
@@ -23,7 +23,7 @@ import cats.implicits._
 import connectors.DataCacheConnector
 import controllers.BaseController
 import models.businessmatching.BusinessMatching
-import models.registrationprogress.Completed
+import models.registrationprogress.{Completed, Section}
 import models.status.{ReadyForRenewal, RenewalSubmitted}
 import play.api.i18n.MessagesApi
 import services.{ProgressService, RenewalService, StatusService}
@@ -44,12 +44,21 @@ class RenewalProgressController @Inject()
   statusService :StatusService
 ) extends BaseController {
 
+  private def amendmentDeclarationAvailable(sections: Seq[Section]) = {
+
+    sections.foldLeft((true, false)) { (acc, s) =>
+      (acc._1 && s.status == Completed,
+        acc._2 || s.hasChanged)
+    } match {
+      case (true, true) => true
+      case _ => false
+    }
+  }
+
   def get() = Authorised.async {
     implicit authContext =>
       implicit request =>
         renewals.getSection flatMap { renewalSection =>
-
-          val canSubmit = renewalSection.status == Completed && renewalSection.hasChanged
 
           val block = for {
             cache <- OptionT(dataCacheConnector.fetchAll)
@@ -58,6 +67,7 @@ class RenewalProgressController @Inject()
             val variationSections = progressService.sections(cache)
             val businessMatching = cache.getEntry[BusinessMatching](BusinessMatching.key)
             val msbOrTcspExists = ControllerHelper.isMSBSelected(businessMatching) || ControllerHelper.isTCSPSelected(businessMatching)
+            val canSubmit = renewalSection.status == Completed && renewalSection.hasChanged && amendmentDeclarationAvailable(variationSections)
 
             statusInfo match {
               case (ReadyForRenewal(renewalDate), _) => Ok(renewal_progress(renewalSection, variationSections, canSubmit, msbOrTcspExists, renewalDate))
