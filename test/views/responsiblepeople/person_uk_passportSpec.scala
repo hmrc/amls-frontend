a3021101ba9d5b8a11c8c89d33706140da5ef461
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2017-06-01 16:13:50 +0100

    Remove fields from rp residenceType, add text to rp ukPassport

--- test/views/responsiblepeople/person_uk_passportSpec.scala
+++ test/views/responsiblepeople/person_uk_passportSpec.scala
@@ -16,6 +16,47 @@
 
 package views.responsiblepeople
 
-class person_uk_passportSpec {
+import forms.{EmptyForm, InvalidForm}
+import jto.validation.{Path, ValidationError}
+import org.scalatest.MustMatchers
+import play.api.i18n.Messages
+import utils.GenericTestHelper
+import views.Fixture
+
+class person_uk_passportSpec extends GenericTestHelper with MustMatchers {
+
+  trait ViewFixture extends Fixture {
+    implicit val requestWithToken = addToken(request)
+  }
+
+  "person_uk_passport view" must {
+    "have correct title, headings and form fields" in new ViewFixture {
+      val form2 = EmptyForm
+
+      val name = "firstName lastName"
+
+      def view = views.html.responsiblepeople.person_uk_passport(form2, true, 1, true, name)
+
+      doc.title must startWith(Messages("responsiblepeople.uk.passport.title"))
+      heading.html must be(Messages("responsiblepeople.uk.passport.heading", name))
+      subHeading.html must include(Messages("summary.responsiblepeople"))
+
+      doc.getElementsByAttributeValue("name", "ukPassport") must not be empty
+      doc.getElementsByAttributeValue("name", "ukPassportNumber") must not be empty
+
+    }
+    "show errors in the correct locations" in new ViewFixture {
+      val form2: InvalidForm = InvalidForm(Map.empty,
+        Seq(
+          (Path \ "ukPassport") -> Seq(ValidationError("not a message Key")),
+          (Path \ "ukPassportNumber") -> Seq(ValidationError("second not a message Key"))
+        ))
+
+      def view = views.html.responsiblepeople.person_uk_passport(form2, true, 1, true, "firstName lastName")
+
+      errorSummary.html() must include("not a message Key")
+      errorSummary.html() must include("second not a message Key")
+    }
+  }
 
 }
