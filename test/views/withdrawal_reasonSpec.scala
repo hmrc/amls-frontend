305323a3e999d1792b8c07d0989994e1123c04d1
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2017-06-22 14:00:40 +0100

    Shell of withdrawal_reason view and test created

---
+++ test/views/withdrawal_reasonSpec.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views
+
+import forms.{EmptyForm, Form2, InvalidForm, ValidForm}
+import org.scalatest.MustMatchers
+import utils.GenericTestHelper
+import jto.validation.Path
+import jto.validation.ValidationError
+import models.withdrawal.WithdrawalReason
+import play.api.i18n.Messages
+import views.Fixture
+
+class withdrawal_reasonSpec extends GenericTestHelper with MustMatchers  {
+
+  trait TestFixture extends Fixture {
+    implicit val requestWithToken = addToken(request)
+  }
+
+  "withdrawal_reasons view" must {
+    "have correct title" in new TestFixture {
+
+      def view = views.html.withdrawal_reason(EmptyForm)
+
+      doc.title must be(Messages("") +
+        " - " + Messages("") +
+        " - " + Messages("title.amls") +
+        " - " + Messages("title.gov"))
+    }
+
+    "have correct headings" in new TestFixture {
+
+      def view = views.html.withdrawal_reason(EmptyForm)
+
+      heading.html must be(Messages(""))
+      subHeading.html must include(Messages(""))
+
+    }
+
+    "show errors in the correct locations" in new TestFixture {
+
+      val form2: InvalidForm = InvalidForm(Map.empty,
+        Seq(
+          (Path \ "withdrawalReason") -> Seq(ValidationError("not another message key")),
+          (Path \ "specifyOtherReason") -> Seq(ValidationError("not yet another message key"))
+        ))
+
+      def view = views.html.withdrawal_reason(form2)
+
+      errorSummary.html() must include("not another message key")
+      errorSummary.html() must include("not yet another message key")
+
+      doc.getElementById("withdrawalReason")
+        .getElementsByClass("error-notification").first().html() must include("not another message key")
+
+      doc.getElementById("specifyOtherReason").parent()
+        .getElementsByClass("error-notification").first().html() must include("not yet another message key")
+
+    }
+  }
+}
