8af40a7df2ff26cfbe89b1c6ed7c0076b036427e
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2017-07-11 17:13:59 +0100

    AMLS-2093: adding view and updating get to retrieve responsible people and status

---
+++ test/views/declaration/register_partnersSpec.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.declaration
+
+import cats.implicits._
+import forms.{EmptyForm, Form2}
+import models.declaration.BusinessNominatedOfficer
+import models.responsiblepeople.{PersonName, ResponsiblePeople}
+import org.scalatest.MustMatchers
+import play.api.i18n.Messages
+import utils.GenericTestHelper
+import views.Fixture
+
+class register_partnersSpec extends GenericTestHelper with MustMatchers {
+
+  trait ViewFixture extends Fixture {
+    implicit val requestWithToken = addToken(request)
+  }
+
+  "register_partners view" must {
+    "have correct title, headings and content" in new ViewFixture {
+
+      def view = views.html.declaration.register_partners("subheading", EmptyForm, Seq.empty[ResponsiblePeople], Seq("partner1"))
+
+      doc.title mustBe s"${Messages("declaration.register.partners.title")} - ${Messages("title.amls")} - ${Messages("title.gov")}"
+      heading.html must be(Messages("declaration.register.partners.title"))
+      subHeading.html must include("subheading")
+    }
+
+    "have a list of responsible people" in new ViewFixture {
+
+      val people = Seq(
+        ResponsiblePeople(PersonName("Test", None, "Person1", None, None).some),
+        ResponsiblePeople(PersonName("Test", None, "Person2", None, None).some)
+      )
+
+      def view = views.html.declaration.register_partners("subheading", EmptyForm, people, Seq("partner1"))
+
+      people map(_.personName.get) foreach { n =>
+        val id = s"value-${n.fullNameWithoutSpace}"
+        val e = doc.getElementById(id)
+
+        Option(e) must be(defined)
+        e.`val` mustBe s"${n.firstName}${n.lastName}"
+
+        val label = doc.select(s"label[for=$id]")
+        label.text() must include(n.fullName)
+      }
+
+    }
+//
+//    "prepopulate the selected nominated officer" in new ViewFixture {
+//
+//      val people = Seq(
+//        ResponsiblePeople(PersonName("Test", None, "Person1", None, None).some),
+//        ResponsiblePeople(PersonName("Test", None, "Person2", None, None).some)
+//      )
+//
+//      val f = Form2(BusinessNominatedOfficer("TestPerson1"))
+//
+//      def view = views.html.declaration.select_business_nominated_officer("subheading", f, people)
+//
+//      doc.select("input[type=radio][id=value-TestPerson1").hasAttr("checked") mustBe true
+//
+//    }
+//
+//    "show the 'register someone else' radio button" in new ViewFixture {
+//      def view = views.html.declaration.select_business_nominated_officer("subheading", EmptyForm, Seq.empty[ResponsiblePeople])
+//
+//      Option(doc.select("input[type=radio][id=value--1]")) must be(defined)
+//      doc.select("label[for=value--1]").text() must include(Messages("lbl.register.some.one.else"))
+//    }
+
+  }
+}
