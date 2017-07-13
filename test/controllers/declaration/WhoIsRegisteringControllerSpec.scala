708dbd475b0759dfa705a51b00ee93715494274a
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-07-13 15:16:19 +0100

    Refactored out the mocks from the GET tests

--- test/controllers/declaration/WhoIsRegisteringControllerSpec.scala
+++ test/controllers/declaration/WhoIsRegisteringControllerSpec.scala
@@ -53,164 +53,118 @@ class WhoIsRegisteringControllerSpec extends GenericTestHelper with MockitoSugar
 
     val pendingReadStatusResponse = ReadStatusResponse(LocalDateTime.now(), "Pending", None, None, None,
       None, false)
+
     val notCompletedReadStatusResponse = ReadStatusResponse(LocalDateTime.now(), "NotCompleted", None, None, None,
       None, false)
-    when {
-      controller.renewalService.getRenewal(any(), any(), any())
-    } thenReturn Future.successful(None)
+
+    val cacheMap = mock[CacheMap]
+
+    val personName = PersonName("firstName", Some("middleName"), "lastName", None, Some("name"))
+    val positions = Positions(Set(BeneficialOwner, InternalAccountant), Some(new LocalDate()))
+
+    val rp = ResponsiblePeople (
+      personName = Some(personName),
+      positions = Some(positions),
+      status = None
+    )
+
+    val rp1 = ResponsiblePeople(
+      personName = Some(personName),
+      positions = Some(positions),
+      status = Some(StatusConstants.Deleted)
+    )
+
+    val responsiblePeople = Seq(rp, rp1)
+
+    def run(status: SubmissionStatus, renewal: Option[Renewal] = None)(block: Unit => Any) = {
+      when {
+        controller.renewalService.getRenewal(any(), any(), any())
+      } thenReturn Future.successful(renewal)
+
+      when(controller.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
+        .thenReturn(Future.successful(Some(cacheMap)))
+
+      when(controller.statusService.getStatus(any(),any(),any()))
+        .thenReturn(Future.successful(status))
+
+      when(cacheMap.getEntry[Seq[ResponsiblePeople]](any())(any()))
+        .thenReturn(Some(responsiblePeople))
+
+      when(cacheMap.getEntry[WhoIsRegistering](WhoIsRegistering.key))
+        .thenReturn(None)
+
+      block()
+    }
   }
 
   val emptyCache = CacheMap("", Map.empty)
 
   "WhoIsRegisteringController" must {
 
-    val personName = PersonName("firstName", Some("middleName"), "lastName", None, Some("name"))
-    val positions = Positions(Set(BeneficialOwner, InternalAccountant), Some(new LocalDate()))
-    val rp = ResponsiblePeople (
-      personName = Some(personName),
-      positions = Some(positions),
-      status = None
-    )
-    val rp1 = ResponsiblePeople(
-      personName = Some(personName),
-      positions = Some(positions),
-      status = Some(StatusConstants.Deleted)
-    )
-    val responsiblePeoples = Seq(rp, rp1)
-
-
     "Get Option:" must {
 
       "load the who is registering page" when {
         "status is pending" in new Fixture {
+          run(SubmissionReadyForReview) { _ =>
+            val result = controller.get()(request)
+            status(result) must be(OK)
 
-          val mockCacheMap = mock[CacheMap]
+            val htmlValue = Jsoup.parse(contentAsString(result))
+            htmlValue.title mustBe Messages("declaration.who.is.registering.amendment.title") + " - " + Messages("title.amls") + " - " + Messages("title.gov")
+            htmlValue.getElementById("person-firstNamelastName").`val`() must be("firstNamelastName")
 
-          when(controller.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
-            .thenReturn(Future.successful(Some(mockCacheMap)))
-
-          when(controller.statusService.getStatus(any(),any(),any()))
-            .thenReturn(Future.successful(SubmissionReadyForReview))
-
-          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any()))
-            .thenReturn(Some(responsiblePeoples))
-
-          when(mockCacheMap.getEntry[WhoIsRegistering](WhoIsRegistering.key))
-            .thenReturn(None)
-
-          val result = controller.get()(request)
-          status(result) must be(OK)
-
-          val htmlValue = Jsoup.parse(contentAsString(result))
-          htmlValue.title mustBe Messages("declaration.who.is.registering.amendment.title") + " - " + Messages("title.amls") + " - " + Messages("title.gov")
-          htmlValue.getElementById("person-firstNamelastName").`val`() must be("firstNamelastName")
-
-          contentAsString(result) must include(Messages("submit.amendment.application"))
+            contentAsString(result) must include(Messages("submit.amendment.application"))
+          }
         }
 
         "status is approved" in new Fixture {
+          run(SubmissionDecisionApproved) { _ =>
+            val result = controller.get()(request)
+            status(result) must be(OK)
 
-          val mockCacheMap = mock[CacheMap]
+            val htmlValue = Jsoup.parse(contentAsString(result))
+            htmlValue.title mustBe Messages("declaration.who.is.registering.amendment.title") + " - " + Messages("title.amls") + " - " + Messages("title.gov")
+            htmlValue.getElementById("person-firstNamelastName").`val`() must be("firstNamelastName")
 
-          when(controller.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
-            .thenReturn(Future.successful(Some(mockCacheMap)))
-
-          when(controller.statusService.getStatus(any(),any(),any()))
-            .thenReturn(Future.successful(SubmissionDecisionApproved))
-
-          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any()))
-            .thenReturn(Some(responsiblePeoples))
-
-          when(mockCacheMap.getEntry[WhoIsRegistering](WhoIsRegistering.key))
-            .thenReturn(None)
-
-          val result = controller.get()(request)
-          status(result) must be(OK)
-
-          val htmlValue = Jsoup.parse(contentAsString(result))
-          htmlValue.title mustBe Messages("declaration.who.is.registering.amendment.title") + " - " + Messages("title.amls") + " - " + Messages("title.gov")
-          htmlValue.getElementById("person-firstNamelastName").`val`() must be("firstNamelastName")
-
-          contentAsString(result) must include(Messages("submit.amendment.application"))
+            contentAsString(result) must include(Messages("submit.amendment.application"))
+          }
         }
 
         "status is pre-submission" in new Fixture {
+          run(SubmissionReady) { _ =>
+            val result = controller.get()(request)
+            status(result) must be(OK)
 
-          val mockCacheMap = mock[CacheMap]
+            val htmlValue = Jsoup.parse(contentAsString(result))
+            htmlValue.title mustBe Messages("declaration.who.is.registering.title") + " - " + Messages("title.amls") + " - " + Messages("title.gov")
+            htmlValue.getElementById("person-firstNamelastName").`val`() must be("firstNamelastName")
 
-          when(controller.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
-            .thenReturn(Future.successful(Some(mockCacheMap)))
-
-          when(controller.statusService.getStatus(any(),any(),any()))
-            .thenReturn(Future.successful(SubmissionReady))
-
-          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any()))
-            .thenReturn(Some(responsiblePeoples))
-
-          when(mockCacheMap.getEntry[WhoIsRegistering](WhoIsRegistering.key))
-            .thenReturn(None)
-
-          val result = controller.get()(request)
-          status(result) must be(OK)
-
-          val htmlValue = Jsoup.parse(contentAsString(result))
-          htmlValue.title mustBe Messages("declaration.who.is.registering.title") + " - " + Messages("title.amls") + " - " + Messages("title.gov")
-          htmlValue.getElementById("person-firstNamelastName").`val`() must be("firstNamelastName")
-
-          contentAsString(result) must include(Messages("submit.registration"))
+            contentAsString(result) must include(Messages("submit.registration"))
+          }
         }
 
 
         "status is renewal amendment" in new Fixture {
+          run(RenewalSubmitted(None)) { _ =>
+            val result = controller.get()(request)
+            status(result) must be(OK)
 
-          val mockCacheMap = mock[CacheMap]
+            val htmlValue = Jsoup.parse(contentAsString(result))
 
-          when(controller.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
-            .thenReturn(Future.successful(Some(mockCacheMap)))
-
-          when(controller.statusService.getStatus(any(),any(),any()))
-            .thenReturn(Future.successful(RenewalSubmitted(None)))
-
-          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any()))
-            .thenReturn(Some(responsiblePeoples))
-
-          when(mockCacheMap.getEntry[WhoIsRegistering](WhoIsRegistering.key))
-            .thenReturn(None)
-
-          val result = controller.get()(request)
-          status(result) must be(OK)
-
-          val htmlValue = Jsoup.parse(contentAsString(result))
-
-          contentAsString(result) must include(Messages("submit.amendment.application"))
+            contentAsString(result) must include(Messages("submit.amendment.application"))
+          }
         }
 
 
         "status is renewal" in new Fixture {
+          run(ReadyForRenewal(None), Some(mock[Renewal])) { _ =>
+            val result = controller.getWithRenewal(request)
+            status(result) must be(OK)
 
-          val mockCacheMap = mock[CacheMap]
+            val htmlValue = Jsoup.parse(contentAsString(result))
 
-          when(controller.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
-            .thenReturn(Future.successful(Some(mockCacheMap)))
-
-          when(controller.statusService.getStatus(any(),any(),any()))
-            .thenReturn(Future.successful(ReadyForRenewal(None)))
-
-          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any()))
-            .thenReturn(Some(responsiblePeoples))
-
-          when(mockCacheMap.getEntry[WhoIsRegistering](WhoIsRegistering.key))
-            .thenReturn(None)
-          when {
-            controller.renewalService.getRenewal(any(), any(), any())
-          } thenReturn Future.successful(Some(mock[Renewal]))
-
-          val result = controller.getWithRenewal(request)
-          status(result) must be(OK)
-
-          val htmlValue = Jsoup.parse(contentAsString(result))
-
-          contentAsString(result) must include(Messages("declaration.renewal.who.is.registering.heading"))
+            contentAsString(result) must include(Messages("declaration.renewal.who.is.registering.heading"))
+          }
         }
       }
     }
@@ -230,7 +184,7 @@ class WhoIsRegisteringControllerSpec extends GenericTestHelper with MockitoSugar
           when(controller.statusService.getStatus(any(),any(),any()))
             .thenReturn(Future.successful(SubmissionReadyForReview))
 
-          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeoples))
+          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeople))
 
           when(controller.dataCacheConnector.save[AddPerson](any(), any())
             (any(), any(), any())).thenReturn(Future.successful(emptyCache))
@@ -254,7 +208,7 @@ class WhoIsRegisteringControllerSpec extends GenericTestHelper with MockitoSugar
           when(controller.statusService.getStatus(any(),any(),any()))
             .thenReturn(Future.successful(SubmissionReady))
 
-          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeoples))
+          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeople))
 
           when(controller.dataCacheConnector.save[AddPerson](any(), any())
             (any(), any(), any())).thenReturn(Future.successful(emptyCache))
@@ -284,7 +238,7 @@ class WhoIsRegisteringControllerSpec extends GenericTestHelper with MockitoSugar
         when(controller.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
           .thenReturn(Future.successful(Some(mockCacheMap)))
 
-        when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeoples))
+        when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeople))
 
         when(controller.dataCacheConnector.save[WhoIsRegistering](any(), any())(any(), any(), any()))
           .thenReturn(Future.successful(emptyCache))
@@ -300,7 +254,7 @@ class WhoIsRegisteringControllerSpec extends GenericTestHelper with MockitoSugar
           val newRequest = request.withFormUrlEncodedBody()
 
           when(controller.dataCacheConnector.fetch[Seq[ResponsiblePeople]](any())(any(), any(), any()))
-            .thenReturn(Future.successful(Some(responsiblePeoples)))
+            .thenReturn(Future.successful(Some(responsiblePeople)))
 
           when(controller.statusService.getStatus(any(),any(),any()))
             .thenReturn(Future.successful(SubmissionReady))
@@ -321,7 +275,7 @@ class WhoIsRegisteringControllerSpec extends GenericTestHelper with MockitoSugar
           val newRequest = request.withFormUrlEncodedBody("person" -> "dfsf")
           val mockCacheMap = mock[CacheMap]
 
-          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeoples))
+          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeople))
 
           when(controller.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
             .thenReturn(Future.successful(Some(mockCacheMap)))
@@ -341,7 +295,7 @@ class WhoIsRegisteringControllerSpec extends GenericTestHelper with MockitoSugar
           val newRequest = request.withFormUrlEncodedBody("person" -> "dfsf")
           val mockCacheMap = mock[CacheMap]
 
-          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeoples))
+          when(mockCacheMap.getEntry[Seq[ResponsiblePeople]](any())(any())).thenReturn(Some(responsiblePeople))
 
           when(controller.dataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
             .thenReturn(Future.successful(Some(mockCacheMap)))
