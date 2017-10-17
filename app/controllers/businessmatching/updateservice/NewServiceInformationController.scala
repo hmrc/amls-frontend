8af5be59f0cfa79ccb7a904360d752ad066073b8
Steve Hobbs <elkdanger@gmail.com>
2017-10-17 11:40:18 +0100

    Added a basic route map for business activities

--- app/controllers/businessmatching/updateservice/NewServiceInformationController.scala
+++ app/controllers/businessmatching/updateservice/NewServiceInformationController.scala
@@ -18,14 +18,25 @@ package controllers.businessmatching.updateservice
 
 import javax.inject.Inject
 
+import cats.data.OptionT
 import cats.implicits._
 import connectors.DataCacheConnector
 import controllers.BaseController
+import models.asp.Asp
+import models.businessmatching._
+import models.estateagentbusiness.EstateAgentBusiness
 import play.api.i18n.MessagesApi
 import services.StatusService
 import services.businessmatching.BusinessMatchingService
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import views.html.businessmatching.updateservice.new_service_information
+import play.api.mvc.Result
+import uk.gov.hmrc.http.cache.client.CacheMap
+import models.moneyservicebusiness.{MoneyServiceBusiness => MSBModel}
+import uk.gov.hmrc.play.frontend.auth.AuthContext
+import uk.gov.hmrc.play.http.HeaderCarrier
+import models.hvd.Hvd
+import models.tcsp.Tcsp
 
 class NewServiceInformationController @Inject()(
                                        val authConnector: AuthConnector,
@@ -34,6 +45,8 @@ class NewServiceInformationController @Inject()(
                                        val businessMatchingService: BusinessMatchingService,
                                        val messages: MessagesApi) extends BaseController {
 
+
+
   def get() = Authorised.async {
     implicit request =>
       implicit authContext => {
@@ -41,6 +54,31 @@ class NewServiceInformationController @Inject()(
           activities <- businessMatchingService.getAdditionalBusinessActivities
         } yield Ok(new_service_information(activities.head))
       } getOrElse InternalServerError("Unable to get business activities")
+  }
+
+  private val activityToUrl = Map[BusinessActivity, String](
+    MoneyServiceBusiness -> controllers.msb.routes.WhatYouNeedController.get().url,
+    HighValueDealing -> controllers.hvd.routes.WhatYouNeedController.get().url,
+    TrustAndCompanyServices -> controllers.tcsp.routes.WhatYouNeedController.get().url,
+    EstateAgentBusinessService -> controllers.estateagentbusiness.routes.WhatYouNeedController.get().url,
+    AccountancyServices -> controllers.asp.routes.WhatYouNeedController.get().url
+  )
+
+  private val activityToData = Map[BusinessActivity, CacheMap => Boolean](
+    MoneyServiceBusiness -> { c => c.getEntry[MSBModel](MSBModel.key).fold(false)(_.isComplete(true, true)) },
+    HighValueDealing -> { c => c.getEntry[Hvd](Hvd.key).fold(false)(_.isComplete) },
+    TrustAndCompanyServices -> { c => c.getEntry[Tcsp](Tcsp.key).fold(false)(_.isComplete) },
+    EstateAgentBusinessService -> { c => c.getEntry[EstateAgentBusiness](EstateAgentBusiness.key).fold(false)(_.isComplete) },
+    AccountancyServices -> { c => c.getEntry[Asp](Asp.key).fold(false)(_.isComplete) }
+  )
+
+  private[controllers] def getNextFlow(implicit hc: HeaderCarrier, ac: AuthContext) = {
+    for {
+      cacheMap <- OptionT(dataCacheConnector.fetchAll)
+      activities <- businessMatchingService.getAdditionalBusinessActivities
+    } yield (activities collectFirst {
+      case act if !activityToData(act)(cacheMap) => Redirect(activityToUrl(act))
+    }).get
   }
 
   def post() = Authorised.async {
---
+++ test/controllers/businessmatching/updateservice/NewServiceInformationControllerSpec.scala
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.businessmatching.updateservice
+
+import cats.data.OptionT
+import cats.implicits._
+import models.businessmatching._
+import org.mockito.Matchers.{any, eq => eqTo}
+import org.mockito.Mockito.when
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mock.MockitoSugar
+import play.api.test.Helpers._
+import services.businessmatching.BusinessMatchingService
+import utils.{AuthorisedFixture, DependencyMocks, FutureAssertions, GenericTestHelper}
+import models.moneyservicebusiness.{MoneyServiceBusiness => MsbModel}
+import models.hvd.Hvd
+import models.tcsp.Tcsp
+import models.asp.Asp
+import models.estateagentbusiness.EstateAgentBusiness
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.mvc.Results.Redirect
+
+import scala.concurrent.Future
+import org.scalatest.time.{Millis, Seconds, Span}
+
+class NewServiceInformationControllerSpec extends GenericTestHelper with MockitoSugar with FutureAssertions with ScalaFutures {
+
+  implicit val defaultPatience =
+    PatienceConfig(timeout = Span(10, Seconds), interval = Span(1000, Millis))
+
+  trait Fixture extends AuthorisedFixture with DependencyMocks { self =>
+    val request = addToken(authRequest)
+
+    val bmService = mock[BusinessMatchingService]
+
+    val controller = new NewServiceInformationController(self.authConnector, mockCacheConnector, mockStatusService, bmService, messagesApi)
+
+    val msbModel = mock[MsbModel]
+    when(msbModel.isComplete(any(), any())) thenReturn false
+    mockCacheGetEntry[MsbModel](Some(msbModel), MsbModel.key)
+
+    val hvdModel = mock[Hvd]
+    when(hvdModel.isComplete) thenReturn false
+    mockCacheGetEntry[Hvd](Some(hvdModel), Hvd.key)
+
+    val tcspModel = mock[Tcsp]
+    when(tcspModel.isComplete) thenReturn false
+    mockCacheGetEntry[Tcsp](Some(tcspModel), Tcsp.key)
+
+    val eabModel = mock[EstateAgentBusiness]
+    when(eabModel.isComplete) thenReturn false
+    mockCacheGetEntry[EstateAgentBusiness](Some(eabModel), EstateAgentBusiness.key)
+
+    val aspModel = mock[Asp]
+    when(aspModel.isComplete) thenReturn false
+    mockCacheGetEntry[Asp](Some(aspModel), Asp.key)
+
+    def setUpActivities(activities: Set[BusinessActivity]) = when {
+      bmService.getAdditionalBusinessActivities(any(), any(), any())
+    } thenReturn OptionT.some[Future, Set[BusinessActivity]](activities)
+  }
+
+  "GET" when {
+    "called" must {
+      "return OK with the service name" in new Fixture {
+        when {
+          bmService.getAdditionalBusinessActivities(any(), any(), any())
+        } thenReturn OptionT.some[Future, Set[BusinessActivity]](Set(MoneyServiceBusiness))
+
+        val result = controller.get()(request)
+
+        status(result) mustBe OK
+
+        contentAsString(result) must include(MoneyServiceBusiness.getMessage)
+      }
+    }
+  }
+
+  "getNextFlow" must {
+    "return the correct state" when {
+      "adding the MSB service" in new Fixture {
+        setUpActivities(Set(MoneyServiceBusiness))
+
+        val result = await(controller.getNextFlow.value)
+
+        result mustBe Some(Redirect(controllers.msb.routes.WhatYouNeedController.get()))
+      }
+
+      "adding the HVD service" in new Fixture {
+        setUpActivities(Set(HighValueDealing))
+
+        val result = await(controller.getNextFlow.value)
+
+        result mustBe Some(Redirect(controllers.hvd.routes.WhatYouNeedController.get()))
+      }
+
+      "adding the TSCP service" in new Fixture {
+        setUpActivities(Set(TrustAndCompanyServices))
+
+        val result = await(controller.getNextFlow.value)
+
+        result mustBe Some(Redirect(controllers.tcsp.routes.WhatYouNeedController.get()))
+      }
+
+      "adding the EAB service" in new Fixture {
+        setUpActivities(Set(EstateAgentBusinessService))
+
+        val result = await(controller.getNextFlow.value)
+
+        result mustBe Some(Redirect(controllers.estateagentbusiness.routes.WhatYouNeedController.get()))
+      }
+
+      "adding the Accountancy service" in new Fixture {
+        setUpActivities(Set(AccountancyServices))
+
+        val result = await(controller.getNextFlow.value)
+
+        result mustBe Some(Redirect(controllers.asp.routes.WhatYouNeedController.get().url))
+      }
+    }
+  }
+}
