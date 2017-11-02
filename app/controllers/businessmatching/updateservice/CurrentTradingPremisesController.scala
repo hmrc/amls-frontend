02b20d365333b2009827659f79f10182d040228c
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2017-11-02 15:27:11 +0000

    WIP: Add index to current tp business matching controllers

--- app/controllers/businessmatching/updateservice/CurrentTradingPremisesController.scala
+++ app/controllers/businessmatching/updateservice/CurrentTradingPremisesController.scala
@@ -18,22 +18,21 @@ package controllers.businessmatching.updateservice
 
 import javax.inject.{Inject, Singleton}
 
-import cats.data.OptionT
 import cats.implicits._
 import connectors.DataCacheConnector
 import controllers.BaseController
+import controllers.businessmatching.updateservice.routes._
 import forms.{EmptyForm, Form2, InvalidForm, ValidForm}
 import models.businessmatching.updateservice._
 import play.api.mvc.Result
 import services.businessmatching.BusinessMatchingService
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.frontend.auth.AuthContext
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import views.html.businessmatching.updateservice.current_trading_premises
 
-import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import uk.gov.hmrc.http.HeaderCarrier
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class CurrentTradingPremisesController @Inject()(val authConnector: AuthConnector,
@@ -42,7 +41,7 @@ class CurrentTradingPremisesController @Inject()(val authConnector: AuthConnecto
 
   private def failure(msg: String = "Unable to get business activities") = InternalServerError(msg)
 
-  def get() = Authorised.async {
+  def get(index: Int = 0) = Authorised.async {
     implicit authContext => implicit request =>
       val result = getActivity map { activity =>
         Ok(current_trading_premises(EmptyForm, activity))
@@ -51,7 +50,7 @@ class CurrentTradingPremisesController @Inject()(val authConnector: AuthConnecto
       result getOrElse failure()
   }
 
-  def post() = Authorised.async {
+  def post(index: Int = 0) = Authorised.async {
     implicit authContext => implicit request =>
       Form2[AreSubmittedActivitiesAtTradingPremises](request.body) match {
           case f: InvalidForm => getActivity map { a => BadRequest(current_trading_premises(f, a)) } getOrElse failure()
@@ -63,11 +62,11 @@ class CurrentTradingPremisesController @Inject()(val authConnector: AuthConnecto
     data match {
       case SubmittedActivitiesAtTradingPremisesYes =>
         (businessMatchingService.fitAndProperRequired map {
-          case true => Redirect(routes.FitAndProperController.get())
-          case false => Redirect(controllers.businessmatching.updateservice.routes.NewServiceInformationController.get())
+          case true => Redirect(FitAndProperController.get())
+          case false => Redirect(NewServiceInformationController.get())
         }) getOrElse InternalServerError("Cannot retrieve activities")
       case SubmittedActivitiesAtTradingPremisesNo =>
-        Future.successful(Redirect(controllers.businessmatching.updateservice.routes.WhichCurrentTradingPremisesController.get()))
+        Future.successful(Redirect(WhichCurrentTradingPremisesController.get()))
     }
 
   private def getActivity(implicit hc: HeaderCarrier, ac: AuthContext) = for {
