370e9d2439c625d99c84948744190f81e702aa08
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2017-11-14 11:30:34 +0000

    Direct to remove from option in changeService

--- app/controllers/businessmatching/updateservice/ChangeServicesController.scala
+++ app/controllers/businessmatching/updateservice/ChangeServicesController.scala
@@ -18,12 +18,15 @@ package controllers.businessmatching.updateservice
 
 import javax.inject.Inject
 
-
+import cats.implicits._
+import cats.data.OptionT
 import connectors.DataCacheConnector
 import controllers.BaseController
 import forms.{EmptyForm, Form2, InvalidForm, ValidForm}
 import models.businessmatching._
-import models.businessmatching.updateservice.ChangeServices
+import models.businessmatching.updateservice.{ChangeServices, ChangeServicesAdd, ChangeServicesRemove}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.frontend.auth.AuthContext
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import utils.RepeatingSection
 import views.html.businessmatching.updateservice.change_services
@@ -35,45 +38,38 @@ class ChangeServicesController @Inject()(
                                           implicit val dataCacheConnector: DataCacheConnector
                                         ) extends BaseController with RepeatingSection {
 
-
-
   def get = Authorised.async {
     implicit authContext =>
       implicit request =>
-        dataCacheConnector.fetchAll map {
-          optionalCache =>
-            (for {
-              cache <- optionalCache
-              businessMatching <- cache.getEntry[BusinessMatching](BusinessMatching.key)
-            } yield {
-              val activities = businessMatching.activities.fold(Set.empty[String])(_.businessActivities.map(_.getMessage))
-              Ok(change_services(EmptyForm, activities))
-
-            }) getOrElse InternalServerError("Unable to show the page")
-        }
+        OptionT(getActivities) map { activities =>
+          Ok(change_services(EmptyForm, activities))
+        } getOrElse InternalServerError("Unable to show the page")
   }
 
   def post() = Authorised.async {
-    implicit authContext => implicit request => {
-      Form2[ChangeServices](request.body) match {
-        case f: InvalidForm => {
-          dataCacheConnector.fetchAll map {
-            optionalCache =>
-              (for {
-                cache <- optionalCache
-                businessMatching <- cache.getEntry[BusinessMatching](BusinessMatching.key)
-              } yield {
-                val activities = businessMatching.activities.fold(Set.empty[String])(_.businessActivities.map(_.getMessage))
-                BadRequest(change_services(f, activities))
-
-              }) getOrElse InternalServerError("Unable to show the page")
+    implicit authContext =>
+      implicit request => {
+        Form2[ChangeServices](request.body) match {
+          case f: InvalidForm =>
+            OptionT(getActivities) map { activities =>
+              BadRequest(change_services(f, activities))
+            } getOrElse InternalServerError("Unable to show the page")
+          case ValidForm(_, data) => data match {
+            case ChangeServicesAdd => Future.successful(Redirect(controllers.businessmatching.routes.RegisterServicesController.get()))
+            case ChangeServicesRemove => Future.successful(Redirect(routes.RemoveActivitiesController.get()))
           }
         }
-
-        case ValidForm(_, data) =>  Future.successful(Redirect(controllers.businessmatching.routes.RegisterServicesController.get()))
       }
-    }
   }
 
+  private def getActivities(implicit dataCacheConnector: DataCacheConnector, hc: HeaderCarrier, ac: AuthContext) = {
+    dataCacheConnector.fetchAll map {
+      optionalCache =>
+        for {
+          cache <- optionalCache
+          businessMatching <- cache.getEntry[BusinessMatching](BusinessMatching.key)
+        } yield businessMatching.activities.fold(Set.empty[String])(_.businessActivities.map(_.getMessage))
+    }
+  }
 
 }
