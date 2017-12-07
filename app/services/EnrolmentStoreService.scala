9cbe90162f618630b65024666497c928455a7a78
Steve Hobbs <elkdanger@gmail.com>
2017-12-07 16:51:58 +0000

    Simplified the search logic

--- app/services/EnrolmentStoreService.scala
+++ app/services/EnrolmentStoreService.scala
@@ -19,7 +19,6 @@ package services
 import javax.inject.Inject
 
 import connectors.EnrolmentStoreConnector
-import models.enrolment.{EnrolmentEntry, EnrolmentIdentifier}
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.frontend.auth.AuthContext
 
@@ -30,12 +29,10 @@ class EnrolmentStoreService @Inject()(connector: EnrolmentStoreConnector) {
   def getAmlsRegistrationNumber(implicit authContext: AuthContext, hc: HeaderCarrier, ec: ExecutionContext): Future[Option[String]] =
     connector.userEnrolments(authContext.user.userId) map {
       case Some(enrolment) if enrolment.totalRecords > 0 && enrolment.enrolments.nonEmpty =>
-        enrolment.enrolments.collectFirst {
-          case EnrolmentEntry("HMRC-MLR-ORG", _, _, idents) if idents.nonEmpty =>
-            idents.collectFirst {
-              case EnrolmentIdentifier("MLRRefNumber", refNumber) => refNumber
-            }
-        }.flatten
+        for {
+          entry <- enrolment.enrolments.find(_.service == "HMRC-MLR-ORG")
+          ident <- entry.identifiers.find(_.key == "MLRRefNumber")
+        } yield ident.value
       case _ => None
     }
 
