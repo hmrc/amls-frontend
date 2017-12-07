c59aff86c4dfd5587e58481ae870765ee9a04f0a
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2017-12-07 14:48:49 +0000

    Duplicate functionality of BankAccountNameController in BankAccountIsUKController

--- app/controllers/bankdetails/BankAccountIsUKController.scala
+++ app/controllers/bankdetails/BankAccountIsUKController.scala
@@ -18,15 +18,19 @@ package controllers.bankdetails
 
 import javax.inject.{Inject, Singleton}
 
+import cats.implicits._
+import cats.data.OptionT
 import config.{AMLSAuditConnector, AMLSAuthConnector}
 import connectors.DataCacheConnector
 import controllers.BaseController
-import forms.{EmptyForm, Form2}
+import forms.{EmptyForm, Form2, InvalidForm, ValidForm}
 import models.bankdetails.{BankAccount, BankDetails}
 import services.StatusService
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
-import utils.{ControllerHelper, RepeatingSection}
+import utils.{ControllerHelper, RepeatingSection, StatusConstants}
+
+import scala.concurrent.Future
 
 @Singleton
 class BankAccountIsUKController @Inject()(
@@ -51,10 +55,42 @@ class BankAccountIsUKController @Inject()(
         }
   }
 
-  def post(index: Int, edit: Boolean = false) = Authorised.async{
+  def post(index: Int, edit: Boolean = false) = Authorised.async {
     implicit authContext =>
-      implicit request =>
-      ???
+      implicit request => {
+
+        lazy val sendAudit = for {
+          details <- OptionT(getData[BankDetails](index))
+          result <- OptionT.liftF(auditConnector.sendEvent(audit.AddBankAccountEvent(details)))
+        } yield result
+
+        Form2[BankAccount](request.body) match {
+          case f: InvalidForm =>
+            Future.successful(BadRequest(views.html.bankdetails.bank_account_is_uk(f, edit, index)))
+          case ValidForm(_, data) =>
+            updateDataStrict[BankDetails](index) { bd =>
+              bd.copy(
+                bankAccount = Some(data),
+                status = Some(if (edit) {
+                  StatusConstants.Updated
+                } else {
+                  StatusConstants.Added
+                })
+              )
+            }.flatMap { _ =>
+              if (edit) {
+                Future.successful(Redirect(routes.SummaryController.get(false)))
+              } else {
+                lazy val redirect = Redirect(routes.BankAccountRegisteredController.get(index))
+                (sendAudit map { _ =>
+                  redirect
+                }) getOrElse redirect
+              }
+            }
+        }
+      }.recoverWith {
+        case _: IndexOutOfBoundsException => Future.successful(NotFound(notFoundView))
+      }
   }
 
 }
