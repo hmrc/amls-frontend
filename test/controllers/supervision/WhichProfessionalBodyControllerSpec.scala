89fec98c810794a04eda347ad75d4bbd8a41c626
Darryl Murphy <darryl.murphy@digital.hmrc.gov.uk>
2018-01-04 15:38:30 +0000

    First test of supervision/WhichProfessionalBodyController

--- test/controllers/supervision/WhichProfessionalBodyControllerSpec.scala
+++ test/controllers/supervision/WhichProfessionalBodyControllerSpec.scala
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.supervision
 
+import org.jsoup.Jsoup
+import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
+import play.api.i18n.Messages
+import play.api.test.Helpers._
 import utils.{AuthorisedFixture, DependencyMocks, GenericTestHelper}
 
-class WhichProfessionalBodyControllerSpec extends PlaySpec with GenericTestHelper {
+class WhichProfessionalBodyControllerSpec extends PlaySpec with GenericTestHelper with MockitoSugar{
 
   trait Fixture extends DependencyMocks with AuthorisedFixture { self =>
 
@@ -20,9 +40,11 @@ class WhichProfessionalBodyControllerSpec extends PlaySpec with GenericTestHelpe
 
     "get" must {
 
-      "be called" in new Fixture {
+      "return OK" in new Fixture {
 
-        val result = controller.get()
+        val result = controller.get()(request)
+
+        status(result) must be(OK)
 
       }
     }
