605f90cf74b5e0c6b61e4c3e0373c1d1d7395504
Graham Robinson <graham@grahamrobinsonsoftware.com>
2018-02-22 11:04:15 +0000

    AMLS-3364 | WIP | Added bacs confirmation pages to the TestOnly controller, and created new bacs_transitional_renewal view for transitional renewal journey.

--- app/controllers/testonly/TestOnlyController.scala
+++ app/controllers/testonly/TestOnlyController.scala
@@ -87,4 +87,17 @@ trait TestOnlyController extends BaseController with Actions {
     implicit authContext => implicit request =>
       Future.successful(Ok(views.html.confirmation.payment_confirmation_transitional_renewal("Company Name", "X123456789")))
   }
+
+  def ConfirmationBacs = Authorised.async {
+    implicit authContext => implicit request =>
+      Future.successful(Ok(views.html.confirmation.confirmation_bacs("Company Name")))
+  }
+
+  def ConfirmationBacsTransitionalRenewal = Authorised.async {
+    implicit authContext => implicit request =>
+      Future.successful(Ok(views.html.confirmation.confirmation_bacs_transitional_renewal("Company Name")))
+  }
+
+
+
 }
---
+++ test/views/confirmation/BacsConfirmationTransitionalRenewalViewSpec.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.confirmation
+
+import org.scalatest.MustMatchers
+import play.api.i18n.Messages
+import utils.GenericTestHelper
+import views.Fixture
+
+class BacsConfirmationTransitionalRenewalViewSpec extends GenericTestHelper with MustMatchers {
+
+  trait ViewFixture extends Fixture {
+    implicit val requestWithToken = addToken(request)
+
+    override def view = views.html.confirmation.confirmation_bacs_transitional_renewal(
+      "businessName"
+    )
+  }
+
+  "The bacs transitional renewal confirmation view" must {
+
+    "show the correct title" in new ViewFixture {
+      doc.title must startWith(Messages("confirmation.payment.bacs.title"))
+    }
+
+    "show the correct header" in new ViewFixture {
+      doc.select(".heading-large").text must include(Messages("confirmation.payment.bacs.header"))
+    }
+
+    "show the correct secondary header" in new ViewFixture {
+      doc.select(".confirmation p").text must include("businessName")
+    }
+
+    "contain the correct content" in new ViewFixture {
+      doc.html() must include(Messages("confirmation.payment.info.transitional.renewal.hmrc_review"))
+      doc.html() must include(Messages("confirmation.payment.info.transitional.renewal.hmrc_review2"))
+    }
+
+    "have a footer with the correct information" in new ViewFixture {
+      doc.html() must include(Messages("confirmation.payment.info.heading.keep_up_to_date"))
+      doc.html() must include(Messages("confirmation.payment.info.keep_up_to_date"))
+      doc.html() must include(Messages("confirmation.payment.info.keep_up_to_date.item1"))
+      doc.html() must include(Messages("confirmation.payment.info.keep_up_to_date.item2"))
+      doc.html() must include(Messages("confirmation.payment.info.keep_up_to_date.item3"))
+      doc.getElementsByClass("print-link").first().text() mustBe "Print"
+      doc.getElementsByClass("button").first().text() mustBe Messages("confirmation.payment.continue_button.text")
+    }
+
+  }
+
+}
