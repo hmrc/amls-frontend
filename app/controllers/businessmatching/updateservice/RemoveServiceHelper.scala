940932304aee9ce94c38a71cfbe7dc175a140ebe
David Wright <davew@MacBook-Pro-3.local>
2018-05-13 09:30:50 +0100

    AMLS-3630 | WIP | Finished RemoveServiceHelper code

--- app/controllers/businessmatching/updateservice/RemoveServiceHelper.scala
+++ app/controllers/businessmatching/updateservice/RemoveServiceHelper.scala
@@ -28,8 +28,8 @@ import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.frontend.auth.AuthContext
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 
-import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class RemoveServiceHelper @Inject()(val authConnector: AuthConnector,
@@ -40,70 +40,71 @@ class RemoveServiceHelper @Inject()(val authConnector: AuthConnector,
 
     for {
       activitiesToRemove <- OptionT.fromOption[Future](model.activitiesToRemove)
+
       currentBusinessMatching <- OptionT(dataCacheConnector.fetch[BMBusinessMatching](BMBusinessMatching.key))
       currentActivities <- OptionT.fromOption[Future](currentBusinessMatching.activities) orElse OptionT.some[Future, BMBusinessActivities](BMBusinessActivities(Set.empty[BMBusinessActivity]))
 
       newBusinessMatching <- {
 
         OptionT(dataCacheConnector.update[BMBusinessMatching](BMBusinessMatching.key) {
-          case Some(bm) if activitiesToRemove.contains(MoneyServiceBusiness)  =>
-            bm.activities(currentActivities.copy(businessActivities = currentActivities.businessActivities - MoneyServiceBusiness))
+
+          case Some(bm) if activitiesToRemove.contains(MoneyServiceBusiness) =>
+            bm.activities(currentActivities.copy(businessActivities = currentActivities.businessActivities -- activitiesToRemove))
               .msbServices(None)
               .businessAppliedForPSRNumber(None)
               .copy(hasAccepted = true)
-          case Some(bm) => bm
-        }
-        )
+
+          case Some(bm) => bm.activities(currentActivities.copy(businessActivities = currentActivities.businessActivities -- activitiesToRemove)).copy(hasAccepted = true)
+        })
       }
     } yield newBusinessMatching
-
   }
 
   def removeTradingPremisesBusinessActivities(model: RemoveServiceFlowModel)(implicit ac: AuthContext, hc: HeaderCarrier, ec: ExecutionContext): OptionT[Future, TradingPremises] = {
 
     for {
       activitiesToRemove <- OptionT.fromOption[Future](model.activitiesToRemove)
+
       currentTradingPremises <- OptionT(dataCacheConnector.fetch[TradingPremises](TradingPremises.key))
-      currentActivities <- OptionT.fromOption[Future](currentTradingPremises.whatDoesYourBusinessDoAtThisAddress) orElse OptionT.some[Future, WhatDoesYourBusinessDo](WhatDoesYourBusinessDo(Set.empty[BMBusinessActivity]))
+      currentTradingPremisesActivities <- OptionT.fromOption[Future](currentTradingPremises.whatDoesYourBusinessDoAtThisAddress) orElse OptionT.some[Future, WhatDoesYourBusinessDo](WhatDoesYourBusinessDo(Set.empty[BMBusinessActivity]))
 
       newTradingPremises <- {
 
         OptionT(dataCacheConnector.update[TradingPremises](TradingPremises.key) {
-          case Some(tp) if activitiesToRemove.contains(MoneyServiceBusiness)  =>
-            tp.whatDoesYourBusinessDoAtThisAddress(currentActivities.copy(activities = currentActivities.activities - MoneyServiceBusiness))
+          case Some(tp) if activitiesToRemove.contains(MoneyServiceBusiness) =>
+            tp.whatDoesYourBusinessDoAtThisAddress(currentTradingPremisesActivities.copy(activities = currentTradingPremisesActivities.activities -- activitiesToRemove))
               .msbServices(None)
               .copy(hasAccepted = true)
-          case Some(tp) => tp
-        }
-        )
+          case Some(tp) => tp.whatDoesYourBusinessDoAtThisAddress(currentTradingPremisesActivities.copy(activities = currentTradingPremisesActivities.activities -- activitiesToRemove))
+            .copy(hasAccepted = true)
+        })
       }
     } yield newTradingPremises
-
   }
 
   def removeFitAndProper(model: RemoveServiceFlowModel)(implicit ac: AuthContext, hc: HeaderCarrier, ec: ExecutionContext): OptionT[Future, ResponsiblePeople] = {
 
     for {
       activitiesToRemove <- OptionT.fromOption[Future](model.activitiesToRemove)
-      currentResponsiblePeople <- OptionT(dataCacheConnector.fetch[ResponsiblePeople](ResponsiblePeople.key))
 
       currentBusinessMatching <- OptionT(dataCacheConnector.fetch[BMBusinessMatching](BMBusinessMatching.key))
       currentActivities <- OptionT.fromOption[Future](currentBusinessMatching.activities) orElse OptionT.some[Future, BMBusinessActivities](BMBusinessActivities(Set.empty[BMBusinessActivity]))
 
+      currentResponsiblePeople <- OptionT(dataCacheConnector.fetch[ResponsiblePeople](ResponsiblePeople.key))
 
       newResponsiblePeople <- {
+
         val hasTCSP: Boolean = currentActivities.businessActivities.contains(TrustAndCompanyServices)
+        val hasMSB: Boolean = currentActivities.businessActivities.contains(MoneyServiceBusiness)
 
         OptionT(dataCacheConnector.update[ResponsiblePeople](ResponsiblePeople.key) {
-          case Some(rp) if activitiesToRemove.contains(MoneyServiceBusiness) && !hasTCSP  =>
+          case Some(rp) if (activitiesToRemove.contains(MoneyServiceBusiness) && !hasTCSP) ||
+            (activitiesToRemove.contains(TrustAndCompanyServices) && !hasMSB) =>
             rp.hasAlreadyPassedFitAndProper(None)
               .copy(hasAccepted = true)
           case Some(rp) => rp
-        }
-
-        )
+        })
       }
     } yield newResponsiblePeople
-
   }
 }
