f277790f3fd90d5cc204b02b681d5b22d11b34df
Fiona Hobbs <fiona.m.mcdonald@gmail.com>
2018-05-14 11:05:26 +0100

    AMLS-3630 UnableToRemoveActivitiesController skeleton

---
+++ app/controllers/businessmatching/updateservice/remove/UnableToRemoveActivitiesController.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.businessmatching.updateservice.remove
+
+import connectors.DataCacheConnector
+import controllers.BaseController
+import javax.inject.{Inject, Singleton}
+import jto.validation.forms.UrlFormEncoded
+import jto.validation.{Path, Rule, RuleLike}
+import models.FormTypes
+import models.businessmatching.{BusinessActivities, BusinessActivity}
+import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
+
+@Singleton
+class UnableToRemoveActivitiesController @Inject()(
+                                          val authConnector: AuthConnector,
+                                          val dataCacheConnector: DataCacheConnector
+                                          ) extends BaseController {
+
+  def get = Authorised.async{
+    implicit authContext =>
+      implicit request => ???
+  }
+
+  def post = Authorised.async{
+    implicit authContext =>
+      implicit request => ???
+  }
+
+}
