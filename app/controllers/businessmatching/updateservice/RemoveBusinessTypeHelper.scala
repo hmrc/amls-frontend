54a221f4be57068678e2a42b4fe268a4f361aa65
Steve Hobbs <elkdanger@gmail.com>
2018-05-21 16:47:10 +0100

    AMLS-3847: TP activity pre-populated when no services left

--- app/controllers/businessmatching/updateservice/RemoveBusinessTypeHelper.scala
+++ app/controllers/businessmatching/updateservice/RemoveBusinessTypeHelper.scala
@@ -87,12 +87,23 @@ class RemoveBusinessTypeHelper @Inject()(val authConnector: AuthConnector,
     val setAccepted = (tp: TradingPremises) => tp.copy(hasAccepted = true)
 
     for {
+      businessMatching <- OptionT(dataCacheConnector.fetch[BMBusinessMatching](BMBusinessMatching.key))
+      currentActivities <- OptionT.fromOption[Future](businessMatching.activities)
       activitiesToRemove <- OptionT.fromOption[Future](model.activitiesToRemove)
       newTradingPremises <- {
         OptionT(dataCacheConnector.update[Seq[TradingPremises]](TradingPremises.key) {
           case Some(tpList) => tpList map { tp =>
             val currentBusinessTypes = tp.whatDoesYourBusinessDoAtThisAddress.getOrElse(WhatDoesYourBusinessDo(Set.empty))
-            val newBusinessTypes = currentBusinessTypes.copy(activities = currentBusinessTypes.activities -- activitiesToRemove)
+
+            val newTPActivities = currentBusinessTypes.activities -- activitiesToRemove
+            val newBusinessActivities = currentActivities.businessActivities -- activitiesToRemove
+
+            val newActivities = (newTPActivities, newBusinessActivities) match {
+              case (x, y) if x.isEmpty && y.size == 1 => y
+              case (x, _) => x
+            }
+
+            val newBusinessTypes = currentBusinessTypes.copy(activities = newActivities)
             val newPremises = tp.whatDoesYourBusinessDoAtThisAddress(newBusinessTypes)
 
             setAccepted {
