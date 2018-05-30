cf326a7020b7eb0ca01aa92e7c384f3db568cbce
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2018-05-30 09:28:16 +0100

    AMLS-3866 |  Added enter_details_hint message for bank details page. Added Paragraph(Line 39) to what you need page. Created unit Test

--- test/views/bankdetails/what_you_needSpec.scala
+++ test/views/bankdetails/what_you_needSpec.scala
@@ -35,19 +35,25 @@ class what_you_needSpec extends AmlsSpec with MustMatchers {
       doc.title must startWith(Messages("title.wyn"))
     }
 
-    "Have the correct Headings" in new ViewFixture{
+    "Have the correct Headings" in new ViewFixture {
       def view = views.html.bankdetails.what_you_need(0)
 
-      heading.html must be (Messages("title.wyn"))
-      subHeading.html must include (Messages("summary.bankdetails"))
+      heading.html must be(Messages("title.wyn"))
+      subHeading.html must include(Messages("summary.bankdetails"))
     }
 
-    "contain the expected content elements" in new ViewFixture{
+    "contain the expected content elements" in new ViewFixture {
       def view = views.html.bankdetails.what_you_need(0)
 
       html must include(Messages("bankdetails.whatyouneed.line_1"))
       html must include(Messages("bankdetails.whatyouneed.line_2"))
       html must include(Messages("bankdetails.whatyouneed.line_3"))
+    }
+
+    "provide the expected hint" in new ViewFixture {
+      def view = views.html.bankdetails.what_you_need(0)
+
+      html must include(Messages("bankdetails.whatyouneed.enter_details_hint"))
     }
   }
 }
