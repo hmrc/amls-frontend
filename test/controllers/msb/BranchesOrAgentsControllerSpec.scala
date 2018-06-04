8761400ffef79945baf0e1b30187258edb74f3ef
David Wright <davew@MacBook-Pro-3.local>
2018-06-04 14:30:02 +0100

    refactor-for-di | merged with master

--- test/controllers/msb/BranchesOrAgentsControllerSpec.scala
+++ test/controllers/msb/BranchesOrAgentsControllerSpec.scala
@@ -16,18 +16,14 @@
 
 package controllers.msb
 
-import connectors.DataCacheConnector
-import controllers.hvd.CashPaymentController
 import models.Country
 import models.moneyservicebusiness.{BranchesOrAgents, MoneyServiceBusiness}
 import org.jsoup.Jsoup
 import org.mockito.Matchers.{eq => eqTo, _}
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
-import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.cache.client.CacheMap
-import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import utils.{AmlsSpec, AuthorisedFixture, DependencyMocks}
 
 import scala.concurrent.Future
