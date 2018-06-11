6a872d713dfe85842317f69f672231fba773b2e8
Matthew Harris <33547722+matthewharris1@users.noreply.github.com>
2018-06-11 13:46:25 +0100

    Amls 3552 remove register another account page (#665)
    * AMLS-3552 | Removed 4th button from bankaccounttypes view
    
    * AMLS-3552 | Removed uneeded test
    
    * AMLS-3552 | Remove register another account page
    
    * AMLS-3552 | Amended AccountRegister redirects to YourBankAccounts

--- test/controllers/bankdetails/BankAccountIsUKControllerSpec.scala
+++ test/controllers/bankdetails/BankAccountIsUKControllerSpec.scala
@@ -163,7 +163,7 @@ class BankAccountIsUKControllerSpec extends AmlsSpec with MockitoSugar {
           val result = controller.post(1)(newRequest)
 
           status(result) must be(SEE_OTHER)
-          redirectLocation(result) must be(Some(routes.BankAccountRegisteredController.get(1).url))
+          redirectLocation(result) must be(Some(routes.YourBankAccountsController.get().url))
         }
 
       }
@@ -226,7 +226,7 @@ class BankAccountIsUKControllerSpec extends AmlsSpec with MockitoSugar {
         val result = controller.post(1)(newRequest)
 
         status(result) must be(SEE_OTHER)
-        redirectLocation(result) must be(Some(routes.BankAccountRegisteredController.get(1).url))
+        redirectLocation(result) must be(Some(routes.YourBankAccountsController.get().url))
 
         val captor = ArgumentCaptor.forClass(classOf[DataEvent])
         verify(controller.auditConnector).sendEvent(captor.capture())(any(), any())
