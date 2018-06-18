4b7ea214a263add8f5830d4a3478c7a3b21694c9
Steve Hobbs <elkdanger@gmail.com>
2018-06-18 14:16:14 +0100

    AMLS-3898 Fixed summary page for MSB

--- app/controllers/msb/SummaryController.scala
+++ app/controllers/msb/SummaryController.scala
@@ -21,6 +21,7 @@ import cats.implicits._
 import connectors.DataCacheConnector
 import controllers.BaseController
 import javax.inject.Inject
+import models.businessmatching.updateservice.ServiceChangeRegister
 import models.businessmatching.{BusinessMatching, MoneyServiceBusiness => MsbActivity}
 import models.moneyservicebusiness.MoneyServiceBusiness
 import services.StatusService
@@ -47,8 +48,9 @@ class SummaryController @Inject()
             cache <- optionalCache
             businessMatching <- cache.getEntry[BusinessMatching](BusinessMatching.key)
             msb <- cache.getEntry[MoneyServiceBusiness](MoneyServiceBusiness.key)
+            register <- cache.getEntry[ServiceChangeRegister](ServiceChangeRegister.key) orElse Some(ServiceChangeRegister())
           } yield {
-            ControllerHelper.allowedToEdit(MsbActivity) map(x => Ok(summary(msb, businessMatching.msbServices, x)))
+            ControllerHelper.allowedToEdit(MsbActivity) map(x => Ok(summary(msb, businessMatching.msbServices, x, register)))
           }) getOrElse Future.successful(Redirect(controllers.routes.RegistrationProgressController.get()))
       }
   }
