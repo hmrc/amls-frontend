3b2b3097989dd60236803f4b8309000eabf95462
Drew Miley <drew.miley@opencastsoftware.com>
2018-06-21 11:16:52 +0100

    AMLS-3978 | Adds empty unit tests for your_messages template

--- test/views/notifications/your_messagesSpec.scala
+++ test/views/notifications/your_messagesSpec.scala
@@ -19,6 +19,7 @@ package views.notifications
 import models.notifications.NotificationRow
 import org.scalatest.MustMatchers
 import play.api.i18n.Messages
+import play.twirl.api.HtmlFormat
 import utils.AmlsSpec
 import views.Fixture
 
@@ -31,24 +32,80 @@ class your_messagesSpec extends AmlsSpec with MustMatchers  {
         val emptyNotifications: Seq[NotificationRow] = Seq()
 
         val businessName = "Fake Name Ltd."
+
+        def view = views.html.notifications.your_messages(businessName, emptyNotifications, emptyNotifications)
     }
 
     "your_messages view" must {
 
         "have correct title" in new ViewFixture {
-
-            def view = views.html.notifications.your_messages(businessName, emptyNotifications)
-
             doc.title must be(Messages("notifications.header") +
                     " - " + Messages("title.amls") +
                     " - " + Messages("title.gov"))
         }
 
+        "have a back button link" in new ViewFixture {
+            override def view: HtmlFormat.Appendable = ???
+        }
+
         "have correct headings" in new ViewFixture {
-
-            def view = views.html.notifications.your_messages(businessName, emptyNotifications)
-
             heading.html must be(Messages("notifications.header"))
+        }
+
+        "have a panel displaying the business name" in new ViewFixture {
+            override def view: HtmlFormat.Appendable = ???
+        }
+
+        "have a first notification table title" in new ViewFixture {
+            override def view: HtmlFormat.Appendable = ???
+        }
+
+        "have a first notification table header" in new ViewFixture {
+            override def view: HtmlFormat.Appendable = ???
+        }
+
+        "have a row for each of the current notifications in the first notification table" in new ViewFixture {
+            override def view: HtmlFormat.Appendable = ???
+        }
+
+        "for a row in a notification table" must {
+
+            "display the subject of the notification" in new ViewFixture {
+                override def view: HtmlFormat.Appendable = ???
+            }
+
+            "display the date of the notification" in new ViewFixture {
+                override def view: HtmlFormat.Appendable = ???
+            }
+
+        }
+
+        "when previous notifications is an empty Seq" must {
+
+            "have one table with class notifications" in new ViewFixture {
+                override def view: HtmlFormat.Appendable = ???
+            }
+
+        }
+
+        "when previous notifications is a Seq containing notifications" must {
+
+            "have a second notification table header" in new ViewFixture {
+                override def view: HtmlFormat.Appendable = ???
+            }
+
+            "have two tables with class notifications" in new ViewFixture {
+                override def view: HtmlFormat.Appendable = ???
+            }
+
+            "have a second notification table title" in new ViewFixture {
+                override def view: HtmlFormat.Appendable = ???
+            }
+
+            "have a row for each of the previous notification" in new ViewFixture {
+                override def view: HtmlFormat.Appendable = ???
+            }
+
         }
 
     }
