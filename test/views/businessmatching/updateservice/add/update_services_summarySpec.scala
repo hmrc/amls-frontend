13cb26b2a16a80d137870e37d9c0e07fe86e4a36
Drew Miley <drew.miley@opencastsoftware.com>
2018-07-06 11:37:48 +0100

    AMLS-4111 | Adds empty unit tests for add service summary page

--- test/views/businessmatching/updateservice/add/update_services_summarySpec.scala
+++ test/views/businessmatching/updateservice/add/update_services_summarySpec.scala
@@ -46,5 +46,128 @@ class update_services_summarySpec  extends AmlsSpec with MustMatchers {
     "have correct subHeading" in new ViewFixture {
       subHeading.html must include(Messages("summary.updateservice"))
     }
+
+    "for which business type you wish to register" must {
+      "have a question title" in new ViewFixture {
+
+      }
+
+      "display the answer" in new ViewFixture {
+
+      }
+
+      "show edit link" in new ViewFixture {
+
+      }
+    }
+
+    "for have any of your responsible people have passed the HMRC fit and proper test" must {
+      "have a question title" in new ViewFixture {
+
+      }
+
+      "display the answer" in new ViewFixture {
+
+      }
+
+      "show edit link" in new ViewFixture {
+
+      }
+    }
+
+    "if any have passed test, which responsible people have passed the HMRC fit and proper test" must {
+      "have a question title" in new ViewFixture {
+
+      }
+
+      "display the answer" in new ViewFixture {
+
+      }
+
+      "show edit link" in new ViewFixture {
+
+      }
+    }
+
+    "for will you do this business type at trading premises" must {
+      "have a question title" in new ViewFixture {
+
+      }
+
+      "display the answer" in new ViewFixture {
+
+      }
+
+      "show edit link" in new ViewFixture {
+
+      }
+    }
+
+    "if yes for will you do, which trading premises you will do this business type at" must {
+      "have a question title" in new ViewFixture {
+
+      }
+
+      "display the answer" in new ViewFixture {
+
+      }
+
+      "show edit link" in new ViewFixture {
+
+      }
+    }
+    
+    "if adding MSB" must {
+      "which services does your business provide" must {
+        "have a question title" in new ViewFixture {
+  
+        }
+  
+        "display the answer" in new ViewFixture {
+  
+        }
+  
+        "show edit link" in new ViewFixture {
+  
+        }
+      }
+  
+      "for what will your business do at these premises" must {
+        "have a question title" in new ViewFixture {
+  
+        }
+  
+        "display the answer" in new ViewFixture {
+  
+        }
+  
+        "for the edit link" must {
+          "not display when business does only one type of MSB subservice" in new ViewFixture {
+  
+          }
+  
+          "display when business does more than one type of MSB subservice" in new ViewFixture {
+  
+          }
+        }
+      }
+      
+      "if adding TransmittingMoney as an MSB subsector" must {
+        "for does your business have a PSR number" must {
+          "have a question title" in new ViewFixture {
+
+          }
+
+          "display the answer" in new ViewFixture {
+
+          }
+
+          "show edit link" in new ViewFixture {
+
+          }
+        }
+      }
+      
+    }
   }
 }
