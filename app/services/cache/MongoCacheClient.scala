57d88331b2c26595084761172127a8fab8f17c0f
Steve Hobbs <elkdanger@gmail.com>
2018-07-11 11:58:55 +0100

    AMLS-4079 Implemented saveAll properly in MongoCacheClient

--- app/services/cache/MongoCacheClient.scala
+++ app/services/cache/MongoCacheClient.scala
@@ -18,7 +18,6 @@ package services.cache
 
 import config.AppConfig
 import connectors.cache.Conversions
-import javax.inject.Inject
 import org.joda.time.{DateTime, DateTimeZone}
 import play.api.Logger
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -28,13 +27,14 @@ import reactivemongo.api.commands.WriteResult
 import reactivemongo.api.indexes.{Index, IndexType}
 import reactivemongo.bson.{BSONDocument, BSONObjectID}
 import reactivemongo.play.json.ImplicitBSONHandlers._
+import uk.gov.hmrc.crypto._
 import uk.gov.hmrc.crypto.json.{JsonDecryptor, JsonEncryptor}
-import uk.gov.hmrc.crypto.{ApplicationCrypto, CompositeSymmetricCrypto, Protected}
 import uk.gov.hmrc.http.cache.client.CacheMap
 import uk.gov.hmrc.mongo.ReactiveRepository
 import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
 
 import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
 
 sealed trait CacheOps {
 
@@ -102,24 +102,11 @@ class MongoCacheClient(appConfig: AppConfig, db: () => DefaultDB)
 
   val timeToLiveInSeconds: Int = 300
 
-  createIndex("lastUpdated", "cacheExpiry", Some(timeToLiveInSeconds))
-  createIndex("cacheId", "cacheIdIndex")
+  createIndex("lastUpdated", "cacheExpiry", timeToLiveInSeconds)
 
-  private def createIndex(field: String, indexName: String, ttl: Option[Int] = None): Future[Boolean] = {
-    collection.indexesManager.ensure(Index(
-      Seq((field, IndexType.Ascending)),
-      Some(indexName),
-      options = BSONDocument("expireAfterSeconds" -> ttl))
-    ) map { result =>
-      Logger.debug(s"Index $indexName set with value $ttl -> result: $result")
-      result
-    } recover {
-      case e => Logger.error("Failed to set TTL index", e); false
-    }
-  }
-
-//  private val expireAfter: Long = defaultExpireAfter
-
+  /**
+    * Inserts data into the cache with the specified key. If the data does not exist, it will be created.
+    */
   def createOrUpdate[T](id: String, data: T, key: String)(implicit writes: Writes[T]): Future[Cache] = {
     val jsonData = if (appConfig.mongoEncryptionEnabled) {
       val jsonEncryptor = new JsonEncryptor[T]()
@@ -136,45 +123,83 @@ class MongoCacheClient(appConfig: AppConfig, db: () => DefaultDB)
         lastUpdated = DateTime.now(DateTimeZone.UTC)
       )
 
-      val selector = BSONDocument("_id" -> BSONObjectID(id))
       val document = Json.toJson(updatedCache)
       val modifier = BSONDocument("$set" -> document)
 
-      collection.update(selector, modifier, upsert = true) map { _ => updatedCache }
+      collection.update(bsonIdQuery(id), modifier, upsert = true) map { _ => updatedCache }
     }
   }
 
-  def find[T](id: String, key: String)(implicit reads: Reads[T]): Future[Option[T]] = {
-//    if (appConfig.mongoEncryptionEnabled) {
-//      cacheRepository.findById(id) map {
-//        case Some(cache) => decryptValue(cache, key)(new JsonDecryptor[T](), reads)
-//        case None => None
-//      }
-//    } else {
-//      cacheRepository.findById(id) map {
-//        case Some(cache) => getValue(cache, key)
-//        case None => None
-//      }
-//    }
-
+  /**
+    * Finds an item in the cache with the specified key. If the item cannot be found, None is returned.
+    */
+  def find[T](id: String, key: String)(implicit reads: Reads[T]): Future[Option[T]] =
     fetchAll(id) map {
-      case Some(cache) => getValue[T](cache, key)
+      case Some(cache) => if(appConfig.mongoEncryptionEnabled) {
+        decryptValue[T](cache, key)(new JsonDecryptor[T](), reads)
+      } else {
+        getValue[T](cache, key)
+      }
       case _ => None
     }
-  }
 
-  def fetchAll(id: String): Future[Option[Cache]] = collection.find(BSONDocument("_id" -> BSONObjectID(id))).one[Cache]
+  /**
+    * Fetches the whole cache
+    */
+  def fetchAll(id: String): Future[Option[Cache]] = collection.find(bsonIdQuery(id)).one[Cache] map {
+    case Some(c) if appConfig.mongoEncryptionEnabled => Some(new CryptoCache(c, compositeSymmetricCrypto))
+    case c => c
+  }
 
   /**
     * Removes the item with the specified id from the cache
     */
-  def removeById(id: String): Future[Boolean] = collection.remove(BSONDocument("_id" -> BSONObjectID(id))) map handleWriteResult
+  def removeById(id: String): Future[Boolean] = collection.remove(bsonIdQuery(id)) map handleWriteResult
 
   /**
     * Saves the cache data into the database
     */
-  def saveAll(cache: Cache): Future[Boolean] = ??? // cacheRepository.save(cache) map handleWriteResult
+  def saveAll(cache: Cache): Future[Boolean] = {
 
+    // Rebuild the cache and decrypt each key if necessary
+    val rebuiltCache = Cache(cache.id, cache.data.foldLeft(Map.empty[String, JsValue]) { (acc, value) =>
+      val plainText = tryDecrypt(Crypted(value._2.toString))
+
+      if (appConfig.mongoEncryptionEnabled) {
+        acc + (value._1 -> JsString(compositeSymmetricCrypto.encrypt(plainText).value))
+      } else {
+        acc + (value._1 -> Json.parse(plainText.value))
+      }
+    })
+
+    collection.update(bsonIdQuery(cache.id), BSONDocument("$set" -> Json.toJson(rebuiltCache)), upsert = true) map handleWriteResult
+
+  }
+
+  /**
+    * Creates a new index on the specified field, using the specified name and the ttl
+    */
+  private def createIndex(field: String, indexName: String, ttl: Int): Future[Boolean] = {
+    collection.indexesManager.ensure(Index(
+      Seq((field, IndexType.Ascending)),
+      Some(indexName),
+      options = BSONDocument("expireAfterSeconds" -> ttl))
+    ) map { result =>
+      Logger.debug(s"Index $indexName set with value $ttl -> result: $result")
+      result
+    } recover {
+      case e => Logger.error("Failed to set TTL index", e); false
+    }
+  }
+
+  /**
+    * Generates a BSON document query for an id
+    */
+  private def bsonIdQuery(id: String) = BSONDocument("_id" -> BSONObjectID(id))
+
+  /**
+    * Handles logging for write results
+    */
   private def handleWriteResult(writeResult: WriteResult) = writeResult match {
     case w if w.ok => true
     case w if w.writeErrors.nonEmpty =>
@@ -182,5 +207,13 @@ class MongoCacheClient(appConfig: AppConfig, db: () => DefaultDB)
       throw new RuntimeException(w.writeErrors.map(_.errmsg).mkString("; "))
     case _ =>
       throw new RuntimeException("Error while removing the session data")
+  }
+
+  private def tryDecrypt(value: Crypted): PlainText = Try {
+    compositeSymmetricCrypto.decrypt(value).value
+  } match {
+    case Success(v) => PlainText(v)
+    case Failure(e) if e.isInstanceOf[SecurityException] => PlainText(value.value)
+    case Failure(e) => throw e
   }
 }
