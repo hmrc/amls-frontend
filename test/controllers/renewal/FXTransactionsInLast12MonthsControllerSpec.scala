183b9e29454ce68c1d9cb2c81d29e12955d35e6b
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2018-08-01 12:12:18 +0100

    AMLS-4026 | AMLS-4027 | Adds view, controller, model for FX renewal question. Project now compiles

---
+++ test/controllers/renewal/FXTransactionsInLast12MonthsControllerSpec.scala
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.renewal
+
+import connectors.DataCacheConnector
+import models.renewal.{FXTransactionsInLast12Months, Renewal}
+import org.mockito.Matchers.{eq => eqTo, _}
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import play.api.i18n.Messages
+import play.api.test.Helpers._
+import services.RenewalService
+import uk.gov.hmrc.http.cache.client.CacheMap
+import utils.{AmlsSpec, AuthorisedFixture}
+
+import scala.concurrent.Future
+
+class FXTransactionsInLast12MonthsControllerSpec extends AmlsSpec with MockitoSugar  {
+
+  trait Fixture extends AuthorisedFixture {
+    self => val request = addToken(authRequest)
+
+    lazy val mockDataCacheConnector = mock[DataCacheConnector]
+    lazy val mockRenewalService = mock[RenewalService]
+
+    val controller = new FXTransactionsInLast12MonthsController (
+      dataCacheConnector = mockDataCacheConnector,
+      authConnector = self.authConnector,
+      renewalService = mockRenewalService
+    )
+  }
+
+  val emptyCache = CacheMap("", Map.empty)
+
+  "MsbForeignExchangeTransactionsController" must {
+
+    "load the page 'How many foreign exchange transactions'" in new Fixture {
+
+      when(controller.dataCacheConnector.fetch[Renewal](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+
+      val result = controller.get()(request)
+      status(result) must be(OK)
+      contentAsString(result) must include(Messages("renewals.msb.fx.transactions.expected.title"))
+    }
+
+    "load the page 'How many foreign exchange transactions' with pre populated data" in new Fixture  {
+
+      when(controller.dataCacheConnector.fetch[Renewal](any())
+        (any(), any(), any())).thenReturn(Future.successful(Some(Renewal(
+        fxTransactionsInLast12Months = Some(FXTransactionsInLast12Months("12345678963"))))))
+
+      val result = controller.get()(request)
+      status(result) must be(OK)
+      contentAsString(result) must include("12345678963")
+
+    }
+
+    "Show error message when user has not filled the mandatory fields" in new Fixture  {
+
+      val newRequest = request.withFormUrlEncodedBody(
+        "fxTransaction" -> ""
+      )
+
+      when(controller.dataCacheConnector.fetch[Renewal](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+
+      when(mockRenewalService.updateRenewal(any())(any(),any(), any()))
+        .thenReturn(Future.successful(emptyCache))
+
+      val result = controller.post()(newRequest)
+      status(result) must be(BAD_REQUEST)
+      contentAsString(result) must include (Messages("error.required.renewal.fx.transactions.in.12months"))
+
+    }
+
+    "Successfully save data in save4later and navigate to Next page" in new Fixture {
+      val newRequest = request.withFormUrlEncodedBody (
+        "fxTransaction" -> "12345678963"
+      )
+
+      when(controller.dataCacheConnector.fetch[Renewal](any())
+        (any(), any(), any())).thenReturn(Future.successful(None))
+
+      when(mockRenewalService.updateRenewal(any())(any(),any(), any()))
+        .thenReturn(Future.successful(emptyCache))
+
+      val result = controller.post()(newRequest)
+      status(result) must be(SEE_OTHER)
+      redirectLocation(result) must be(Some(controllers.renewal.routes.SummaryController.get().url))
+    }
+
+    "Successfully save data in save4later and navigate to Summary page in edit mode" in new Fixture {
+
+      val incomingModel = Renewal(
+      )
+
+      val outgoingModel = incomingModel.copy(
+        fxTransactionsInLast12Months = Some(
+          FXTransactionsInLast12Months("12345678963")
+        ), hasChanged = true
+      )
+
+      val newRequest = request.withFormUrlEncodedBody (
+        "ceTransaction" -> "12345678963"
+      )
+
+      when(controller.dataCacheConnector.fetch[Renewal](eqTo(Renewal.key))
+        (any(), any(), any())).thenReturn(Future.successful(Some(incomingModel)))
+
+      when(mockRenewalService.updateRenewal(any())(any(),any(), any()))
+        .thenReturn(Future.successful(emptyCache))
+
+      val result = controller.post(true)(newRequest)
+      status(result) must be(SEE_OTHER)
+      redirectLocation(result) must be(Some(controllers.renewal.routes.SummaryController.get().url))
+    }
+
+  }
+}
---
+++ test/models/renewal/FXTransactionsInLast12MonthsSpec.scala
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.renewal
+
+import jto.validation.{Invalid, Path, Valid, ValidationError}
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.{JsPath, JsSuccess}
+
+class FXTransactionsInLast12MonthsSpec extends PlaySpec {
+
+  "FXTransactionIn" should {
+
+    "Form Validation" must {
+
+      "Successfully read form data for option yes" in {
+
+        val map = Map("fxTransaction" -> Seq("12345678963"))
+
+        FXTransactionsInLast12Months.formRule.validate(map) must be(Valid(FXTransactionsInLast12Months("12345678963")))
+      }
+
+      "fail validation on missing field" in {
+
+        val map = Map("fxTransaction" -> Seq(""))
+
+        FXTransactionsInLast12Months.formRule.validate(map) must be(Invalid(
+          Seq( Path \ "fxTransaction" -> Seq(ValidationError("error.required.renewal.fx.transactions.in.12months")))))
+      }
+
+      "fail validation on invalid field" in {
+
+        val map = Map("fxTransaction" -> Seq("asas"))
+        FXTransactionsInLast12Months.formRule.validate(map) must be(Invalid(
+          Seq( Path \ "fxTransaction" -> Seq(ValidationError("error.invalid.msb.transactions.in.12months")))))
+      }
+
+      "fail validation on invalid field when it exceeds the max length" in {
+
+        val map = Map("fxTransaction" -> Seq("123"*10))
+        FXTransactionsInLast12Months.formRule.validate(map) must be(Invalid(
+          Seq( Path \ "fxTransaction" -> Seq(ValidationError("error.invalid.msb.transactions.in.12months")))))
+      }
+
+      "fail validation on invalid field1" in {
+
+        val map = Map("fxTransaction" -> Seq("123456"))
+        FXTransactionsInLast12Months.formRule.validate(map) must be(Valid(FXTransactionsInLast12Months("123456")))
+      }
+
+
+      "successfully write form data" in {
+
+        FXTransactionsInLast12Months.formWrites.writes(FXTransactionsInLast12Months("12345678963")) must be(Map("fxTransaction" -> Seq("12345678963")))
+      }
+    }
+
+    "Json Validation" must {
+
+      "Successfully read/write Json data" in {
+
+        FXTransactionsInLast12Months.format.reads(FXTransactionsInLast12Months.format.writes(
+          FXTransactionsInLast12Months("12345678963"))) must be(JsSuccess(FXTransactionsInLast12Months("12345678963"), JsPath \ "fxTransaction"))
+
+      }
+    }
+  }
+}
