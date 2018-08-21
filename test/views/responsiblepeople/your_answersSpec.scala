353229cfb1fd002182fe61c7b9cc948e1133c999
--list <slawomir.bedra@opencastsoftware.com>
2018-08-21 14:20:12 +0100

    AMLS-4210 Added missing unit tests and id's to complete/incomplete headers.

--- test/views/responsiblepeople/your_answersSpec.scala
+++ test/views/responsiblepeople/your_answersSpec.scala
@@ -17,9 +17,12 @@
 package views.responsiblepeople
 
 import forms.EmptyForm
-import models.responsiblepeople.ResponsiblePerson
+import models.bankdetails.BankDetails
+import models.responsiblepeople.{PersonName, ResponsiblePerson}
+import org.openqa.selenium.WebElement
 import org.scalatest.MustMatchers
 import play.api.i18n.Messages
+import play.twirl.api.HtmlFormat
 import utils.AmlsSpec
 import views.Fixture
 
@@ -30,8 +33,18 @@ class your_answersSpec extends AmlsSpec with MustMatchers {
     implicit val requestWithToken = addToken(request)
   }
 
+  val completePersonName: Option[PersonName] = Some(PersonName("Katie", None, "Test"))
+  val incompletePersonName: Option[PersonName] = Some(PersonName("John", None, "Test"))
+  val completeRp1 = ResponsiblePerson(completePersonName)
+  val completeRp2 = ResponsiblePerson()
+  val incompleteRp1 = ResponsiblePerson()
+  val incompleteRp2 = ResponsiblePerson(incompletePersonName)
+
+  val completeRpSeq = Seq((completeRp1,0),(completeRp2,1))
+  val incompleteRpSeq = Seq((incompleteRp1,2),(incompleteRp2,3))
+
   "What you need View" must {
-    "have correct title, headings and form fields" in new ViewFixture {
+    "have correct title, headings, form fields and bullet list of types of RP's to register displayed" in new ViewFixture {
       val form2 = EmptyForm
 
       def view = views.html.responsiblepeople.your_answers(Seq((ResponsiblePerson(),0)), Seq((ResponsiblePerson(),0)))
@@ -45,6 +58,51 @@ class your_answersSpec extends AmlsSpec with MustMatchers {
 
       doc.getElementsByAttributeValue("id", "addResponsiblePerson") must not be empty
 
+      html must include(Messages("responsiblepeople.whomustregister.line_1"))
+      html must include(Messages("responsiblepeople.whomustregister.line_2"))
+      html must include(Messages("responsiblepeople.whomustregister.line_3"))
+      html must include(Messages("responsiblepeople.whomustregister.line_4"))
+      html must include(Messages("responsiblepeople.whomustregister.line_5"))
+
+    }
+
+    "have list with Incomplete and Complete headers displayed when there are both types of lists" in new ViewFixture {
+      def  view = views.html.responsiblepeople.your_answers(completeRpSeq, incompleteRpSeq)
+
+      html must include(Messages("responsiblepeople.check_your_answers.incomplete"))
+      html must include(Messages("responsiblepeople.check_your_answers.complete"))
+
+    }
+
+    "have list with Incomplete header displayed when there are only incomplete RP's" in new ViewFixture {
+      def  view = views.html.responsiblepeople.your_answers(Seq.empty[(ResponsiblePerson, Int)], incompleteRpSeq)
+
+      html must include(Messages("responsiblepeople.check_your_answers.incomplete"))
+      html must not include Messages("responsiblepeople.check_your_answers.complete")
+
+    }
+
+    "have list without Complete/Incomplete headers displayed when there are only complete RP's" in new ViewFixture {
+      def  view = views.html.responsiblepeople.your_answers(completeRpSeq, Seq.empty[(ResponsiblePerson, Int)])
+
+      html must not include Messages("responsiblepeople.check_your_answers.incomplete")
+      html must not include Messages("responsiblepeople.check_your_answers.complete")
+    }
+
+    "have an add a responsible person link with the correct text and going to the what you need page" in new ViewFixture {
+      def  view = views.html.responsiblepeople.your_answers(incompleteRpSeq, completeRpSeq)
+
+      doc.getElementById("addResponsiblePerson").text must be(Messages("responsiblepeople.check_your_answers.add"))
+      doc.getElementById("addResponsiblePerson").attr("href") must be(controllers.responsiblepeople.routes.ResponsiblePeopleAddController.get(false).url)
+    }
+
+    "have an incomplete/complete sections with people names displayed" in new ViewFixture {
+      def view = views.html.responsiblepeople.your_answers(incompleteRpSeq, completeRpSeq)
+
+      doc.getElementById("incomplete-header").text must include(Messages("responsiblepeople.check_your_answers.incomplete"))
+      html must include("Katie Test")
+      doc.getElementById("complete-header").text must include(Messages("responsiblepeople.check_your_answers.complete"))
+      html must include("John Test")
     }
   }
 }
