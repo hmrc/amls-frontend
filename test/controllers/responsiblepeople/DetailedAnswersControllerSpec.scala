7cbee09651a77994019371492ec12270fdc37018
--list <slawomir.bedra@opencastsoftware.com>
2018-08-23 09:43:35 +0100

    AMLS-4210 Fixed test for DetailedAnswersController after last code changes.

--- test/controllers/responsiblepeople/DetailedAnswersControllerSpec.scala
+++ test/controllers/responsiblepeople/DetailedAnswersControllerSpec.scala
@@ -60,7 +60,7 @@ class DetailedAnswersControllerSpec extends AmlsSpec with MockitoSugar {
 
   "DetailedAnswersController" when {
 
-    "get is called - from the yourAnswers controller" must {
+    "get is called - from the yourResponsiblePeople controller" must {
       "respond with OK and show the detailed answers page with a 'confirm and continue'" in new Fixture {
 
         val result = controller.get(1)(request)
@@ -68,7 +68,7 @@ class DetailedAnswersControllerSpec extends AmlsSpec with MockitoSugar {
 
         val document = Jsoup.parse(contentAsString(result))
         contentAsString(result) must include(Messages("responsiblepeople.detailed_answers.title"))
-        contentAsString(result) must not include "/anti-money-laundering/responsible-people/check-your-answers"
+        contentAsString(result) must include("/anti-money-laundering/responsible-people/check-your-answers")
       }
     }
 
