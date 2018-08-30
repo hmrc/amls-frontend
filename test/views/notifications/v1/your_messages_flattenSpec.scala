a4f2dfa63b44d9a70ce50d40778629395978e258
Drew Miley <drew.miley@opencastsoftware.com>
2018-08-30 15:01:24 +0100

    AMLS-3873 | Adds incomplete unit tests for your_messages

--- test/views/notifications/v1/your_messages_flattenSpec.scala
+++ test/views/notifications/v1/your_messages_flattenSpec.scala
@@ -16,6 +16,64 @@
 
 package views.notifications.v1
 
-class your_messages_flattenSpec {
+import models.notifications.{IDType, NotificationRow}
+import org.joda.time.DateTime
+import org.scalatest.MustMatchers
+import utils.AmlsSpec
+import views.Fixture
+
+class your_messages_flattenSpec extends AmlsSpec with MustMatchers {
+
+    trait ViewFixture extends Fixture {
+        implicit val requestWithToken = addToken(request)
+        val currentNotifications: Seq[NotificationRow] = Seq(
+            NotificationRow(None, None, None, variation = true, new DateTime(2018, 1, 1, 0, 0), isRead = true, "XA000", IDType("id"))
+        )
+        val previousNotifications: Seq[NotificationRow] = Seq(
+            NotificationRow(None, None, None, variation = true, new DateTime(2018, 1, 1, 0, 0), isRead = true, "XA000", IDType("id"))
+        )
+    }
+
+    "your_messages flattened view" must {
+        "be the same as non-flattened view when there are current and previous notifications" in new ViewFixture {
+            val viewV1 = views.html.notifications.v1.your_messages("businessName", currentNotifications, previousNotifications)
+            val htmlV1 = viewV1.body.filterNot(Set('\n', '\t', ' ').contains)
+
+            val view = views.html.notifications.your_messages("businessName", currentNotifications, previousNotifications)
+            val htmlUnflattened = view.body.filterNot(Set('\n', '\t', ' ').contains)
+
+            htmlV1 mustEqual htmlUnflattened
+        }
+
+        "be the same as non-flattened view when there are current notifications only" in new ViewFixture {
+            val viewV1 = views.html.notifications.v1.your_messages("businessName", currentNotifications, Seq())
+            val htmlV1 = viewV1.body.filterNot(Set('\n', '\t', ' ').contains)
+
+            val view = views.html.notifications.your_messages("businessName", currentNotifications, Seq())
+            val htmlUnflattened = view.body.filterNot(Set('\n', '\t', ' ').contains)
+
+            htmlV1 mustEqual htmlUnflattened
+        }
+
+        "be the same as non-flattened view when there are previous notifications only" in new ViewFixture {
+            val viewV1 = views.html.notifications.v1.your_messages("businessName", Seq(), previousNotifications)
+            val htmlV1 = viewV1.body.filterNot(Set('\n', '\t', ' ').contains)
+
+            val view = views.html.notifications.your_messages("businessName", Seq(), previousNotifications)
+            val htmlUnflattened = view.body.filterNot(Set('\n', '\t', ' ').contains)
+
+            htmlV1 mustEqual htmlUnflattened
+        }
+
+        "be the same as non-flattened view when there are neither current or previous notifications" in new ViewFixture {
+            val viewV1 = views.html.notifications.v1.your_messages("businessName", Seq(), Seq())
+            val htmlV1 = viewV1.body.filterNot(Set('\n', '\t', ' ').contains)
+
+            val view = views.html.notifications.your_messages("businessName", Seq(), Seq())
+            val htmlUnflattened = view.body.filterNot(Set('\n', '\t', ' ').contains)
+
+            htmlV1 mustEqual htmlUnflattened
+        }
+    }
 
 }
