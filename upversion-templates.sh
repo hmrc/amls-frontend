27e280dcd4289b1675360241cd527c6053c4874c
Drew Miley <drew.miley@opencastsoftware.com>
2018-09-05 15:57:45 +0100

    AMLS-3873 | AMLS-4069 | Gets array of package names

--- upversion-templates.sh
+++ upversion-templates.sh
@@ -3,3 +3,11 @@ if [ "$1" = "minor" ]; then
     minor=true
 fi
 echo $minor
+
+packages=(./app/views/notifications/*/)
+
+for i in "${!packages[@]}"; do
+    packages[$i]=${packages[$i]:26:4}
+done
+
+echo $packages
