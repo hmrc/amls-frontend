5e7bbbf7fe403b25f9a4e153b840fb8dd9f41a37
Drew Miley <drew.miley@opencastsoftware.com>
2018-09-05 16:22:52 +0100

    AMLS-3873 | AMLS-4069 | forms new package version variable

--- upversion-templates.sh
+++ upversion-templates.sh
@@ -11,3 +11,24 @@ for i in "${!packages[@]}"; do
 done
 
 echo $packages
+
+majorversion=${packages[0]:1:1}
+
+echo $majorversion
+
+minorversion=${packages[0]:3:1}
+
+echo $minorversion
+
+if $minor; then
+    ((minorversion++))
+    echo $minorversion
+else
+    ((majorversion++))
+    minorversion=0
+    echo $majorversion
+fi
+
+newpackageversion=v${majorversion}m${minorversion}
+
+echo $newpackageversion
