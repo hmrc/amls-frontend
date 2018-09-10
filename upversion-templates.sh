bc4b552cdcfc7e0e47e62c62f50e5f9a9fbea045
Drew Miley <drew.miley@opencastsoftware.com>
2018-09-10 14:59:36 +0100

    AMLS-3873 | Adds echo statements to upversioning script

--- upversion-templates.sh
+++ upversion-templates.sh
@@ -37,11 +37,17 @@ fi
 
 newpackageversion=v${majorversion}m${minorversion}
 
+echo "Creating new template version $newpackageversion"
+
 mkdir ./app/views/notifications/${newpackageversion}
 cp -r ./app/views/notifications/${previouspackageversion}/. ./app/views/notifications/${newpackageversion}
 
+echo "Copied directory ./app/views/notifications/..."
+
 mkdir ./test/views/notifications/${newpackageversion}
 cp -r ./test/views/notifications/${previouspackageversion}/. ./test/views/notifications/${newpackageversion}
+
+echo "Copied directory ./test/views/notifications/..."
 
 viewunittestfilesfornewpackageversion=(./test/views/notifications/${newpackageversion}/*)
 
@@ -50,11 +56,17 @@ for i in "${!viewunittestfilesfornewpackageversion[@]}"; do
     sed -i '' "s/${previouspackageversion}/${newpackageversion}/g" $viewunittestfile
 done
 
+echo "Updated package for new directory ./test/views/notifications/$newpackageversion"
+
 mkdir ./app/services/notifications/${newpackageversion}
 cp -r ./app/services/notifications/${previouspackageversion}/. ./app/services/notifications/${newpackageversion}
 
+echo "Copied directory ./app/services/notifications/..."
+
 mkdir ./test/services/notifications/${newpackageversion}
 cp -r ./test/services/notifications/${previouspackageversion}/. ./test/services/notifications/${newpackageversion}
+
+echo "Copied directory ./test/services/notifications/..."
 
 servicefilesfornewpackageversion=(./app/services/notifications/${newpackageversion}/*)
 
@@ -63,6 +75,8 @@ for i in "${!servicefilesfornewpackageversion[@]}"; do
     sed -i '' "s/${previouspackageversion}/${newpackageversion}/g" $servicefile
 done
 
+echo "Updated package for new directory ./app/services/notifications/$newpackageversion"
+
 serviceunittestfilesfornewpackageversion=(./test/services/notifications/${newpackageversion}/*)
 
 for i in "${!serviceunittestfilesfornewpackageversion[@]}"; do
@@ -70,7 +84,16 @@ for i in "${!serviceunittestfilesfornewpackageversion[@]}"; do
     sed -i '' "s/${previouspackageversion}/${newpackageversion}/g" $serviceunittestfile
 done
 
+echo "Updated package for new directory ./test/services/notifications/$newpackageversion"
+
 mkdir ./conf/notifications/${newpackageversion}
 cp -r ./conf/notifications/${previouspackageversion}/. ./conf/notifications/${newpackageversion}
 
+echo "Copied checksums files in ./conf/notifications/..."
+
 sed -i '' "s/${previouspackageversion}\"/${previouspackageversion}\",\"${newpackageversion}\"/g" ./test/NotificationsCheckSumSpec.scala
+
+echo "Added $newpackageversion to ./test/NotificationsCheckSumSpec.scala"
+echo "TODO: checksums for services will have to be updated"
+
+echo "Upversion complete"
