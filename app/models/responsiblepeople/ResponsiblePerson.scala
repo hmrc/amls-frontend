9f23fd0c5be4cb3b291e3cc83b6b57ca7a1885d9
Drew Miley <drew.miley@opencastsoftware.com>
2018-09-19 15:22:55 +0100

    AMLS-4131 | Adds validation to Phase2 ResponsiblePerson isComplete logic for person with no previous name

--- app/models/responsiblepeople/ResponsiblePerson.scala
+++ app/models/responsiblepeople/ResponsiblePerson.scala
@@ -143,7 +143,7 @@ case class ResponsiblePerson(personName: Option[PersonName] = None,
     this match {
       case ResponsiblePerson(Some(_),Some(_),Some(_),Some(_),Some(_), _, _, Some(_),Some(_),Some(_), Some(pos),Some(_), _,Some(_),Some(_), Some(_), _, true, _, _, _, otherBusinessSP)
         if pos.startDate.isDefined & checkVatField(otherBusinessSP) & validateAddressHistory & ApplicationConfig.phase2ChangesToggle => true
-      case ResponsiblePerson(Some(_),Some(pName),None,Some(_),Some(_), _, _, Some(_),Some(_),Some(_), Some(pos),Some(_), _,Some(_),Some(_), _, _, true, _, _, _, otherBusinessSP)
+      case ResponsiblePerson(Some(_),Some(pName),None,Some(_),Some(_), _, _, Some(_),Some(_),Some(_), Some(pos),Some(_), _,Some(_),Some(_), Some(_), _, true, _, _, _, otherBusinessSP)
         if pos.startDate.isDefined & checkVatField(otherBusinessSP) & validateAddressHistory && !pName.hasPreviousName.get & ApplicationConfig.phase2ChangesToggle => true
       case ResponsiblePerson(Some(_),Some(_),Some(_),Some(_),Some(_), _, _, _,Some(_),Some(_), Some(pos),Some(_), _,Some(_),Some(_), _, _, true, _, _, _, otherBusinessSP)
         if pos.startDate.isDefined & checkVatField(otherBusinessSP) & validateAddressHistory & !ApplicationConfig.phase2ChangesToggle => true
