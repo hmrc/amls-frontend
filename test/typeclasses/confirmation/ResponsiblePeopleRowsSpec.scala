f0f647a7f76d183b5e18bbf3c16fd1a027af5745
matthewharris1 <matthew.harris@digital.hmrc.gov.uk>
2018-09-24 16:28:30 +0100

    Implemented mock responsible people rows

--- test/typeclasses/confirmation/ResponsiblePeopleRowsSpec.scala
+++ test/typeclasses/confirmation/ResponsiblePeopleRowsSpec.scala
@@ -16,30 +16,69 @@
 
 package typeclasses.confirmation
 
+import models.businessactivities.BusinessActivities
+import models.{AmendVariationRenewalResponse, SubmissionResponse, SubscriptionFees, SubscriptionResponse}
+import models.businessmatching.{BusinessActivity, MoneyServiceBusiness}
+import models.confirmation.BreakdownRow
+import models.responsiblepeople.{PersonName, ResponsiblePerson}
 import org.scalatestplus.play.PlaySpec
 
 // TODO: Implement
 class ResponsiblePeopleRowsSpec extends PlaySpec {
 
+    val amendVariationRenewalResponse = AmendVariationRenewalResponse(
+        processingDate = "",
+        etmpFormBundleNumber = "",
+        registrationFee = 1.0,
+        fpFee = Some(2.0),
+        fpFeeRate = Some(3.0),
+        premiseFee = 4.0,
+        premiseFeeRate = Some(5.0),
+        totalFees = 6.0,
+        paymentReference = None,
+        difference = Some(7.0),
+        addedResponsiblePeople = 8,
+        addedResponsiblePeopleFitAndProper = 9,
+        addedFullYearTradingPremises = 10,
+        halfYearlyTradingPremises = 11,
+        zeroRatedTradingPremises = 12
+    )
+
+    val subscriptionResponse = SubscriptionResponse(
+      etmpFormBundleNumber = "",
+      amlsRefNo = "",
+      subscriptionFees = Some(SubscriptionFees(
+        registrationFee = 1.0,
+        fpFee = Some(2.0),
+        fpFeeRate = Some(3.0),
+        premiseFee = 4.0,
+        premiseFeeRate = Some(5.0),
+        totalFees = 6.0,
+        paymentReference = ""
+      )),
+      previouslySubmitted = None
+    )
+
+    val activities: Set[BusinessActivity] = Set(
+        MoneyServiceBusiness
+    )
+
+    val responsiblePeople = Some(Seq(
+        ResponsiblePerson(personName = Some(PersonName("firstName", None, "lastName")))
+    ))
+
     "value is a AmendVariationRenewalResponse" when {
         "businessActivities is None" must {
             "set BreakdownRows for responsible people" in {
-
+              val breakdownRows: Seq[BreakdownRow] = ResponsiblePeopleRowsInstances.
+                responsiblePeopleRowsFromVariation(
+                  amendVariationRenewalResponse,
+                  activities,
+                  responsiblePeople
+                )
             }
 
             "set BreakdownRows for fit & proper charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises full year charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises half year charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises no charge" in {
 
             }
         }
@@ -50,42 +89,18 @@ class ResponsiblePeopleRowsSpec extends PlaySpec {
             }
 
             "set BreakdownRows for fit & proper charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises full year charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises half year charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises no charge" in {
 
             }
         }
     }
 
-    "value is a SubmissionResponse" when {
+    "value is a SubscriptionResponse" when {
         "businessActivities is None" must {
             "set BreakdownRows for responsible people" in {
 
             }
 
             "set BreakdownRows for fit & proper charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises full year charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises half year charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises no charge" in {
 
             }
         }
@@ -96,18 +111,6 @@ class ResponsiblePeopleRowsSpec extends PlaySpec {
             }
 
             "set BreakdownRows for fit & proper charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises full year charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises half year charge" in {
-
-            }
-
-            "set BreakdownRows for trading premises no charge" in {
 
             }
         }
