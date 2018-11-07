55a6d57aa7890ca1e528cb04c141b41845fc4f31
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2018-11-07 13:59:32 +0000

    AMLS-4023 | Added unit tests for dublicate subscription view + added to messages file

---
+++ test/views/submission/duplicate_submissionSpec.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.submission
+
+import org.scalatest.MustMatchers
+import play.api.i18n.Messages
+import utils.AmlsSpec
+import views.Fixture
+
+
+class duplicate_submissionSpec extends AmlsSpec with MustMatchers {
+
+  trait ViewFixture extends Fixture {
+    implicit val requestWithToken = addToken(request)
+
+    val errorMsg = "Error message"
+
+    def view = views.html.submission.duplicate_submission(errorMsg)
+  }
+  "The duplicate submission view" must {
+    "display the correct title and headings" in new ViewFixture {
+
+      doc.title mustBe s"${Messages("error.submission.duplicate.title")} - ${Messages("title.amls")} - ${Messages("title.gov")}"
+      doc.select("header h1").text() mustBe Messages("error.submission.duplicate.title")
+
+      doc.html must include(errorMsg)
+
+      validateParagraphizedContent("error.submission.duplicate_submission.content")
+    }
+  }
+
+}
