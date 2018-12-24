f21ce6abd322328223990f4dd152139445aad581
Sam Zaroug <sam.zaroug@digital.hmrc.gov.uk>
2018-12-24 10:50:29 +0000

    AMLS-3260 | AgentName no longer depends on release7 toggle

--- test/models/tradingpremises/AgentNameSpec.scala
+++ test/models/tradingpremises/AgentNameSpec.scala
@@ -26,61 +26,6 @@ import play.api.test.FakeApplication
 
 class AgentNameSpec extends PlaySpec with OneAppPerSuite {
 
-  override lazy val app = FakeApplication(additionalConfiguration = Map("microservice.services.feature-toggle.release7" -> false))
-
-  "AgentName" must {
-
-    "validate form Read" in {
-      val formInput = Map(
-        "agentName" -> Seq("sometext")
-      )
-
-      AgentName.formReads.validate(formInput) must be(Valid(AgentName("sometext", None)))
-    }
-
-    "throw error when required name field is missing" in {
-      val formInput = Map("agentName" -> Seq(""))
-      AgentName.formReads.validate(formInput) must be(Invalid(Seq((Path \ "agentName", Seq(ValidationError("error.required.tp.agent.name"))))))
-    }
-
-    "throw error when name input exceeds max length" in {
-      val formInput = Map("agentName" -> Seq("sometesttexttest" * 11))
-      AgentName.formReads.validate(formInput) must be(Invalid(Seq((Path \ "agentName") -> Seq(ValidationError("error.invalid.tp.agent.name")))))
-    }
-
-    "throw error when name input has invalid data" in {
-      val formInput = Map("agentName" -> Seq("<sometesttexttest>"))
-      AgentName.formReads.validate(formInput) must be(Invalid(Seq((Path \ "agentName") -> Seq(ValidationError("err.text.validation")))))
-    }
-
-    "validate form write" in {
-      AgentName.formWrites.writes(AgentName("sometext")) must be(Map("agentName" -> Seq("sometext")))
-    }
-
-  }
-
-  "Json Validation" must {
-    "Successfully read/write Json data" in {
-      AgentName.format.reads(AgentName.format.writes(
-        AgentName("test", Some(DateOfChange(new LocalDate(2017, 1, 1)))))) must be(
-        JsSuccess(
-          AgentName("test", Some(DateOfChange(new LocalDate(2017, 1, 1))))))
-    }
-
-    "Succesfully read/write Json data with agent dob" in {
-
-      AgentName.format.reads(AgentName.format.writes(
-        AgentName("test", Some(DateOfChange(new LocalDate(2017, 1, 1))), Some(new LocalDate(2015, 10, 10))))) must be(
-        JsSuccess(
-          AgentName("test", Some(DateOfChange(new LocalDate(2017, 1, 1))), Some(new LocalDate(2015, 10, 10)))))
-
-    }
-
-  }
-}
-
-class AgentNameSpecR7 extends PlaySpec with OneAppPerSuite {
-
   override lazy val app = FakeApplication(additionalConfiguration = Map("microservice.services.feature-toggle.release7" -> true))
 
   "AgentName" must {
@@ -109,6 +54,24 @@ class AgentNameSpecR7 extends PlaySpec with OneAppPerSuite {
 
       AgentName.formReads.validate(noContentModel) must be(Invalid(Seq((Path \ "agentDateOfBirth", Seq(ValidationError("error.expected.jodadate.format", "yyyy-MM-dd"))))))
     }
+  }
+  "Json Validation" must {
+    "Successfully read/write Json data" in {
+      AgentName.format.reads(AgentName.format.writes(
+        AgentName("test", Some(DateOfChange(new LocalDate(2017, 1, 1)))))) must be(
+        JsSuccess(
+          AgentName("test", Some(DateOfChange(new LocalDate(2017, 1, 1))))))
+    }
+
+    "Succesfully read/write Json data with agent dob" in {
+
+      AgentName.format.reads(AgentName.format.writes(
+        AgentName("test", Some(DateOfChange(new LocalDate(2017, 1, 1))), Some(new LocalDate(2015, 10, 10))))) must be(
+        JsSuccess(
+          AgentName("test", Some(DateOfChange(new LocalDate(2017, 1, 1))), Some(new LocalDate(2015, 10, 10)))))
+
+    }
+
   }
 
 }
