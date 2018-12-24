4dceefd18a52bbefecb0949174a09fd00c293f1d
Sam Zaroug <sam.zaroug@digital.hmrc.gov.uk>
2018-12-24 14:13:01 +0000

    AMLS-3260 | removed release7 flag from SummaryControllers and view

--- test/views/tradingpremises/agent_nameSpec.scala
+++ test/views/tradingpremises/agent_nameSpec.scala
@@ -16,71 +16,28 @@
 
 package views.tradingpremises
 
-import forms.{EmptyForm, InvalidForm, ValidForm}
-import models.tradingpremises.AgentName
+import forms.{EmptyForm, InvalidForm}
+import jto.validation.{Path, ValidationError}
 import org.scalatest.MustMatchers
 import play.api.i18n.Messages
-import play.api.test.FakeApplication
 import utils.AmlsSpec
 import views.Fixture
-import jto.validation.{Path, ValidationError}
 
 class agent_nameSpec extends AmlsSpec with MustMatchers {
 
-  override lazy val app = FakeApplication(additionalConfiguration = Map("microservice.services.feature-toggle.release7" -> false))
-
-
   trait ViewFixture extends Fixture {
     implicit val requestWithToken = addToken(request)
   }
 
-  "agent_name view" must {
+  "have correct title and heading" in new ViewFixture {
 
-    "have correct title and heading" in new ViewFixture {
+    def view: _root_.play.twirl.api.HtmlFormat.Appendable =
+      views.html.tradingpremises.agent_name(EmptyForm, 0, false)
 
-      def view: _root_.play.twirl.api.HtmlFormat.Appendable =
-        views.html.tradingpremises.agent_name(EmptyForm, 0, false)
+    doc.title() must startWith(Messages("tradingpremises.agentname.title") + " - " + Messages("summary.tradingpremises"))
+    heading.html() must be(Messages("tradingpremises.agentname.title"))
 
-      doc.title() must startWith(Messages("tradingpremises.agentname.title") + " - " + Messages("summary.tradingpremises"))
-      heading.html() must be(Messages("tradingpremises.agentname.title"))
-
-    }
-
-    "not include date of birth" in new ViewFixture {
-
-      def view: _root_.play.twirl.api.HtmlFormat.Appendable =
-        views.html.tradingpremises.agent_name(EmptyForm, 0, false)
-
-      doc.html() must not include (Messages("tradingpremises.agentname.name.dateOfBirth.lbl"))
-    }
-
-    "show errors in correct places when validation fails" in new ViewFixture {
-
-      val messageKey1 = "definitely not a message key"
-      val agentNameField = "agentName"
-      val form2: InvalidForm = InvalidForm(Map("thing" -> Seq("thing")),
-        Seq((Path \ agentNameField, Seq(ValidationError(messageKey1)))))
-
-      def view: _root_.play.twirl.api.HtmlFormat.Appendable =
-        views.html.tradingpremises.agent_name(form2, 0, false)
-
-      errorSummary.html() must include(messageKey1)
-      doc.getElementById(agentNameField).parent().getElementsByClass("error-notification").first().html() must include(messageKey1)
-
-    }
   }
-
-}
-
-class agent_nameSpecR7 extends AmlsSpec with MustMatchers {
-
-  override lazy val app = FakeApplication(additionalConfiguration = Map("microservice.services.feature-toggle.release7" -> true))
-
-
-  trait ViewFixture extends Fixture {
-    implicit val requestWithToken = addToken(request)
-  }
-
 
   "include date of birth" in new ViewFixture {
 
