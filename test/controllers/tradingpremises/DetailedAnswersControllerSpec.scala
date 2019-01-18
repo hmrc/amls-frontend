d3de69fd7767d51c20d91f69152502ce86fada1d
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2019-01-18 12:59:22 +0000

    AMLS-3551 | Unit tests update

--- test/controllers/tradingpremises/DetailedAnswersControllerSpec.scala
+++ test/controllers/tradingpremises/DetailedAnswersControllerSpec.scala
@@ -16,26 +16,73 @@
 
 package controllers.tradingpremises
 
+import java.util.UUID
+
+import models.tradingpremises.TradingPremises
+import play.api.test.Helpers.{OK, contentAsString, status}
 import connectors.DataCacheConnector
-import controllers.tradingpremises.DetailedAnswersController
+import models.businessmatching.{AccountancyServices, BillPaymentServices, BusinessMatching, EstateAgentBusinessService, BusinessActivities => BusinessMatchingActivities, _}
+import models.status.SubmissionDecisionApproved
+import org.jsoup.Jsoup
+import org.mockito.Matchers.{eq => eqTo, _}
+import org.mockito.Mockito.when
 import org.scalatest.mock.MockitoSugar
+import play.api.i18n.Messages
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import services.StatusService
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.cache.client.CacheMap
+import uk.gov.hmrc.play.frontend.auth.AuthContext
 import utils.{AmlsSpec, AuthorisedFixture}
+
+import scala.concurrent.Future
+
 
 class DetailedAnswersControllerSpec extends AmlsSpec with MockitoSugar {
 
+  implicit val request = FakeRequest
+  val userId = s"user-${UUID.randomUUID()}"
+  val mockDataCacheConnector = mock[DataCacheConnector]
+  val mockCacheMap = mock[CacheMap]
+  val statusService = mock[StatusService]
+
   trait Fixture extends AuthorisedFixture {
-    self => val request = addToken(authRequest)
+    self =>
+    val request = addToken(authRequest)
 
-    val controller = new DetailedAnswersController(self.authConnector,
-      mock[DataCacheConnector])
+    val controller = new DetailedAnswersController(self.authConnector, mock[DataCacheConnector])
 
+    when(statusService.getStatus(any(), any(), any())) thenReturn Future.successful(SubmissionDecisionApproved)
+
+    val model = TradingPremises()
   }
 
   "DetailedAnswersController" when {
 
     "get is called" must {
       "respond with OK and show the detailed answers page" in new Fixture {
+        when(mockDataCacheConnector.fetchAll(any[HeaderCarrier], any[AuthContext]))
+          .thenReturn(Future.successful(Some(mockCacheMap)))
 
+        val businessMatchingActivitiesAll = BusinessMatchingActivities(
+          Set(AccountancyServices, BillPaymentServices, EstateAgentBusinessService, MoneyServiceBusiness))
+
+        val businessMatchingMsbServices = BusinessMatchingMsbServices(
+          Set(TransmittingMoney, CurrencyExchange))
+
+        when(mockCacheMap.getEntry[BusinessMatching](BusinessMatching.key))
+          .thenReturn(Some(BusinessMatching(None, Some(businessMatchingActivitiesAll), Some(businessMatchingMsbServices))))
+
+        when(mockCacheMap.getEntry[Seq[TradingPremises]](eqTo(TradingPremises.key))
+          (any())).thenReturn(Some(Seq(TradingPremises())))
+
+        val result = controller.get(1)(request)
+        status(result) must be(OK)
+
+        val document = Jsoup.parse(contentAsString(result))
+        contentAsString(result) must include(Messages("title.cya"))
+        contentAsString(result) must include("/anti-money-laundering/trading-premises/check-your-answers")
       }
     }
 
