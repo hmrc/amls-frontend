54836ce9c1b17408cb97e15e2d5dec3238b7f26c
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2019-01-30 13:47:57 +0000

    AMLS-4639 | Converted WhatYouNeed to DI alongside spec

--- app/controllers/responsiblepeople/WhatYouNeedController.scala
+++ app/controllers/responsiblepeople/WhatYouNeedController.scala
@@ -16,22 +16,20 @@
 
 package controllers.responsiblepeople
 
-import config.AMLSAuthConnector
+import com.google.inject.Inject
 import controllers.BaseController
+import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import views.html.responsiblepeople._
 
 import scala.concurrent.Future
 
-trait WhatYouNeedController extends BaseController {
+class WhatYouNeedController @Inject () (
+                                       override val authConnector:AuthConnector
+                                       ) extends BaseController {
 
   def get(index: Int, flow: Option[String] = None) =
     Authorised.async {
       implicit authContext => implicit request =>
         Future.successful(Ok(what_you_need(index, flow)))
     }
-}
-
-object WhatYouNeedController extends WhatYouNeedController {
-  // $COVERAGE-OFF$
-  override val authConnector = AMLSAuthConnector
 }
