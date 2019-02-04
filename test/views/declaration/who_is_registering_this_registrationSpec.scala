630a76adaefb36b3e1f0139d766ef5ca08933965
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2019-02-04 09:21:02 +0000

    AMLS-4548 | Added back button and spec file for who is registering registration

---
+++ test/views/declaration/who_is_registering_this_registrationSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.declaration
+
+import forms.{Form2, InvalidForm, ValidForm}
+import jto.validation.{Path, ValidationError}
+import models.declaration.WhoIsRegistering
+import models.responsiblepeople.ResponsiblePerson
+import org.scalatest.MustMatchers
+import play.api.i18n.Messages
+import utils.AmlsSpec
+import views.Fixture
+
+
+class who_is_registering_this_registrationSpec extends AmlsSpec with MustMatchers  {
+
+  trait ViewFixture extends Fixture {
+    implicit val requestWithToken = addToken(request)
+  }
+
+  "who_is_registering_this_renewal view" must {
+    "have correct title" in new ViewFixture {
+      val form2: ValidForm[WhoIsRegistering] = Form2(WhoIsRegistering("PersonName"))
+
+
+      def view = views.html.declaration.who_is_registering_this_registration(form2, Seq(ResponsiblePerson()))
+
+      doc.title mustBe s"${Messages("declaration.who.is.registering.title")} - ${Messages("title.amls")} - ${Messages("title.gov")}"
+      heading.html must be(Messages("declaration.who.is.registering.title", "submit.registration"))
+      doc.getElementsByAttributeValue("class", "link-back") must not be empty
+
+      doc.getElementsContainingOwnText(Messages("declaration.who.is.registering.text")).hasText must be(true)
+
+      doc.select("input[type=radio]").size mustBe 1
+    }
+
+    "show errors in the correct locations" in new ViewFixture {
+
+      val form2: InvalidForm = InvalidForm(Map.empty,
+        Seq(
+          (Path \ "person") -> Seq(ValidationError("not a message Key"))
+        ))
+
+      def view = views.html.declaration.who_is_registering_this_registration(form2, Seq(ResponsiblePerson()))
+
+      errorSummary.html() must include("not a message Key")
+
+      doc.getElementById("person")
+        .getElementsByClass("error-notification").first().html() must include("not a message Key")
+
+    }
+  }
+}
