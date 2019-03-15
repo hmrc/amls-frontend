6a69f0fd5c8e7214f0d6f4e4d1042f3ec02cc228
Sam Zaroug <sam.zaroug@digital.hmrc.gov.uk>
2019-03-15 14:34:01 +0000

    AMLS-4515 | moved tests on registration_amendment to own spec file

--- test/views/registrationamendment/registration_amendmentSpec.scala
+++ test/views/registrationamendment/registration_amendmentSpec.scala
@@ -25,29 +25,33 @@ import play.api.mvc.Call
 import utils.AmlsSpec
 import views.Fixture
 
-class registration_progressSpec extends AmlsSpec with MockitoSugar with AddressGenerator {
+class registration_amendmentSpec extends AmlsSpec with MockitoSugar with AddressGenerator {
 
   trait ViewFixture extends Fixture {
     implicit val requestWithToken = addToken(request)
 
     val sections = Seq(
-        Section("section1", Completed, true, mock[Call])
+      Section("section1", Completed, true, mock[Call])
     )
   }
 
   "The registration progress view" must {
-    "have correct title, headings and form fields" in new ViewFixture {
-      val form2 = EmptyForm
+    "display the correct visual content for incomplete sections" when {
+      "making an amendment" in new ViewFixture {
+        def view =
+          views.html.registrationamendment.registration_amendment(
+            Seq(Section("section1", Completed, true, mock[Call]),
+              Section("section2", Started, true, mock[Call])),
+            true,
+            addressGen.sample.get,
+            Seq.empty,
+            true
+          )
 
-      def view = views.html.registrationprogress.registration_progress(sections, true, addressGen.sample.get, Seq.empty[String], true)
+        doc.select("#section1-status").text mustBe Messages("progress.visuallyhidden.view.amend")
+        doc.select("#section2-status").text mustBe Messages("progress.visuallyhidden.view.started")
 
-      doc.title must be(Messages("progress.title") + " - " +
-        Messages("title.yapp") + " - " +
-        Messages("title.amls") + " - " + Messages("title.gov"))
-      heading.html must be(Messages("progress.title"))
-
-      // the first h2.heading-small should be info about progress section
-      doc.select("h2.heading-small").first().ownText() must be("Your business")
+      }
     }
   }
 }
