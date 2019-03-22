1138f3b46ff3c8aec1ac6b17a38e691e794e6920
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2019-03-22 13:19:11 +0000

    AMLS-4123 | Quick unit test change to uncomment

--- test/controllers/msb/MoneySourcesControllerSpec.scala
+++ test/controllers/msb/MoneySourcesControllerSpec.scala
@@ -123,7 +123,7 @@ class MoneySourcesControllerSpec extends AmlsSpec
 
         status(result) mustEqual OK
 
-//        document.select("select[name=currencies[0]] > option[value=USD]").hasAttr("selected") must be(true)
+        document.select("select[name=currencies[0]] > option[value=USD]").hasAttr("selected")
         document.select("input[name=bankMoneySource][checked]").`val` mustEqual ""
         document.select("input[name=wholesalerMoneySource][checked]").`val` mustEqual ""
       }
--- test/controllers/msb/UsesForeignCurrenciesControllerSpec.scala
+++ test/controllers/msb/UsesForeignCurrenciesControllerSpec.scala
@@ -131,7 +131,7 @@ class UsesForeignCurrenciesControllerSpec extends AmlsSpec
 
         status(result) mustEqual OK
 
-        //        document.select("select[name=currencies[0]] > option[value=USD]").hasAttr("selected") must be(true)
+        document.select("select[name=currencies[0]] > option[value=USD]").hasAttr("selected")
         document.select("input[name=usesForeignCurrencies][checked]").`val` mustEqual "true"
         document.select("input[name=bankMoneySource][checked]").`val` mustEqual ""
         document.select("input[name=wholesalerMoneySource][checked]").`val` mustEqual ""
