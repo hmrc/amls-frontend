0a8c1da6accf07aca0f56aedb3e2f82191413b7f
Sam Zaroug <sam.zaroug@digital.hmrc.gov.uk>
2019-04-04 08:26:13 +0100

    AMLS-4768 | using obj type instead of string value

--- app/controllers/tcsp/SummaryController.scala
+++ app/controllers/tcsp/SummaryController.scala
@@ -21,7 +21,7 @@ import cats.implicits._
 import connectors.DataCacheConnector
 import controllers.BaseController
 import javax.inject.Inject
-import models.tcsp.Tcsp
+import models.tcsp.{CompanyFormationAgent, Tcsp}
 import play.api.i18n.Messages
 import services.StatusService
 import services.businessmatching.ServiceFlow
@@ -44,11 +44,11 @@ class SummaryController @Inject()
       types <- data.tcspTypes
       providers <- Some(types.serviceProviders)
       labels <- Some(providers.collect {
-          case provider if provider.value != "05" => Messages(s"tcsp.service.provider.lbl.${provider.value}")
+          case provider if !provider.isInstanceOf[CompanyFormationAgent] => Messages(s"tcsp.service.provider.lbl.${provider.value}")
         }
       )
       specialCase <- Some(providers.collect {
-          case provider if provider.value == "05" => Messages(s"tcsp.service.provider.lbl.05")
+          case _: CompanyFormationAgent => Messages(s"tcsp.service.provider.lbl.05")
         }
       )
     } yield labels.toList.sorted ++ specialCase.toList
