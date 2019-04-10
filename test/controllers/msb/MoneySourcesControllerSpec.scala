74adfab4e323a0154fab844be50a941e0f69cedf
slavvco <13876870+slavvco@users.noreply.github.com>
2019-04-10 07:26:29 +0100

    AMLS-4880 | Removed commented lines in spec

--- test/controllers/msb/MoneySourcesControllerSpec.scala
+++ test/controllers/msb/MoneySourcesControllerSpec.scala
@@ -153,11 +153,6 @@ class MoneySourcesControllerSpec extends AmlsSpec
         "redirect to FXTransactions in the next 12 months controller" in new DealsInForeignCurrencyFixture {
           val result = controller.post()(newRequest)
 
-          //mockCacheFetchAll
-
-          //mockCacheGetEntry[MoneyServiceBusiness](Some(MoneyServiceBusiness()), MoneyServiceBusiness.key)
-          //mockCacheGetEntry[BusinessMatching](Some(BusinessMatching(msbServices = msbServices)), BusinessMatching.key)
-
           status(result) must be(SEE_OTHER)
           redirectLocation(result) mustBe Some(controllers.msb.routes.FXTransactionsInNext12MonthsController.get().url)
         }
