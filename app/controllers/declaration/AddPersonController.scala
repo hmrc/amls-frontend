9e3d7d36932e2e25501170b5efc96380bc71448c
Sam Zaroug <sam.zaroug@digital.hmrc.gov.uk>
2019-05-15 11:33:25 +0100

    AMLs-4943 | test for business type LPrLLP is now passing

--- app/controllers/declaration/AddPersonController.scala
+++ app/controllers/declaration/AddPersonController.scala
@@ -87,6 +87,7 @@ class AddPersonController @Inject () (
       case Some(BusinessType.LimitedCompany) => "Select if you are a beneficial shareholder, an external accountant, a director, a nominated officer, or other"
       case Some(BusinessType.SoleProprietor) => "Select if you are an external accountant, a nominated officer, a sole proprietor or other"
       case Some(BusinessType.Partnership) => "Select if you are an external accountant, a nominated officer, a partnership or other"
+      case Some(BusinessType.LPrLLP) => "Select if you are a designated member, an external accountant, a nominated officer, or other"
       case _ => "not implemented yet"
     }
 
