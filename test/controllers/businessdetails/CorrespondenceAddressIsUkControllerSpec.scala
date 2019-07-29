66a3e824a871f9648909802d88845d7c09c4801d
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2019-07-29 13:07:09 +0100

    Coverage - 1 unit test failing in CorrespondanceAddressIsUK

--- test/controllers/businessdetails/CorrespondenceAddressIsUkControllerSpec.scala
+++ test/controllers/businessdetails/CorrespondenceAddressIsUkControllerSpec.scala
@@ -17,11 +17,11 @@
 package controllers.businessdetails
 
 import connectors.DataCacheConnector
-import models.businessdetails.BusinessDetails
+import models.businessdetails._
 import org.jsoup.Jsoup
 import org.jsoup.nodes.{Document, Element}
 import org.jsoup.select.Elements
-import org.mockito.Matchers._
+import org.mockito.Matchers.{eq => meq, _}
 import org.mockito.Mockito._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mock.MockitoSugar
@@ -33,7 +33,7 @@ import uk.gov.hmrc.play.audit.http.connector.AuditResult.Success
 import utils.{AmlsSpec, AuthorisedFixture}
 
 import scala.collection.JavaConversions._
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 
 class CorrespondenceAddressIsUkControllerSpec extends AmlsSpec with MockitoSugar with ScalaFutures {
 
@@ -49,9 +49,34 @@ class CorrespondenceAddressIsUkControllerSpec extends AmlsSpec with MockitoSugar
     when {
       controller.auditConnector.sendEvent(any())(any(), any())
     } thenReturn Future.successful(Success)
+
+    val correspondenceAddress = CorrespondenceAddress(Some(CorrespondenceAddressUk("test", "test", "line1", "line2", Some("line3"), Some("line4"), "AA1 1AA")), None)
+    val businessDetails = BusinessDetails(None, None, None, None, None, None, None, None, Some(CorrespondenceAddressIsUk(true)), Some(correspondenceAddress))
   }
 
   "CorrespondenceAddressIsUkController" should {
+    "respond to a get request correctly" when {
+      "load IsUK page" in new Fixture {
+
+        when(controller.dataConnector.fetch[BusinessDetails](meq(BusinessDetails.key))
+          (any(), any(), any())).thenReturn(Future.successful(None))
+
+        val result = controller.get()(request)
+        status(result) must be(OK)
+        contentAsString(result) must include(Messages("businessdetails.correspondenceaddress.isuk.title"))
+      }
+
+      "load isUk result when there is already an isUK answer in BusinessDetails" in new Fixture {
+
+        when(controller.dataConnector.fetch[BusinessDetails](meq(BusinessDetails.key))
+          (any(), any(), any())).thenReturn(Future.successful(Some(businessDetails)))
+
+        val result = controller.get()(request)
+ 
+        status(result) must be(OK)
+        redirectLocation(result) must be(Some(controllers.businessdetails.routes.CorrespondenceAddressIsUkController.get().url))
+      }
+    }
 
     "respond to a post request correctly" when {
 
