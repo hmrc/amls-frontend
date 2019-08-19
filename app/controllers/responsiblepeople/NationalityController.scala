f7f92867b749e07402191be6c3345356d5a54c6a
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2019-08-19 11:36:17 +0100

    AMLS-5090 | Nationality controller and specs

--- app/controllers/responsiblepeople/NationalityController.scala
+++ app/controllers/responsiblepeople/NationalityController.scala
@@ -18,11 +18,11 @@ package controllers.responsiblepeople
 
 import com.google.inject.Inject
 import connectors.DataCacheConnector
-import controllers.BaseController
+import controllers.{BaseController, DefaultBaseController}
 import forms.{EmptyForm, Form2, InvalidForm, ValidForm}
 import models.responsiblepeople.{Nationality, ResponsiblePerson}
 import services.AutoCompleteService
-import utils.{ControllerHelper, RepeatingSection}
+import utils.{AuthAction, ControllerHelper, RepeatingSection}
 import views.html.responsiblepeople.nationality
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 
@@ -30,14 +30,14 @@ import scala.concurrent.Future
 
 class NationalityController @Inject () (
                                        val dataCacheConnector: DataCacheConnector,
-                                       val authConnector: AuthConnector,
+                                       authAction: AuthAction,
                                        val autoCompleteService: AutoCompleteService
-                                       ) extends RepeatingSection with BaseController {
+                                       ) extends RepeatingSection with DefaultBaseController {
 
 
-  def get(index: Int, edit: Boolean = false, flow: Option[String] = None) = Authorised.async {
-        implicit authContext => implicit request =>
-          getData[ResponsiblePerson](index) map {
+  def get(index: Int, edit: Boolean = false, flow: Option[String] = None) = authAction.async {
+        implicit request =>
+          getData[ResponsiblePerson](request.credId, index) map {
             case Some(ResponsiblePerson(Some(personName),_,_,_,Some(residencyType),_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_))
             => residencyType.nationality match {
                 case Some(country) => Ok(nationality(Form2[Nationality](country), edit, index, flow, personName.titleName, autoCompleteService.getCountries))
@@ -50,18 +50,16 @@ class NationalityController @Inject () (
           }
       }
 
-  def post(index: Int, edit: Boolean = false, flow: Option[String] = None) =
-      Authorised.async {
-        implicit authContext => implicit request =>
-
+  def post(index: Int, edit: Boolean = false, flow: Option[String] = None) = authAction.async {
+        implicit request =>
           Form2[Nationality](request.body) match {
             case f: InvalidForm =>
-              getData[ResponsiblePerson](index) map { rp =>
+              getData[ResponsiblePerson](request.credId, index) map { rp =>
                 BadRequest(nationality(f, edit, index, flow, ControllerHelper.rpTitleName(rp), autoCompleteService.getCountries))
               }
             case ValidForm(_, data) => {
               for {
-                _ <- updateDataStrict[ResponsiblePerson](index) { rp =>
+                _ <- updateDataStrict[ResponsiblePerson](request.credId, index) { rp =>
                   val residenceType = rp.personResidenceType.map(x => x.copy(nationality = Some(data)))
                   rp.personResidenceType(residenceType)
                 }
