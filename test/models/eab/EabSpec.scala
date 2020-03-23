8e53f285374d7e599127d1578fb0dbe40e75a7df
ajchilts <40767309+ajchilts@users.noreply.github.com>
2020-03-23 09:27:13 +0000

    Add missing date of change spec tests

--- test/models/eab/EabSpec.scala
+++ test/models/eab/EabSpec.scala
@@ -67,6 +67,8 @@ class EabSpec extends AmlsSpec {
     "residential",
     "socialHousingProvision")
 
+  val completeDate = Json.obj("dateOfChange" -> "2002-02-02")
+
   val completeServices = Json.obj("eabServicesProvided" -> completeServiceList )
 
   val completeServicesWithoutResidential = Json.obj(
@@ -80,6 +82,7 @@ class EabSpec extends AmlsSpec {
   "A constructed Eab model" when {
     "data are complete" must {
       val completeData = completeServices ++
+        completeDate ++
         completeEstateAgencyActPenalty ++
         completePenalisedProfessionalBody ++
         completeRedressScheme ++
@@ -140,6 +143,8 @@ class EabSpec extends AmlsSpec {
 
     val oldServices = Json.obj("services" -> Json.arr("08", "03", "07", "02", "05", "01", "06", "09", "04" ))
 
+    val oldDateOfChange = Json.obj("dateOfChange" -> "2002-02-02")
+
     val oldProfessionalBody = Json.obj(
       "penalised" -> true,
       "professionalBody" -> "test10")
@@ -158,6 +163,7 @@ class EabSpec extends AmlsSpec {
     "data are complete" must {
       val completeOldEab = (
         oldServices ++
+        oldDateOfChange ++
         oldRedressScheme ++
         oldProfessionalBody ++
         oldEstateAct ++ Json.obj(
@@ -170,6 +176,7 @@ class EabSpec extends AmlsSpec {
     "data are complete no redress" must {
       val completeOldEab = (
         oldServices ++
+          oldDateOfChange ++
           oldRedressSchemeNoRedress ++
           oldProfessionalBody ++
           oldEstateAct ++ Json.obj(
