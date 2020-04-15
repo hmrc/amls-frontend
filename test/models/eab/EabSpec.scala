97459595b3a6b287bac1131dfa584c32a8e73b61
ajchilts <40767309+ajchilts@users.noreply.github.com>
2020-04-15 10:29:34 +0100

    Revert "Amls 5850 - Missing Date Of Change in EAB Flow"

--- test/models/eab/EabSpec.scala
+++ test/models/eab/EabSpec.scala
@@ -16,9 +16,7 @@
 
 package models.eab
 
-import models.DateOfChange
 import models.estateagentbusiness._
-import org.joda.time.LocalDate
 import play.api.libs.json._
 import utils.AmlsSpec
 
@@ -80,14 +78,9 @@ class EabSpec extends AmlsSpec {
     "eabServicesProvided" -> completeServiceList.filter(s => !s.equals("lettings"))
   )
 
-  val completeDateOfChange = Json.obj(
-    "dateOfChange" -> "2019-01-01"
-  )
-
   "A constructed Eab model" when {
     "data are complete" must {
       val completeData = completeServices ++
-        completeDateOfChange ++
         completeEstateAgencyActPenalty ++
         completePenalisedProfessionalBody ++
         completeRedressScheme ++
@@ -131,7 +124,7 @@ class EabSpec extends AmlsSpec {
                 Lettings,
                 Relocation
               ),
-              Some(DateOfChange(new LocalDate(2019, 1, 1)))
+              None
             )
           ),
           Some(PropertyRedressScheme),
@@ -180,8 +173,6 @@ class EabSpec extends AmlsSpec {
 
     val oldServices = Json.obj("services" -> Json.arr("08", "03", "07", "02", "05", "01", "06", "09", "04" ))
 
-    val oldDateOfChange = Json.obj("dateOfChange" -> "2002-02-02")
-
     val oldProfessionalBody = Json.obj(
       "penalised" -> true,
       "professionalBody" -> "test10")
@@ -203,7 +194,6 @@ class EabSpec extends AmlsSpec {
     "data are complete" must {
       val completeOldEab = (
         oldServices ++
-        oldDateOfChange ++
         oldRedressScheme ++
         oldProfessionalBody ++
         oldEstateAct ++
@@ -217,7 +207,6 @@ class EabSpec extends AmlsSpec {
     "data are complete no redress" must {
       val completeOldEab = (
         oldServices ++
-          oldDateOfChange ++
           oldRedressSchemeNoRedress ++
           oldProfessionalBody ++
           oldEstateAct ++ Json.obj(
