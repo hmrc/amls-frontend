a0af4b69b23df36a37dd3773e315efe424f4b036
slavvco <13876870+slavvco@users.noreply.github.com>
2020-05-29 14:53:30 +0100

    Revert "AMLS-5918 | QA enrolments test with SafeId"

--- app/services/SubmissionService.scala
+++ app/services/SubmissionService.scala
@@ -58,7 +58,7 @@ class SubmissionService @Inject()(val cacheConnector: DataCacheConnector,
                    (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[_] =
 
     if (config.enrolmentStoreToggle) {
-      authEnrolmentsService.enrol(amlsRegistrationNumber, postcode, groupId, credId, safeId)
+      authEnrolmentsService.enrol(amlsRegistrationNumber, postcode, groupId, credId)
     } else {
       ggService.enrol(amlsRegistrationNumber, safeId, postcode)
     }
