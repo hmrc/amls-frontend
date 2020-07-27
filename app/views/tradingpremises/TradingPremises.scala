9f40c4de1ded0c816e7000e8f4052c135cc3ddf7
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2020-07-27 12:33:51 +0100

    [CU][DLS-2847]: WIP: DI for views

---
+++ app/views/tradingpremises/TradingPremises.scala
@@ -0,0 +1,26 @@
+package views.tradingpremises
+
+import javax.inject.Inject
+import views.html.tradingpremises._
+
+class TradingPremises @Inject()(
+                                 activity_start_date: activity_start_date,
+                                 agent_company_details: agent_company_details,
+                                 agent_company_name: agent_company_name,
+                                 agent_name: agent_name,
+                                 agent_partnership: agent_partnership,
+                                 business_structure: business_structure,
+                                 confirm_address: confirm_address,
+                                 is_residential: is_residential,
+                                 msb_services: msb_services,
+                                 registering_agent_premises: registering_agent_premises,
+                                 remove_agent_premises_reasons: remove_agent_premises_reasons,
+                                 remove_trading_premises: remove_trading_premises,
+                                 summary: summary,
+                                 what_does_your_business_do: what_does_your_business_do,
+                                 what_you_need: what_you_need,
+                                 where_are_trading_premises: where_are_trading_premises,
+                                 your_trading_premises: your_trading_premises
+                               ) {
+
+}
