006e1b9e7a23547d1c0c8a1b62a38786ef3a95c0
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2024-02-06 09:30:08 +0000

    DDCYLS-4024 refactor of deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,37 +1,29 @@
-import sbt.Keys.dependencyOverrides
 import sbt.{Def, _}
 
 private object AppDependencies {
 
   import play.sbt.PlayImport._
-  import play.core.PlayVersion
 
-  private val playPartialsVersion = "9.1.0"
-  private val httpCachingClientVersion = "11.1.0"
-  private val flexmarkVersion = "0.64.8"
-  private val okHttpVersion = "3.9.1"
-  private val jsonEncryptionVersion = "5.1.0-play-28"
-  private val hmrcMongoVersion = "1.4.0"
-  private val domain = "9.0.0"
+  private final val playV = "play-30"
+  private final val playJsonV = "3.0.2"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"       %% "domain-play-30"                        % domain,
-    "uk.gov.hmrc"       %% "play-partials-play-30"                 % playPartialsVersion,
-    "uk.gov.hmrc"       %% "http-caching-client-play-30"           % httpCachingClientVersion,
-    "uk.gov.hmrc"       %% "json-encryption"                       % jsonEncryptionVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % "8.0.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "8.4.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "2.0.0",
-
-    "com.vladsch.flexmark" % "flexmark-all" % flexmarkVersion,
-    "com.beachape" %% "enumeratum-play" % "1.8.0",
-    "com.squareup.okhttp3" % "mockwebserver" % okHttpVersion,
-    "org.playframework" %% "play-json" % "3.0.2",
-    "org.playframework" %% "play-json-joda" % "3.0.2",
-    "org.typelevel"     %% "cats-core"      % "2.10.0",
-    "commons-codec" % "commons-codec" % "1.15"
+    "uk.gov.hmrc"          %% s"domain-$playV"                        % "9.0.0",
+    "uk.gov.hmrc"          %% s"play-partials-$playV"                 % "9.1.0",
+    "uk.gov.hmrc"          %% s"http-caching-client-$playV"           % "11.1.0",
+    "uk.gov.hmrc.mongo"    %% s"hmrc-mongo-$playV"                    % "1.4.0",
+    "uk.gov.hmrc"          %% s"bootstrap-frontend-$playV"            % "8.4.0",
+    "uk.gov.hmrc"          %% s"play-frontend-hmrc-$playV"            % "8.4.0",
+    "uk.gov.hmrc"          %% s"play-conditional-form-mapping-$playV" % "2.0.0",
+    "uk.gov.hmrc"          %% "json-encryption"                       % "5.1.0-play-28",
+    "com.vladsch.flexmark"  % "flexmark-all"                          % "0.64.8",
+    "com.beachape"         %% "enumeratum-play"                       % "1.8.0",
+    "com.squareup.okhttp3"  % "mockwebserver"                         % "3.9.1",
+    "org.playframework"    %% "play-json"                             % playJsonV,
+    "org.playframework"    %% "play-json-joda"                        % playJsonV,
+    "org.typelevel"        %% "cats-core"                             % "2.10.0",
+    "commons-codec"         % "commons-codec"                         % "1.15"
   )
 
   trait ScopeDependencies {
@@ -39,19 +31,16 @@ private object AppDependencies {
     val dependencies: Seq[ModuleID]
   }
 
-  private val pegdownVersion = "1.6.0"
-  private val jsoupVersion = "1.13.1"
-
   object Test {
     def apply() = new ScopeDependencies {
       override val scope = "test"
       override lazy val dependencies = Seq(
-        "org.scalacheck" %% "scalacheck" % "1.14.3" % scope,
-        "org.pegdown" % "pegdown" % pegdownVersion % scope,
-        "org.jsoup" % "jsoup" % jsoupVersion % scope,
-        "org.playframework" %% "play-test" % "3.0.1" % scope,
-        "org.mockito" % "mockito-all" % "1.10.19" % scope,
-        "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % scope
+        "org.scalacheck"         %% "scalacheck"         % "1.14.3"  % scope,
+        "org.pegdown"             % "pegdown"            % "1.6.0"   % scope,
+        "org.jsoup"               % "jsoup"              % "1.13.1"  % scope,
+        "org.playframework"      %% "play-test"          % "3.0.1"   % scope,
+        "org.mockito"             % "mockito-all"        % "1.10.19" % scope,
+        "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0"   % scope
       )
     }.dependencies
   }
