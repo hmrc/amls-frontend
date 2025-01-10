65d31c21da6b773e3c5bf4db7e7518b1a453b4c5
AthishThayalan <120419238+AthishThayalan@users.noreply.github.com>
2025-01-10 13:35:25 +0000

    fix : correctly retrieves PSR number to display on additional summary page

--- app/controllers/businessmatching/PSRNumberController.scala
+++ app/controllers/businessmatching/PSRNumberController.scala
@@ -50,14 +50,8 @@ class PSRNumberController @Inject()(authAction: AuthAction,
         bm <- businessMatchingService.getModel(request.credId)
         status <- OptionT.liftF(statusService.getStatus(request.amlsRefNumber, request.accountTypeId, request.credId))
       } yield {
-
-        val originalPsrNumber = request.session.get("originalPsrNumber").getOrElse("")
-
-        val form = bm.businessAppliedForPSRNumber.fold(formProvider()) {
-          case BusinessAppliedForPSRNumberYes(_) =>
-            formProvider().fill(BusinessAppliedForPSRNumberYes(originalPsrNumber))
-          case other => formProvider().fill(other)
-        }
+        // Use the model's PSR number directly for prepopulation
+        val form = bm.businessAppliedForPSRNumber.fold(formProvider())(formProvider().fill)
 
         Ok(psr_number(form, edit, bm.preAppComplete, statusService.isPreSubmission(status), bm.businessAppliedForPSRNumber.isDefined))
       }) getOrElse Redirect(controllers.routes.RegistrationProgressController.get())
