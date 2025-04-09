7da7e835fc8798d2b15ec6892586c22743354fdd
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2025-04-09 13:43:26 +0100

    Updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,20 +6,20 @@ private object AppDependencies {
   private val playV = "play-30"
   private val flexmarkVersion = "0.64.8"
   private val bootstrapV = "9.6.0"
-  private val hmrcMongoV = "2.3.0"
+  private val hmrcMongoV = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     // GOV UK
     "uk.gov.hmrc"           %% s"domain-$playV"                        % "10.0.0",
     "uk.gov.hmrc"           %% s"play-partials-$playV"                 % "10.0.0",
-    "uk.gov.hmrc"           %% s"crypto-json-$playV"                   % "8.1.0",
+    "uk.gov.hmrc"           %% s"crypto-json-$playV"                   % "8.2.0",
     "uk.gov.hmrc.mongo"     %% s"hmrc-mongo-$playV"                    % hmrcMongoV,
     "uk.gov.hmrc"           %% s"bootstrap-frontend-$playV"            % bootstrapV,
-    "uk.gov.hmrc"           %% s"play-frontend-hmrc-$playV"            % "11.8.0",
-    "uk.gov.hmrc"           %% s"play-conditional-form-mapping-$playV" % "3.2.0",
+    "uk.gov.hmrc"           %% s"play-frontend-hmrc-$playV"            % "11.13.0",
+    "uk.gov.hmrc"           %% s"play-conditional-form-mapping-$playV" % "3.3.0",
     // OTHER
-    "com.vladsch.flexmark"   % "flexmark"                             % flexmarkVersion exclude("org.apache.pdfbox", "pdfbox"),
+    "com.vladsch.flexmark"   % "flexmark"                              % flexmarkVersion exclude("org.apache.pdfbox", "pdfbox"),
     "com.beachape"          %% "enumeratum-play"                       % "1.8.2",
     "org.typelevel"         %% "cats-core"                             % "2.12.0",
     "commons-codec"          % "commons-codec"                         % "1.17.2"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,9 +10,9 @@ ThisBuild / libraryDependencySchemes ++= Seq(
 )
 
 addSbtPlugin("uk.gov.hmrc"        %  "sbt-auto-build"         % "3.24.0")
-addSbtPlugin("uk.gov.hmrc"        %  "sbt-distributables"     % "2.5.0")
+addSbtPlugin("uk.gov.hmrc"        %  "sbt-distributables"     % "2.6.0")
 addSbtPlugin("org.playframework"  %  "sbt-plugin"             % "3.0.6")
-addSbtPlugin("org.scoverage"      %% "sbt-scoverage"          % "2.3.0")
+addSbtPlugin("org.scoverage"      %% "sbt-scoverage"          % "2.3.1")
 addSbtPlugin("org.scalastyle"     %% "scalastyle-sbt-plugin"  % "1.0.0")
 addSbtPlugin("com.typesafe.sbt"   %  "sbt-digest"             % "1.1.4")
 addSbtPlugin("net.virtual-void"   %  "sbt-dependency-graph"   % "0.9.2")
