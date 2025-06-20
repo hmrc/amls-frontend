f15fb1bb1d6c2ddb6127a23097f13b987d79415c
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2025-06-20 13:31:53 +0100

    completing PR comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,18 +5,18 @@ private object AppDependencies {
 
   private val playV = "play-30"
   private val flexmarkVersion = "0.64.8"
-  private val bootstrapV = "9.11.0"
+  private val bootstrapV = "9.13.0"
   private val hmrcMongoV = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     // GOV UK
     "uk.gov.hmrc"           %% s"domain-$playV"                        % "10.0.0",
-    "uk.gov.hmrc"           %% s"play-partials-$playV"                 % "10.0.0",
+    "uk.gov.hmrc"           %% s"play-partials-$playV"                 % "10.1.0",
     "uk.gov.hmrc"           %% s"crypto-json-$playV"                   % "8.2.0",
     "uk.gov.hmrc.mongo"     %% s"hmrc-mongo-$playV"                    % hmrcMongoV,
     "uk.gov.hmrc"           %% s"bootstrap-frontend-$playV"            % bootstrapV,
-    "uk.gov.hmrc"           %% s"play-frontend-hmrc-$playV"            % "12.1.0",
+    "uk.gov.hmrc"           %% s"play-frontend-hmrc-$playV"            % "12.6.0",
     "uk.gov.hmrc"           %% s"play-conditional-form-mapping-$playV" % "3.3.0",
     // OTHER
     "com.vladsch.flexmark"   % "flexmark"                             % flexmarkVersion exclude("org.apache.pdfbox", "pdfbox"),
